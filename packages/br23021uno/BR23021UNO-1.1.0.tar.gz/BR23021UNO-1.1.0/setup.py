a3a5521c44bc1f46932cf883cd9220db7b9c1013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 14:04:23 +0000

    Add package br23021uno/1.1.0/BR23021UNO-1.1.0.tar.gz

---
+++ packages/br23021uno/BR23021UNO-1.1.0.tar.gz/BR23021UNO-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BR23021UNO",
+    version="1.1.0",
+    author="Joyser Leonel Barrera Romero",
+    description="Librería para resolver sistemas de ecuaciones lineales y no lineales.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "numpy"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
