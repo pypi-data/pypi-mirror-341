d4229257588edeee5b11002ad2daf61404574636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 06:03:44 +0000

    Add package partnersimulator/0.0.3/partnersimulator-0.0.3.tar.gz

---
+++ packages/partnersimulator/partnersimulator-0.0.3.tar.gz/partnersimulator-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Partner Simulator'
+with open('partnersimulator/ReadMe.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='partnersimulator',
+    version=VERSION,
+    author='Aditya Jain',
+    author_email='aditya.jain22@imperial.ac.uk',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['openai', 'polars'],
+    keywords=['creepy','parasocial'],
+    classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows"
+    ]
+)
