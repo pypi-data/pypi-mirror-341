55d71199e6ad1df5a4a112aa15674e437f4211a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:07 +0000

    Add package rasa-pro/3.11.7/rasa_pro-3.11.7.tar.gz

---
+++ packages/rasa-pro/rasa_pro-3.11.7.tar.gz/rasa_pro-3.11.7/rasa/core/channels/voice_stream/asr/asr_event.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ASREvent:
+    @classmethod
+    def name(cls) -> str:
+        return cls.__name__
+
+
+@dataclass
+class NewTranscript(ASREvent):
+    text: str
+
+
+@dataclass
+class UserIsSpeaking(ASREvent):
+    pass
