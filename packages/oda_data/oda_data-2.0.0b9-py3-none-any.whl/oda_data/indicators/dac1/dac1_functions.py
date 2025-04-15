625fa49d0b8260aad711110e18eeeb110ac33ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:58 +0000

    Add package oda_data/2.0.0b9/oda_data-2.0.0b9-py3-none-any.whl

---
+++ packages/oda_data/oda_data-2.0.0b9-py3-none-any.whl/oda_data/indicators/dac1/dac1_functions.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def official_oda(data: pd.DataFrame) -> pd.DataFrame:
+    """Filter the data to only include Official Development Assistance (ODA).
+    Pre-2018, ODA is defined as aidtype_code 1010.
+    Post-2018, ODA is defined as aidtype_code 11010.
+    """
+    query = "(year < 2018 & aidtype_code == 1010) | (year >= 2018 & aidtype_code == 11010)"
+
+    data = data.query(query)
+
+    return data
