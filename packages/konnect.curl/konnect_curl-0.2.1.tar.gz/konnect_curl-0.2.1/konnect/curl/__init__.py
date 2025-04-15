e8dd83127609f12f34cc50c08e2e8d3ecfdd7338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 14:04:29 +0000

    Add package konnect.curl/0.2.1/konnect_curl-0.2.1.tar.gz

---
+++ packages/konnect.curl/konnect_curl-0.2.1.tar.gz/konnect_curl-0.2.1/konnect/curl/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2023  Dom Sekotill <dom.sekotill@kodo.org.uk>
+
+from ._enums import MILLISECONDS as MILLISECONDS
+from ._enums import SECONDS as SECONDS
+from ._enums import Time as Time
+from ._exceptions import CurlError as CurlError
+from ._multi import Multi as Multi
+from .requests import Request as Request
