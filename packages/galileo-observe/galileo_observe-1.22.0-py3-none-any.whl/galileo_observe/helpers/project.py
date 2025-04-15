12c709ebd2f54841617a5562a1e8ccf307fd3f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:06:02 +0000

    Add package galileo-observe/1.22.0/galileo_observe-1.22.0-py3-none-any.whl

---
+++ packages/galileo-observe/galileo_observe-1.22.0-py3-none-any.whl/galileo_observe/helpers/project.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from pydantic import UUID4
+
+from galileo_core.helpers.project import get_project as core_get_project
+from galileo_core.helpers.project import get_projects
+from galileo_core.schemas.core.project import ProjectResponse, ProjectType
+from galileo_observe.schema.config import ObserveConfig
+
+
+def list_projects(project_type: Optional[ProjectType] = None) -> list[ProjectResponse]:
+    config = ObserveConfig.get()
+    return get_projects(project_type=project_type, config=config)
+
+
+def get_project(project_id: Optional[UUID4] = None, project_name: Optional[str] = None) -> Optional[ProjectResponse]:
+    config = ObserveConfig.get()
+    return core_get_project(
+        project_id=project_id, project_name=project_name, project_type=ProjectType.llm_monitor, config=config
+    )
