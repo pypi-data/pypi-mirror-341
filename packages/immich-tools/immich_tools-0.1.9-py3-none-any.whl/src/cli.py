2b1eef443b0b8ed6e6d2ab0d12421b9665401cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 05:03:42 +0000

    Add package immich-tools/0.1.9/immich_tools-0.1.9-py3-none-any.whl

---
+++ packages/immich-tools/immich_tools-0.1.9-py3-none-any.whl/src/cli.py
@@ -0,0 +1,39 @@
+import click
+from src.commands import merge_xmp, refresh_album_metadata, run_job
+import logging
+from datetime import datetime
+
+log = logging.getLogger("immich-tools")
+log.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(levelname)-5s - %(message)s")
+
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+log.addHandler(console_handler)
+
+
+@click.group()
+@click.option("-d", "--debug", is_flag=True, help="Enable debug mode")
+@click.option("-l", "--log-file", is_flag=True, help="Saves logs to file")
+@click.option("--log-path", help="path to directory where logs will be stored, works only if --log-file flag is set to True")
+@click.pass_context
+def main(ctx, debug:bool, log_file: bool, log_path: str):
+    """Tools for immich"""
+    ctx.ensure_object(dict)
+    log.info("Running immich-tools")
+    if log_file:
+        log_directory = log_path if log_path else "." 
+        file_handler = logging.FileHandler(datetime.now().strftime(f'{log_directory}/log_%d_%m_%Y.log'))
+        file_handler.setLevel(logging.DEBUG if debug else logging.INFO)
+        file_handler.setFormatter(formatter)
+        log.addHandler(file_handler)
+    if debug:
+        log.setLevel(logging.DEBUG)
+        log.debug("Debug mode is ON")
+    
+        
+main.add_command(refresh_album_metadata.refresh_album_metadata)
+main.add_command(merge_xmp.merge_xmp)
+main.add_command(run_job.run_job)
