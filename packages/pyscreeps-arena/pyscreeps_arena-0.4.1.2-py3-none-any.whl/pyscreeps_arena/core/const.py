4f2ff40092298c7a89bfee5cab449ebb3963d2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:37 +0000

    Add package pyscreeps-arena/0.4.1.2/pyscreeps_arena-0.4.1.2-py3-none-any.whl

---
+++ packages/pyscreeps-arena/pyscreeps_arena-0.4.1.2-py3-none-any.whl/pyscreeps_arena/core/const.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# pyscreeps_arena - const.py
+# Author: 我阅读理解一直可以的
+# Template: V0.1
+# Versions:
+# .2025 01 01 - v0.1:
+#   Created.
+#
+import re
+
+VERSION = "0.4.1"
+AUTHOR = "●ω＜🤍♪"
+STEAM_ID = "1029562896"
+GITHUB_NAME = "EagleBaby"
+BILIBILI_NAME = "我阅读理解一直可以的"
+
+ARENA_GREEN = "spawn_and_swamp"
+ARENA_BLUE = "capture_the_flag"
+ARENA_RED = "collect_and_control"
+ARENA_GRAY = "tutorial"
+
+ARENA_NAMES = {
+    "green": ARENA_GREEN,
+    "blue" : ARENA_BLUE,
+    "red"  : ARENA_RED,
+    "gray" : ARENA_GRAY,
+}
