ccead3a7cc70e05b853bef2630863cab5bfa089f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:45 +0000

    Add package LT22004UNO/0.1.2/lt22004uno-0.1.2.tar.gz

---
+++ packages/LT22004UNO/lt22004uno-0.1.2.tar.gz/lt22004uno-0.1.2/README.md
@@ -0,0 +1,19 @@
+¡Hola! esta libreria esta disenada para resolver sistemas de ecuaciones lineales y no lineales.
+
+## Metodos implementados
+
+### Sistemas lineales
+- Eliminacion Gaussiana
+- Gauss-Jordan
+- Regla de Cramer
+- Descomposición LU
+- Jacobi
+- Gauss-Seidel
+
+### Sistemas no lineales
+- Metodo de Bisección
+
+## Instalacion
+
+```bash
+pip install 0265831UNO
---
+++ packages/LT22004UNO/lt22004uno-0.1.2.tar.gz/lt22004uno-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LT22004UNO",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.20.0',
+    ],
+    author="Fabio Leiva",
+    author_email="LT22004@ues.edu.sv",
+    description="Librería para resolver sistemas de ecuaciones lineales y no lineales",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/drumsplease-fab/CDA_corto1",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
