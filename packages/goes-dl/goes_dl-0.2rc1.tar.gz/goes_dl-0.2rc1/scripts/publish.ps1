9b0a51562173afb834b5cb8d0a675f901805588d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:02 +0000

    Add package goes-dl/0.2rc1/goes_dl-0.2rc1.tar.gz

---
+++ packages/goes-dl/goes_dl-0.2rc1.tar.gz/goes_dl-0.2rc1/scripts/publish.ps1
@@ -0,0 +1,43 @@
+##
+## Upload package to PyPI
+##
+
+$venv = '.venv'
+
+$usage = "Usage: publish.ps1 v1.0-rc1"
+
+if (-not $args[0]) {
+    Write-Host $usage
+    Write-Host ""
+    exit
+}
+
+$release = "dist/" + $args[0]
+
+mkdir $release
+Move-Item *.gz $release
+Move-Item *.whl $release
+
+# Activate the environment if it is not active
+
+$isVirtualEnvActive = $true
+
+if (-not $env:VIRTUAL_ENV) {
+    $isVirtualEnvActive = $false
+    & $venv/Scripts/Activate.ps1
+}
+
+# Display the version of python and pip
+
+python --version
+python -m pip --version
+
+# Upload the package
+
+twine upload $release/*
+
+# Deactivate the environment if it was not active
+
+if (-not $isVirtualEnvActive) {
+    deactivate
+}
