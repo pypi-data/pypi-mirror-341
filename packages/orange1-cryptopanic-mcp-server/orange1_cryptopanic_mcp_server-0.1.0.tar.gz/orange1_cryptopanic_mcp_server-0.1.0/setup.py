c552d04d18bdbdb07d211d4e36956f498097b3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:42 +0000

    Add package orange1-cryptopanic-mcp-server/0.1.0/orange1_cryptopanic_mcp_server-0.1.0.tar.gz

---
+++ packages/orange1-cryptopanic-mcp-server/orange1_cryptopanic_mcp_server-0.1.0.tar.gz/orange1_cryptopanic_mcp_server-0.1.0/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="orange1-cryptopanic-mcp-server",
+    version="0.1.0",
+    description="Provide the latest cryptocurrency news for AI agents.",
+    author="orange1",
+    author_email="support@orange.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['dotenv>=0.9.9', 'mcp[cli]>=1.3.0', 'requests>=2.32.3'],
+    keywords=["orange1"] + [],
+)
