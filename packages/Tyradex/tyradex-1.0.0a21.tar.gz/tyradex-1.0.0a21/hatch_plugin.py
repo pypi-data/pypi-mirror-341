9a5cc12986085f1c7cd86bf9720e2da92a7cb0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:50 +0000

    Add package Tyradex/1.0.0a21/tyradex-1.0.0a21.tar.gz

---
+++ packages/Tyradex/tyradex-1.0.0a21.tar.gz/tyradex-1.0.0a21/hatch_plugin.py
@@ -0,0 +1,27 @@
+import json
+from pathlib import Path
+
+from hatchling.metadata.plugin.interface import MetadataHookInterface
+
+class CustomMetadataHook(MetadataHookInterface):
+    def update(self, metadata):
+        metadata_file = Path(self.root) / "metadata.json"
+        metadata_file_2 = Path(self.root) / "Tyradex/metadata.json"
+        if not metadata_file.exists():
+            raise FileNotFoundError(f"Metadata file not found at: {metadata_file}")
+
+        with metadata_file.open(encoding="utf-8") as f:
+            meta = json.load(f)
+
+        required_fields = ["name", "version", "description", "authors"]
+        for field in required_fields:
+            if field not in meta:
+                raise ValueError(f"Missing required field '{field}' in metadata.json")
+
+        with open(metadata_file_2, "w", encoding="utf-8") as f:
+            json.dump(meta, f)
+
+        metadata["name"] = meta["name"]
+        metadata["version"] = meta["version"]
+        metadata["description"] = meta["description"]
+        metadata["authors"] = meta["authors"]
