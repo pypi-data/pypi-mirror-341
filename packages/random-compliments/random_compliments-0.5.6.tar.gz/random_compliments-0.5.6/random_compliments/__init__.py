0d9d3f9e28e3b0b7c81a1c3522076e851358cca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:04:57 +0000

    Add package random-compliments/0.5.6/random_compliments-0.5.6.tar.gz

---
+++ packages/random-compliments/random_compliments-0.5.6.tar.gz/random_compliments-0.5.6/random_compliments/__init__.py
@@ -0,0 +1,20 @@
+"""
+Random Compliments about Boaz Package
+"""
+
+from random_compliments.compliments import get_compliment
+
+__version__ = "0.5.6"
+
+def get_version():
+    """Return the package version"""
+    return __version__
+
+def get_random_compliment():
+    """
+    Get a random compliment about Boaz.
+    
+    Returns:
+        str: A random compliment about Boaz.
+    """
+    return get_compliment() 
