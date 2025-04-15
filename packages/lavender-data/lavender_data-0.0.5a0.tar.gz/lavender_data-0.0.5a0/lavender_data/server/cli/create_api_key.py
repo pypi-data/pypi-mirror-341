8f77471a844f3dd9dfea64793a97421448d0d116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:22 +0000

    Add package lavender-data/0.0.5a0/lavender_data-0.0.5a0.tar.gz

---
+++ packages/lavender-data/lavender_data-0.0.5a0.tar.gz/lavender_data-0.0.5a0/lavender_data/server/cli/create_api_key.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import Optional
+
+from lavender_data.server.auth import generate_api_key_secret
+from lavender_data.server.db import get_session, setup_db
+from lavender_data.server.db.models import ApiKey
+from lavender_data.server.settings import get_settings
+
+
+def create_api_key(
+    note: Optional[str] = None,
+    expires_at: Optional[datetime] = None,
+):
+    setup_db(get_settings().lavender_data_db_url)
+    session = next(get_session())
+
+    api_key = ApiKey(secret=generate_api_key_secret(), note=note, expires_at=expires_at)
+    session.add(api_key)
+    session.commit()
+    session.refresh(api_key)
+    return api_key
