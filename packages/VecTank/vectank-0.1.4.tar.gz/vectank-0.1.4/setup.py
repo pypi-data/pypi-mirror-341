0a3a570b029460e3edf49942316a5161d1fb0fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 14:04:23 +0000

    Add package VecTank/0.1.4/vectank-0.1.4.tar.gz

---
+++ packages/VecTank/vectank-0.1.4.tar.gz/vectank-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='VecTank',
+    version='0.1.4',
+    description='軽量で高速なインメモリベクトルデータベースライブラリ',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author='satorunnlg',
+    author_email='satorunnlg@gmail.com',
+    url="https://github.com/satorunnlg/VecTank.git",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vectank-run=vectank.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
