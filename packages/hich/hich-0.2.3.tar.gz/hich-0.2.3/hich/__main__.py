132e6e48b873f807622e31dd24497649d21b5a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 06:04:12 +0000

    Add package hich/0.2.3/hich-0.2.3.tar.gz

---
+++ packages/hich/hich-0.2.3.tar.gz/hich-0.2.3/hich/__main__.py
@@ -0,0 +1,21 @@
+import click
+from hich.commands.pairs import pairs
+from hich.commands.fasta import fasta
+from hich.commands.report import report
+from hich.commands.matrix import matrix
+
+@click.group()
+def hich():
+    """CLI tools to process Hi-C data
+    
+    Version: 0.2.3
+    """
+    pass
+
+hich.add_command(fasta)
+hich.add_command(pairs)
+hich.add_command(report)
+hich.add_command(matrix)
+
+if __name__ == "__main__":
+    hich()
