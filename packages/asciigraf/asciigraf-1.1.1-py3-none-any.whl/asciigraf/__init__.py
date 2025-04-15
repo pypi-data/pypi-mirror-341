89a4a17f31809e555c25a1ff8886e74597a07f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:57 +0000

    Add package asciigraf/1.1.1/asciigraf-1.1.1-py3-none-any.whl

---
+++ packages/asciigraf/asciigraf-1.1.1-py3-none-any.whl/asciigraf/__init__.py
@@ -0,0 +1,19 @@
+#############################################################################
+# Copyright (c) 2017-present, Opus One Energy Solutions Corporation
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#############################################################################
+
+from .asciigraf import graph_from_ascii  # noqa F401
+
+__all__ = ["graph_from_ascii"]
+
+
+def get_version():
+    from importlib.resources import files
+
+    return files(__name__).joinpath("VERSION").open("r").read().strip()
+
+
+__version__ = get_version()
