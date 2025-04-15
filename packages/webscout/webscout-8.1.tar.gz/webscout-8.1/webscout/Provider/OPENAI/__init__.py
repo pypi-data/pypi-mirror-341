e5e8361b61a501e8d82f39c5e111dda17079713e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:19:03 +0000

    Add package webscout/8.1/webscout-8.1.tar.gz

---
+++ packages/webscout/webscout-8.1.tar.gz/webscout-8.1/webscout/Provider/OPENAI/__init__.py
@@ -0,0 +1,17 @@
+# This file marks the directory as a Python package.
+from .deepinfra import *
+from .glider import *
+from .chatgptclone import *
+from .x0gpt import *
+from .wisecat import *
+from .venice import *
+from .exaai import *
+from .typegpt import *
+from .scirachat import *
+from .freeaichat import *
+from .llmchatco import *
+from .yep import * # Add YEPCHAT
+from .heckai import *
+from .sonus import *
+from .exachat import *
+from .netwrck import *
