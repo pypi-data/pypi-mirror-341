f6e2829a02fe523d318c39ee3f7cad8b9e4fb831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:58 +0000

    Add package itg-cli/1.0.4/itg_cli-1.0.4-py3-none-any.whl

---
+++ packages/itg-cli/itg_cli-1.0.4-py3-none-any.whl/itg_cli/__init__.py
@@ -0,0 +1,20 @@
+from itg_cli.commands import (
+    add_pack,
+    add_song,
+    censor,
+    get_censored,
+    uncensor,
+    OverwriteException,
+    UncensorException,
+)
+
+__all__ = [
+    "add_pack",
+    "add_song",
+    "censor",
+    "get_censored",
+    "uncensor",
+    "OverwriteException",
+    "UncensorException",
+]
+__version__ = "1.0.4"
