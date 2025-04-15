2002d0de0d007d97910c9ebd0636ee1d89d47cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:46 +0000

    Add package genutility/0.0.113/genutility-0.0.113-py3-none-any.whl

---
+++ packages/genutility/genutility-0.0.113-py3-none-any.whl/genutility/asynchronous.py
@@ -0,0 +1,100 @@
+import sys
+from numbers import Number
+from time import time
+from typing import Iterable, Optional, Sequence, TextIO, Union
+
+from .iter import _lstr, progressdata
+
+
+class progress_content:
+    def __init__(
+        self,
+        it: Union[Iterable, Sequence],
+        length: Optional[int] = None,
+        refresh: Number = 1,
+        file: Optional[TextIO] = sys.stdout,
+    ) -> None:
+        self.it = it
+        self.length = length
+        self.refresh = refresh
+        self.file = file
+
+    def __iter__(self):
+        return progressdata(self.it, self.length, self.refresh, file=self.file)
+
+    def __aiter__(self):
+        return self.AsyncIterProgress(self.it, self.length, self.refresh, file=self.file)
+
+    class AsyncIterProgress:
+        def __init__(
+            self, it: Union[Iterable, Sequence], length: Optional[int], refresh: Number, file: Optional[TextIO]
+        ):
+            self.it = it.__aiter__()
+            self.refresh = refresh
+            self.file = file
+
+            self.length, self.lstr = _lstr(self.it, length)
+            self.last = self.start = time()
+            self.total = 0
+
+        async def __anext__(self):
+            try:
+                elm = await self.it.__anext__()
+                self.total += len(elm)
+                current = time()
+                if current - self.last > self.refresh:
+                    self.last = current
+                    duration = current - self.start
+                    print(
+                        f"{self.total}{self.lstr}, running for {int(duration)} seconds ({self.total / duration:0.2e}/s).",
+                        end="\r",
+                        file=self.file,
+                    )
+                return elm
+
+            except StopAsyncIteration:
+                print(f"Finished {self.total} in {int(self.last - self.start)} seconds.", end="\r", file=self.file)
+                raise StopAsyncIteration
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    class gensync:
+        def __init__(self):
+            self.i = 3
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self.i > 0:
+                self.i -= 1
+                return "asd"
+            else:
+                raise StopIteration
+
+    class genasync:
+        def __init__(self):
+            self.i = 3
+
+        def __aiter__(self):
+            return self
+
+        async def __anext__(self):
+            if self.i > 0:
+                self.i -= 1
+                return "asd"
+            else:
+                raise StopAsyncIteration
+
+    for _i in progress_content(gensync()):
+        pass
+
+    print()
+
+    async def main():
+        async for _i in progress_content(genasync()):
+            pass
+
+    asyncio.run(main())
---
+++ packages/genutility/genutility-0.0.113-py3-none-any.whl/genutility/tqdm.py
@@ -0,0 +1,63 @@
+from typing import Any, Iterable, Iterator, Optional, Sequence, TypeVar, Union
+
+from typing_extensions import Self
+
+from tqdm import tqdm
+
+from .callbacks import BaseTask
+from .callbacks import Progress as _Progress
+from .callbacks import _Default
+
+T = TypeVar("T")
+
+
+class Task(BaseTask):
+    def __init__(self, total: Optional[float] = None, description: Optional[float] = None, **kwargs: Any) -> None:
+        self.pbar = tqdm(desc=description, total=total, **kwargs)
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, *args):
+        self.pbar.close()
+
+    def advance(self, delta: float) -> None:
+        self.pbar.update(n=delta)
+
+    def update(
+        self,
+        *,
+        completed: Optional[float] = _Default,
+        total: Optional[float] = _Default,
+        description: Optional[str] = _Default,
+        **kwargs: Any,
+    ) -> None:
+        if completed is not _Default:
+            self.pbar.n = completed
+        if total is not _Default:
+            self.pbar.total = total
+        if description is not _Default:
+            self.pbar.set_description(description)
+
+
+class Progress(_Progress):
+    def track(
+        self,
+        sequence: Union[Iterable[T], Sequence[T]],
+        total: Optional[float] = None,
+        description: Optional[str] = None,
+        **kwargs: Any,
+    ) -> Iterator[T]:
+        return tqdm(sequence, description, total, **kwargs)
+
+    def task(self, total: Optional[float] = None, description: Optional[str] = None, **kwargs: Any):
+        return Task(total, description, **kwargs)
+
+    def set_prolog(self, prolog: str) -> None:
+        raise NotImplementedError
+
+    def set_epilog(self, epilog: str) -> None:
+        raise NotImplementedError
+
+    def print(self, s: str, end="\n") -> None:
+        tqdm.write(s, end=end)
