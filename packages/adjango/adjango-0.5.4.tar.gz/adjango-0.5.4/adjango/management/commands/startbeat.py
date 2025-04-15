8a5683073d9c4baffb1f3d9f5dada197a0cddc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:24 +0000

    Add package adjango/0.5.4/adjango-0.5.4.tar.gz

---
+++ packages/adjango/adjango-0.5.4.tar.gz/adjango-0.5.4/adjango/management/commands/startbeat.py
@@ -0,0 +1,18 @@
+try:
+    from celery import current_app
+    from django.core.management.base import BaseCommand
+
+    from django_celery_beat.schedulers import DatabaseScheduler
+
+
+    class Command(BaseCommand):
+        help = 'Starts the Celery Beat Scheduler'
+
+        def handle(self, *args, **options):
+            beat = current_app.Beat(
+                scheduler=DatabaseScheduler,
+                loglevel='INFO',
+            )
+            beat.run()
+except ImportError:
+    pass
