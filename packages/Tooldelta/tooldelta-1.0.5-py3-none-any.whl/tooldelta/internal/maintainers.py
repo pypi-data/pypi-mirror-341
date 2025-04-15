c5f086a34fdb45bfdb2e72f1b2b3ce51d71ac00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 17:04:40 +0000

    Add package Tooldelta/1.0.5/tooldelta-1.0.5-py3-none-any.whl

---
+++ packages/Tooldelta/tooldelta-1.0.5-py3-none-any.whl/tooldelta/internal/maintainers.py
@@ -0,0 +1,200 @@
+from collections.abc import Callable
+from typing import TYPE_CHECKING
+from threading import Event
+
+from ..utils import fmts, create_result_cb
+from .types.player import Player, UnreadyPlayer
+from .types.player_abilities import Abilities, update_player_ability_from_server
+from ..constants import PacketIDS
+
+if TYPE_CHECKING:
+    from tooldelta import ToolDelta
+    from .packet_handler import PacketHandler
+
+
+class PlayerInfoMaintainer:
+    def __init__(self, frame: "ToolDelta"):
+        self.frame = frame
+        self.name_to_player: dict[str, Player] = {}
+        self.uq_to_player: dict[int, Player] = {}
+        self.uuid_to_player: dict[str, Player] = {}
+        self.xuid_to_player: dict[str, Player] = {}
+        self.player_abilities: dict[int, Abilities] = {}
+        self.player_abilities_getter_callback: dict[int, Callable[[bool], None]] = {}
+        self.inited_event = Event()
+
+    def hook_packet_handler(self, packet_handler: "PacketHandler"):
+        packet_handler.add_packet_listener(
+            packet_id=PacketIDS.PlayerList, cb=self._hook_playerlist, priority=100
+        )
+        packet_handler.add_packet_listener(
+            packet_id=PacketIDS.UpdateAbilities,
+            cb=self._hook_update_abilities,
+            priority=100,
+        )
+
+    def block_init(self):
+        # 载入初始玩家数据
+        if not self.name_to_player:
+            playerinfs = self.frame.launcher.get_players_info()
+            if playerinfs:
+                for unready_player in playerinfs.values():
+                    self.add_player(unready_player)
+                fmts.print_suc("已从接入点获取全局玩家数据")
+            else:
+                self._block_until_inited()
+
+    def getBotInfo(self) -> Player:
+        """
+        获取机器人本身的玩家对象。
+
+        Raises:
+            ValueError: 无法通过机器人名称获取信息
+
+        Returns:
+            Player: 玩家对象
+        """
+        bot_inf = self.name_to_player.get(self.frame.launcher.bot_name)
+        if bot_inf is None:
+            raise ValueError("无法获取机器人信息")
+        return bot_inf
+
+    def getPlayerByName(self, name: str) -> Player | None:
+        """
+        通过玩家名称获取玩家对象。
+
+        Args:
+            name (str): 玩家名
+
+        Returns:
+            Player | None: 玩家对象
+        """
+        return self.name_to_player.get(name)
+
+    def getPlayerByUniqueID(self, uqID: int) -> Player | None:
+        """
+        通过玩家uniqueID获取玩家对象。
+
+        Args:
+            uqID (int): uniqueID
+
+        Returns:
+            Player | None: 玩家对象
+        """
+        return self.uq_to_player.get(uqID)
+
+    def getPlayerByUUID(self, uuid: str) -> Player | None:
+        """
+        通过玩家UUID获取玩家对象。
+        Args:
+            uuid (str): UUID
+
+        Returns:
+            Player | None: 玩家对象
+        """
+        return self.uuid_to_player.get(uuid)
+
+    def getPlayerByXUID(self, uuid: str) -> Player | None:
+        """
+        通过玩家XUID获取玩家对象。
+        Args:
+            uuid (str): XUID
+
+        Returns:
+            Player | None: 玩家对象
+        """
+        return self.xuid_to_player.get(uuid)
+
+    def getAllPlayers(self) -> list[Player]:
+        """
+        获取所有当前在线的玩家的玩家对象列表。
+
+        Returns:
+            list[Player]: 玩家对象列表
+        """
+        return list(self.name_to_player.values())
+
+    def add_player(self, player: UnreadyPlayer):
+        ready_player = player.ready(self)
+        self.name_to_player[player.name] = ready_player
+        self.uq_to_player[player.unique_id] = ready_player
+        self.uuid_to_player[player.uuid] = ready_player
+        self.xuid_to_player[player.xuid] = ready_player
+
+    def remove_player(self, player: Player):
+        del self.name_to_player[player.name]
+        del self.uq_to_player[player.unique_id]
+        del self.uuid_to_player[player.uuid]
+        del self.xuid_to_player[player.xuid]
+        if player.unique_id in self.player_abilities:
+            del self.player_abilities[player.unique_id]
+        player.online = False
+
+    def _block_until_inited(self):
+        # 阻塞直到获取到包含全局玩家身份的 PlayerList 数据包
+        fmts.print_inf("正在等待获取全局玩家数据..")
+        ok = self.inited_event.wait(30)
+        if ok:
+            fmts.print_suc("已收到全局玩家数据")
+        else:
+            raise SystemExit("全局玩家数据获取超时")
+
+    def _hook_update_abilities(self, packet: dict):
+        ab_data = packet["AbilityData"]
+        uqID = ab_data["EntityUniqueID"]
+        player = self.getPlayerByUniqueID(uqID)
+        if player is None:
+            fmts.print_war(
+                f"[internal] PlayerInfoMaintainer: hook_update_abilities: 找不到玩家的 uniqueUD: {uqID}"
+            )
+            return False
+        update_player_ability_from_server(self, player, packet)
+        if uqID in self.player_abilities_getter_callback:
+            self.player_abilities_getter_callback[uqID](True)
+        return False
+
+    def _hook_playerlist(self, packet: dict):
+        self.inited_event.set()
+        if packet["ActionType"] == 0:
+            for entry in packet["Entries"]:
+                self._hook_add_player(entry)
+        else:
+            for entry in packet["Entries"]:
+                self._hook_remove_player(entry)
+        return False
+
+    def _hook_add_player(self, entry: dict):
+        unique_id = entry["EntityUniqueID"]
+        playername = entry["Username"]
+        self.add_player(
+            UnreadyPlayer(
+                uuid=entry["UUID"],
+                unique_id=unique_id,
+                name=playername,
+                xuid=entry["XUID"],
+                platform_chat_id=entry["PlatformChatID"],
+                build_platform=entry["BuildPlatform"],
+                online=True,
+            )
+        )
+
+    def _hook_remove_player(self, entry: dict):
+        if player := self.getPlayerByUUID(entry["UUID"]):
+            self.remove_player(player)
+        else:
+            fmts.print_war(
+                f"[internal] PlayerInfoMaintainer: remove_player: 找不到玩家的 UUID: {entry['UUID']}"
+            )
+
+    def get_player_ability(self, player: Player) -> Abilities:
+        ab = self.player_abilities.get(player.unique_id)
+        if ab is None:
+            getter, setter = create_result_cb()
+            self.player_abilities_getter_callback[player.unique_id] = setter
+            res = getter(20)
+            del self.player_abilities_getter_callback[player.unique_id]
+            if res is None:
+                raise ValueError("[internal] 获取玩家能力超时")
+            return self.player_abilities[player.unique_id]
+        else:
+            return ab
---
+++ packages/Tooldelta/tooldelta-1.0.5-py3-none-any.whl/tooldelta/internal/types/player.py
@@ -0,0 +1,158 @@
+from typing import TYPE_CHECKING
+from dataclasses import dataclass
+from tooldelta import game_utils
+from .player_abilities import Abilities, upload_player_abilities
+
+if TYPE_CHECKING:
+    from ..maintainers import PlayerInfoMaintainer
+
+
+@dataclass
+class UnreadyPlayer:
+    uuid: str
+    unique_id: int
+    name: str
+    xuid: str
+    platform_chat_id: str
+    build_platform: int
+    online: bool = True
+    abilities: Abilities | None = None
+
+    def ready(self, _parent: "PlayerInfoMaintainer"):
+        p = Player(
+            _parent,
+            self.uuid,
+            self.unique_id,
+            self.name,
+            self.xuid,
+            self.platform_chat_id,
+            self.build_platform,
+            self.online,
+        )
+        if self.abilities:
+            p.setAbilities(self.abilities, upload=False)
+        return p
+
+
+@dataclass
+class Player:
+    _parent: "PlayerInfoMaintainer"
+    uuid: str
+    unique_id: int
+    name: str
+    xuid: str
+    platform_chat_id: str
+    build_platform: int
+    online: bool = True
+
+    def show(self, text: str):
+        """
+        对玩家显示聊天栏消息
+        Args:
+            text: 消息文本
+        """
+        self._parent.frame.get_game_control().say_to(self.name, text)
+
+    def setTitle(self, title: str, sub_title: str = ""):
+        """
+        设置玩家标题
+        Args:
+            text: 标题文本
+        """
+        self._parent.frame.get_game_control().player_title(self.name, title)
+        if sub_title.strip():
+            self._parent.frame.get_game_control().player_subtitle(self.name, sub_title)
+
+    def setActionbar(self, text: str):
+        """
+        设置玩家行动栏文本
+        Args:
+            text: 动作条文本
+        """
+        self._parent.frame.get_game_control().player_actionbar(self.name, text)
+
+    def getSelector(self):
+        """
+        获取玩家选择器
+        Returns:
+            str: 选择器
+        """
+        return f'@a[name="{self.name}"]'
+
+    def getPos(self, timeout: float = 5) -> tuple[int, float, float, float]:
+        """
+        获取玩家坐标
+        Args:
+            timeout: 超时时间, 默认 5 秒
+        Returns:
+            tuple[int, float, float, float]: 维度ID, x, y, z
+        """
+        data = game_utils.getPos(self.name, timeout)
+        dim = data["dimension"]
+        pos = data["position"]
+        x, y, z = pos["x"], pos["y"], pos["z"]
+        return dim, x, y, z
+
+    def getItemCount(self, item_id: str, item_data: int = -1, timeout: float = 5):
+        """
+        获取玩家物品数量
+        Args:
+            item_id: 物品ID
+            item_data: 物品数据值
+            timeout: 超时时间, 默认 5 秒
+        Returns:
+            int: 物品数量
+        """
+        return game_utils.getItem(self.name, item_id, item_data)
+
+    def getScore(self, scoreboard_name: str, timeout: float = 5):
+        """
+        获取玩家计分板分数
+        Args:
+            scoreboard_name: 计分板名称
+            timeout: 超时时间, 默认 5 秒
+        Returns:
+            int: 计分板分数
+        """
+        return game_utils.getScore(scoreboard_name, self.getSelector(), timeout)
+
+    def input(self, prompt: str = "", timeout: float = 30):
+        """
+        获取玩家输入
+        Args:
+            prompt: 提示文本
+            timeout: 超时时间, 默认 30 秒
+        Returns:
+            str: 玩家输入
+        """
+        if prompt.strip():
+            self.show(prompt)
+        return game_utils.waitMsg(self.name, timeout)
+
+    @property
+    def abilities(self):
+        """
+        获取玩家能力
+        Returns:
+            Abilities: 玩家能力
+        """
+        return self._parent.get_player_ability(self)
+
+    @abilities.setter
+    def abilities(self, abilities: Abilities):
+        self.setAbilities(abilities)
+
+    def setAbilities(self, abilities: Abilities, upload=True):
+        """
+        设置玩家能力
+        Args:
+            abilities: 玩家能力
+        """
+        self._parent.player_abilities[self.unique_id] = abilities
+        if upload:
+            upload_player_abilities(
+                self._parent.frame.get_game_control(), self.unique_id, abilities
+            )
+
+    def is_op(self):
+        return self.abilities.command_permissions >= 3
