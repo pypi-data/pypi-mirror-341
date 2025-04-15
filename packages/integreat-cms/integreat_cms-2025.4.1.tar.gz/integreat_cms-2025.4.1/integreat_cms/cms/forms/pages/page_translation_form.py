a07fd7057e7aced1795bf96d777037aa475f0966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:05:20 +0000

    Add package integreat-cms/2025.4.1/integreat_cms-2025.4.1.tar.gz

---
+++ packages/integreat-cms/integreat_cms-2025.4.1.tar.gz/integreat_cms-2025.4.1/integreat_cms/cms/forms/pages/page_translation_form.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import logging
+
+from ...models import PageTranslation
+from ..machine_translation_form import MachineTranslationForm
+
+logger = logging.getLogger(__name__)
+
+
+class PageTranslationForm(MachineTranslationForm):
+    """
+    Form for creating and modifying page translation objects
+    """
+
+    class Meta:
+        """
+        This class contains additional meta configuration of the form class, see the :class:`django.forms.ModelForm`
+        for more information.
+        """
+
+        #: The model of this :class:`django.forms.ModelForm`
+        model = PageTranslation
+        #: The fields of the model which should be handled by this form
+        fields = [
+            *MachineTranslationForm.Meta.fields,
+            "slug",
+            "hix_score",
+            "hix_feedback",
+        ]
---
+++ packages/integreat-cms/integreat_cms-2025.4.1.tar.gz/integreat_cms-2025.4.1/integreat_cms/cms/views/pages/partial_page_tree_view.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import json
+from collections import defaultdict
+
+from django.http import HttpRequest, JsonResponse
+from django.template.loader import render_to_string
+from django.utils.translation import get_language
+from django.views.decorators.http import require_POST
+
+from ...decorators import permission_required
+from ...models.languages.language import Language
+from ..pages.page_context_mixin import PageContextMixin
+
+
+@permission_required("cms.view_page")
+@require_POST
+def render_partial_page_tree_views(
+    request: HttpRequest,
+    region_slug: str,
+    language_slug: str,
+) -> JsonResponse:
+    r"""
+    Retrieve the rendered subtree of a given root page
+
+    :param request: The current request
+    :param language_slug: The slug of the current language
+    :return: The rendered template responses
+    """
+    requested_tree_ids = [int(i) for i in json.loads(request.body.decode("utf-8"))]
+
+    region = request.region
+    language = region.get_language_or_404(language_slug, only_active=True)
+
+    backend_language = Language.objects.filter(slug=get_language()).first()
+
+    all_pages = (
+        region.pages.filter(tree_id__in=requested_tree_ids)
+        .prefetch_major_translations()
+        .prefetch_related("mirroring_pages")
+        .cache_tree(archived=False)
+    )
+
+    pages_by_id = defaultdict(list)
+    for page in all_pages:
+        pages_by_id[page.tree_id].append(page)
+
+    sub_trees = []
+    for tree_id in requested_tree_ids:
+        # Skip page trees which do not exist
+        if tree_id not in pages_by_id:
+            continue
+        # Get the tree of the given id
+        pages = pages_by_id[tree_id]
+        # The first element must be the root node
+        parent = pages[0]
+        # The remaining pages are the descendants
+        children = pages[1:]
+        sub_trees.append(
+            render_to_string(
+                "pages/_page_tree_children.html",
+                {
+                    **PageContextMixin().get_context_data(),
+                    "backend_language": backend_language,
+                    "pages": children,
+                    "language": language,
+                    "languages": region.active_languages,
+                    "parent_id": parent.id,
+                },
+                request,
+            ),
+        )
+
+    return JsonResponse({"data": sub_trees})
