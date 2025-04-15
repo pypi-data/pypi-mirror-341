bfcb59c732f75afd68870b9b7d2642e24854c6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:06:01 +0000

    Add package hurmodel/1.0.3/hurmodel-1.0.3.tar.gz

---
+++ packages/hurmodel/hurmodel-1.0.3.tar.gz/hurmodel-1.0.3/setup.py
@@ -0,0 +1,26 @@
+# THIS IS AN EXTENSION NETWORK OF THE SKILLS OF THE HURNET ARTIFICIAL NEURAL NETWORK
+# The HurModel (Hur of HurNet and Model of language model) is a sophisticated artificial neural network computational algorithm that proposes a new architecture for language models based on Transformers.
+# This architecture uses aspects already known from Transformers applied to GPT models, such as normalizers, tokenizers, embeddings and attention mechanisms,
+# but adds new concepts such as intelligent initialization of weights using layers of the HurNet network, insertion of auxiliary HurNet layers and use of HurNet networks in the final adjustment of weights.
+# As an integral part of the architecture, HurModel models do not require hyperparameter configurations, since this can be done completely autonomously without developer intervention,
+# allowing the programmer to focus only on the data that is the most important part of the model. All these features make the HurModel language model architecture superior to the already known GPT models.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from setuptools import setup, find_packages
+package_name = 'hurmodel'
+version = '1.0.3'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    install_requires=['torch==2.4.1', 'tiktoken==0.4.0', 'psutil==7.0.0'],
+    url='https://github.com/',
+    license='Proprietary Software'
+)
+# THIS IS AN EXTENSION NETWORK OF THE SKILLS OF THE HURNET ARTIFICIAL NEURAL NETWORK
+# The HurModel (Hur of HurNet and Model of language model) is a sophisticated artificial neural network computational algorithm that proposes a new architecture for language models based on Transformers.
+# This architecture uses aspects already known from Transformers applied to GPT models, such as normalizers, tokenizers, embeddings and attention mechanisms,
+# but adds new concepts such as intelligent initialization of weights using layers of the HurNet network, insertion of auxiliary HurNet layers and use of HurNet networks in the final adjustment of weights.
+# As an integral part of the architecture, HurModel models do not require hyperparameter configurations, since this can be done completely autonomously without developer intervention,
+# allowing the programmer to focus only on the data that is the most important part of the model. All these features make the HurModel language model architecture superior to the already known GPT models.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
