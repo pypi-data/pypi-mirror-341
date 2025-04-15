269027c27927fe3dc8a8ea1584c1ee3f4275d38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:51 +0000

    Add package ifgen/4.0.2/ifgen-4.0.2.tar.gz

---
+++ packages/ifgen/ifgen-4.0.2.tar.gz/ifgen-4.0.2/ifgen/struct/source.py
@@ -0,0 +1,24 @@
+"""
+A module for generating struct source files.
+"""
+
+# internal
+from ifgen.generation.interface import GenerateTask
+from ifgen.struct.methods import struct_methods
+from ifgen.struct.methods.fields import bit_fields
+from ifgen.struct.stream import struct_stream_methods
+
+
+def create_struct_source(task: GenerateTask) -> None:
+    """Create a header file based on a struct definition."""
+
+    if task.is_python:
+        return
+
+    if task.instance["stream"] or task.instance["codec"]:
+        with task.source_boilerplate([]) as writer:
+            struct_methods(task, writer, False)
+            bit_fields(task, writer, False)
+            if task.instance["stream"]:
+                writer.empty()
+                struct_stream_methods(task, writer, False)
