4e1cb4143c3b7d44b82ad6716bd082798ee02e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:19:09 +0000

    Add package bkmr/4.18.0/bkmr-4.18.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/bkmr/bkmr-4.18.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/bkmr/src/application/services/mod.rs
@@ -0,0 +1,9 @@
+// src/application/services/mod.rs
+pub mod action_service;
+pub mod bookmark_service;
+pub mod bookmark_service_impl;
+pub mod factory;
+pub mod interpolation;
+pub mod tag_service;
+pub mod tag_service_impl;
+pub mod template_service;
