c94c44b463e89473c15723614d9c8e50ecf6c6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:37 +0000

    Add package ks_eastmoney_fundamental_api/0.0.33/ks_eastmoney_fundamental_api-0.0.33-py3-none-any.whl

---
+++ packages/ks_eastmoney_fundamental_api/ks_eastmoney_fundamental_api-0.0.33-py3-none-any.whl/ks_eastmoney_fundamental_api/__about__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present liuyanzhi08 <702368372@qq.com>
+#
+# SPDX-License-Identifier: MIT
+# __version__ = "0.0.30"
+# __version__ = "0.0.32" # 增加YOYOR,YOYNI,CAGRTOR
+__version__ = "0.0.33" # 增加css_mry函数，支持批量获取最近N年年报数据
