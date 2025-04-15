37da2993990dc108d52b30be12f7d7f0569c6dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:56 +0000

    Add package hometaxbot/0.2.6/hometaxbot-0.2.6-py2.py3-none-any.whl

---
+++ packages/hometaxbot/hometaxbot-0.2.6-py2.py3-none-any.whl/hometaxbot/scraper/reports.py
@@ -0,0 +1,91 @@
+from datetime import date
+from typing import Generator
+
+from hometaxbot import models
+from hometaxbot.scraper import HometaxScraper, model_from_hometax_json
+from hometaxbot.scraper import model_from_hometax_xml
+
+
+def 전자신고결과조회(scraper: HometaxScraper, begin: date, end: date):
+    scraper.request_permission('teht', 'UTERNAAZ43')
+
+    for 세목코드 in {'10': '종합소득세', '14': '원천세', '31': '법인세', '41': '부가세'}:
+        for element in scraper.paginate_action_json(
+                'ATERNABA016R14', 'UTERNAAZ43', real_screen_id='UTERNAAZ43', subdomain='teht',
+                json={
+                    "befCallYn": "",
+                    "itrfCd": 세목코드,
+                    "mdfRtnPyppApplcCtl": "100202 410202 330202 100203 410203 310203 450202 220202 220203 320202 240202 240203",
+                    "ntplInfpYn": "Y",
+                    "pubcUserNo": scraper.pubcUserNo,
+                    "rtnDtEnd": end.strftime("%Y%m%d"),
+                    "rtnDtSrt": begin.strftime("%Y%m%d"),
+                    "sbmsMatePubcPrslBrkdYn": "Y",
+                    "startBsno": "",
+                    "tin": "",
+                    "txprRgtNo": scraper.selected_trader.납세자번호,
+                    "wrtnRtnYn": "N",
+                    "rtnCvaId": "",
+                    "endBsno": "",
+                    "gubun": "",
+                    "resultCnt": "0",
+                    "pageSize": "0",
+                    "pageNum": "0",
+                }):
+            yield model_from_hometax_json(models.전자신고결과조회, element)
+
+
+def 납부내역(scraper: HometaxScraper, begin: date, end: date) -> Generator[models.납부내역, None, None]:
+    scraper.request_permission('teht')
+    for element in scraper.paginate_action_json(
+            'ATERMAAA001R01', 'UTERMAAD01', subdomain='teht',
+            json={
+                "excelYn": "N",
+                "inqrClCd": "01",
+                'lgnUserTxprDscmNo': scraper.selected_trader.납세자번호,
+                "pmtDtEnd": end.strftime("%Y%m%d"),
+                "pmtDtStrt": begin.strftime("%Y%m%d"),
+                "tin": scraper.tin,
+            }):
+        yield model_from_hometax_json(models.납부내역, element)
+
+
+def 환급금조회(scraper: HometaxScraper, begin: date, end: date) -> Generator[models.환급금조회, None, None]:
+    scraper.request_permission('teht')
+    for element in scraper.paginate_action_json(
+            'ATERDAAA001R01', 'UTERDAAA01', subdomain='teht',
+            json={
+                "inqrClCd": "",
+                "strtDt": begin.strftime("%Y%m%d"),
+                "endDt": end.strftime("%Y%m%d"),
+                'txaaId': '',
+                "txaaYn": "N",
+                'txprDscmNo': '',
+            }):
+        yield model_from_hometax_json(models.환급금조회, element)
+
+
+def 체납내역(scraper: HometaxScraper, begin: date, end: date) -> Generator[models.체납내역, None, None]:
+    # My홈택스 체납
+    scraper.request_permission(screen_id='UTXPPBAB73')
+    for element in scraper.paginate_action_json(
+            'ATXPPBAA001R17', 'UTXPPBAB73',
+            json={"endDt": "",
+                  "ntplCrpClCd": "03",
+                  "strtDt": "",
+                  "tin": scraper.tin,
+                  "survTtl": ""}):
+        yield model_from_hometax_json(models.체납내역, element)
+
+
+def 고지내역(scraper: HometaxScraper, begin: date, end: date) -> Generator[models.고지내역, None, None]:
+    # My홈택스 고지
+    scraper.request_permission(screen_id='UTXPPBAB72')
+    for element in scraper.paginate_action_json(
+            'ATXPPBAA001R11', 'UTXPPBAB72',
+            json={"endDt": end.strftime("%Y%m%d"),
+                  "ntplCrpClCd": "03",
+                  "strtDt": begin.strftime("%Y%m%d"),
+                  "tin": scraper.tin,
+                  "survTtl": ""}):
+        yield model_from_hometax_json(models.고지내역, element)
