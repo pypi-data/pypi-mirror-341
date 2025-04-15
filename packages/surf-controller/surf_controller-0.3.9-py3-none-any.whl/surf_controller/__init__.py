2538b8d264bbfa75bf80c26ca3458b2f42cfc92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 05:03:38 +0000

    Add package surf-controller/0.3.9/surf_controller-0.3.9-py3-none-any.whl

---
+++ packages/surf-controller/surf_controller-0.3.9-py3-none-any.whl/surf_controller/__init__.py
@@ -0,0 +1,24 @@
+import shutil
+from pathlib import Path
+
+
+def setup_config():
+    # Define the user's config directory
+    user_config_dir = Path.home() / ".surf_controller"
+    user_config_file = user_config_dir / "config.toml"
+
+    # Create the directory if it doesn't exist
+    user_config_dir.mkdir(parents=True, exist_ok=True)
+
+    # If the user's config file doesn't exist, copy the default one
+    if not user_config_file.exists():
+        print("You are running surf_controller for the first time.")
+        print("Surfcontroller will guide you through the setup process.")
+        default_config = Path(__file__).parent / "config.toml"
+        shutil.copy(default_config, user_config_file)
+
+
+setup_config()
+
+__version__ = "0.3.9"
+print(f"Welcome to surf_controller version {__version__}")
