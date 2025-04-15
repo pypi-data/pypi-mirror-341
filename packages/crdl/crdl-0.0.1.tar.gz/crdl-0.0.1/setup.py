4256d5aa963f67ca4f461e8ced0f3ab49e1d2c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:06:12 +0000

    Add package crdl/0.0.1/crdl-0.0.1.tar.gz

---
+++ packages/crdl/crdl-0.0.1.tar.gz/crdl-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="crdl",
+    version="0.0.1",
+    author="TanmoyTheBoT",
+    author_email="tanmoysarkershuvo@gmail.com",
+    description="A simple crunchyroll downloader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TanmoyTheBoT/Crunchy-Downloader",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "requests",
+        "pywidevine",
+        "pathlib",
+        "setuptools",
+    ],
+    entry_points={
+        "console_scripts": [
+            "crdl=crunchyroll.cli:main",
+        ],
+    },
+
+) 
