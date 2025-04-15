8ec2557c0a7a8db1542cd884d8e12714bc0fa443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:37 +0000

    Add package CodeToClassDiagram/1.1.4/codetoclassdiagram-1.1.4.tar.gz

---
+++ packages/CodeToClassDiagram/codetoclassdiagram-1.1.4.tar.gz/codetoclassdiagram-1.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CodeToClassDiagram',
+    version='1.1.4',
+    description='Génération de diagrammes de classes à partir d’un projet source.',
+    author='Votre Nom',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "regex",
+        "tree-sitter",
+        "tree-sitter-c-sharp"
+    ],
+    entry_points={
+        'console_scripts': [
+            'code2class=CodeToClassDiagram.main:main',
+        ],
+    },
+)
