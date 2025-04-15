090262ac44a167a9982fc35e1e3761424444564c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 17:04:47 +0000

    Add package portal-cli/0.1.17/portal_cli-0.1.17.tar.gz

---
+++ packages/portal-cli/portal_cli-0.1.17.tar.gz/portal_cli-0.1.17/main_cli.py
@@ -0,0 +1,24 @@
+import click
+from commands import upload, search, scan, chart, transfer, deploy, get_scan_data
+
+# Define the version directly in this file
+__version__ = "0.1.15"  # Change this to your desired version
+
+@click.group()
+@click.version_option(version=__version__, prog_name="portal-cli", help="Show version and exit",
+                       message='%(prog)s, version %(version)s')
+def cli():
+    """Multi-action CLI for image management."""
+    pass
+
+cli.add_command(upload.upload)
+cli.add_command(search.search)
+cli.add_command(scan.scan)
+cli.add_command(chart.chart)
+cli.add_command(transfer.transfer)
+cli.add_command(deploy.deploy)
+cli.add_command(get_scan_data.get_scan_data)
+
+if __name__ == "__main__":
+    cli()
+
