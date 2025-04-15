8c056292f7df798d7e30ff8a404a15407a530166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:05:20 +0000

    Add package rat-tools-v2-shv/0.0.2/rat_tools_v2_shv-0.0.2.tar.gz

---
+++ packages/rat-tools-v2-shv/rat_tools_v2_shv-0.0.2.tar.gz/rat_tools_v2_shv-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rat_tools_v2_shv',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+    include_package_data=True,
+)
