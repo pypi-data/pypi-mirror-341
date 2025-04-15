f30614445b045df47ef092b4caa8db5a3889390c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:22 +0000

    Add package hgraph/0.4.31/hgraph-0.4.31.tar.gz

---
+++ packages/hgraph/hgraph-0.4.31.tar.gz/hgraph-0.4.31/src/hgraph/_impl/_operators/_to_json.py
@@ -0,0 +1,279 @@
+import json
+from datetime import datetime, date, time, timedelta
+from enum import Enum
+from functools import singledispatch
+from itertools import chain
+from typing import Callable, Any
+
+from frozendict import frozendict as fd
+
+from hgraph import (
+    compute_node,
+    to_json,
+    TS,
+    HgTypeMetaData,
+    HgCompoundScalarType,
+    HgAtomicType,
+    HgTSTypeMetaData,
+    TIME_SERIES_TYPE,
+    AUTO_RESOLVE,
+    HgDictScalarType,
+    HgTupleCollectionScalarType,
+    from_json,
+    OUT,
+    DEFAULT,
+    HgTSLTypeMetaData,
+    HgTSSTypeMetaData,
+    HgTSBTypeMetaData,
+    HgTSDTypeMetaData, REMOVE_IF_EXISTS,
+)
+
+__all__ = []
+
+from hgraph._operators._to_json import to_json_builder, from_json_builder
+
+
+def error_wrapper(fn: Callable[[Any], str], context: str) -> Callable[[Any], str]:
+    def _fn(v):
+        try:
+            return fn(v)
+        except Exception as e:
+            raise RuntimeError(f"Error while converting {context} with value {v}:\n{e}")
+
+    return _fn
+
+
+@singledispatch
+def to_json_converter(value: HgTypeMetaData, delta=False) -> Callable[[Any], str]:
+    raise RuntimeError(f"Cannot convert {value} to JSON")
+
+
+@to_json_converter.register(HgCompoundScalarType)
+def _(value: HgCompoundScalarType, delta=False) -> Callable[[Any], str]:
+    to_json = []
+    for k, tp in value.meta_data_schema.items():
+        m = to_json_converter(tp, delta)
+        to_json.append(
+            error_wrapper(lambda v, m=m, k=k: f'"{k}": {m(v_)}' if (v_ := getattr(v, k, None)) is not None else "",
+                          f"{str(value)}: {k}:{str(tp)}"))
+    return error_wrapper(lambda v, to_json=to_json: f'{{{", ".join(v_ for fn in to_json if (v_ := fn(v)))}}}',
+                         f"{str(value)}")
+
+
+@to_json_converter.register(HgAtomicType)
+def _(value: HgAtomicType, delta=False) -> Callable[[Any], str]:
+    if issubclass(value.py_type, Enum):
+        return lambda v: f'"{v.name}"'
+
+    return {
+        bool: lambda v: None if v is None else "true" if v else "false",
+        int: lambda v: None if v is None else f"{v}",
+        float: lambda v: None if v is None else f"{v}",
+        str: lambda v: None if v is None else f'"{v}"',
+        date: lambda v: None if v is None else f'"{v.strftime("%Y-%m-%d")}"',
+        time: lambda v: None if v is None else f'"{v.strftime("%H:%M:%S.%f")}"',
+        timedelta: _td_to_str,
+        datetime: lambda v: None if v is None else f'"{v.strftime("%Y-%m-%d %H:%M:%S.%f")}"',
+    }[value.py_type]
+
+
+@to_json_converter.register(HgDictScalarType)
+def _(value: HgDictScalarType, delta=False) -> Callable[[Any], str]:
+    k_fn = to_json_converter(value.key_type, delta)
+    if not issubclass(value.key_type.py_type, (str, date, time, timedelta, datetime)):
+        k_fn_inner = k_fn
+        k_fn = lambda k, k_fn=k_fn_inner: json.dumps(k_fn(k))  # escape the string
+    v_fn = to_json_converter(value.value_type, delta)
+
+    def _to_json(v, k_fn=k_fn, v_fn=v_fn):
+        items = (f"{k_fn(k)}: {v_fn(v_)}" for k, v_ in v.items())
+        return f'{{{", ".join(items)}}}'
+
+    return _to_json
+
+
+@to_json_converter.register(HgTupleCollectionScalarType)
+def _(value: HgTupleCollectionScalarType, delta=False) -> Callable[[Any], str]:
+    v_fn = to_json_converter(value.element_type, delta)
+    return error_wrapper(lambda v, v_fn=v_fn: f'[{", ".join(v_fn(i) for i in v)}]', f"{str(value)}")
+
+
+@to_json_converter.register(HgTSTypeMetaData)
+def _(value: HgTSTypeMetaData, delta=False) -> Callable[[Any], str]:
+    fn = to_json_converter(value.value_scalar_tp, delta)
+    return lambda v, fn=fn: fn(v.value) if v.valid else ""
+
+
+@to_json_converter.register(HgTSLTypeMetaData)
+def _(value: HgTSLTypeMetaData, delta=False) -> Callable[[Any], str]:
+    fn = to_json_converter(value.value_tp, delta)
+
+    def qstr(i):
+        return f'"{str(i)}"'
+
+    if delta:
+        return lambda v, fn=fn: f'{{{", ".join(qstr(i) + ": " + fn(t) for i, t in v.modified_items())}}}'
+    else:
+        return lambda v, fn=fn: f'[{", ".join(fn(i) for i in v)}]'
+
+
+@to_json_converter.register(HgTSBTypeMetaData)
+def _(value: HgTSBTypeMetaData, delta=False) -> Callable[[Any], str]:
+    schema = {}
+    for k, tp in value.bundle_schema_tp.meta_data_schema.items():
+        f = to_json_converter(tp, delta)
+        schema[k] = lambda i, t, f=f: f'"{i}": {f(t)}'
+    if delta:
+        return lambda v, schema=schema: f'{{{", ".join(schema[i](i, t) for i, t in v.items())}}}'
+    else:
+        return lambda v, schema=schema: f'{{{", ".join(schema[i](i, t) for i, t in v.modified_items())}}}'
+
+
+@to_json_converter.register(HgTSSTypeMetaData)
+def _(value: HgTSSTypeMetaData, delta=False) -> Callable[[Any], str]:
+    fn = to_json_converter(value.value_scalar_tp, delta)
+    if not delta:
+        return lambda v, fn=fn: f'[{", ".join(fn(i) for i in v.values())}]'
+    else:
+        def f_i(a, v, f):
+            return f'"{a}": [{", ".join(fn(i) for i in v)}]' if len(v) else None
+
+        return lambda v, fn=fn: (
+            f'{{{", ".join(i for i in (f_i("added", v.added(), fn), f_i("removed", v.removed(), fn)) if i)}}}')
+
+
+@to_json_converter.register(HgTSDTypeMetaData)
+def _(value: HgTSDTypeMetaData, delta=False) -> Callable[[Any], str]:
+    k_fn = to_json_converter(value.key_tp)
+    if not issubclass(value.key_tp.py_type, (str, date, time, timedelta, datetime)):
+        k_fn_inner = k_fn
+        k_fn = lambda k, k_fn=k_fn_inner: json.dumps(k_fn(k))  # escape the string
+    v_fn = to_json_converter(value.value_tp, delta)
+    f = lambda k, v, k_fn=k_fn, v_fn=v_fn: f'{k_fn(k)}: {v_fn(v)}' if v is not None else f'{k_fn(k)}: null'
+    if not delta:
+        return lambda v, f=f: f'{{{", ".join(f(i, t) for i, t in v.items())}}}'
+    else:
+        return lambda v, f=f: f'{{{", ".join(f(i, t) for i, t in chain(v.modified_items(), ((k, None) for k in v.removed_keys())))}}}'
+
+def _td_to_str(delta: timedelta) -> str:
+    if delta is None:
+        return None
+    days = delta.days
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    ms = delta.microseconds
+    return f'"{days}:{hours}:{minutes}:{seconds}.{ms:06}"'
+
+
+@singledispatch
+def from_json_converter(value: HgTypeMetaData, delta=False) -> Callable[[dict], Any]:
+    raise RuntimeError(f"Cannot convert to '{value}' from JSON")
+
+
+@from_json_converter.register(HgTSTypeMetaData)
+def _(value: HgTSTypeMetaData, delta=False) -> Callable[[Any], Any]:
+    return from_json_converter(value.value_scalar_tp, delta)
+
+
+@from_json_converter.register(HgCompoundScalarType)
+def _(value: HgCompoundScalarType, delta=False) -> Callable[[Any], Any]:
+    fns = []
+    for k, tp in value.meta_data_schema.items():
+        fns.append((k, error_wrapper(lambda v1, tp=tp, k=k: from_json_builder(tp, delta)(v1.get(k, None)), f"{str(value)} {k}: {str(tp)}")))
+    return error_wrapper(lambda v2, fns=fns, tp=value.py_type: tp(**{k: v_ for k, fn in fns if (v_ := fn(v2)) is not None}), f"{str(value)}")
+
+
+@from_json_converter.register(HgAtomicType)
+def _(value: HgAtomicType, delta=False) -> Callable[[Any], Any]:
+    if issubclass(value.py_type, Enum):
+        return lambda v, tp=value.py_type: None if v is None else getattr(tp, v)
+
+    return {
+        date: lambda v: None if v is None else datetime.strptime(v, "%Y-%m-%d").date(),
+        time: lambda v: None if v is None else datetime.strptime(v, "%H:%M:%S.%f").time(),
+        timedelta: _str_to_td,
+        datetime: lambda v: None if v is None else datetime.strptime(v, "%Y-%m-%d %H:%M:%S.%f"),
+    }.get(value.py_type, lambda v: v)
+
+
+def _str_to_td(s: str) -> timedelta:
+    if s is None:
+        return None
+    days, hours, minutes, seconds_ms = s.split(":")
+    seconds, ms = seconds_ms.split(".")
+    return timedelta(
+        days=int(days),
+        hours=int(hours),
+        minutes=int(minutes),
+        seconds=int(seconds),
+        microseconds=int(ms),
+    )
+
+
+@from_json_converter.register(HgDictScalarType)
+def _(value: HgDictScalarType, delta=False) -> Callable[[dict], Any]:
+    k_fn = from_json_converter(value.key_type, delta)
+    if not issubclass(value.key_type.py_type, (str, date, time, timedelta, datetime)):
+        k_fn_inner = k_fn
+        k_fn = lambda k, k_fn=k_fn_inner: k_fn(json.loads(k))
+    v_fn = from_json_converter(value.value_type, delta)
+    return lambda v, k_fn=k_fn, v_fn=v_fn: {k_fn(k_): v_fn(v_) for k_, v_ in v.items()}
+
+
+@from_json_converter.register(HgTupleCollectionScalarType)
+def _(value: HgTupleCollectionScalarType, delta=False) -> Callable[[list], Any]:
+    v_fn = from_json_converter(value.element_type, delta)
+    return lambda v, v_fn=v_fn: tuple(v_fn(i) for i in v)
+
+
+@from_json_converter.register(HgTSLTypeMetaData)
+def _(value: HgTSLTypeMetaData, delta=False) -> Callable[[list], Any]:
+    fn = from_json_converter(value.value_tp, delta)
+    return lambda v, fn=fn: (
+        {int(k): fn(i) for k, i in v.items()}
+        if isinstance(v, dict)
+        else tuple(fn(i) for i in v))
+
+
+@from_json_converter.register(HgTSSTypeMetaData)
+def _(value: HgTSSTypeMetaData, delta=False) -> Callable[[list], Any]:
+    from hgraph._impl._types._tss import PythonSetDelta
+
+    fn = from_json_converter(value.value_scalar_tp, delta)
+    return lambda v, fn=fn: (
+        PythonSetDelta(added={fn(i) for i in v.get("added", ())}, removed={fn(i) for i in v.get("removed", ())})
+        if isinstance(v, dict)
+        else tuple(fn(i) for i in v))
+
+
+@from_json_converter.register(HgTSBTypeMetaData)
+def _(value: HgTSBTypeMetaData, delta=False) -> Callable[[dict], Any]:
+    schema = {}
+    for k, tp in value.bundle_schema_tp.meta_data_schema.items():
+        f = from_json_converter(tp, delta)
+        schema[k] = lambda i, t, f=f: f(t)
+
+    return lambda v, schema=schema: {i: schema[i](i, t) for i, t in v.items()}
+
+
+@from_json_converter.register(HgTSDTypeMetaData)
+def _(value: HgTSDTypeMetaData, delta=False) -> Callable[[dict], Any]:
+    k_fn = from_json_converter(value.key_tp)
+    if not issubclass(value.key_tp.py_type, (str, date, time, timedelta, datetime)):
+        k_fn_inner = k_fn
+        k_fn = lambda k, k_fn=k_fn_inner: k_fn(json.loads(k))
+    v_fn = from_json_converter(value.value_tp, delta)
+
+    return lambda v, k_fn=k_fn, v_fn=v_fn: {k_fn(k): v_fn(v) if v is not None else REMOVE_IF_EXISTS for k, v in
+                                            v.items()}
+
+
+@compute_node(overloads=to_json)
+def to_json_generic(ts: TIME_SERIES_TYPE, _tp: type[TIME_SERIES_TYPE] = AUTO_RESOLVE, delta: bool = False) -> TS[str]:
+    return to_json_builder(_tp, delta)(ts)
+
+
+@compute_node(overloads=from_json)
+def from_json_generic(ts: TS[str], _tp: type[OUT] = AUTO_RESOLVE, delta: bool = False) -> DEFAULT[OUT]:
+    value = json.loads(ts.value)
+    return from_json_builder(_tp, delta)(value)
---
+++ packages/hgraph/hgraph-0.4.31.tar.gz/hgraph-0.4.31/src/hgraph/_wiring/_wiring_node_instance.py
@@ -0,0 +1,248 @@
+import typing
+from dataclasses import dataclass, field
+from typing import Any, MutableMapping
+
+from frozendict import frozendict
+
+from hgraph._wiring._wiring_errors import CustomMessageWiringError
+from hgraph._wiring._wiring_node_signature import WiringNodeType
+
+if typing.TYPE_CHECKING:
+    from hgraph import (
+        WiringNodeClass,
+        WiringNodeSignature,
+        HgTimeSeriesTypeMetaData,
+        NodeSignature,
+        NodeBuilder,
+        Edge,
+        WiringPort,
+        HgTSBTypeMetaData,
+)
+
+__all__ = ("WiringNodeInstance", "WiringNodeInstanceContext", "create_wiring_node_instance")
+
+
+class InputsKey:
+
+    def __init__(self, inputs):
+        self._inputs = inputs
+        try:
+            self._hash = hash(inputs)
+        except TypeError:
+            # Degrade to best effort hash, this works around issues where some of the values are not hashable
+            # but are still logically immutable.
+            self._hash = hash(tuple((k, _safe_hash(v)) for k, v in self._inputs.items()))
+
+    def __eq__(self, other: Any) -> bool:
+        return all(
+            v.__orig_eq__(other._inputs[k]) if hasattr(v, "__orig_eq__") else v == other._inputs[k]
+            for k, v in self._inputs.items()
+        )
+
+    def __hash__(self) -> int:
+        return self._hash
+
+
+def _safe_hash(v):
+    try:
+        return hash(v)
+    except TypeError:
+        return id(v)
+
+
+class WiringNodeInstanceContext:
+    """
+    This must exist when wiring and is used to cache the WiringNodeInstances created during the
+    graph building process.
+    """
+
+    __stack__: ["WiringNodeInstanceContext"] = []
+
+    def __init__(self, depth=1, error_capture_options=None):
+        self._node_instances: dict[tuple, WiringNodeInstance] = {}
+        self._depth = depth
+        self._error_capture_options = error_capture_options or (
+            self.__stack__[-1]._error_capture_options if self.__stack__ else None
+        )
+
+    def create_wiring_node_instance(
+        self, node: "WiringNodeClass", resolved_signature: "WiringNodeSignature", inputs: frozendict[str, Any]
+    ) -> "WiringNodeInstance":
+        key = (InputsKey(inputs), resolved_signature, node)
+        if (node_instance := self._node_instances.get(key, None)) is None:
+            from hgraph import WiringGraphContext
+
+            self._node_instances[key] = node_instance = WiringNodeInstance(
+                node=node,
+                resolved_signature=resolved_signature,
+                inputs=inputs,
+                wiring_path_name=(WiringGraphContext.instance() or WiringGraphContext(None)).wiring_path_name(),
+                **(self._error_capture_options or {}),
+            )
+        return node_instance
+
+    @classmethod
+    def instance(cls) -> "WiringNodeInstanceContext":
+        return cls.__stack__[-1] if cls.__stack__ else None
+
+    def graph_nesting_depth(self) -> int:
+        return sum(c._depth for c in self.__stack__)
+
+    def __enter__(self):
+        self.__stack__.append(self)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__stack__.pop()
+
+
+def create_wiring_node_instance(
+    node: "WiringNodeClass",
+    resolved_signature: "WiringNodeSignature",
+    inputs: frozendict[str, Any],
+) -> "WiringNodeInstance":
+    return WiringNodeInstanceContext.instance().create_wiring_node_instance(node, resolved_signature, inputs)
+
+
+@dataclass  # We will write our own equality check, but still want a hash
+class WiringNodeInstance:
+
+    NODE_SIGNATURE = None
+
+    node: "WiringNodeClass"
+    resolved_signature: "WiringNodeSignature"
+    inputs: frozendict[str, Any]  # This should be a mix of WiringPort for time series inputs and scalar values.
+    wiring_path_name: str
+    label: str = ""
+    error_handler_registered: bool = False
+    trace_back_depth: int = 1  # TODO: decide how to pick this up, probably via the error context?
+    capture_values: bool = False
+    _treat_as_source_node: bool = False
+    non_input_dependencies: list["WiringNodeInstance"] = field(default_factory=list)
+    ranking_alternatives: list["WiringNodeInstance"] = field(default_factory=list)
+
+    def __lt__(self, other: "WiringNodeInstance") -> bool:
+        # The last part gives potential for inconsistent ordering, a better solution would be to
+        # consider the inputs, but that may contain values which do not support ordering.
+        # So for now we use this simple hack which is sufficient for the short term.
+        return self.resolved_signature.signature < other.resolved_signature.signature or id(self) < id(other)
+
+    def __eq__(self, other):
+        # Rely on WiringNodeInstances to be interned data structures
+        return self is other
+
+    def __hash__(self) -> int:
+        # Rely on WiringNodeInstances to be interned data structures
+        return id(self)
+
+    def __repr__(self):
+        return self.resolved_signature.signature
+
+    def mark_error_handler_registered(self, trace_back_depth: int = 1, capture_values: bool = False):
+        self.error_handler_registered = True
+        self.trace_back_depth = trace_back_depth
+        self.capture_values = capture_values
+
+    @property
+    def is_stub(self) -> bool:
+        from hgraph._wiring._wiring_node_class._stub_wiring_node_class import StubWiringNodeClass
+
+        return isinstance(self.node, StubWiringNodeClass)
+
+    @property
+    def is_source_node(self) -> bool:
+        return (
+            self.resolved_signature.node_type in (WiringNodeType.PUSH_SOURCE_NODE, WiringNodeType.PULL_SOURCE_NODE)
+            or self._treat_as_source_node
+        )
+
+    def mark_treat_as_source_node(self):
+        self._treat_as_source_node = True
+
+    def add_ranking_alternative(self, wp: "WiringNodeInstance"):
+        self.ranking_alternatives.append(wp)
+
+    def add_indirect_dependency(self, wp: "WiringNodeInstance"):
+        self.non_input_dependencies.append(wp)
+
+    @property
+    def output_type(self) -> "HgTimeSeriesTypeMetaData":
+        return self.resolved_signature.output_type
+
+    @property
+    def node_signature(self) -> "NodeSignature":
+        from hgraph._runtime import NodeSignature, NodeTypeEnum
+        if WiringNodeInstance.NODE_SIGNATURE is None:
+            WiringNodeInstance.NODE_SIGNATURE = NodeSignature
+
+        node_type: NodeTypeEnum
+        match self.resolved_signature.node_type:
+            case WiringNodeType.SINK_NODE:
+                node_type = NodeTypeEnum.SINK_NODE
+            case (
+                WiringNodeType.COMPUTE_NODE
+                | WiringNodeType.REQ_REP_SVC
+                | WiringNodeType.SUBS_SVC
+                | WiringNodeType.COMPONENT
+            ):
+                node_type = NodeTypeEnum.COMPUTE_NODE
+            case WiringNodeType.PULL_SOURCE_NODE | WiringNodeType.REF_SVC | WiringNodeType.SVC_IMPL:
+                node_type = NodeTypeEnum.PULL_SOURCE_NODE
+            case WiringNodeType.PUSH_SOURCE_NODE:
+                node_type = NodeTypeEnum.PUSH_SOURCE_NODE
+            case _:
+                raise CustomMessageWiringError(f"Unknown node type: {self.resolved_signature.node_type}")
+
+        return WiringNodeInstance.NODE_SIGNATURE(
+            name=self.resolved_signature.name,
+            node_type=node_type,
+            args=self.resolved_signature.args,
+            time_series_inputs=self.resolved_signature.time_series_inputs,
+            time_series_output=self.resolved_signature.output_type,
+            scalars=self.resolved_signature.scalar_inputs,
+            src_location=self.resolved_signature.src_location,
+            active_inputs=self.resolved_signature.active_inputs,
+            valid_inputs=self.resolved_signature.valid_inputs,
+            all_valid_inputs=self.resolved_signature.all_valid_inputs,
+            context_inputs=self.resolved_signature.context_inputs,
+            injectable_inputs=self.resolved_signature.injectable_inputs,
+            capture_exception=self.error_handler_registered,
+            trace_back_depth=self.trace_back_depth,
+            wiring_path_name=self.wiring_path_name,
+            label=self.label,
+            capture_values=self.capture_values,
+            record_replay_id=self.resolved_signature.record_and_replay_id,
+        )
+
+    @property
+    def error_output_type(self) -> "HgTimeSeriesTypeMetaData":
+        return self.node.error_output_type
+
+    @property
+    def recordable_state_output_type(self) -> "HgTSBTypeMetaData":
+        return self.resolved_signature.recordable_state.tsb_type
+
+    def create_node_builder_and_edges(
+        self, node_map: MutableMapping["WiringNodeInstance", int], nodes: ["NodeBuilder"]
+    ) -> tuple["NodeBuilder", set["Edge"]]:
+        """Create an runtime node instance"""
+        # Collect appropriate inputs and construct the node
+        node_index = len(nodes)
+        node_map[self] = node_index  # Update this wiring nodes index in the graph
+
+        scalars = frozendict({
+            k: t.injector if t.is_injectable else self.inputs[k]
+            for k, t in self.resolved_signature.scalar_inputs.items()
+        })
+
+        node_builder = self.node.create_node_builder_instance(self.resolved_signature, self.node_signature, scalars)
+        # Extract out edges
+
+        edges = set()
+        for ndx, arg in enumerate(
+            raw_arg for raw_arg in self.resolved_signature.args if raw_arg in self.resolved_signature.time_series_args
+        ):
+            input_: WiringPort = self.inputs.get(arg)
+            if input_ is not None:
+                edges.update(input_.edges_for(node_map, node_index, (ndx,)))
+
+        return node_builder, edges
