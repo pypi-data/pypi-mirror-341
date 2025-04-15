c2cdd285ba60990db4be2cd97343bde1916e7c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:24 +0000

    Add package vmklib/2.0.6/vmklib-2.0.6-py3-none-any.whl

---
+++ packages/vmklib/vmklib-2.0.6-py3-none-any.whl/vmklib/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.2.1
+# hash=47379f915ef7dead802c2f9c9a090260
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
+PKG_NAME = "vmklib"
+VERSION = "2.0.6"
