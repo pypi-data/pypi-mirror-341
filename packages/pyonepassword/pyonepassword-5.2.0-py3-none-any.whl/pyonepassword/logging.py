6c4d8006fb227d93046d7ac579da8fd9689d7a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 14:03:49 +0000

    Add package pyonepassword/5.2.0/pyonepassword-5.2.0-py3-none-any.whl

---
+++ packages/pyonepassword/pyonepassword-5.2.0-py3-none-any.whl/pyonepassword/logging.py
@@ -0,0 +1,19 @@
+import logging
+
+DEBUG = logging.DEBUG
+INFO = logging.INFO
+
+
+def console_logger(name: str, level: int):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    logger.addHandler(ch)
+
+    return logger
+
+
+def console_debug_logger(name: str):
+    logger = console_logger(name, DEBUG)
+    return logger
