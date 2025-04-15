6c808e319c515ecc2ef524d6a590aeef10e4ab87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 14:04:06 +0000

    Add package ocr-sdk/0.0.1/ocr_sdk-0.0.1.tar.gz

---
+++ packages/ocr-sdk/ocr_sdk-0.0.1.tar.gz/ocr_sdk-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ocr_sdk",
+    version="0.0.1",
+    author="tianyishen",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "ocr_sdk"},
+    packages=setuptools.find_packages(where="ocr_sdk"),
+    python_requires=">=3.11",
+    setup_require=[
+        'pillow==11.1.0',
+        'numpy==1.26.4',
+        'onnxruntime==1.21.0',
+        'rapidocr-onnxruntime==1.3.25',
+        'torch==2.6.0',
+        'opencv-python==4.11.0.86',
+        'opencv-python-headless==4.11.0.86',
+    ]
+)
