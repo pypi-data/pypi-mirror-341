1d36649df49b7700f1732c2bfd8a775c439ad49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:19:06 +0000

    Add package nonebot-plugin-mahjong-utils/0.5.0/nonebot_plugin_mahjong_utils-0.5.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-mahjong-utils/nonebot_plugin_mahjong_utils-0.5.0-py3-none-any.whl/nonebot_plugin_mahjong_utils/config.py
@@ -0,0 +1,16 @@
+from nonebot import get_driver
+from pydantic import BaseSettings
+
+
+class Config(BaseSettings):
+    mahjong_utils_sniff_mode: bool = True
+    mahjong_utils_command_mode: bool = True
+    mahjong_utils_send_image: bool = True
+
+    mahjong_utils_test: bool = False
+
+    class Config:
+        extra = "ignore"
+
+
+conf = Config(**get_driver().config.dict())
