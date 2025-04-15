df79f384e1db9847ddd27b74aeb593158c4e63ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 14:03:43 +0000

    Add package scoras/0.2.3/scoras-0.2.3-py3-none-any.whl

---
+++ packages/scoras/scoras-0.2.3-py3-none-any.whl/scoras/__init__.py
@@ -0,0 +1,12 @@
+"""
+Scoras package initialization.
+"""
+
+__version__ = "0.2.3"
+__author__ = "Anderson L. Amaral"
+
+# Import core functionality
+from .core import ScoringMixin, Graph, Node, Edge
+
+# Provide convenience imports
+__all__ = ['ScoringMixin', 'Graph', 'Node', 'Edge']
