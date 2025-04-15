7041b8e308402666481444dc774f385c40ab4d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:47 +0000

    Add package libit/4.3.0/libit-4.3.0.tar.gz

---
+++ packages/libit/libit-4.3.0.tar.gz/libit-4.3.0/libit/__init__.py
@@ -0,0 +1,56 @@
+from .lib import (
+    bytes_addr,
+    bytes_eth,
+    bytes_trx,
+    bytes_wif,
+    dec_eth,
+    dec_trx,
+    dec_addr,
+    dec_trx,
+    eth_addr,
+    hex_bytes,
+    passphrase_addr,
+    privatekey_addr,
+    privatekey_decimal,
+    privatekey_wif,
+    trx_addr,
+    wif_addr,
+    Ethereum,
+    tron
+)
+from .Tron import Wallet
+from .reuse import extract_key
+from . import reuse
+
+# tron address from private key hex format
+tronAddress = Wallet.get_address
+# tron address to hash string format
+tronHash = Wallet.get_hashAddress
+# tron address to hex string format (evm)
+tronHex = Wallet.get_hexAddress
+# private key to tron decimal format
+tronDec = Wallet.get_decimal
+
+__version__ = "4.3.0"
+__all__ = [
+    "bytes_addr",
+    "bytes_eth",
+    "bytes_trx",
+    "bytes_wif",
+    "dec_eth",
+    "dec_trx",
+    "dec_addr",
+    "dec_trx",
+    "eth_addr",
+    "hex_bytes",
+    "passphrase_addr",
+    "privatekey_addr",
+    "privatekey_decimal",
+    "privatekey_wif",
+    "trx_addr",
+    "wif_addr",
+    "Ethereum",
+    # REUSED FUNCTIONS
+    "extract_key",
+    "reuse"
+]
