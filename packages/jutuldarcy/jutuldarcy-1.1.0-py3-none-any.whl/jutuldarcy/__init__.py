04d996dc615841d16ecf5c6000dea5b5aa04bfb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:03:59 +0000

    Add package jutuldarcy/1.1.0/jutuldarcy-1.1.0-py3-none-any.whl

---
+++ packages/jutuldarcy/jutuldarcy-1.1.0-py3-none-any.whl/jutuldarcy/__init__.py
@@ -0,0 +1,13 @@
+
+from .julia_import import jl
+from .simulate import *
+from .parsing import *
+from .conversion import *
+from .reservoir import *
+from .units import *
+from .meshes import *
+from .wells import *
+from .systems import *
+from .model import *
+from .utils import *
+from .plotting import *
