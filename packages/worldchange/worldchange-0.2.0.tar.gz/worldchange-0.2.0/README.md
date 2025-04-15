94029cb0a16b5d607dcfca8196e86676ef80eb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:29 +0000

    Add package worldchange/0.2.0/worldchange-0.2.0.tar.gz

---
+++ packages/worldchange/worldchange-0.2.0.tar.gz/worldchange-0.2.0/README.md
@@ -0,0 +1,3 @@
+# worldchange
+
+가상의 세계를 바꾸는 마법 같은 Python 모듈입니다.
---
+++ packages/worldchange/worldchange-0.2.0.tar.gz/worldchange-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="worldchange",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    author_email="your-email@example.com",
+    description="A fun module to simulate changing the world.",
+)
