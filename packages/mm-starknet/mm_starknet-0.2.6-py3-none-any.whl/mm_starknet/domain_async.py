5ea32e9fd8bfbad29221049a8ce2c46abb2b49f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:22 +0000

    Add package mm-starknet/0.2.6/mm_starknet-0.2.6-py3-none-any.whl

---
+++ packages/mm-starknet/mm_starknet-0.2.6-py3-none-any.whl/mm_starknet/domain_async.py
@@ -0,0 +1,18 @@
+from mm_std import DataResult, http_request, str_contains_any
+
+
+async def address_to_domain(address: str, timeout: float = 5.0, proxy: str | None = None) -> DataResult[str | None]:
+    url = "https://api.starknet.id/addr_to_domain"
+    res = await http_request(url, params={"addr": address}, proxy=proxy, timeout=timeout)
+    if (
+        res.status_code == 400
+        and res.body is not None
+        and str_contains_any(res.body.lower(), ["no data found", "no domain found"])
+    ):
+        return DataResult.ok(None, res.model_dump())
+    if res.is_error():
+        return res.to_data_result_err()
+    domain = res.parse_json_body("domain")
+    if domain:
+        return DataResult.ok(domain, data=res.model_dump())
+    return DataResult.err("unknown_response", res.model_dump())
