bb43d81ce443fae0eae1983cd6f3aba21df34e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:35 +0000

    Add package streamlit-list-of-links/0.1.9/streamlit_list_of_links-0.1.9.tar.gz

---
+++ packages/streamlit-list-of-links/streamlit_list_of_links-0.1.9.tar.gz/streamlit_list_of_links-0.1.9/README.md
@@ -0,0 +1,2 @@
+# streamlit-list-of-links
+Streamlit component that renders a list of clickable links
---
+++ packages/streamlit-list-of-links/streamlit_list_of_links-0.1.9.tar.gz/streamlit_list_of_links-0.1.9/setup.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import setuptools
+
+__version__ = "0.1.9"
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-list-of-links",
+    version=__version__,
+    author="Marc Argent",
+    author_email="margent@gmail.com",
+    description="Streamlit component that renders a list of links",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.10",
+    install_requires=[
+        "streamlit >= 1.43.0",
+    ],
+    extras_require={
+        "tests": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.48.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+        ],
+    }
+)
