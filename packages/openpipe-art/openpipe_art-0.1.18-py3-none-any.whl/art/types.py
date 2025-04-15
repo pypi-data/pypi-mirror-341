8c0f8703fdda7e3e75e07f53a7a3eb4a0eeb5011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 06:04:28 +0000

    Add package openpipe-art/0.1.18/openpipe_art-0.1.18-py3-none-any.whl

---
+++ packages/openpipe-art/openpipe_art-0.1.18-py3-none-any.whl/art/types.py
@@ -0,0 +1,27 @@
+from openai.types.chat.chat_completion import Choice
+from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
+import pydantic
+from typing import Literal
+
+BaseModel = Literal[
+    "Qwen/Qwen2.5-0.5B-Instruct",
+    "Qwen/Qwen2.5-1.5B-Instruct",
+    "Qwen/Qwen2.5-3B-Instruct",
+    "Qwen/Qwen2.5-7B-Instruct",
+    "Qwen/Qwen2.5-14B-Instruct",
+    "Qwen/Qwen2.5-32B-Instruct",
+    "Qwen/Qwen2.5-72B-Instruct",
+]
+
+Message = ChatCompletionMessageParam
+MessageOrChoice = Message | Choice
+Messages = list[Message]
+MessagesAndChoices = list[MessageOrChoice]
+
+
+class TrainConfig(pydantic.BaseModel):
+    learning_rate: float = 5e-6
+    beta: float = 0.0
+
+
+Verbosity = Literal[0, 1, 2]
