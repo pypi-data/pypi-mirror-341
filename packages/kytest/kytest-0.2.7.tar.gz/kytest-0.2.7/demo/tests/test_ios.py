035b847389fa2f8af4b7c7f679e9e832f013c938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:21 +0000

    Add package kytest/0.2.7/kytest-0.2.7.tar.gz

---
+++ packages/kytest/kytest-0.2.7.tar.gz/kytest-0.2.7/demo/tests/test_ios.py
@@ -0,0 +1,25 @@
+"""
+@Author: kang.yang
+@Date: 2025/4/11 16:16
+"""
+import kytest
+from page.ios_page import IosPage
+
+
+class TestIosDemo(kytest.IosTC):
+
+    def start(self):
+        self.start_app()
+        self.sleep(5)
+        self.ip = IosPage(self.dr)
+
+    def end(self):
+        self.stop_app()
+
+    def test_switch_to_lg(self):
+        self.ip.music_tab.click()
+        self.sleep(5)
+
+    def test_switch_to_jb(self):
+        self.ip.gold_tab.click()
+        self.sleep(5)
