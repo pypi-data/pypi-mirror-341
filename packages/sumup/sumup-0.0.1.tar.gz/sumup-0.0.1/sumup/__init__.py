b530d0f87eb7d259cb98d0d6041cbec375d60905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 06:04:00 +0000

    Add package sumup/0.0.1/sumup-0.0.1.tar.gz

---
+++ packages/sumup/sumup-0.0.1.tar.gz/sumup-0.0.1/sumup/__init__.py
@@ -0,0 +1,5 @@
+from sumup._client import Sumup, AsyncSumup
+from sumup._service import Service, AsyncService
+from sumup.merchant.types import MerchantAccount
+
+__all__ = ["Service", "AsyncService", "Sumup", "AsyncSumup", "MerchantAccount"]
