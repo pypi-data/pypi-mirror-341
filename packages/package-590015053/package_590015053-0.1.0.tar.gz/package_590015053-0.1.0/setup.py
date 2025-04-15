9702b7a9b8d5bca95db4570ecc6c10a137064b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:43 +0000

    Add package package-590015053/0.1.0/package_590015053-0.1.0.tar.gz

---
+++ packages/package-590015053/package_590015053-0.1.0.tar.gz/package_590015053-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="package_590015053",
+    description="A sample package for testing purposes",
+    version="0.1.0",
+    author="Krish Mishra",
+    packages=find_packages()
+)
