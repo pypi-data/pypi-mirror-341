37a775e058ca0c1eb2da9ef03355384ccd7b1ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:30 +0000

    Add package sempler/0.2.14/sempler-0.2.14.tar.gz

---
+++ packages/sempler/sempler-0.2.14.tar.gz/sempler-0.2.14/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="sempler",
+    version="0.2.14",
+    author="Juan L Gamella",
+    author_email="juangamella@gmail.com",
+    packages=["sempler", "drf"],
+    scripts=[],
+    url="https://github.com/juangamella/sempler",
+    license="BSD 3-Clause License",
+    description="Sample from general structural causal models (SCMs).",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["numpy>=1.17.0", "pandas>=1.2.1"],
+    extras_require={"DRFNet": ["rpy2>=3.4.1"]},
+)
