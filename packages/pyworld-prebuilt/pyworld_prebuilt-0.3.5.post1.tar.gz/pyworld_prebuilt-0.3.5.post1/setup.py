46fe220ac605c5abc875dea209401384d64fb9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:55 +0000

    Add package pyworld-prebuilt/0.3.5.post1/pyworld_prebuilt-0.3.5.post1.tar.gz

---
+++ packages/pyworld-prebuilt/pyworld_prebuilt-0.3.5.post1.tar.gz/pyworld_prebuilt-0.3.5.post1/setup.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import, print_function, with_statement
+
+import sys
+from glob import glob
+from os.path import join
+
+import numpy
+from setuptools import Extension, find_packages, setup
+from setuptools.command.build_ext import build_ext
+
+
+_VERSION = '0.3.5-post1'
+
+
+world_src_top = join("lib", "World", "src")
+world_sources = glob(join(world_src_top, "*.cpp"))
+
+ext_modules = [
+    Extension(
+        name="pyworld.pyworld",
+        include_dirs=[world_src_top, numpy.get_include()],
+        sources=[join("pyworld", "pyworld.pyx")] + world_sources,
+        language="c++")]
+
+kwargs = {"encoding": "utf-8"} if int(sys.version[0]) > 2 else {}
+setup(
+    name="pyworld-prebuilt",
+    description="PyWorld: a Python wrapper for WORLD vocoder",
+    long_description=open("README.md", "r", **kwargs).read(),
+    long_description_content_type="text/markdown",
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext},
+    version=_VERSION,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    extras_require={
+        'test': ['nose'],
+        'sdist': ['numpy', 'cython>=3.0'],
+    },
+    author="Pyworld Contributors",
+    author_email="jeremycchsu@gmail.com",
+    url="https://github.com/tsukumijima/pyworld-prebuilt",
+    keywords=['vocoder'],
+    classifiers=[],
+)
