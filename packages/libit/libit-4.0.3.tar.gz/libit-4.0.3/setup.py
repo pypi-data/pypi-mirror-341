ddac3b968813fe8e32fb27662662d72759d6e1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:44 +0000

    Add package libit/4.0.3/libit-4.0.3.tar.gz

---
+++ packages/libit/libit-4.0.3.tar.gz/libit-4.0.3/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='libit',
+    version="4.0.3",
+    license='http://opensource.org/licenses/MIT',
+    packages=find_packages(),
+    install_requires=[
+        'pycryptodome',
+        'ecdsa'
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+    author='Mmdrza',
+
+    keywords=['bitcoin', 'cryptography', 'python', 'library', 'crypto', 'libit', 'ethereum', 'tron'],
+
+    include_package_data=True,
+
+    author_email='Pymmdrza@gmail.com',
+
+    description='Library Bitcoin package for python convert and generate wallet',
+
+    url='https://github.com/pylibit',
+
+    project_urls={
+        'Bug Tracker': 'https://github.com/pylibit/issues',
+
+        'Source Code': 'https://github.com/pylibit/libit',
+
+        'Documentation': 'https://pylibit.github.io/libit/',
+
+        'Website': 'https://mmdrza.com',
+
+        'Medium': 'https://mdrza.medium.com'
+
+    }
+
+)
