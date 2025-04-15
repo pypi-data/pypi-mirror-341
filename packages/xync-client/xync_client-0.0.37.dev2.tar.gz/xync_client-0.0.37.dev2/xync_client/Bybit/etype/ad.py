fb7203019571bcb5e6d0734499bdb7f5df965ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:19:03 +0000

    Add package xync-client/0.0.37.dev2/xync_client-0.0.37.dev2.tar.gz

---
+++ packages/xync-client/xync_client-0.0.37.dev2.tar.gz/xync_client-0.0.37.dev2/xync_client/Bybit/etype/ad.py
@@ -0,0 +1,108 @@
+from typing import List, Optional, Dict, Any
+from pydantic import BaseModel
+from xync_schema.types import BaseAd
+
+class Currency(BaseModel):
+    currencyId: str
+    exchangeId: str
+    id: str
+    orgId: str
+    scale: int
+
+class Token(BaseModel):
+    exchangeId: str
+    id: str
+    orgId: str
+    scale: int
+    sequence: int
+    tokenId: str
+
+class SymbolInfo(BaseModel):
+    buyAd: Optional[Any]
+    buyFeeRate: str
+    currency: Currency
+    currencyId: str
+    currencyLowerMaxQuote: str
+    currencyMaxQuote: str
+    currencyMinQuote: str
+    exchangeId: str
+    id: str
+    itemDownRange: str
+    itemSideLimit: int
+    itemUpRange: str
+    kycCurrencyLimit: str
+    lowerLimitAlarm: int
+    orderAutoCancelMinute: int
+    orderFinishMinute: int
+    orgId: str
+    sellAd: Optional[Any]
+    sellFeeRate: str
+    status: int
+    token: Token
+    tokenId: str
+    tokenMaxQuote: str
+    tokenMinQuote: str
+    tradeSide: int
+    upperLimitAlarm: int
+
+class TradingPreferenceSet(BaseModel):
+    completeRateDay30: str
+    hasCompleteRateDay30: int
+    hasNationalLimit: int
+    hasOrderFinishNumberDay30: int
+    hasRegisterTime: int
+    hasUnPostAd: int
+    isEmail: int
+    isKyc: int
+    isMobile: int
+    nationalLimit: str
+    orderFinishNumberDay30: int
+    registerTimeThreshold: int
+
+class Ad(BaseAd):
+    accountId: str
+    authStatus: int
+    authTag: List[str]
+    ban: bool
+    baned: bool
+    blocked: str
+    createDate: str
+    currencyId: str
+    executedQuantity: str
+    fee: str
+    finishNum: int
+    frozenQuantity: str
+    id: str
+    isOnline: bool
+    itemType: str
+    lastLogoutTime: str
+    lastQuantity: str
+    makerContact: bool
+    maxAmount: str
+    minAmount: str
+    nickName: str
+    orderNum: int
+    paymentPeriod: int
+    payments: List[str]
+    premium: str
+    price: str
+    priceType: int
+    quantity: str
+    recentExecuteRate: int
+    recentOrderNum: int
+    recommend: bool
+    recommendTag: str
+    remark: str
+    side: int
+    status: int
+    symbolInfo: SymbolInfo
+    tokenId: str
+    tokenName: str
+    tradingPreferenceSet: TradingPreferenceSet
+    userId: str
+    userMaskId: str
+    userType: str
+    verificationOrderAmount: str
+    verificationOrderLabels: List[Any]
+    verificationOrderSwitch: bool
+    version: int
