8f1ff17756485d507ac99a72e7aeba1345df2670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 05:05:07 +0000

    Add package esphome-dashboard/20250415.0/esphome_dashboard-20250415.0.tar.gz

---
+++ packages/esphome-dashboard/esphome_dashboard-20250415.0.tar.gz/esphome_dashboard-20250415.0/esphome_dashboard/static/js/esphome/c.CYOm8OZV.js
@@ -0,0 +1,10 @@
+import{b as e,c as o,n as s,s as i,x as t}from"./index-BMHRAnq5.js";import"./c.B-et3y2-.js";import"./c.C-K8bPZ0.js";let a=class extends i{render(){return t`
+      <esphome-process-dialog
+        .heading=${`Clean MQTT discovery topics for ${this.configuration}`}
+        .type=${"clean-mqtt"}
+        .spawnParams=${{configuration:this.configuration}}
+        @closed=${this._handleClose}
+      >
+      </esphome-process-dialog>
+    `}_handleClose(){this.parentNode.removeChild(this)}};e([o()],a.prototype,"configuration",void 0),a=e([s("esphome-clean-mqtt-dialog")],a);
+//# sourceMappingURL=c.CYOm8OZV.js.map
---
+++ packages/esphome-dashboard/esphome_dashboard-20250415.0.tar.gz/esphome_dashboard-20250415.0/esphome_dashboard/static/js/esphome/c.CtZWRzOg.js
@@ -0,0 +1,8 @@
+import{b as e,n as s,s as l,x as o}from"./index-BMHRAnq5.js";import"./c.B-et3y2-.js";import"./c.C-K8bPZ0.js";let d=class extends l{render(){return o`
+      <esphome-process-dialog
+        heading="Update All"
+        .type=${"update-all"}
+        @closed=${this._handleClose}
+      ></esphome-process-dialog>
+    `}_handleClose(){this.parentNode.removeChild(this)}};d=e([s("esphome-update-all-dialog")],d);
+//# sourceMappingURL=c.CtZWRzOg.js.map
