72b3d4200e7f75ed2493c6bf2c18f2bdb5c26009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 14:04:06 +0000

    Add package ceshigogo/0.1.2/ceshigogo-0.1.2.tar.gz

---
+++ packages/ceshigogo/ceshigogo-0.1.2.tar.gz/ceshigogo-0.1.2/ceshigogo/__init__.py
@@ -0,0 +1,1 @@
+print("ceshigogo init")
---
+++ packages/ceshigogo/ceshigogo-0.1.2.tar.gz/ceshigogo-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+print("ceshigogo setup")
+
+setup(
+    name="ceshigogo",
+    version="0.1.2",
+    packages=find_packages(),
+    description="123",
+    author='Baxter Rogers',
+    author_email='baxpr@vu1.org',
+    python_requires='>=3.6', 
+    platforms=['MacOS', 'Linux'],
+    license='MIT', 
+
+)
