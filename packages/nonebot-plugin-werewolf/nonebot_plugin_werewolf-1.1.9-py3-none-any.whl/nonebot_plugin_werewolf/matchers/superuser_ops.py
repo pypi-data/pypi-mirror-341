1aa9b6a083ff0a73612d6171196e9484182905a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 17:04:45 +0000

    Add package nonebot-plugin-werewolf/1.1.9/nonebot_plugin_werewolf-1.1.9-py3-none-any.whl

---
+++ packages/nonebot-plugin-werewolf/nonebot_plugin_werewolf-1.1.9-py3-none-any.whl/nonebot_plugin_werewolf/matchers/superuser_ops.py
@@ -0,0 +1,24 @@
+from nonebot.permission import SUPERUSER
+from nonebot.rule import to_me
+from nonebot_plugin_alconna import Alconna, MsgTarget, UniMessage, on_alconna
+
+from ..game import get_running_games
+
+
+def rule_game_running(target: MsgTarget) -> bool:
+    return any(target.verify(g.group) for g in get_running_games())
+
+
+force_stop = on_alconna(
+    Alconna("中止游戏"),
+    rule=to_me() & rule_game_running,
+    permission=SUPERUSER,
+    use_cmd_start=True,
+)
+
+
+@force_stop.handle()
+async def _(target: MsgTarget) -> None:
+    game = next(g for g in get_running_games() if target.verify(g.group))
+    game.terminate()
+    await UniMessage.text("已中止当前群组的游戏进程").finish(reply_to=True)
