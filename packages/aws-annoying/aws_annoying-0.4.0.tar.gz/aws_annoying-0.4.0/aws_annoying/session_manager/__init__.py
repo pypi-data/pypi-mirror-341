e3388a0fdd1a68da5162de8ed5e0c77e3fe6ab75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:01 +0000

    Add package aws-annoying/0.4.0/aws_annoying-0.4.0.tar.gz

---
+++ packages/aws-annoying/aws_annoying-0.4.0.tar.gz/aws_annoying-0.4.0/aws_annoying/session_manager/__init__.py
@@ -0,0 +1,4 @@
+from .errors import PluginNotInstalledError, SessionManagerError, UnsupportedPlatformError
+from .session_manager import SessionManager
+
+__all__ = ("PluginNotInstalledError", "SessionManager", "SessionManagerError", "UnsupportedPlatformError")
