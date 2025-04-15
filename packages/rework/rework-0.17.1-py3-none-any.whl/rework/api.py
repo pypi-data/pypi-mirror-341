ccb14d5557698aa567be45e800e429bad96e040e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:55 +0000

    Add package rework/0.17.1/rework-0.17.1-py3-none-any.whl

---
+++ packages/rework/rework-0.17.1-py3-none-any.whl/rework/api.py
@@ -0,0 +1,373 @@
+import sys
+from pickle import dumps
+from datetime import timedelta
+from contextlib import contextmanager
+import json
+import traceback
+from pathlib import Path
+
+from sqlalchemy.exc import IntegrityError
+from sqlhelp import select, insert
+from icron import croniter
+import isodate
+
+from rework.helper import (
+    filterio,
+    host,
+    InputEncoder,
+    iospec,
+    ops_and_domains,
+    pack_io,
+    setuplogger
+)
+from rework.task import (
+    __task_inputs__,
+    __task_outputs__,
+    __task_registry__,
+    Task
+)
+from rework.monitor import Monitor
+
+
+def task(*args, **kw):
+    """decorator to turn a python function into a rework operation
+    that will be executable as a task
+
+    There are two ways to use it:
+
+    .. code-block:: python
+
+        from rework import api
+
+        @api.task
+        def mytask(task):
+            pass
+
+        @api.task(domain='somedomain', timeout=timedelta(minutes=3))
+        def mytask(task):
+            pass
+
+        @api.task(inputs={'myfile': file, 'foo': int, 'bar': str})
+
+        @api.task(outputs={'myfile': file, 'foo': int, 'bar': str})
+
+    If you want to specify either a non-default domain or a timeout
+    parameter, the second notation (with keywords) must be used.
+
+    All operation functions must take a single `task` parameter.
+
+    """
+    msg = "Use either @task or @task(domain='domain', timeout=..., inputs=..., outputs=...)"
+    if args:
+        assert callable(args[0]), msg
+    else:
+        assert 'domain' in kw or 'timeout' in kw or 'inputs' in kw or 'outputs' in kw, msg
+    domain = 'default'
+    timeout = None
+    inputs = None
+    outputs = None
+
+    def register_task(func):
+        __task_registry__[(domain, func.__name__)] = (func, timeout)
+        if inputs is None and outputs is None:
+            return func
+
+        if inputs:
+            msg = 'inputs must be a tuple'
+            assert isinstance(inputs, tuple), msg
+        if outputs:
+            msg = 'outputs must be a tuple'
+            assert isinstance(outputs, tuple), msg
+
+        if inputs is not None:
+            __task_inputs__[(domain, func.__name__)] = inputs
+        if outputs is not None:
+            __task_outputs__[(domain, func.__name__)] = outputs
+        return func
+
+    if args and callable(args[0]):
+        return register_task(args[0])
+
+    domain = kw.pop('domain', domain)
+    timeout = kw.pop('timeout', None)
+    inputs = kw.pop('inputs', None)
+    outputs = kw.pop('outputs', None)
+    assert domain or timeout or inputs or outputs
+    if timeout is not None:
+        msg = 'timeout must be a timedelta object'
+        assert isinstance(timeout, timedelta), msg
+
+    return register_task
+
+
+def schedule(engine,
+             opname,
+             inputdata=None,
+             rawinputdata=None,
+             hostid=None,
+             module=None,
+             domain=None,
+             metadata=None):
+    """schedule an operation to be run by a worker
+
+    It returns a `Task` object.
+    The operation name is the only mandatory parameter.
+
+    The `domain` can be specified to avoid an ambiguity if an
+    operation is defined within several domains.
+
+    An `inputdata` object can be given. It can be any picklable python
+    object. It will be available through `task.input`.
+
+    Alternatively `rawinputdata` can be provided. It must be a byte
+    string. It can be useful to transmit file contents and avoid the
+    pickling overhead. It will be available through `task.rawinput`.
+
+    Lastly, `metadata` can be provided as a json-serializable python
+    dictionary. It can contain anything.
+
+    """
+    if metadata:
+        assert isinstance(metadata, dict)
+
+    if rawinputdata is None:
+        spec = filterio(iospec(engine), opname, domain, hostid)
+        if spec is not None:
+            rawinputdata = pack_io(spec, inputdata)
+        elif inputdata is not None:
+            rawinputdata = dumps(inputdata, protocol=2)
+
+    q = select('id').table('rework.operation').where(
+        name=opname
+    )
+
+    if hostid is not None:
+        q.where(host=hostid)
+
+    if module is not None:
+        q.where(modname=module)
+
+    if domain is not None:
+        q.where(domain=domain)
+
+    with engine.begin() as cn:
+        opids = q.do(cn).fetchall()
+        if len(opids) > 1:
+            if hostid is None:
+                return schedule(
+                    engine,
+                    opname,
+                    rawinputdata=rawinputdata,
+                    hostid=host(),
+                    module=module,
+                    domain=domain,
+                    metadata=metadata
+                )
+            raise ValueError(f'Ambiguous operation selection `{opname}`')
+        if not len(opids):
+            raise Exception(
+                f'No operation was found for these parameters: '
+                f'operation=`{opname}` domain=`{domain}` host=`{host()}`'
+            )
+        opid = opids[0][0]
+        q = insert(
+            'rework.task'
+        ).values(
+            operation=opid,
+            input=rawinputdata,
+            status='queued',
+            metadata=json.dumps(metadata)
+        )
+        tid = q.do(cn).scalar()
+
+    return Task(engine, tid, opid)
+
+
+def prepare(engine,
+            opname,
+            rule=None,
+            domain='default',
+            inputdata=None,
+            host=None,
+            metadata=None,
+            rawinputdata=None,
+            _anyrule=False):
+    if metadata:
+        assert isinstance(metadata, dict)
+
+    ops_doms = ops_and_domains(engine)
+    if (opname, domain) not in ops_doms:
+        raise Exception(f'{opname} / {domain} are not known')
+
+    # validate the rules
+    if not _anyrule and rule.startswith('*'):
+        raise ValueError('"every second" rule is forbidden')
+    assert croniter.is_valid(rule)
+
+    spec = filterio(iospec(engine), opname, domain, host)
+    inputdata = inputdata or {} if spec else inputdata
+    if rawinputdata is None and inputdata is not None:
+        if spec is not None:
+            rawinputdata = pack_io(spec, inputdata)
+        else:
+            rawinputdata = dumps(inputdata, protocol=2)
+
+    with engine.begin() as cn:
+        opid = select('id').table('rework.operation').where(
+            name=opname
+        ).do(cn).scalar()
+
+        # check replacement if operation + domain + host + rule + inputs match
+        q = select('id').table('rework.sched').where(
+            operation=opid,
+            domain=domain,
+            rule=rule
+        )
+        if rawinputdata:
+            q.where(inputdata=rawinputdata)
+        if host:
+            q.where(host=host)
+        sid = q.do(cn).scalar()
+
+        if sid is not None:
+            cn.execute(
+                'delete from rework.sched where id=%(sid)s',
+                sid=sid
+            )
+        q = insert('rework.sched').values(
+            operation=opid,
+            domain=domain,
+            inputdata=rawinputdata,
+            host=host,
+            metadata=json.dumps(metadata),
+            rule=str(rule)
+        )
+        sid = q.do(cn).scalar()
+    return sid
+
+
+def unprepare(engine, sid):
+    with engine.begin() as cn:
+        count = cn.execute(
+            'with deleted as ('
+            ' delete from rework.sched where id=%(sid)s '
+            ' returning 1'
+            ')'
+            'select count(*) from deleted',
+            sid=sid
+        ).scalar()
+    return count
+
+
+def freeze_operations(engine, domain=None, hostid=None, reset=True):
+    values = []
+    if hostid is None:
+        hostid = host()
+
+    for (fdomain, fname), (func, timeout) in __task_registry__.items():
+        if domain is not None and domain != fdomain:
+            continue
+        if timeout is not None:
+            timeout = isodate.duration_isoformat(timeout)
+        funcmod = func.__module__
+        module = sys.modules[funcmod]
+        modpath = module.__file__
+        modpath = str(Path(modpath).resolve())
+        val = {
+            'host': hostid,
+            'name': fname,
+            'path': modpath,
+            'domain': fdomain,
+            'timeout': timeout,
+        }
+        # inputs
+        if (fdomain, fname) in __task_inputs__:
+            val['inputs'] = InputEncoder().encode(
+                __task_inputs__[(fdomain, fname)]
+            )
+        # outputs
+        if (fdomain, fname) in __task_outputs__:
+            val['outputs'] = InputEncoder().encode(
+                __task_outputs__[(fdomain, fname)]
+            )
+        values.append(val)
+
+    if reset:
+        with engine.begin() as cn:
+            for path in [val['path'] for val in values]:
+                cn.execute(
+                    'delete from rework.operation '
+                    'where path=%(path)s',
+                    path=path
+                )
+
+    recorded = []
+    alreadyknown = []
+    for value in values:
+        with engine.begin() as cn:
+            try:
+                q = insert(
+                    'rework.operation'
+                ).values(
+                    **value
+                )
+                q.do(cn)
+                recorded.append(value)
+            except IntegrityError:
+                alreadyknown.append(value)
+
+    return recorded, alreadyknown
+
+
+@contextmanager
+def workers(engine, domain='default',
+            minworkers=0, maxworkers=1,
+            maxruns=0, maxmem=0,
+            debug=False, start_timeout=30):
+    """context manager to set up a test monitor for a given domain
+
+    It can be configured with `domain`, `maxworkers`, `minworkers`, `maxruns`,
+    `maxmem` and `debug`.
+
+    Here's an example usage:
+
+    .. code-block:: python
+
+        with workers(engine, numworkers=2) as monitor:
+            t1 = api.schedule(engine, 'compute_sum', [1, 2, 3, 4, 5])
+            t2 = api.schedule(engine, 'compute_sum', 'abcd')
+
+            t1.join()
+            assert t1.output == 15
+            t2.join()
+            assert t2.traceback.endswith(
+                "TypeError: unsupported operand type(s) "
+                "for +: 'int' and 'str'"
+            )
+
+            assert len(monitor.wids) == 2
+
+    At the end of the block, the monitor-controlled workers are
+    cleaned up.
+
+    However the database entries are still there.
+
+    """
+    mon = Monitor(
+        engine, setuplogger(), domain,
+        minworkers, maxworkers, maxruns, maxmem, debug,
+        start_timeout=start_timeout
+    )
+    mon.register()
+    mon.ensure_workers()
+    try:
+        yield mon
+    except:
+        mon.killall(
+            'Something killed the monitor',
+            traceback.format_exc()
+        )
+        raise
+    finally:
+        mon.killall()
+        mon.unregister()
---
+++ packages/rework/rework-0.17.1-py3-none-any.whl/rework/helper.py
@@ -0,0 +1,604 @@
+import os
+import io
+import itertools as it
+from threading import Thread
+import socket
+import time
+import logging
+from datetime import datetime
+from pathlib import Path
+import json
+import struct
+import tzlocal
+import importlib
+import sys
+
+from icron import croniter_range
+import pyzstd as zstd
+import pytz
+import psutil
+from sqlalchemy.engine import url
+from sqlhelp import select
+from inireader import reader
+
+from rework.io import _iobase
+
+
+def setuplogger(level=logging.DEBUG):
+    logger = logging.getLogger('rework')
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(message)s',
+            '%Y-%m-%d %H:%M:%S'
+        )
+    )
+    logger.addHandler(handler)
+    logger.setLevel(level)
+    return logger
+
+
+def load_source(modname, filename):
+    loader = importlib.machinery.SourceFileLoader(
+        modname, filename
+    )
+    spec = importlib.util.spec_from_file_location(
+        modname, filename, loader=loader
+    )
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[modname] = module
+    loader.exec_module(module)
+    return module
+
+
+def utcnow():
+    return datetime.utcnow().replace(tzinfo=pytz.utc)
+
+
+def partition(pred, iterable):
+    t1, t2 = it.tee(iterable)
+    return list(filter(pred, t2)), list(it.filterfalse(pred, t1))
+
+
+def memory_usage(pid):
+    try:
+        process = psutil.Process(pid)
+    except psutil.NoSuchProcess:
+        return 0
+    return int(process.memory_info().rss / float(2 ** 20))
+
+
+def cpu_usage(pid):
+    try:
+        proc = psutil.Process(pid)
+        return _cpu_tree_usage(proc)
+    except psutil.NoSuchProcess:
+        return 0
+
+
+def _cpu_tree_usage(proc):
+    try:
+        cpu = proc.cpu_percent(interval=0.02)
+    except psutil.NoSuchProcess:
+        return 0
+    for child in proc.children():
+        cpu += _cpu_tree_usage(child)
+    return cpu
+
+
+def wait_true(func, timeout=6, sleeptime=.1):
+    outcome = []
+
+    def loop():
+        start = time.time()
+        while True:
+            if (time.time() - start) > timeout:
+                return
+            output = func()
+            if output:
+                outcome.append(output)
+                return
+            time.sleep(sleeptime)
+
+    th = Thread(target=loop)
+    th.daemon = True
+    th.start()
+    th.join()
+    assert outcome
+    return outcome[0]
+
+
+def guard(engine, sql, expr, timeout=6):
+
+    def check():
+        with engine.begin() as cn:
+            return expr(cn.execute(sql))
+
+    return wait_true(check, timeout)
+
+
+def host():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))
+    return s.getsockname()[0]
+
+
+# read stuff
+
+def ops_and_domains(engine):
+    res = engine.execute(
+        'select name, domain from rework.operation'
+    ).fetchall()
+    return set((name, domain) for name, domain in res)
+
+
+# db cleanup
+
+def cleanup_workers(engine, finished, domain):
+    with engine.begin() as cn:
+        count = cn.execute(
+            'with deleted as '
+            '(delete from rework.worker '
+            '        where running = false and '
+            '              finished < %(finished)s and'
+            '              domain = %(domain)s'
+            ' returning 1) '
+            'select count(*) from deleted',
+            finished=finished,
+            domain=domain
+        ).scalar()
+    return count
+
+
+def cleanup_tasks(engine, finished, domain, status='done'):
+    with engine.begin() as cn:
+        count = cn.execute(
+            'with deleted as '
+            '(delete from rework.task as t'
+            '        using rework.operation as o'
+            '        where t.status = %(status)s and '
+            '              t.finished < %(finished)s and'
+            '              t.operation = o.id and '
+            '              o.domain = %(domain)s'
+            ' returning 1) '
+            'select count(*) from deleted',
+            finished=finished,
+            domain=domain,
+            status=status
+        ).scalar()
+    return count
+
+
+# process handling
+
+def kill(pid, timeout=3):
+    def on_terminate(proc):
+        print(f'process {proc} terminated with exit code {proc.returncode}')
+
+    # TERM then KILL
+    try:
+        proc = psutil.Process(pid)
+        proc.terminate()
+        _, alive = psutil.wait_procs([proc], timeout=timeout, callback=on_terminate)
+        if alive:
+            proc.kill()
+            _, alive = psutil.wait_procs([proc], timeout=timeout, callback=on_terminate)
+            if alive:
+                return False
+    except psutil.NoSuchProcess:
+        return True
+    return True
+
+
+def has_ancestor_pid(pid):
+    parent = psutil.Process(os.getpid()).parent()
+    while parent:
+        if pid == parent.pid:
+            return True
+        parent = parent.parent()
+    return False
+
+
+def kill_process_tree(pid, timeout=3):
+    """Terminate all the children of this process.
+    inspired from https://psutil.readthedocs.io/en/latest/#terminate-my-children
+    """
+    try:
+        procs = psutil.Process(pid).children()
+    except psutil.NoSuchProcess:
+        print(f'process {pid} is already dead')
+        return True
+    for proc in procs:
+        kill_process_tree(proc.pid, timeout)
+        kill(proc.pid)
+    return kill(pid)
+
+
+# configuration lookup
+
+def get_cfg_path():
+    if 'REWORKCFGPATH' in os.environ:
+        cfgpath = Path(os.environ['REWORKCFGPATH'])
+        if cfgpath.exists():
+            return cfgpath
+    cfgpath = Path('rework.cfg')
+    if cfgpath.exists():
+        return cfgpath
+    cfgpath = Path('~/rework.cfg').expanduser()
+    if cfgpath.exists():
+        return cfgpath
+
+    return None
+
+
+def find_dburi(something):
+    try:
+        url.make_url(something)
+    except Exception:
+        pass
+    else:
+        return something
+
+    # lookup in the env, then in cwd, then in the home
+    cfgpath = get_cfg_path()
+    if not cfgpath:
+        raise Exception('could not use nor look up the db uri')
+
+    try:
+        cfg = reader(cfgpath)
+        return cfg['dburi'][something]
+    except Exception as exc:
+        raise Exception((
+            'could not find the `{}` entry in the '
+            '[dburi] section of the `{}` '
+            'conf file (cause: {} -> {})').format(
+                something, cfgpath.resolve(),
+                exc.__class__.__name__, exc)
+        )
+
+
+# Logging
+
+
+class PGLogHandler(logging.Handler):
+    maxqueue = 100
+
+    def __init__(self, task):
+        super(PGLogHandler, self).__init__()
+        self.task = task
+        self.lastflush = time.time()
+        self.queue = []
+        self.formatter = logging.Formatter(
+            '%(name)s:%(levelname)s: %(asctime)s: %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+
+    def emit(self, record):
+        self.queue.append(record)
+
+        if ((time.time() - self.lastflush) > 1 or
+            len(self.queue) > self.maxqueue):
+            self.flush()
+
+
+    _sql = (
+        'insert into rework.log '
+        '(task, tstamp, line) '
+        'values (%(task)s, %(tstamp)s, %(line)s)'
+    )
+
+    def flush(self):
+        if not self.queue:
+            return
+
+        values = [
+            {
+                'task': self.task.tid,
+                'tstamp': record.created,
+                'line': self.formatter.format(record)
+            }
+            for record in self.queue
+        ]
+        self.queue = []
+        self.lastflush = time.time()
+
+        with self.task.engine.begin() as cn:
+            cn.execute(self._sql, values)
+
+    def close(self):
+        pass
+
+
+class PGLogWriter:
+    __slots__ = ('stream', 'handler', 'level', 'pending')
+
+    def __init__(self, stream, handler):
+        self.stream = stream
+        self.handler = handler
+        if 'out' in self.stream:
+            self.level = logging.INFO
+        else:
+            self.level = logging.WARNING
+        self.pending = []
+
+    # TextIO interface
+
+    def write(self, message):
+        linefeed = '\n' in message
+        if not linefeed and not message.strip('\n\r'):
+            # empty message with maybe just an \r
+            return
+        self.pending.append(message)
+        if linefeed:
+            self.flush()
+
+    def close(self):
+        self.flush(force=True)
+
+    def flush(self, force=False):
+        message = ''.join(msg for msg in self.pending)
+        if not message or '\n' not in message and not force:
+            return
+
+        self.pending = []
+        for part in message.splitlines():
+            self.handler.emit(
+                    logging.LogRecord(
+                        self.stream, self.level, '', -1, part, (), None
+                    )
+                )
+
+
+# cron handling
+
+def iter_stamps_from_cronrules(rulemap, start, stop):
+    for rule, payload in rulemap:
+        for stamp in croniter_range(start, stop, rule):
+            yield stamp, payload
+
+
+def schedule_plan(engine, domain, delta):
+    tz = tzlocal.get_localzone()
+    q = select(
+        's.rule', 'op.name'
+    ).table('rework.sched as s', 'rework.operation as op'
+    ).where('s.operation = op.id'
+    ).where('s.domain = %(domain)s', domain=domain)
+    out = q.do(engine).fetchall()
+    now = datetime.now(tz)
+    for stamp, op in sorted(iter_stamps_from_cronrules(
+            out,
+            now,
+            now + delta)):
+        yield stamp, op
+
+
+# json input serializer
+
+class InputEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        if getattr(o, '__json_encode__', None):
+            return o.__json_encode__()
+        if isinstance(o, datetime):
+            return o.isoformat()
+        return super().default(o)
+
+
+# inputs spec reader
+
+def iospec(engine, attr='inputs'):
+    assert attr in ('inputs', 'outputs')
+    q = select(
+        'id', 'host', 'name', 'domain', attr
+    ).table('rework.operation'
+    ).where(f'{attr} is not null'
+    ).order('domain, name')
+
+    out = []
+    for row in q.do(engine).fetchall():
+        out.append(
+            (row.id,
+             row.name,
+             row.domain,
+             row.host,
+             row[attr]
+            )
+        )
+    return out
+
+
+def filterio(specs, operation, domain=None, hostid=None):
+    out = []
+    for sid, opname, dom, host, spec in specs:
+        if opname == operation:
+            if domain and domain != dom:
+                continue
+            if hostid and hostid != host:
+                continue
+            out.append(spec)
+
+    if not len(out):
+        return None
+
+    if len(out) > 1:
+        raise ValueError(f'Ambiguous operation selection `{operation}`')
+
+    return out[0]
+
+
+def prepared(engine, operation, domain):
+    q = (
+        'select s.id, s.inputdata, s.metadata, s.rule '
+        'from rework.sched as s, rework.operation as o '
+        'where s.domain = %(domain)s and '
+        '      s.operation = o.id and '
+        '      o.name = %(operation)s'
+    )
+    return [
+        (sid, nary_unpack(inp), meta, rule)
+        for sid, inp, meta, rule in
+        engine.execute(
+            q,
+            operation=operation,
+            domain=domain
+        ).fetchall()
+    ]
+
+
+# binary serializer
+
+def nary_pack(*bytestr):
+    sizes = [
+        struct.pack('!L', len(b))
+        for b in bytestr
+    ]
+    sizes_size = struct.pack('!L', len(sizes))
+    stream = io.BytesIO()
+    stream.write(sizes_size)
+    stream.write(b''.join(sizes))
+    for bstr in bytestr:
+        stream.write(bstr)
+    return zstd.compress(
+        stream.getvalue()
+    )
+
+
+def convert_io(spec, args):
+    """Tries to convert string values into the well typed input values
+    needed for prepare or schedule calls.
+
+    May be usefull for user interfaces (or coming from json values).
+
+    """
+    if args is None and not len(spec):
+        return
+
+    typed = {}
+    for field in spec:
+        inp = _iobase.from_type(
+            field['type'], field['name'], field['required'], field['choices'], field['default']
+        )
+        val = inp.from_string(args)
+        if val is not None:
+            typed[inp.name] = val
+
+    return typed
+
+
+def pack_io(spec, args):
+    "Prepare the args for a .prepare or .schedule api call"
+    if args is None and not len(spec):
+        return
+
+    raw = {}
+    for field in spec:
+        inp = _iobase.from_type(
+            field['type'], field['name'], field['required'], field['choices'], field.get('default')
+        )
+        val = inp.binary_encode(args)
+        if val is not None:
+            raw[inp.name] = val
+
+    spec_keys = {field['name'] for field in spec}
+    unknown_keys = set(args) - spec_keys
+
+    if unknown_keys:
+        raise ValueError(
+            f'unknown inputs: {",".join(unknown_keys)}'
+        )
+
+    return nary_pack(*(
+        [k.encode('utf-8') for k in raw] +
+        list(raw.values()))
+    )
+
+
+def nary_unpack(packedbytes):
+    try:
+        packedbytes = zstd.decompress(packedbytes.tobytes())
+    except zstd.ZstdError:
+        raise TypeError('wrong input format')
+
+    [sizes_size] = struct.unpack(
+        '!L', packedbytes[:4]
+    )
+    payloadoffset = 4 + sizes_size * 4
+    sizes = struct.unpack(
+        f'!{"L"*sizes_size}',
+        packedbytes[4: payloadoffset]
+    )
+    fmt = ''.join('%ss' % size for size in sizes)
+    return struct.unpack(fmt, packedbytes[payloadoffset:])
+
+
+def _raw_unpack_io(spec, packedbytes):
+    byteslist = nary_unpack(packedbytes)
+    middle = len(byteslist) // 2
+    keys = [
+        k.decode('utf-8')
+        for k in byteslist[:middle]
+    ]
+    values = byteslist[middle:]
+    return dict(zip(keys, values))
+
+
+def unpack_io(spec,
+              packedbytes,
+              nofiles=False):
+    output = _raw_unpack_io(spec, packedbytes)
+
+    for field in spec:
+        fname = field['name']
+        if nofiles:
+            if field['type'] == 'file':
+                output.pop(fname, None)
+                continue
+        inp = _iobase.from_type(
+            field['type'], fname, field['required'], field['choices'], field.get('default')
+        )
+        val = inp.binary_decode(output)
+        if val is None:
+            continue
+
+        output[inp.name] = val
+
+    return output
+
+
+def unpack_iofiles_length(spec, packedbytes):
+    output = _raw_unpack_io(spec, packedbytes)
+
+    for field in spec:
+        fname = field['name']
+        if field['type'] != 'file':
+            output.pop(fname, None)
+            continue
+
+        inp = _iobase.from_type(
+            'file', fname, field['required'], field['choices'], None
+        )
+        val = inp.binary_decode(output)
+        if val is None:
+            continue
+
+        output[inp.name] = len(val)
+
+    return output
+
+
+def unpack_iofile(spec, packedbytes, name):
+    output = _raw_unpack_io(spec, packedbytes)
+
+    for field in spec:
+        fname = field['name']
+        if fname != name:
+            output.pop(fname, None)
+            continue
+
+        assert field['type'] == 'file'
+
+        inp = _iobase.from_type(
+            'file', fname, field['required'], field['choices'], None
+        )
+        return inp.binary_decode(output)
