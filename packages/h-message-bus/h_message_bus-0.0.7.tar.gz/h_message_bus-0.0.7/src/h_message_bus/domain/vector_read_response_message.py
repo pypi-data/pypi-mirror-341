38199999067e33162663c5c51aad8b07050d989d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:06:01 +0000

    Add package h-message-bus/0.0.7/h_message_bus-0.0.7.tar.gz

---
+++ packages/h-message-bus/h_message_bus-0.0.7.tar.gz/h_message_bus-0.0.7/src/h_message_bus/domain/vector_read_response_message.py
@@ -0,0 +1,35 @@
+from typing import Type, TypeVar, Dict, Any
+
+from .topics import MessageTopic
+from ..domain.hai_message import HaiMessage
+
+T = TypeVar('T', bound='HaiMessage')
+
+class VectorReadResponseMessage(HaiMessage):
+    """Response Message from reading vector data"""
+
+    @classmethod
+    def create(cls: Type[T], topic: str, payload: Dict[Any, Any]) -> T:
+        """Create a message - inherited from HaiMessage"""
+        return super().create(topic=topic, payload=payload)
+
+    @classmethod
+    def create_message(cls, results: [str], dimensions: [str]) -> 'VectorReadResponseMessage':
+        """Create a message requesting Twitter user data"""
+        return cls.create(
+            topic=MessageTopic.AI_VECTORS_QUERY_RESPONSE,
+            payload={
+                "dimensions": dimensions,
+                "results": results
+            },
+        )
+
+    @property
+    def dimensions(self) -> list[str]:
+        """Returns the dimensions from the message payload"""
+        return self.payload.get("dimensions", [])
+
+    @property
+    def results(self) -> list[str]:
+        """Returns the results from the message payload"""
+        return self.payload.get("results", [])
