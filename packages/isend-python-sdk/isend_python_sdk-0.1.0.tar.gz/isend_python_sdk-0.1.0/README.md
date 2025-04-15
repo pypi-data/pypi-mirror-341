2a143763eb9ee20daf3ef2cbd78c097e3fbbf3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:31 +0000

    Add package isend-python-sdk/0.1.0/isend_python_sdk-0.1.0.tar.gz

---
+++ packages/isend-python-sdk/isend_python_sdk-0.1.0.tar.gz/isend_python_sdk-0.1.0/README.md
@@ -0,0 +1,25 @@
+# iSend Python SDK
+
+Official Python SDK for sending emails and events using the iSend API.
+
+## Installation
+
+You can install it via pip:
+
+```bash
+pip install isend-python-sdk
+
+## Usage
+
+from trusender import TruSender
+
+# Initialize with your auth token
+sdk = TruSender("your_auth_token")
+
+# Send an email
+response = sdk.send_email("WelcomeEmail", "user@example.com", {"name": "John"})
+print(response)
+
+# Send an event
+event_response = sdk.send_event("UserSignup", "user@example.com", {"property": "value"})
+print(event_response)
---
+++ packages/isend-python-sdk/isend_python_sdk-0.1.0.tar.gz/isend_python_sdk-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="isend-python-sdk",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    author="Manohar Reddy",
+    author_email="devmanoharrr@gmail.com",
+    description="Python SDK for iSend API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/devmanoharrr/isend-python-sdk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
