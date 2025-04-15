38c406be90d127a498169feefaf760e5fc4e87d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 14:04:10 +0000

    Add package ifgen/4.0.1/ifgen-4.0.1-py3-none-any.whl

---
+++ packages/ifgen/ifgen-4.0.1-py3-none-any.whl/ifgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.2.1
+# hash=f35c6412454b45e754d2604bdd0badff
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "An interface generator for distributed computing."
+PKG_NAME = "ifgen"
+VERSION = "4.0.1"
---
+++ packages/ifgen/ifgen-4.0.1-py3-none-any.whl/ifgen/common/__init__.py
@@ -0,0 +1,71 @@
+"""
+A module for generating shared headers and sources.
+"""
+
+# internal
+from ifgen.generation.interface import GenerateTask
+from ifgen.generation.test import unit_test_boilerplate
+
+
+def create_common_test(task: GenerateTask) -> None:
+    """Create a unit test for the enum string-conversion methods."""
+
+    if task.is_python:
+        return
+
+    with unit_test_boilerplate(task, declare_namespace=True) as writer:
+        writer.cpp_comment("TODO.")
+
+
+def create_common(task: GenerateTask) -> None:
+    """Create a unit test for the enum string-conversion methods."""
+
+    if task.is_python:
+        return
+
+    streams = task.stream_implementation
+
+    includes = [
+        "<cstdint>",
+        "<bit>",
+        "<span>" if not streams else "<spanstream>",
+        "<utility>",
+    ]
+
+    # probably get rid of everything besides the spanstream
+    if streams:
+        includes.extend(["<streambuf>", "<istream>", "<ostream>"])
+
+    with task.boilerplate(includes=includes) as writer:
+        writer.c_comment("Enforce that this isn't a mixed-endian system.")
+        writer.write(
+            "static_assert(std::endian::native == std::endian::big or"
+        )
+        writer.write(
+            "              std::endian::native == std::endian::little);"
+        )
+
+        with writer.padding():
+            writer.c_comment("Create useful aliases for bytes.")
+            writer.write("template <std::size_t Extent = std::dynamic_extent>")
+            writer.write("using byte_span = std::span<std::byte, Extent>;")
+            writer.write(
+                (
+                    "template <std::size_t size> using byte_array = "
+                    "std::array<std::byte, size>;"
+                )
+            )
+
+        if streams:
+            writer.c_comment("Abstract byte-stream interfaces.")
+            writer.write("using byte_istream = std::basic_istream<std::byte>;")
+            writer.write("using byte_ostream = std::basic_ostream<std::byte>;")
+
+            writer.empty()
+            writer.c_comment(
+                "Concrete byte-stream interfaces (based on span)."
+            )
+            writer.write("using byte_spanbuf = std::basic_spanbuf<std::byte>;")
+            writer.write(
+                "using byte_spanstream = std::basic_spanstream<std::byte>;"
+            )
