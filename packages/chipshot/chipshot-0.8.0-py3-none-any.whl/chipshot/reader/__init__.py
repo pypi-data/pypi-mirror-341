71cc2b9ba1750fbdaffb29ecc43e4c139d7bd7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:06:15 +0000

    Add package chipshot/0.8.0/chipshot-0.8.0-py3-none-any.whl

---
+++ packages/chipshot/chipshot-0.8.0-py3-none-any.whl/chipshot/reader/__init__.py
@@ -0,0 +1,33 @@
+# This file is a part of Chipshot <https://github.com/kurtmckee/chipshot>
+# Copyright 2022-2025 Kurt McKee <contactme@kurtmckee.org>
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import logging
+import pathlib
+import typing as t
+
+from ..shared import FileInfo
+from . import encoding, header, identity, newlines, prologue
+
+log = logging.getLogger(__name__)
+
+
+def read(path: pathlib.Path, config: dict[str, t.Any]) -> FileInfo:
+    """Read a file and return its contents and metadata."""
+
+    raw_contents = path.read_bytes()
+    info = FileInfo(path=path, raw_contents=raw_contents)
+
+    # If the file is empty, skip all other steps.
+    if not raw_contents:
+        return info
+
+    encoding.handle(info, config)
+    newlines.handle(info)
+    identity.handle(info, config)
+    prologue.handle(info, config)
+    header.handle(info, config)
+
+    return info
---
+++ packages/chipshot/chipshot-0.8.0-py3-none-any.whl/chipshot/writer.py
@@ -0,0 +1,39 @@
+# This file is a part of Chipshot <https://github.com/kurtmckee/chipshot>
+# Copyright 2022-2025 Kurt McKee <contactme@kurtmckee.org>
+# SPDX-License-Identifier: MIT
+
+import logging
+
+from .shared import FileInfo
+
+log = logging.getLogger(__name__)
+
+
+def write(file: FileInfo) -> None:
+    two_newlines = (file.newlines * 2).encode(file.encoding)
+    add_two_newlines = False
+
+    with file.path.open("wb") as f:
+        f.write(file.bom)
+
+        if file.prologue:
+            f.write(file.prologue.replace("\n", file.newlines).encode(file.encoding))
+            add_two_newlines = True
+
+        if file.header:
+            if add_two_newlines:
+                f.write(two_newlines)
+            f.write(file.header.replace("\n", file.newlines).encode(file.encoding))
+            add_two_newlines = True
+
+        if file.original_header:
+            if add_two_newlines:
+                f.write(two_newlines)
+            f.write(
+                file.original_header.replace("\n", file.newlines).encode(file.encoding)
+            )
+            add_two_newlines = True
+
+        if add_two_newlines:
+            f.write(two_newlines)
+        f.write(file.contents.replace("\n", file.newlines).encode(file.encoding))
