a889d24a423e6a9f8e1c81a5eb080c91ab001077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 04:05:45 +0000

    Add package financial-backtesting/0.2.2/financial_backtesting-0.2.2-py3-none-any.whl

---
+++ packages/financial-backtesting/financial_backtesting-0.2.2-py3-none-any.whl/backtesting/__init__.py
@@ -0,0 +1,7 @@
+from .backtesting import Backtest
+from .order_class import BuyOrder, SellOrder
+from .positions import Positions
+
+__version__ = "0.2.2"
+__all__ = ["Backtest", "BuyOrder", "SellOrder", "Positions"]
+
