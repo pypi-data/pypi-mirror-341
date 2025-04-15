1894e1fba9b175b6e872e60692ecb69b8498767a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 15:04:20 +0000

    Add package meu-investimento-belle/0.1/meu_investimento_belle-0.1.tar.gz

---
+++ packages/meu-investimento-belle/meu_investimento_belle-0.1.tar.gz/meu_investimento_belle-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meu-investimento-belle',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[], 
+    author='Thiago S Adriano',
+    author_email='tadriano.dev@gmail.com',
+    description='Uma biblioteca para cálculos de investimentos.',
+    url='https://github.com/tadrianonet/meu_investimento',
+    classifiers=['Programming Language :: Python :: 3','License :: OSI Approved :: MIT License','Operating System :: OS Independent',],
+    python_requires='>=3.6',)
