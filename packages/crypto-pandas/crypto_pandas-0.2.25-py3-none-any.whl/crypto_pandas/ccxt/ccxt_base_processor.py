93785dabb7cdf6db9a3a7ffe302bd950dcc56092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 16:04:47 +0000

    Add package crypto-pandas/0.2.25/crypto_pandas-0.2.25-py3-none-any.whl

---
+++ packages/crypto-pandas/crypto_pandas-0.2.25-py3-none-any.whl/crypto_pandas/ccxt/ccxt_base_processor.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, field
+from typing import Dict, Any
+
+import pandera as pa
+from pandera.typing import Series
+
+from crypto_pandas.base_processor import BaseProcessor
+from crypto_pandas.order_schema import OrderSchema
+
+
+class CCXTOrderSchema(pa.DataFrameModel):
+    """Base schema for CCXT orders."""
+
+    id: str = pa.Field(nullable=True, default=None, required=False)
+    symbol: str = pa.Field()
+    side: str = pa.Field(isin=["buy", "sell"])
+    type: str = pa.Field(isin=["limit", "market", "stop_loss", "take_profit"])
+    amount: float = pa.Field(gt=0)
+    price: float = pa.Field(ge=0, nullable=True, default=None, required=False)
+    params: dict = pa.Field(nullable=True, default=None, required=False)
+
+
+@dataclass
+class CCXTProcessor(BaseProcessor):
+    order_schema: OrderSchema = field(default=CCXTOrderSchema)
