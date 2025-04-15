fef2434c32125f3599adfac90f66344c3b5e04b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 14:04:29 +0000

    Add package br23021uno/1.1.1/BR23021UNO-1.1.1-py3-none-any.whl

---
+++ packages/br23021uno/BR23021UNO-1.1.1-py3-none-any.whl/BR23021UNO/__init__.py
@@ -0,0 +1,17 @@
+from .eliminacion_gauss import eliminacion_gauss
+from .gauss_jordan import gauss_jordan
+from .crammer import crammer
+from .descomposicion_lu import descomposicion_lu
+from .jacobi import jacobi
+from .gauss_seidel import gauss_seidel
+from .biseccion import biseccion
+
+__all__ = [
+    "eliminacion_gauss",
+    "gauss_jordan",
+    "crammer",
+    "descomposicion_lu",
+    "jacobi",
+    "gauss_seidel",
+    "biseccion"
+]
