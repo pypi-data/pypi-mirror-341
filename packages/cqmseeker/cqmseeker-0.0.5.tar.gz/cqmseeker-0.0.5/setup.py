a593d3517ff934ee55a3d8c1a3c2afaa145fc448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 05:04:36 +0000

    Add package cqmseeker/0.0.5/cqmseeker-0.0.5.tar.gz

---
+++ packages/cqmseeker/cqmseeker-0.0.5.tar.gz/cqmseeker-0.0.5/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages, Extension
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Meinolf Sellmann",
+    author_email='info@insideopt.com',
+    python_requires='>=3.11.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: Microsoft :: Windows'
+    ],
+    description="InsideOpt Seeker Windows Python 311 Distribution",
+    install_requires=requirements,
+    long_description=readme, 
+    keywords='insideopt, seeker, optimization',
+    name='cqmseeker',
+    test_suite='tests',
+    version='0.0.5',
+    packages=find_packages(include=['cqmseeker', 'cqmseeker.*', '*.so', '*.pyd', '*.dll']),
+    package_data={'cqmseeker': ['*.so', 'cqmseeker.py', 'bin/*', 'scripts/*', '*.pyd', '*.dll']},
+    zip_safe=False,
+)
