af88f3bc4a254f8e1586b9e2c73d1885f9bebbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 03:18:41 +0000

    Add package superduper-snowflake/0.5.27/superduper_snowflake-0.5.27.tar.gz

---
+++ packages/superduper-snowflake/superduper_snowflake-0.5.27.tar.gz/superduper_snowflake-0.5.27/superduper_snowflake/__init__.py
@@ -0,0 +1,11 @@
+from .vector_search import SnowflakeVectorSearcher as VectorSearcher
+from .data_backend import SnowflakeDataBackend as DataBackend
+from .secrets import check_secret_updates
+
+__version__ = "0.5.27"
+
+__all__ = [
+    "VectorSearcher",
+    "DataBackend",
+    "check_secret_updates",
+]
---
+++ packages/superduper-snowflake/superduper_snowflake-0.5.27.tar.gz/superduper_snowflake-0.5.27/superduper_snowflake/data_backend.py
@@ -0,0 +1,154 @@
+from functools import wraps
+import os
+import re
+import threading
+import time
+
+import ibis
+import pandas
+import snowflake.connector
+
+from superduper_snowflake.schema import (
+    ibis_schema_to_snowpark_cols,
+    snowpark_cols_to_schema,
+)
+from superduper import logging
+from superduper_ibis.data_backend import IbisDataBackend
+from snowflake.snowpark import Session
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+
+db_lock = threading.Lock()
+SESSION_DIR = os.environ.get('SNOWFLAKE_SESSION_DIR') or '/snowflake/session'
+
+
+# Create token watcher with watchdog on /session/token
+class SnowflakeTokenWatcher(FileSystemEventHandler):
+    timeout = 60
+    def __init__(self, databackend):
+        super().__init__()
+        self.databackend = databackend
+
+    def on_any_event(self, event):
+        logging.warn(str(event))
+
+        if event.src_path.endswith('data_tmp') and event.event_type == 'moved':
+            with db_lock:
+                self.databackend.reconnect()
+                self.databackend.datalayer.metadata.reconnect()
+
+
+def watch_token_file(databackend):
+    observer = Observer()
+    handler = SnowflakeTokenWatcher(databackend)
+
+    logging.info(f'Starting Snowflake token watcher on {SESSION_DIR}/token')
+
+    observer.schedule(handler, SESSION_DIR, recursive=False)
+    observer.start()
+    logging.info('Started Snowflake token watcher')
+    return observer
+
+
+class SnowflakeDataBackend(IbisDataBackend):
+    @wraps(IbisDataBackend.__init__)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.snowpark = self._get_snowpark_session(self.uri)
+        self.observer = None
+        if self.uri == 'snowflake://':
+            self.observer = watch_token_file(self)
+
+    def disconnect(self):
+        if self.observer is not None:
+            self.observer.stop()
+            self.observer.join()
+        super().disconnect()
+        self.snowpark.close()
+
+    @staticmethod
+    def _get_snowpark_session(uri):
+        logging.info(
+            'Creating Snowpark session for' ' snowflake vector-search implementation'
+        )
+        if uri == 'snowflake://':
+            connection_parameters = dict(
+                host=os.environ['SNOWFLAKE_HOST'],
+                port=int(os.environ['SNOWFLAKE_PORT']),
+                account=os.environ['SNOWFLAKE_ACCOUNT'],
+                authenticator='oauth',
+                token=open(SESSION_DIR + '/token').read(),
+                warehouse=os.environ['SNOWFLAKE_WAREHOUSE'],
+                database=os.environ['SNOWFLAKE_DATABASE'],
+                schema=os.environ['SUPERDUPER_DATA_SCHEMA'],
+            )
+        else:
+            if '?warehouse=' not in uri:
+                match = re.match('^snowflake:\/\/(.*):(.*)\@(.*)\/(.*)\/(.*)$', uri)
+                user, password, account, database, schema = match.groups()
+                warehouse = None
+            else:
+                match = re.match(
+                    '^snowflake://(.*):(.*)@(.*)/(.*)/(.*)?warehouse=(.*)$', uri
+                )
+                user, password, account, database, schema, warehouse = match.groups()
+
+            connection_parameters = dict(
+                user=user,
+                password=password,
+                account=account,
+                warehouse=warehouse,
+                database=database,
+                schema=schema,
+            )
+
+        return Session.builder.configs(connection_parameters).create()
+
+    @staticmethod
+    def _do_connection_callback(uri):
+        logging.info('Using env variables and OAuth to connect!')
+        return snowflake.connector.connect(
+            host=os.environ['SNOWFLAKE_HOST'],
+            port=int(os.environ['SNOWFLAKE_PORT']),
+            account=os.environ['SNOWFLAKE_ACCOUNT'],
+            authenticator='oauth',
+            token=open(SESSION_DIR + '/token').read(),
+            warehouse=os.environ['SNOWFLAKE_WAREHOUSE'],
+            database=os.environ['SNOWFLAKE_DATABASE'],
+            schema=os.environ['SUPERDUPER_DATA_SCHEMA'],
+        )
+
+    def _connection_callback(self, uri):
+        if uri != 'snowflake://':
+            return IbisDataBackend._connection_callback(uri)
+        return (
+            ibis.snowflake.from_connection(
+                self._do_connection_callback(uri), create_object_udfs=False
+            ),
+            'snowflake',
+            False,
+        )
+
+    def reconnect(self):
+        logging.info('Reconnecting to Snowflake')
+        super().reconnect()
+        self.snowpark = self._get_snowpark_session(self.uri)
+        logging.info('Reconnecting to Snowflake... DONE')
+
+    def insert(self, table_name, raw_documents):
+        ibis_schema = self.conn.table(table_name).schema()
+        df = pandas.DataFrame(raw_documents)
+        rows = list(df.itertuples(index=False, name=None))
+        columns = list(ibis_schema.keys())
+        df = df.to_dict(orient='records')
+        get_row = lambda row: [row[col] for col in columns]
+        rows = list(map(get_row, df))
+        snowpark_cols = ibis_schema_to_snowpark_cols(ibis_schema)
+        snowpark_schema = snowpark_cols_to_schema(snowpark_cols, columns)
+        native_df = self.snowpark.create_dataframe(rows, schema=snowpark_schema)
+        return native_df.write.saveAsTable(f'"{table_name}"', mode='append')
+
+    def raw_sql(self, query):
+        return self.conn.raw_sql(query)
