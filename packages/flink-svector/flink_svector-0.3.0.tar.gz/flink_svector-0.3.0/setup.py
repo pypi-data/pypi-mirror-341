0b6f67b09d75ab1b06dee3cd1f1eb0227ff7d0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 05:03:37 +0000

    Add package flink-svector/0.3.0/flink_svector-0.3.0.tar.gz

---
+++ packages/flink-svector/flink_svector-0.3.0.tar.gz/flink_svector-0.3.0/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import find_packages, setup
+
+setup(
+    name="flink-svector",
+    version="0.3.0",
+    packages=find_packages(),
+    install_requires=[
+        'firebase-admin',
+        'tabulate',
+    ],
+    entry_points={
+        'console_scripts': [
+            'flink=flink.flink:main',
+        ],
+    },
+    author="Siddharth Shah",
+    author_email="your.email@example.com",
+    description="A custom Git-like version control system",
+    long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    long_description_content_type="text/markdown",
+    url="https://github.com/siddharthshah/flink",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
