d2c42d8ce33c99a0541b3446aea42434bca57b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 06:03:46 +0000

    Add package robotframework-scaffold/0.1.4/robotframework_scaffold-0.1.4-py3-none-any.whl

---
+++ packages/robotframework-scaffold/robotframework_scaffold-0.1.4-py3-none-any.whl/robotframework_scaffold/scaffolder/structure.py
@@ -0,0 +1,5 @@
+PROJECT_TYPES = {
+    "api": ["connections", "data", "routes", "utils", "common"],
+    "web": ["data", "locators", "pages", "utils", "common"],
+    "mobile": ["app", "data", "locators", "pages", "utils", "common"]
+}
