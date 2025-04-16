96ec528cb58269ab96efc41918e9d211715b00b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:05:56 +0000

    Add package pysmhi/1.0.2/pysmhi-1.0.2-py3-none-any.whl

---
+++ packages/pysmhi/pysmhi-1.0.2-py3-none-any.whl/pysmhi/const.py
@@ -0,0 +1,14 @@
+"""Constants for SMHI."""
+
+from __future__ import annotations
+
+import logging
+
+API_POINT_FORECAST = (
+    "https://opendata-download-metfcst.smhi.se/api/category"
+    "/pmp3g/version/2/geotype/point/lon/{}/lat/{}/data.json"
+)
+
+LOGGER = logging.getLogger(__package__)
+
+DEFAULT_TIMEOUT = 8
