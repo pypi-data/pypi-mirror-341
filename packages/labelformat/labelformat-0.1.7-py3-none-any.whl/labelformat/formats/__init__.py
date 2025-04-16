834cdad23123695282189f7d7c13c28340ca0ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:20 +0000

    Add package labelformat/0.1.7/labelformat-0.1.7-py3-none-any.whl

---
+++ packages/labelformat/labelformat-0.1.7-py3-none-any.whl/labelformat/formats/__init__.py
@@ -0,0 +1,82 @@
+from labelformat.formats.coco import (
+    COCOInstanceSegmentationInput,
+    COCOInstanceSegmentationOutput,
+    COCOObjectDetectionInput,
+    COCOObjectDetectionOutput,
+)
+from labelformat.formats.cvat import CVATObjectDetectionInput, CVATObjectDetectionOutput
+from labelformat.formats.kitti import (
+    KittiObjectDetectionInput,
+    KittiObjectDetectionOutput,
+)
+from labelformat.formats.labelbox import LabelboxObjectDetectionInput
+from labelformat.formats.lightly import (
+    LightlyObjectDetectionInput,
+    LightlyObjectDetectionOutput,
+)
+from labelformat.formats.pascalvoc import (
+    PascalVOCObjectDetectionInput,
+    PascalVOCObjectDetectionOutput,
+)
+from labelformat.formats.yolov5 import (
+    YOLOv5ObjectDetectionInput,
+    YOLOv5ObjectDetectionOutput,
+)
+from labelformat.formats.yolov6 import (
+    YOLOv6ObjectDetectionInput,
+    YOLOv6ObjectDetectionOutput,
+)
+from labelformat.formats.yolov7 import (
+    YOLOv7ObjectDetectionInput,
+    YOLOv7ObjectDetectionOutput,
+)
+from labelformat.formats.yolov8 import (
+    YOLOv8InstanceSegmentationInput,
+    YOLOv8InstanceSegmentationOutput,
+    YOLOv8ObjectDetectionInput,
+    YOLOv8ObjectDetectionOutput,
+)
+from labelformat.formats.yolov9 import (
+    YOLOv9ObjectDetectionInput,
+    YOLOv9ObjectDetectionOutput,
+)
+from labelformat.formats.yolov10 import (
+    YOLOv10ObjectDetectionInput,
+    YOLOv10ObjectDetectionOutput,
+)
+from labelformat.formats.yolov11 import (
+    YOLOv11ObjectDetectionInput,
+    YOLOv11ObjectDetectionOutput,
+)
+
+__all__ = [
+    "COCOInstanceSegmentationInput",
+    "COCOInstanceSegmentationOutput",
+    "COCOObjectDetectionInput",
+    "COCOObjectDetectionOutput",
+    "CVATObjectDetectionInput",
+    "CVATObjectDetectionOutput",
+    "KittiObjectDetectionInput",
+    "KittiObjectDetectionOutput",
+    "LabelboxObjectDetectionInput",
+    "LightlyObjectDetectionInput",
+    "LightlyObjectDetectionOutput",
+    "PascalVOCObjectDetectionInput",
+    "PascalVOCObjectDetectionOutput",
+    "YOLOv5ObjectDetectionInput",
+    "YOLOv5ObjectDetectionOutput",
+    "YOLOv6ObjectDetectionInput",
+    "YOLOv6ObjectDetectionOutput",
+    "YOLOv7ObjectDetectionInput",
+    "YOLOv7ObjectDetectionOutput",
+    "YOLOv8InstanceSegmentationInput",
+    "YOLOv8InstanceSegmentationOutput",
+    "YOLOv8ObjectDetectionInput",
+    "YOLOv8ObjectDetectionOutput",
+    "YOLOv9ObjectDetectionInput",
+    "YOLOv9ObjectDetectionOutput",
+    "YOLOv10ObjectDetectionInput",
+    "YOLOv10ObjectDetectionOutput",
+    "YOLOv11ObjectDetectionInput",
+    "YOLOv11ObjectDetectionOutput",
+]
