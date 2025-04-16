76e344e7fdc8d4bd7342d182ad639f5919f136d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:19:30 +0000

    Add package textura/0.1.0/textura-0.1.0.tar.gz

---
+++ packages/textura/textura-0.1.0.tar.gz/textura-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textura",
+    version="0.1.0",
+    author="Your Name",
+    author_email="you@example.com",
+    description="Terminal text styling made simple, like colorama but cooler.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/imAnesYT/textura",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    include_package_data=True,
+    package_data={
+        'textura': ['README.md'],
+    },
+)
