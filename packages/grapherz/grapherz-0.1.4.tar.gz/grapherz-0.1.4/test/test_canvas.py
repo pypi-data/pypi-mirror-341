664f5e25c37a578834e9a6dcedc95e463082ae08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:40 +0000

    Add package grapherz/0.1.4/grapherz-0.1.4.tar.gz

---
+++ packages/grapherz/grapherz-0.1.4.tar.gz/grapherz-0.1.4/test/test_canvas.py
@@ -0,0 +1,21 @@
+import sys
+import os
+# 将项目根目录添加到 Python 路径
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import re
+import pytest
+
+from grapher.canvas import Canvas,Color
+
+@pytest.fixture
+def canvas():
+    canva = Canvas(file_path='test/模拟资质认证.canvas')
+    yield canva
+    canva.to_file(file_path='test/模拟资质认证output.canvas')
+
+
+
+def test_canvas(canvas):
+
+    canvas.select_edges_by_text(key='处理')
+    canvas.select_by_color(key=Color.yellow,type='node')[4].color = Color.green.value
