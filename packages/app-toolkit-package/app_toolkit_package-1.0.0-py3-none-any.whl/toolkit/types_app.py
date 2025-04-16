397219151e625817df59db8c0102f590bf7a65a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:38 +0000

    Add package app-toolkit-package/1.0.0/app_toolkit_package-1.0.0-py3-none-any.whl

---
+++ packages/app-toolkit-package/app_toolkit_package-1.0.0-py3-none-any.whl/toolkit/types_app.py
@@ -0,0 +1,33 @@
+from collections.abc import Callable
+from typing import Annotated, Any, TypeAlias, TypeVar
+
+from aiogram.types import CallbackQuery, Message
+from pydantic import BaseModel, Field
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
+from .models.base import Base, TypeModel, TypePK  # noqa
+
+# TODO: to sort out
+
+_F = TypeVar("_F", bound=Callable[..., Any])
+_TM = TypeVar("_TM")  # , bound=TypeModel)
+_AS = TypeVar("_AS", bound=AsyncSession)
+_ASM = TypeVar("_ASM", bound=async_sessionmaker)
+
+JsonType: TypeAlias = dict[str, str]
+DictType: TypeAlias = dict[str, Any]
+NonEmptyStr = Annotated[str, Field(min_length=1, max_length=5000)]
+ResponseType: TypeAlias = dict[int, dict[BaseModel, Any]]
+
+
+# Bot types ===============================================
+# from typing import TypeAlias
+# from aiogram.types import CallbackQuery, Message
+
+EventType: TypeAlias = CallbackQuery | Message
+
+
+# TESTS ====================
+TypeFieldsListNone: TypeAlias = list[str] | None
+TypeFieldsDict: TypeAlias = dict[str, Any]
+TypeResponseJson: TypeAlias = dict | list | None
