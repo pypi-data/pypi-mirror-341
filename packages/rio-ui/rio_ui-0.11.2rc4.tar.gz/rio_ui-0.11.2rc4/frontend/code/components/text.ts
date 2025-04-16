208aa4d3704fbdd6ce3a273d4a7f4b1335ded11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:05:40 +0000

    Add package rio-ui/0.11.2rc4/rio_ui-0.11.2rc4.tar.gz

---
+++ packages/rio-ui/rio_ui-0.11.2rc4.tar.gz/rio_ui-0.11.2rc4/frontend/code/components/text.ts
@@ -0,0 +1,186 @@
+import {
+    Color,
+    SolidFill,
+    LinearGradientFill,
+    ImageFill,
+    TextStyle,
+} from "../dataModels";
+import { applyTextStyleCss, textfillToCss, textStyleToCss } from "../cssUtils";
+import { ComponentBase, ComponentState, DeltaState } from "./componentBase";
+
+export type TextState = ComponentState & {
+    _type_: "Text-builtin";
+    text: string;
+    selectable: boolean;
+    style: "heading1" | "heading2" | "heading3" | "text" | "dim" | TextStyle;
+    justify: "left" | "right" | "center" | "justify";
+    overflow: "nowrap" | "wrap" | "ellipsize";
+
+    font: string | null;
+    fill: Color | SolidFill | LinearGradientFill | ImageFill;
+    font_size: number | null;
+    italic: boolean | null;
+    font_weight: "normal" | "bold" | null;
+    underlined: boolean | null;
+    strikethrough: boolean | null;
+    all_caps: boolean | null;
+};
+
+export class TextComponent extends ComponentBase<TextState> {
+    private inner: HTMLElement;
+
+    createElement(): HTMLElement {
+        let element = document.createElement("div");
+        element.classList.add("rio-text");
+
+        this.inner = document.createElement("span");
+        element.appendChild(this.inner);
+
+        return element;
+    }
+
+    updateElement(
+        deltaState: DeltaState<TextState>,
+        latentComponents: Set<ComponentBase>
+    ): void {
+        super.updateElement(deltaState, latentComponents);
+
+        // BEFORE WE DO ANYTHING ELSE, replace the inner HTML element
+        if (deltaState.style !== undefined) {
+            // Change the element to <h1>, <h2>, <h3> or <span> as necessary
+            let tagName: string = "SPAN";
+            if (typeof deltaState.style === "string") {
+                tagName =
+                    {
+                        heading1: "H1",
+                        heading2: "H2",
+                        heading3: "H3",
+                    }[deltaState.style] || "SPAN";
+            }
+
+            if (tagName !== this.inner.tagName) {
+                let newInner = document.createElement(tagName);
+
+                this.inner.remove();
+                this.element.appendChild(newInner);
+                this.inner = newInner;
+
+                // Turn the whole state into a deltaState so that the new
+                // element is initialized correctly
+                deltaState = { ...this.state, ...deltaState };
+
+                // Shut up the type checker
+                if (deltaState.style === undefined) {
+                    return;
+                }
+            }
+        }
+
+        // Styling
+        if (
+            deltaState.style !== undefined ||
+            deltaState.font !== undefined ||
+            deltaState.fill !== undefined ||
+            deltaState.font_size !== undefined ||
+            deltaState.italic !== undefined ||
+            deltaState.font_weight !== undefined ||
+            deltaState.underlined !== undefined ||
+            deltaState.strikethrough !== undefined ||
+            deltaState.all_caps !== undefined
+        ) {
+            let textStyleCss = textStyleToCss(
+                deltaState.style ?? this.state.style
+            );
+
+            if (deltaState.font) {
+                textStyleCss["font-family"] = deltaState.font;
+            }
+
+            if (deltaState.fill !== undefined) {
+                Object.assign(textStyleCss, textfillToCss(deltaState.fill));
+            }
+
+            if (deltaState.font_size) {
+                textStyleCss["font-size"] = `${deltaState.font_size}rem`;
+            }
+
+            if (deltaState.italic) {
+                textStyleCss["font-style"] = deltaState.italic
+                    ? "italic"
+                    : "normal";
+            }
+
+            if (deltaState.font_weight) {
+                textStyleCss["font-weight"] = deltaState.font_weight;
+            }
+
+            let textDecorations: string[] = [];
+
+            if (deltaState.underlined) {
+                textDecorations.push("underline");
+            }
+
+            if (deltaState.strikethrough) {
+                textDecorations.push("line-through");
+            }
+
+            textStyleCss["text-decoration"] = textDecorations.join(" ");
+
+            if (deltaState.all_caps) {
+                textStyleCss["text-transform"] = deltaState.all_caps
+                    ? "uppercase"
+                    : "none";
+            }
+
+            applyTextStyleCss(this.inner, textStyleCss);
+        }
+
+        // Text content
+        if (deltaState.text !== undefined) {
+            this.inner.textContent = deltaState.text;
+        }
+
+        // How to handle overlong text?
+        switch (deltaState.overflow) {
+            case "nowrap":
+                this.inner.style.whiteSpace = "pre";
+                this.inner.style.width = "max-content";
+                this.inner.style.textOverflow = "clip";
+                this.inner.style.overflow = "unset";
+                break;
+            case "wrap":
+                this.inner.style.whiteSpace = "pre-wrap";
+                this.inner.style.width = "min-content";
+                this.inner.style.textOverflow = "clip";
+                this.inner.style.overflow = "unset";
+                break;
+            case "ellipsize":
+                this.inner.style.whiteSpace = "pre";
+                this.inner.style.width = "0"; // No `min-width: 100%` required
+                this.inner.style.textOverflow = "ellipsis";
+
+                // The element must have `overflow: hidden`. In theory it should
+                // be fine for this setting to be permanent, but in practice it
+                // sometimes cuts off a pixel of text. So unfortunately have to
+                // turn it on via JS here.
+                this.inner.style.overflow = "hidden";
+                break;
+        }
+
+        // Selectable
+        if (deltaState.selectable !== undefined) {
+            if (deltaState.selectable) {
+                this.inner.style.pointerEvents = "auto";
+                this.inner.style.userSelect = "auto";
+            } else {
+                this.inner.style.pointerEvents = "none";
+                this.inner.style.userSelect = "none";
+            }
+        }
+
+        // Text alignment
+        if (deltaState.justify !== undefined) {
+            this.inner.style.textAlign = deltaState.justify;
+        }
+    }
+}
---
+++ packages/rio-ui/rio_ui-0.11.2rc4.tar.gz/rio_ui-0.11.2rc4/rio/inspection.py
@@ -0,0 +1,164 @@
+from __future__ import annotations
+
+import collections
+import functools
+import inspect
+import sys
+import typing as t
+
+import introspection.typing
+
+import rio
+
+__all__ = [
+    "get_local_annotations",
+    "get_resolved_type_annotations",
+    "get_child_component_containing_attribute_names",
+    "get_child_component_containing_attribute_names_for_builtin_components",
+]
+
+
+_EXPLICITLY_SET_STATE_PROPERTY_NAMES_CACHE: dict[
+    tuple[t.Type[rio.Component], int, frozenset[str]], frozenset[str]
+] = {}
+
+
+# Note: This function purposely isn't cached because calling it at different
+# times can give different outputs. For example, if called immediately after the
+# input class has been created, some forward references may not be evaluatable
+# yet.
+class get_local_annotations(t.Mapping[str, introspection.types.TypeAnnotation]):
+    def __init__(self, cls: type, *, strict: bool = False) -> None:
+        # Note: Don't use `typing.get_type_hints` because it has a stupid bug in
+        # python 3.10 where it dies if something is annotated as
+        # `dataclasses.KW_ONLY`.
+        self._annotations: dict = vars(cls).get("__annotations__", {})
+        self._module = sys.modules[cls.__module__]
+        self._strict = strict
+
+    def __getitem__(self, name: str) -> introspection.types.TypeAnnotation:
+        return introspection.typing.resolve_forward_refs(
+            self._annotations[name],
+            self._module,
+            mode="ast",
+            strict=self._strict,
+            treat_name_errors_as_imports=True,
+        )
+
+    def __iter__(self) -> t.Iterator[str]:
+        return iter(self._annotations)
+
+    def __len__(self) -> int:
+        return len(self._annotations)
+
+
+def get_resolved_type_annotations(
+    cls: type,
+) -> t.Mapping[str, type]:
+    maps = [get_local_annotations(c, strict=True) for c in cls.__mro__]
+    return collections.ChainMap(*maps)  # type: ignore
+
+
+@functools.lru_cache(maxsize=None)
+def get_child_component_containing_attribute_names(
+    cls: type[rio.Component],
+) -> t.Collection[str]:
+    from . import serialization
+
+    attr_names: list[str] = []
+
+    for attr_name, serializer in serialization.get_attribute_serializers(
+        cls
+    ).items():
+        # : Component
+        if serializer is serialization._serialize_child_component:
+            attr_names.append(attr_name)
+        elif isinstance(serializer, functools.partial):
+            # : Component | None
+            if (
+                serializer.func is serialization._serialize_optional
+                and serializer.keywords["serializer"]
+                is serialization._serialize_child_component
+            ):
+                attr_names.append(attr_name)
+            # : list[Component]
+            elif (
+                serializer.func is serialization._serialize_sequence
+                and serializer.keywords["item_serializer"]
+                is serialization._serialize_child_component
+            ):
+                attr_names.append(attr_name)
+
+    return tuple(attr_names)
+
+
+@functools.lru_cache(maxsize=None)
+def get_child_component_containing_attribute_names_for_builtin_components() -> (
+    t.Mapping[str, t.Collection[str]]
+):
+    from .components.fundamental_component import FundamentalComponent
+
+    result = {
+        cls._unique_id_: get_child_component_containing_attribute_names(cls)
+        for cls in introspection.iter_subclasses(FundamentalComponent)
+        if cls._unique_id_.endswith("-builtin")
+    }
+
+    result.update(
+        {
+            "HighLevelComponent-builtin": ["_child_"],
+        }
+    )
+    return result
+
+
+def get_explicitly_set_state_property_names(
+    component: rio.Component,
+    args: tuple[object, ...],
+    kwargs: dict[str, object],
+) -> frozenset[str]:
+    """
+    Given a component and the arguments passed to its constructor, return the
+    names of the state properties that were explicitly provided to the
+    constructor. The results are cached.
+
+    Note that really only _state properties_ are returned. Any parameters that
+    don't match state properties are discarded.
+    """
+
+    # Try to get the result from the cache
+    key = (type(component), len(args), frozenset(kwargs.keys()))
+
+    try:
+        return _EXPLICITLY_SET_STATE_PROPERTY_NAMES_CACHE[key]
+    except KeyError:
+        pass
+
+    # No cache entry for this exists yet so determine the result the slow way
+    signature = inspect.signature(component.__init__)
+    bound_args = signature.bind(*args, **kwargs)
+
+    # Anything passed in is explicitly set
+    result = set(bound_args.arguments)
+
+    # *args and **kwargs are always explicitly set
+    result.update(
+        param.name
+        for param in signature.parameters.values()
+        if param.kind
+        in (
+            inspect.Parameter.VAR_POSITIONAL,
+            inspect.Parameter.VAR_KEYWORD,
+        )
+    )
+
+    # Discard parameters that don't correspond to state properties
+    result.intersection_update(type(component)._observable_properties_)
+
+    # Freeze the result so nobody gets silly ideas
+    result = frozenset(result)
+
+    # Store the result in the cache for future use
+    _EXPLICITLY_SET_STATE_PROPERTY_NAMES_CACHE[key] = result
+
+    return result
