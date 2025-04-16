463b5db058a25388d0af18805c76437698b87273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:32 +0000

    Add package webdotnet/0.1.0/webdotnet-0.1.0-py3-none-any.whl

---
+++ packages/webdotnet/webdotnet-0.1.0-py3-none-any.whl/webdotnet/__init__.py
@@ -0,0 +1,16 @@
+def addition(numbers: list) -> float:
+    return sum(numbers)
+def show_code():
+    code = '''
+Using CurrencyConv.ServiceReference1;
+
+CurrencyClient client = new CurrencyClient();
+Console.WriteLine("Enter the Currency in Indian Rupees : ");
+double d double.Parse(Console.ReadLine());
+Console.WriteLine(client.InrToDollar(d));
+Console.WriteLine("Enter any key to Exit...");
+Console.ReadKey();
+Console.ReadLine();
+
+    '''
+    print(code)
