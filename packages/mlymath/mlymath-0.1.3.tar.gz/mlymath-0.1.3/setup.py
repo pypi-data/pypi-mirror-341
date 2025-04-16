c8290e6821b371332e8e3f8dfd1c411a9f7b2e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:18:47 +0000

    Add package mlymath/0.1.3/mlymath-0.1.3.tar.gz

---
+++ packages/mlymath/mlymath-0.1.3.tar.gz/mlymath-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlymath',
+    version='0.1.3',
+    packages=find_packages(),
+    author='mly',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
