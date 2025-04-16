dfc3ff0ba5acd6de30d3ce0d8e90fcdea0412f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:47 +0000

    Add package memium/0.30.2/memium-0.30.2.tar.gz

---
+++ packages/memium/memium-0.30.2.tar.gz/memium-0.30.2/memium/destination/ankiconnect/test_anki_prompt_qa.py
@@ -0,0 +1,59 @@
+from collections.abc import Sequence
+from dataclasses import dataclass, field
+
+import genanki
+
+from .anki_prompt_qa import AnkiQA
+
+
+def fake_tag_factory() -> Sequence[str]:
+    return ["FakeTag"]
+
+
+@dataclass(frozen=True)
+class FakeAnkiQA(AnkiQA):
+    base_deck: str = "FakeBaseDeck"
+    tags: Sequence[str] = field(default_factory=fake_tag_factory)
+    question: str = "FakeQuestion"
+    answer: str = "FakeAnswer"
+    css: str = "FakeCSS"
+    uuid: int = 0
+    edit_url: str = "FakeEditURL"
+    source_title: str | None = None
+    render_parent_doc: bool = False
+
+
+from dataclasses import dataclass
+
+import pytest
+
+
+@dataclass(frozen=True)
+class QAExample:
+    card: AnkiQA
+    deck: str
+
+
+@pytest.mark.parametrize(
+    ("example"),
+    [
+        QAExample(FakeAnkiQA(tags=["anki/deck/Subdeck"]), "FakeBaseDeck::Subdeck"),
+        QAExample(
+            FakeAnkiQA(tags=["anki/deck/Subdeck"], question="What are _Wikilinks_ on _Wikipedia_?"),
+            "FakeBaseDeck::Subdeck::Wikilinks-Wikipedia",
+        ),
+        QAExample(
+            FakeAnkiQA(question="On _Wikipedia_, what are _Wikilinks_?"),
+            "FakeBaseDeck::Wikilinks-Wikipedia",
+        ),
+        QAExample(FakeAnkiQA(question="Without wikilinks?"), "FakeBaseDeck"),
+    ],
+)
+def test_ankiqa_deck_inference(example: QAExample):
+    assert example.card.deck == example.deck
+
+
+def test_formatting(snapshot: genanki.Note):
+    card = FakeAnkiQA(question="Q. This is a _question_?")
+    note = card.to_genanki_note()
+    assert snapshot == note.fields  # type: ignore
---
+++ packages/memium/memium-0.30.2.tar.gz/memium-0.30.2/memium/source/extractors/test_extractor_table.py
@@ -0,0 +1,76 @@
+from collections.abc import Sequence
+from dataclasses import dataclass
+from pathlib import Path
+
+import pytest
+
+from memium.source.extractors.test_prompt import DummyQAPrompt
+
+from ..document import Document
+from .extractor_table import TableExtractor
+from .to_line_blocks import LineBlock, to_line_blocks
+
+
+@dataclass(frozen=True)
+class TableExtractorExample:
+    table_prompt: str  # What the example is testing
+    expectation: Sequence[DummyQAPrompt]  # Expected prompts
+
+
+# p3: if the row contains a wikilink with a pipe, it will be split into two cells
+# e.g. [[Name|Alias]] will be parsed incorrectly.
+
+
+@pytest.mark.parametrize(
+    ("example"),
+    [
+        TableExtractorExample(
+            table_prompt="Descending // |Column one|? // |Column two|.",
+            expectation=[DummyQAPrompt(question="11?", answer="22.")],
+        ),
+        TableExtractorExample(
+            table_prompt="Ascending // |Column one|? // |Column two|.",
+            expectation=[
+                DummyQAPrompt(question="21?", answer="12."),
+                DummyQAPrompt(question="31?", answer="22."),
+            ],
+        ),
+        TableExtractorExample(
+            table_prompt="Rowwise // |Column one|? // |Column two|.",
+            expectation=[
+                DummyQAPrompt(question="11?", answer="12."),
+                DummyQAPrompt(question="21?", answer="22."),
+            ],
+        ),
+    ],
+    ids=lambda x: x.table_prompt,
+)
+def test_table_extractor(example: TableExtractorExample):
+    input_doc = Document(
+        content=f"""
+    Block one
+
+| Column one | Column two |
+| --- | --- |
+| 11 | 12 |
+| 21 | 22 |
+| 31 |      |
+{example.table_prompt}""",
+        source_path=Path(__file__),
+    )
+
+    assert {
+        example.to_qa_from_doc(doc=input_doc, line_nr=len(input_doc.content.split("\n")) - 1)
+        for example in example.expectation
+    } == set(TableExtractor().extract_prompts(input_doc))
+
+
+def test_line_block_extractor():
+    document = """Block 1
+
+Block 2
+With multiline"""
+    assert to_line_blocks(document) == [
+        LineBlock(starting_line=0, lines=["Block 1"]),
+        LineBlock(starting_line=2, lines=["Block 2", "With multiline"]),
+    ]
