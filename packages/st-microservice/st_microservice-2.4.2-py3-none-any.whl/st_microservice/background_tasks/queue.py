a2bb3f7a3e4532e582d5625cc22a78e53ce2c211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:19:10 +0000

    Add package st-microservice/2.4.2/st_microservice-2.4.2-py3-none-any.whl

---
+++ packages/st-microservice/st_microservice-2.4.2-py3-none-any.whl/st_microservice/background_tasks/queue.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from redis import from_url
+from redis.asyncio import from_url as afrom_url
+from rq import Queue
+from rq.job import Job, JobStatus
+
+from ..general_utils import get_required_env
+
+BROKER_URI = get_required_env("BROKER_URI")
+queue = Queue(connection=from_url(BROKER_URI))
+aredis = afrom_url(BROKER_URI, decode_responses=True)
+
+
+async def wait_and_receive_messages(job: Job):
+    sub = aredis.pubsub(ignore_subscribe_messages=True)
+    await sub.subscribe(f"task-messages-{job.id}")
+    while job.get_status() not in [
+        JobStatus.FINISHED,
+        JobStatus.CANCELED,
+        JobStatus.FAILED,
+        JobStatus.STOPPED,
+    ]:
+        await asyncio.sleep(0.5)
+        while (message_raw := await sub.get_message()) is not None:
+            yield message_raw["data"]
+    # Collect remaining messages
+    while (message_raw := await sub.get_message()) is not None:
+        yield message_raw["data"]
