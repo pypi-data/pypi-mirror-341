32bca45ef3c57f9075c2f2ce376500cbd100cc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:36 +0000

    Add package EC4py/0.7.1/ec4py-0.7.1.tar.gz

---
+++ packages/EC4py/ec4py-0.7.1.tar.gz/ec4py-0.7.1/docs/source/package/ec4py_keywords.md
@@ -0,0 +1,27 @@
+---
+title: Keywords
+parent: Package
+nav_order: 100
+---
+
+# Keywords
+
+| Keyword        | Meaning           | Where to use  |
+| ------------- |:-------------| -----:|
+| IRCOMP = "Z" | iR-compensation using measured <br> absolute impedance| When loading a dataset |
+| IRCOMP = "R" | iR-compensation using measured <br>real part of the impedance |When loading a dataset|
+| IRCOMP = 1.0 | Manual iR-compensation<br>where the number corresponds to the Rsol | When loading a dataset |
+
+## For plotting
+
+| Keyword        | Meaning           | Where to use  |
+| ------------- |:-------------| -----:|
+| title = "string" | Change the title of a plot. | When plotting a dataset|
+| xlabel = "string" | Change the xlabel of a plot. | When plotting a dataset|
+| ylabel = "string" | Change the ylabel of a plot. | When plotting a dataset|
+| xscale = "log" | Change the x scale to log. | When plotting a dataset|
+| yscale = "log" | Change the y scale to log. | When plotting a dataset|
+| y_smooth = 3 | moving averaging of the y data <br> number correspond to the width of the moving average. | When plotting a dataset|
+| x_smooth = 4 | moving averaging of the y data <br> number correspond to the width of the moving average.| When plotting a dataset |
+ 
+
---
+++ packages/EC4py/ec4py-0.7.1.tar.gz/ec4py-0.7.1/test/test_ec_datas_util.py
@@ -0,0 +1,80 @@
+
+import copy
+from ec4py.ec_setup import EC_Setup
+from ec4py.util import Quantity_Value_Unit as QVU
+from ec4py.util_graph import LEGEND
+from ec4py.ec_datas_util import EC_Datas_base
+#"import inc_dec    # "The code to test
+import unittest   # The test framework
+import numpy as np
+from pathlib import Path
+
+
+class test_EC_Datas_base(unittest.TestCase):
+
+    def test_base(self):
+        datas = EC_Datas_base()
+        
+        datas.append(float(1))
+        datas.append(float(2))
+        
+        
+    def test_append_and_len(self):
+        datas = EC_Datas_base()
+        datas.append(float(1))
+        datas.append(float(2))
+        self.assertEqual(len(datas), 2)
+        self.assertEqual(datas[0], 1)
+        self.assertEqual(datas[1], 2)
+        datas.pop(1)
+        self.assertEqual(len(datas), 1)
+
+    def test_append_and_len_all(self):
+        pop_and_len(self,EC_Datas_base(),float(1),float(2) ) 
+        
+    def test_quantities(self):
+        datas = EC_Datas_base()
+        datas.append(EC_Setup())
+        datas.append(EC_Setup())
+        datas[0].set_area("2 m^2" )
+        datas[1].set_area("1 m^2" )
+        datas[0].set_mass("3 g")
+        datas[1].set_mass("2 g")
+        #datas[0].set_rate("1 V /s")
+        #datas[1].set_rate("2 V /s")
+        test_quantities(self, datas)
+         
+
+def pop_and_len(self, datasType,value1,value2):
+    datas = datasType
+    datas.append(value1)
+    datas.append(value2)
+    self.assertEqual(len(datas), 2)
+    self.assertEqual(datas[0], value1)
+    self.assertEqual(datas[1], value2)
+    datas.pop(1)
+    self.assertEqual(len(datas), 1)
+         
+def  test_quantities(self, datasType_with_a_length):
+    datas = datasType_with_a_length
+    length = len(datasType_with_a_length)
+    for data in datas:
+        data.set_area("2 m^2" )
+        data.set_area("1 m^2" )
+        data.set_mass("3 g")
+        data.set_mass("2 g")
+    
+    
+    area = datas.area
+    self.assertEqual(len(area), length)    
+    for a in area:
+        self.assertEqual(a.quantity, "A")
+    #mass
+    mass = datas.mass
+    #mass
+    self.assertEqual(len(mass), length)    
+    for a in mass:
+        self.assertEqual(a.quantity, "m")
+
+if __name__ == '__main__':
+    unittest.main()
