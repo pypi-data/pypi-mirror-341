c04409ef7684da8594c69c062704da84e8e4cf35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:05:10 +0000

    Add package mseep-wecom-bot-mcp-server/0.6.7/mseep_wecom_bot_mcp_server-0.6.7.tar.gz

---
+++ packages/mseep-wecom-bot-mcp-server/mseep_wecom_bot_mcp_server-0.6.7.tar.gz/mseep_wecom_bot_mcp_server-0.6.7/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-wecom-bot-mcp-server",
+    version="0.6.7",
+    description="WeCom Bot MCP Server - A Python server for WeCom (WeChat Work) bot following the Model Context Protocol (MCP)",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires={'python': '>=3.10,<4.0', 'mcp': '>=1.3.0', 'notify-bridge': '>=0.3.0', 'platformdirs': '>=4.2.0', 'pydantic': '>=2.6.1', 'ftfy': '>=6.3.1', 'httpx': '>=0.28.1', 'pillow': '>=10.2.0', 'svglib': '>=1.5.1', 'tenacity': '>=9.0.0', 'loguru': '>=0.7.3', 'aiohttp': '>=3.11.13'},
+    keywords=["mseep"] + [],
+)
