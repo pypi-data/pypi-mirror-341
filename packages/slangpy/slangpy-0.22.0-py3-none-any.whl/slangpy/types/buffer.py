324339db50192c6f45392959d27c48599f8ff5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:04:37 +0000

    Add package slangpy/0.22.0/slangpy-0.22.0-py3-none-any.whl

---
+++ packages/slangpy/slangpy-0.22.0-py3-none-any.whl/slangpy/types/buffer.py
@@ -0,0 +1,209 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+from typing import Any, Optional
+
+from slangpy.core.native import Shape, NativeNDBuffer, NativeNDBufferDesc
+from slangpy.core.shapes import TShapeOrTuple
+from slangpy.core.struct import Struct
+
+from slangpy.backend import (DataType, Device, MemoryType,
+                             BufferUsage, TypeLayoutReflection,
+                             TypeReflection, CommandEncoder)
+from slangpy.bindings.marshall import Marshall
+from slangpy.bindings.typeregistry import get_or_create_type
+from slangpy.reflection import ScalarType, SlangProgramLayout, SlangType
+
+global_lookup_modules: dict[Device, SlangProgramLayout] = {}
+
+SLANG_TO_CUDA_TYPES = {
+    TypeReflection.ScalarType.float16: DataType.float16,
+    TypeReflection.ScalarType.float32: DataType.float32,
+    TypeReflection.ScalarType.float64: DataType.float64,
+    TypeReflection.ScalarType.int8: DataType.int8,
+    TypeReflection.ScalarType.int16: DataType.int16,
+    TypeReflection.ScalarType.int32: DataType.int32,
+    TypeReflection.ScalarType.int64: DataType.int64,
+    TypeReflection.ScalarType.uint8: DataType.uint8,
+    TypeReflection.ScalarType.uint16: DataType.uint16,
+    TypeReflection.ScalarType.uint32: DataType.uint32,
+    TypeReflection.ScalarType.uint64: DataType.uint64,
+    TypeReflection.ScalarType.bool: DataType.bool,
+}
+
+
+def _on_device_close(device: Device):
+    del global_lookup_modules[device]
+
+
+def get_lookup_module(device: Device) -> SlangProgramLayout:
+    if device not in global_lookup_modules:
+        dummy_module = device.load_module_from_source(
+            "slangpy_layout", 'import "slangpy";')
+        global_lookup_modules[device] = SlangProgramLayout(dummy_module.layout)
+        device.register_device_close_callback(_on_device_close)
+
+    return global_lookup_modules[device]
+
+
+def resolve_program_layout(device: Device, element_type: Any, program_layout: Optional[SlangProgramLayout]) -> SlangProgramLayout:
+    if program_layout is None:
+        if isinstance(element_type, SlangType):
+            program_layout = element_type.program
+        elif isinstance(element_type, Marshall):
+            program_layout = element_type.slang_type.program
+        elif isinstance(element_type, Struct):
+            program_layout = element_type.module.layout
+        else:
+            program_layout = get_lookup_module(device)
+    return program_layout
+
+
+def resolve_element_type(program_layout: SlangProgramLayout, element_type: Any) -> SlangType:
+    if isinstance(element_type, SlangType):
+        pass
+    elif isinstance(element_type, str):
+        element_type = program_layout.find_type_by_name(element_type)
+    elif isinstance(element_type, Struct):
+        if element_type.module.layout == program_layout:
+            element_type = element_type.struct
+        else:
+            element_type = program_layout.find_type_by_name(element_type.name)
+    elif isinstance(element_type, TypeReflection):
+        element_type = program_layout.find_type(element_type)
+    elif isinstance(element_type, TypeLayoutReflection):
+        element_type = program_layout.find_type(element_type.type)
+    elif isinstance(element_type, Marshall):
+        if element_type.slang_type.program == program_layout:
+            element_type = element_type.slang_type
+        else:
+            element_type = program_layout.find_type_by_name(
+                element_type.slang_type.full_name)
+    # elif element_type == float:
+    #    element_type = program_layout.scalar_type(TypeReflection.ScalarType.float32)
+    # elif element_type == int:
+    #    element_type = program_layout.scalar_type(TypeReflection.ScalarType.int32)
+    else:
+        bt = get_or_create_type(program_layout, element_type)
+        element_type = bt.slang_type
+    if element_type is None:
+        raise ValueError("Element type could not be resolved")
+    return element_type
+
+
+class NDBuffer(NativeNDBuffer):
+    """
+    An N dimensional buffer of a given slang type. The supplied type can come from a SlangType (via
+    reflection), a struct read from a Module, or simply a name.
+
+    When specifying just a type name, it is advisable to also supply the program_layout for the
+    module in question (see Module.layout), as this ensures type information is looked up from
+    the right place.
+    """
+
+    def __init__(
+        self,
+        device: Device,
+        dtype: Any,
+        element_count: Optional[int] = None,
+        shape: Optional[TShapeOrTuple] = None,
+        usage: BufferUsage = BufferUsage.shader_resource
+        | BufferUsage.unordered_access,
+        memory_type: MemoryType = MemoryType.device_local,
+        program_layout: Optional[SlangProgramLayout] = None
+    ):
+        if element_count is None and shape is None:
+            raise ValueError("Either element_count or shape must be provided")
+        if element_count is not None and shape is not None:
+            raise ValueError("Only one of element_count or shape can be provided")
+
+        # Slang program layout of module that defines the element type for this buffer.
+        program_layout = resolve_program_layout(device, dtype, program_layout)
+
+        # Slang element type.
+        dtype = resolve_element_type(program_layout, dtype)
+
+        if element_count is None:
+            if shape is None:
+                raise ValueError("Either element_count or shape must be provided")
+            element_count = 1
+            for dim in shape:
+                element_count *= dim
+            shape = Shape(shape)
+        elif shape is None:
+            if element_count is None:
+                raise ValueError("Either element_count or shape must be provided")
+            shape = Shape(element_count)
+        else:
+            raise ValueError("element_count or shape must be provided")
+
+        strides = []
+        total = 1
+        for dim in reversed(shape):
+            strides.append(total)
+            total *= dim
+        strides = Shape(tuple(reversed(strides)))
+
+        desc = NativeNDBufferDesc()
+        desc.usage = usage
+        desc.memory_type = memory_type
+        desc.shape = shape
+        desc.strides = strides
+        desc.dtype = dtype
+        desc.element_layout = dtype.buffer_layout.reflection
+
+        super().__init__(device, desc)
+
+        # Tell typing the dtype is a valid slang type
+        self.dtype: 'SlangType'
+
+    @property
+    def is_writable(self):
+        """
+        Returns True if this buffer is writable from the GPU, i.e. if it has unordered access resource usage.
+        """
+        return (self.usage & BufferUsage.unordered_access) != 0
+
+    def to_torch(self, override_type: Optional[DataType] = None):
+        """
+        Returns the buffer as a torch tensor.
+        """
+        if isinstance(self.dtype, ScalarType):
+            return self.storage.to_torch(type=SLANG_TO_CUDA_TYPES[self.dtype.slang_scalar_type], shape=self.shape.as_tuple(), strides=self.strides.as_tuple())
+        else:
+            raise ValueError("Only scalar types can be converted to torch tensors")
+
+    def clear(self, command_buffer: Optional[CommandEncoder] = None):
+        """
+        Fill the ndbuffer with zeros. If no command buffer is provided, a new one is created and
+        immediately submitted. If a command buffer is provided the clear is simply appended to it
+        but not automatically submitted.
+        """
+        if command_buffer:
+            command_buffer.clear_buffer(self.storage)
+        else:
+            cmd = self.storage.device.create_command_encoder()
+            cmd.clear_buffer(self.storage)
+            self.device.submit_command_buffer(cmd.finish())
+
+    @staticmethod
+    def zeros(device: Device,
+              dtype: Any,
+              element_count: Optional[int] = None,
+              shape: Optional[TShapeOrTuple] = None,
+              usage: BufferUsage = BufferUsage.shader_resource
+              | BufferUsage.unordered_access,
+              memory_type: MemoryType = MemoryType.device_local,
+              program_layout: Optional[SlangProgramLayout] = None
+              ) -> 'NDBuffer':
+        """
+        Creates a zero-initialized nbuffer with the requested shape and element type.
+        """
+        buffer = NDBuffer(device, dtype, element_count, shape, usage, memory_type, program_layout)
+        buffer.clear()
+        return buffer
+
+    @staticmethod
+    def zeros_like(other: 'NDBuffer') -> 'NDBuffer':
+        """
+        Creates a zero-initialized ndbuffer with the same shape and element type as the given ndbuffer.
+        """
+        return NDBuffer.zeros(other.storage.device, shape=other.shape, dtype=other.dtype)
---
+++ packages/slangpy/slangpy-0.22.0-py3-none-any.whl/slangpy/types/tensor.py
@@ -0,0 +1,214 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+from __future__ import annotations
+
+from slangpy.backend import Device, Buffer, BufferUsage, TypeReflection, CommandBuffer
+from slangpy.core.utils import shape_to_contiguous_strides
+from slangpy.reflection import SlangType, ScalarType, SlangProgramLayout
+from slangpy.reflection import reflectiontypes
+from slangpy.core.native import Shape
+from slangpy.core.shapes import TShapeOrTuple
+from slangpy.types.buffer import get_lookup_module, resolve_element_type, resolve_program_layout
+from slangpy.core.native import Shape, NativeTensor, NativeTensorDesc
+
+from typing import Optional, Any, cast
+import numpy as np
+import math
+
+ST = TypeReflection.ScalarType
+_numpy_to_sgl = {
+    'int8': ST.int8, 'int16': ST.int16, 'int32': ST.int32, 'int64': ST.int64,
+    'uint8': ST.uint8, 'uint16': ST.uint16, 'uint32': ST.uint32, 'uint64': ST.uint64,
+    'float16': ST.float16, 'float32': ST.float32, 'float64': ST.float64
+}
+_sgl_to_numpy = {
+    y: x for x, y in _numpy_to_sgl.items()
+}
+
+
+def innermost_type(slang_type: SlangType) -> SlangType:
+    while True:
+        if slang_type.element_type is not None and slang_type.element_type is not slang_type:
+            slang_type = slang_type.element_type
+        else:
+            return slang_type
+
+
+def _slang_to_numpy(slang_dtype: SlangType):
+    elem_type = innermost_type(slang_dtype)
+    if isinstance(elem_type, ScalarType) and elem_type.slang_scalar_type in _sgl_to_numpy:
+        return np.dtype(_sgl_to_numpy[elem_type.slang_scalar_type])
+    return None
+
+
+def _numpy_to_slang(np_dtype: np.dtype[Any], device: Device) -> Optional[SlangType]:
+    name = np_dtype.base.name
+    if name not in _numpy_to_sgl:
+        return None
+    slang_dtype = reflectiontypes.scalar_names[_numpy_to_sgl[name]]
+    if np_dtype.ndim > 0:
+        for dim in reversed(np_dtype.shape):
+            slang_dtype += f'[{dim}]'
+
+    return get_lookup_module(device).find_type_by_name(slang_dtype)
+
+
+class Tensor(NativeTensor):
+    """
+    Represents an N-D view of an underlying buffer with given shape and element type,
+    and has optional gradient information attached. Element type must be differentiable.
+
+    Strides and offset can optionally be specified and are given in terms of elements, not bytes.
+    If omitted, a dense N-D grid is assumed (row-major).
+    """
+
+    def __init__(self, storage: Buffer, dtype: SlangType, shape: TShapeOrTuple,
+                 strides: Optional[TShapeOrTuple] = None, offset: int = 0,
+                 grad_in: Optional[Tensor] = None, grad_out: Optional[Tensor] = None):
+
+        # Setup shape and stride.
+        shape = Shape(shape)
+        if strides is None:
+            strides = shape_to_contiguous_strides(shape.as_tuple())
+        if len(strides) != len(shape):
+            raise ValueError("Number of strides must match number of dimensions")
+
+        # Fill out native descriptor and initialize.
+        desc = NativeTensorDesc()
+        desc.shape = Shape(shape)
+        desc.strides = Shape(strides)
+        desc.offset = offset
+        desc.dtype = dtype
+        desc.offset = offset
+        desc.element_layout = dtype.buffer_layout.reflection
+        super().__init__(desc, storage, grad_in, grad_out)
+
+        # Fix up some typing info
+        self.dtype: SlangType
+        self.grad_in: Optional[Tensor]
+        self.grad_out: Optional[Tensor]
+
+    # flatten_dtype Not used anywhere - do we need it? If so, need to make native implementation
+    """
+    def flatten_dtype(self) -> Tensor:
+        new_dtype = innermost_type(self.dtype)
+        dtype_shape = self.dtype.shape.as_tuple()
+        dtype_strides = shape_to_contiguous_strides(dtype_shape)
+        stride_multiplier = math.prod(dtype_shape)
+
+        new_shape = self.shape.as_tuple() + dtype_shape
+        new_strides = tuple(s * stride_multiplier for s in self.strides) + dtype_strides
+        new_offset = self.offset * stride_multiplier
+
+        return Tensor(self.storage, new_dtype, new_shape, new_strides, new_offset)
+    """
+
+    def broadcast_to(self, shape: TShapeOrTuple):
+        """
+        Returns a new tensor view of the same buffer with the requested shape, following standard broadcasting rules.
+        """
+        return super().broadcast_to(Shape(shape))
+
+    def __str__(self):
+        ndarray = self.to_numpy()
+        if ndarray is None:
+            return f"Tensor({self.shape}, {self.dtype.name})"
+        else:
+            return str(ndarray)
+
+    def to_numpy(self) -> np.ndarray[Any, Any]:
+        """
+        Copies tensor data into a numpy array with the same shape and strides. This may fail if the
+        element type does not have an equivalent in numpy.
+        """
+        return cast(np.ndarray[Any, Any], super().to_numpy())
+
+    def with_grads(self, grad_in: Optional[Tensor] = None, grad_out: Optional[Tensor] = None, zero: bool = False):
+        """
+        Returns a new tensor view with gradients attached. If called with no arguments, the
+        tensor defaults to attaching a zeros-like initialized gradient tensor for both input and 
+        output gradients.
+
+        Specifying input gradients (grad_in) and/or output gradients (grad_out) allows more precise
+        control over the gradient tensors, and is key when using a function that has inout parameters,
+        so will want to both read and write gradients without causing race conditions. 
+
+        When differentiating a slang call that wrote results to a tensor, gradients of the output will
+        be read from grad_in (if not None). When differentiating a slang call that read inputs from a
+        tensor, input gradients will be written to grad_out (if not None).
+        """
+        return super().with_grads(grad_in, grad_out, zero)
+
+    def clear(self, command_buffer: Optional[CommandBuffer] = None):
+        """
+        Fill the tensor with zeros. If no command buffer is provided, a new one is created and
+        immediately submitted. If a command buffer is provided the clear is simply appended to it
+        but not automatically submitted.
+        """
+        super().clear()
+
+    @staticmethod
+    def numpy(device: Device, ndarray: np.ndarray[Any, Any]) -> Tensor:
+        """
+        Creates a new tensor with the same contents, shape and strides as the given numpy array.
+        """
+
+        dtype = _numpy_to_slang(ndarray.dtype, device)
+        if dtype is None:
+            raise ValueError(f"Unsupported numpy dtype {ndarray.dtype}")
+        if (ndarray.nbytes % ndarray.itemsize) != 0:
+            raise ValueError(f"Unsupported numpy array")
+        for stride in ndarray.strides:
+            if (stride % ndarray.itemsize) != 0:
+                raise ValueError(f"Unsupported numpy array")
+
+        N = ndarray.nbytes // ndarray.itemsize
+        flattened = np.lib.stride_tricks.as_strided(ndarray, (N, ), (ndarray.itemsize, ))
+        strides = tuple(stride // ndarray.itemsize for stride in ndarray.strides)
+
+        usage = BufferUsage.shader_resource | BufferUsage.unordered_access
+        buffer = device.create_buffer(ndarray.nbytes, usage=usage, data=flattened)
+
+        return Tensor(buffer, dtype, tuple(ndarray.shape), strides)
+
+    @staticmethod
+    def empty(device: Device, shape: TShapeOrTuple, dtype: Any, program_layout: Optional[SlangProgramLayout] = None) -> Tensor:
+        """
+        Creates a tensor with the requested shape and element type without attempting to initialize the data.
+        """
+        # If dtype supplied is not a SlangType, resolve it using the same mechanism as NDBuffer
+        if not isinstance(dtype, SlangType):
+            program_layout = resolve_program_layout(device, dtype, program_layout)
+            dtype = resolve_element_type(program_layout, dtype)
+
+        shape_tuple = shape if isinstance(shape, tuple) else shape.as_tuple()
+        num_elems = math.prod(shape_tuple)
+
+        usage = BufferUsage.shader_resource | BufferUsage.unordered_access
+        buffer = device.create_buffer(element_count=num_elems,
+                                      struct_size=dtype.buffer_layout.stride,
+                                      usage=usage)
+
+        return Tensor(buffer, dtype, shape)
+
+    @staticmethod
+    def zeros(device: Device, shape: TShapeOrTuple, dtype: Any) -> Tensor:
+        """
+        Creates a zero-initialized tensor with the requested shape and element type.
+        """
+        tensor = Tensor.empty(device, shape, dtype)
+        tensor.clear()
+        return tensor
+
+    @staticmethod
+    def empty_like(other: Tensor) -> Tensor:
+        """
+        Creates a new tensor with the same shape and element type as the given tensor, without initializing the data.
+        """
+        return Tensor.empty(other.storage.device, other.shape, other.dtype)
+
+    @staticmethod
+    def zeros_like(other: Tensor) -> Tensor:
+        """
+        Creates a zero-initialized tensor with the same shape and element type as the given tensor.
+        """
+        return Tensor.zeros(other.storage.device, other.shape, other.dtype)
