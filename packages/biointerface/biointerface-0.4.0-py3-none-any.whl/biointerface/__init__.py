9f795e9361af9d9001f2718bb0b7dbd24cfd38aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:05:18 +0000

    Add package biointerface/0.4.0/biointerface-0.4.0-py3-none-any.whl

---
+++ packages/biointerface/biointerface-0.4.0-py3-none-any.whl/biointerface/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for BioInterface."""
+
+from biointerface.core import Interface, build_interfaces
+
+__author__ = """Alessandro Pandolfi"""
+__email__ = "alessandro.pandolfi@protonmail.com"
+__version__ = "0.4.0"
+
+__all__ = ["Interface", "build_interfaces"]
