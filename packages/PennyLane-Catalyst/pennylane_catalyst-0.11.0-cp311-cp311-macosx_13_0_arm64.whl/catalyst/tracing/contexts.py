b76d99257488361233d599357c9b994213178fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:05:46 +0000

    Add package PennyLane-Catalyst/0.11.0/pennylane_catalyst-0.11.0-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/PennyLane-Catalyst/pennylane_catalyst-0.11.0-cp311-cp311-macosx_13_0_arm64.whl/catalyst/tracing/contexts.py
@@ -0,0 +1,356 @@
+# Copyright 2022-2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module provides context classes to manage and query Catalyst's and JAX's tracing state.
+"""
+
+import logging
+from contextlib import contextmanager
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from typing import ContextManager, Dict, List, Optional, Set, Tuple
+
+from jax._src.core import MainTrace as JaxMainTrace
+from jax._src.core import cur_sublevel, new_base_main
+from jax._src.interpreters.partial_eval import (
+    DynamicJaxprTrace,
+    JaxprStackFrame,
+    extend_jaxpr_stack,
+)
+from jax._src.source_info_util import reset_name_stack
+from jax.core import find_top_trace
+from pennylane.queuing import QueuingManager
+
+from catalyst.jax_extras import new_dynamic_main2
+from catalyst.logging import debug_logger_init
+from catalyst.utils.exceptions import CompileError
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+class GradContext:
+    """This class tells us the level of nestedness of grad."""
+
+    # Conceptually:
+    #
+    #   _grad_stack : List[bool] = []
+    #
+    # is a class member variable that reflects the level of
+    # nested-ness of the grad operation. If we have the following code:
+    #
+    #   grad(fun)(x)
+    #
+    # and we are inside fun, then List _grad_stack will contain [True]
+    # if we have:
+    #
+    #  grad(grad(fun))(x)
+    #
+    # and we are tracing fun, then _grad_stack will contain [True, True]
+    #
+    # I say conceptually because you can already tell that we don't need
+    # anything besides an integer that gets incremented and decremented
+    # each time you enter a grad or jacobian operation.
+    #
+    # The reason why we started with a stack is because I think it makes more sense.
+    #
+    # You might also ask, why do we need keep track of the order of the derivative?
+    # Isn't it enough to know that we are inside the grad context or outside?
+    # It is when you are only limited to the first order derivative, but once
+    # you have a higher order derivatives, it will be important to know whether
+    # you need the derivative of the derivative...
+    #
+    # The final question to be asked here is whether we need a new stack for this
+    # context or whether we should reuse the old one.
+    # I think we should keep it simple and this is simpler.
+
+    _grad_stack: int = 0
+    # This message will be used in an assertion because it is not expected
+    # to be a user facing error ever.
+
+    def __init__(self, peel=False):
+        """Peel is useful when we want to temporarily create a context
+        where we peel one order of the derivative"""
+        self.peel = peel
+
+    def __enter__(self, peel=False):
+        _ = GradContext._pop() if self.peel else GradContext._push()
+
+    def __exit__(self, _exc_type, _exc, _exc_tb):
+        _ = GradContext._push() if self.peel else GradContext._pop()
+
+    @staticmethod
+    def am_inside_grad():
+        """Return true if we are currently tracing inside a grad operation."""
+        return GradContext._peek() > 0
+
+    @staticmethod
+    def _pop():
+        retval = GradContext._peek()
+        msg = "This is an impossible state. "
+        msg += "One cannot derive to derive to a negative order / integrate"
+        assert retval > 0, msg
+        GradContext._grad_stack -= 1
+        return retval
+
+    @staticmethod
+    def _push():
+        GradContext._grad_stack += 1
+
+    @staticmethod
+    def _peek():
+        return GradContext._grad_stack
+
+
+class AccelerateContext:
+    _am_inside_accelerate: int = 0
+
+    def __enter__(self):
+        AccelerateContext._am_inside_accelerate += 1
+
+    def __exit__(self, _exc_type, _exc, _exc_tb):
+        AccelerateContext._am_inside_accelerate -= 1
+
+    @staticmethod
+    def am_inside_accelerate():
+        return AccelerateContext._am_inside_accelerate > 0
+
+
+class EvaluationMode(Enum):
+    """Enumerate the evaluation modes supported by Catalyst:
+    INTERPRETATION - native Python execution of a Catalyst program
+    QUANTUM_COMPILATION - JAX tracing followed by MLIR compilation in the presence of quantum
+                          instructions
+    CLASSICAL_COMPILATION - JAX tracing followed JAX compilation of classical-only Catalyst
+                            programs.
+    """
+
+    INTERPRETATION = 0
+    QUANTUM_COMPILATION = 1
+    CLASSICAL_COMPILATION = 2
+
+
+@dataclass
+class JaxTracingContext:
+    """JAX tracing context supporting nested quantum operations. Keeps track of the re-entrable
+    tracing frames. The tracing algorithm visits these frames several times: first during the
+    classical tracing, then during the quantum tracing and also during the optional transformations.
+
+    Args:
+        main: Base JAX tracing data structure.
+        frames: JAX tracing frames; holding the JAXPR equations.
+        mains: Secondary JAX tracing structrures. Each structure has one frame and
+               corresponds to a :class:`~.jax_tracer.HybridOpRegion`
+        trace: Current JAX trace object.
+    """
+
+    main: JaxMainTrace
+    frames: Dict[DynamicJaxprTrace, JaxprStackFrame]
+    mains: Dict[DynamicJaxprTrace, JaxMainTrace]
+    trace: Optional[DynamicJaxprTrace]
+
+    @debug_logger_init
+    def __init__(self, main: JaxMainTrace):
+        self.main, self.frames, self.mains, self.trace = main, {}, {}, None
+
+
+class EvaluationContext:
+    """Utility context managing class keeping track of various modes of Catalyst executions.
+
+    It is used to determine whether the program is currently tracing or not and if so, tracking the
+    tracing contexts. Contexts can be nested.
+    """
+
+    _tracing_stack: List[Tuple[EvaluationMode, Optional[JaxTracingContext]]] = []
+    _mlir_plugins: Set[Path] = set()
+
+    @debug_logger_init
+    def __init__(self, mode: EvaluationMode):
+        """Initialise a new instance of the Evaluation context.
+        Args:
+            mode: Evaluation mode of this instance
+        """
+        self.mode = mode
+        self.ctx = None
+
+    @classmethod
+    def add_plugin(cls, plugin: Path):
+        """Add an MLIR plugin to the set of MLIR plugins encountered in the
+        program"""
+        cls._mlir_plugins.add(plugin)
+
+    @classmethod
+    def get_plugins(cls):
+        """Get and reset all plugins encountered during the trace of the
+        program"""
+        retval = cls._mlir_plugins
+        cls._mlir_plugins = set()
+        return retval
+
+    @classmethod
+    @contextmanager
+    def _create_tracing_context(cls, mode) -> ContextManager[JaxTracingContext]:
+        with new_base_main(DynamicJaxprTrace, dynamic=True) as main:
+            main.jaxpr_stack = ()
+            cls._tracing_stack.append((mode, JaxTracingContext(main)))
+            try:
+                yield cls._tracing_stack[-1][1]
+            finally:
+                cls._tracing_stack.pop()
+
+    @classmethod
+    @contextmanager
+    def _create_interpretation_context(cls) -> ContextManager[JaxTracingContext]:
+        cls._tracing_stack.append((EvaluationMode.INTERPRETATION, None))
+        try:
+            yield cls._tracing_stack[-1][1]
+        finally:
+            cls._tracing_stack.pop()
+
+    @classmethod
+    @contextmanager
+    def frame_tracing_context(
+        cls, ctx: JaxTracingContext, trace: Optional[DynamicJaxprTrace] = None
+    ) -> ContextManager[DynamicJaxprTrace]:
+        """Start a new JAX tracing frame, e.g. to trace a region of some
+        :class:`~.jax_tracer.HybridOp`. Not applicable in non-tracing evaluation modes."""
+        assert ctx is cls._tracing_stack[-1][1], f"{ctx=}"
+        main = ctx.mains[trace] if trace is not None else None
+        with new_dynamic_main2(DynamicJaxprTrace, main=main) as nmain:
+            nmain.jaxpr_stack = ()
+            frame = JaxprStackFrame() if trace is None else ctx.frames[trace]
+            with extend_jaxpr_stack(nmain, frame), reset_name_stack():
+                parent_trace = ctx.trace
+                ctx.trace = DynamicJaxprTrace(nmain, cur_sublevel()) if trace is None else trace
+                ctx.frames[ctx.trace] = frame
+                ctx.mains[ctx.trace] = nmain
+                try:
+                    yield ctx.trace
+                finally:
+                    ctx.trace = parent_trace
+
+    @classmethod
+    def get_main_tracing_context(cls, hint=None) -> JaxTracingContext:
+        """Return the current JAX tracing context, raise an exception if not in tracing mode."""
+        msg = f"{hint or 'catalyst functions'} can only be used from within @qjit decorated code."
+        EvaluationContext.check_is_tracing(msg)
+        return cls._tracing_stack[-1][1]
+
+    def __enter__(self):
+        if self.mode in [EvaluationMode.QUANTUM_COMPILATION, EvaluationMode.CLASSICAL_COMPILATION]:
+            self.ctx = self._create_tracing_context(self.mode)
+        else:
+            assert self.mode in {EvaluationMode.INTERPRETATION}, f"Unknown mode {self.mode}"
+            self.ctx = self._create_interpretation_context()
+        return self.ctx.__enter__()
+
+    def __exit__(self, *args, **kwargs):
+        self.ctx.__exit__(*args, **kwargs)
+
+    @classmethod
+    def get_evaluation_mode(cls) -> Tuple[EvaluationMode, Optional[JaxTracingContext]]:
+        """Return the name of the evaluation mode, paired with tracing context if applicable"""
+        if not EvaluationContext._tracing_stack:
+            return (EvaluationMode.INTERPRETATION, None)
+        return cls._tracing_stack[-1]
+
+    @classmethod
+    def get_mode(cls):
+        """Return the name of current evaluation mode."""
+        return cls.get_evaluation_mode()[0]
+
+    @classmethod
+    def is_tracing(cls):
+        """Returns true or false depending on whether the execution is currently being
+        traced.
+        """
+        return cls.get_mode() in [
+            EvaluationMode.CLASSICAL_COMPILATION,
+            EvaluationMode.QUANTUM_COMPILATION,
+        ]
+
+    @classmethod
+    def is_quantum_tracing(cls):
+        """Returns true or false depending on whether the execution is currently being
+        traced.
+        """
+        return cls.get_mode() == EvaluationMode.QUANTUM_COMPILATION
+
+    @classmethod
+    def check_modes(cls, modes, msg):
+        """Asserts if the execution mode is not among the expected ``modes``.
+
+        Raises: CompileError
+        """
+        if cls.get_mode() not in modes:
+            raise CompileError(msg)
+
+    @classmethod
+    def check_is_quantum_tracing(cls, msg):
+        """Asserts if the current evaluation mode is quantum tracing.
+
+        Raises: CompileError
+        """
+        cls.check_modes([EvaluationMode.QUANTUM_COMPILATION], msg)
+
+    @classmethod
+    def check_is_classical_tracing(cls, msg):
+        """Asserts if the current evaluation mode is classical tracing.
+
+        Raises: CompileError
+        """
+        cls.check_modes([EvaluationMode.CLASSICAL_COMPILATION], msg)
+
+    @classmethod
+    def check_is_tracing(cls, msg):
+        """Asserts if the current evaluation mode is not a tracing.
+
+        Raises: CompileError
+        """
+        cls.check_modes(
+            [EvaluationMode.CLASSICAL_COMPILATION, EvaluationMode.QUANTUM_COMPILATION], msg
+        )
+
+    @classmethod
+    def check_is_not_tracing(cls, msg):
+        """Asserts if the current execution mode is a tracing.
+
+        Raises: CompileError
+        """
+        if cls.is_tracing():
+            raise CompileError(msg)
+
+    @classmethod
+    def find_jaxpr_frame(cls, *args):
+        """Obtain the current JAXPR frame, in which primitives are being inserted.
+
+        Raises: CompileError
+        """
+        cls.check_is_tracing("No JAXPR frames exist outside a tracing context.")
+        return find_top_trace(args).frame
+
+    @classmethod
+    def find_quantum_queue(cls):
+        """Obtain the current quantum queuing context, in which operations are being inserted.
+
+        Raises: CompileError
+        """
+        cls.check_is_quantum_tracing("No quantum queueing context found.")
+
+        queuing_context = QueuingManager.active_context()
+        assert queuing_context is not None
+
+        return queuing_context
---
+++ packages/PennyLane-Catalyst/pennylane_catalyst-0.11.0-cp311-cp311-macosx_13_0_arm64.whl/catalyst/utils/jax_cpu_lapack_kernels/lapack_kernels_using_lapack.cpp
@@ -0,0 +1,176 @@
+// Copyright 2024 Xanadu Quantum Technologies Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file has been modified from its original form in the JAX project at
+// https://github.com/google/jax released under the Apache License, Version 2.0,
+// with the following copyright notice:
+
+// Copyright 2021 The JAX Authors.
+
+/*
+ * This file is a modified version of
+ *
+ *   https://github.com/google/jax/blob/jaxlib-v0.4.28/jaxlib/cpu/lapack_kernels_using_lapack.cc
+ *
+ * from jaxlib-v0.4.28.
+ *
+ * See note in lapack_kernels.h for a high-level explanation of the
+ * modifications and the motivation for them. Specifically, the names of the
+ * BLAS and LAPACK routine symbols have been changed from the FORTRAN interfaces
+ * to the equivalent C interfaces. For example, the `dtrsm` BLAS routine has
+ * been changed from `dtrsm_` to `cblas_dtrsm`, and the `dgetrf` LAPACK routine
+ * has been changed from `dgetrf_` to `LAPACKE_dgetrf`.
+ */
+
+#include "lapack_kernels.hpp"
+
+// With SciPy OpenBLAS, symbols are now prefixed with scipy_ when using scipy>=1.14 or
+// scipy-openblas32, whereas this is not the case for the reference implementation used on M1 mac.
+#if defined(__APPLE__) && defined(__arm64__)
+#define SYM_PREFIX
+#else
+#define SYM_PREFIX scipy_
+#endif
+
+// The CONCAT macro and its helper CONCAT_ are required here since macro arguments are not expanded
+// around a ## preprocessing token. See http://port70.net/%7Ensz/c/c11/n1570.html#6.10.3.1.
+#define CONCAT_(X, Y) X##Y
+#define CONCAT(X, Y) CONCAT_(X, Y)
+#define GET_SYMBOL(X) CONCAT(SYM_PREFIX, X)
+
+// From a Python binary, JAX obtains its LAPACK/BLAS kernels from Scipy, but
+// a C++ user should link against LAPACK directly. This is needed when using
+// JAX-generated HLO from C++.
+
+extern "C" {
+
+jax::RealTrsm<float>::FnType GET_SYMBOL(cblas_strsm);
+jax::RealTrsm<double>::FnType GET_SYMBOL(cblas_dtrsm);
+jax::ComplexTrsm<std::complex<float>>::FnType GET_SYMBOL(cblas_ctrsm);
+jax::ComplexTrsm<std::complex<double>>::FnType GET_SYMBOL(cblas_ztrsm);
+
+jax::Getrf<float>::FnType GET_SYMBOL(LAPACKE_sgetrf);
+jax::Getrf<double>::FnType GET_SYMBOL(LAPACKE_dgetrf);
+jax::Getrf<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_cgetrf);
+jax::Getrf<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zgetrf);
+
+jax::Geqrf<float>::FnType GET_SYMBOL(LAPACKE_sgeqrf);
+jax::Geqrf<double>::FnType GET_SYMBOL(LAPACKE_dgeqrf);
+jax::Geqrf<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_cgeqrf);
+jax::Geqrf<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zgeqrf);
+
+jax::Orgqr<float>::FnType GET_SYMBOL(LAPACKE_sorgqr);
+jax::Orgqr<double>::FnType GET_SYMBOL(LAPACKE_dorgqr);
+jax::Orgqr<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_cungqr);
+jax::Orgqr<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zungqr);
+
+jax::Potrf<float>::FnType GET_SYMBOL(LAPACKE_spotrf);
+jax::Potrf<double>::FnType GET_SYMBOL(LAPACKE_dpotrf);
+jax::Potrf<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_cpotrf);
+jax::Potrf<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zpotrf);
+
+jax::RealGesdd<float>::FnType GET_SYMBOL(LAPACKE_sgesdd);
+jax::RealGesdd<double>::FnType GET_SYMBOL(LAPACKE_dgesdd);
+jax::ComplexGesdd<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_cgesdd);
+jax::ComplexGesdd<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zgesdd);
+
+jax::RealSyevd<float>::FnType GET_SYMBOL(LAPACKE_ssyevd);
+jax::RealSyevd<double>::FnType GET_SYMBOL(LAPACKE_dsyevd);
+jax::ComplexHeevd<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_cheevd);
+jax::ComplexHeevd<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zheevd);
+
+jax::RealGeev<float>::FnType GET_SYMBOL(LAPACKE_sgeev);
+jax::RealGeev<double>::FnType GET_SYMBOL(LAPACKE_dgeev);
+jax::ComplexGeev<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_cgeev);
+jax::ComplexGeev<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zgeev);
+
+jax::RealGees<float>::FnType GET_SYMBOL(LAPACKE_sgees);
+jax::RealGees<double>::FnType GET_SYMBOL(LAPACKE_dgees);
+jax::ComplexGees<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_cgees);
+jax::ComplexGees<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zgees);
+
+jax::Gehrd<float>::FnType GET_SYMBOL(LAPACKE_sgehrd);
+jax::Gehrd<double>::FnType GET_SYMBOL(LAPACKE_dgehrd);
+jax::Gehrd<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_cgehrd);
+jax::Gehrd<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zgehrd);
+
+jax::Sytrd<float>::FnType GET_SYMBOL(LAPACKE_ssytrd);
+jax::Sytrd<double>::FnType GET_SYMBOL(LAPACKE_dsytrd);
+jax::Sytrd<std::complex<float>>::FnType GET_SYMBOL(LAPACKE_chetrd);
+jax::Sytrd<std::complex<double>>::FnType GET_SYMBOL(LAPACKE_zhetrd);
+
+} // extern "C"
+
+namespace jax {
+
+static auto init = []() -> int {
+    RealTrsm<float>::fn = GET_SYMBOL(cblas_strsm);
+    RealTrsm<double>::fn = GET_SYMBOL(cblas_dtrsm);
+    ComplexTrsm<std::complex<float>>::fn = GET_SYMBOL(cblas_ctrsm);
+    ComplexTrsm<std::complex<double>>::fn = GET_SYMBOL(cblas_ztrsm);
+
+    Getrf<float>::fn = GET_SYMBOL(LAPACKE_sgetrf);
+    Getrf<double>::fn = GET_SYMBOL(LAPACKE_dgetrf);
+    Getrf<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_cgetrf);
+    Getrf<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zgetrf);
+
+    Geqrf<float>::fn = GET_SYMBOL(LAPACKE_sgeqrf);
+    Geqrf<double>::fn = GET_SYMBOL(LAPACKE_dgeqrf);
+    Geqrf<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_cgeqrf);
+    Geqrf<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zgeqrf);
+
+    Orgqr<float>::fn = GET_SYMBOL(LAPACKE_sorgqr);
+    Orgqr<double>::fn = GET_SYMBOL(LAPACKE_dorgqr);
+    Orgqr<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_cungqr);
+    Orgqr<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zungqr);
+
+    Potrf<float>::fn = GET_SYMBOL(LAPACKE_spotrf);
+    Potrf<double>::fn = GET_SYMBOL(LAPACKE_dpotrf);
+    Potrf<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_cpotrf);
+    Potrf<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zpotrf);
+
+    RealGesdd<float>::fn = GET_SYMBOL(LAPACKE_sgesdd);
+    RealGesdd<double>::fn = GET_SYMBOL(LAPACKE_dgesdd);
+    ComplexGesdd<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_cgesdd);
+    ComplexGesdd<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zgesdd);
+
+    RealSyevd<float>::fn = GET_SYMBOL(LAPACKE_ssyevd);
+    RealSyevd<double>::fn = GET_SYMBOL(LAPACKE_dsyevd);
+    ComplexHeevd<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_cheevd);
+    ComplexHeevd<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zheevd);
+
+    RealGeev<float>::fn = GET_SYMBOL(LAPACKE_sgeev);
+    RealGeev<double>::fn = GET_SYMBOL(LAPACKE_dgeev);
+    ComplexGeev<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_cgeev);
+    ComplexGeev<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zgeev);
+
+    RealGees<float>::fn = GET_SYMBOL(LAPACKE_sgees);
+    RealGees<double>::fn = GET_SYMBOL(LAPACKE_dgees);
+    ComplexGees<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_cgees);
+    ComplexGees<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zgees);
+
+    Gehrd<float>::fn = GET_SYMBOL(LAPACKE_sgehrd);
+    Gehrd<double>::fn = GET_SYMBOL(LAPACKE_dgehrd);
+    Gehrd<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_cgehrd);
+    Gehrd<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zgehrd);
+
+    Sytrd<float>::fn = GET_SYMBOL(LAPACKE_ssytrd);
+    Sytrd<double>::fn = GET_SYMBOL(LAPACKE_dsytrd);
+    Sytrd<std::complex<float>>::fn = GET_SYMBOL(LAPACKE_chetrd);
+    Sytrd<std::complex<double>>::fn = GET_SYMBOL(LAPACKE_zhetrd);
+
+    return 0;
+}();
+
+} // namespace jax
