b706f970010b14dc08f89d85204befee15bb340e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 07:04:26 +0000

    Add package linalgplus/0.1.1/linalgplus-0.1.1.tar.gz

---
+++ packages/linalgplus/linalgplus-0.1.1.tar.gz/linalgplus-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='linalgplus',  # lowercase, no spaces
+    version='0.1.1',
+    author='Your Name',
+    author_email='you@example.com',
+    description='A lightweight linear algebra library for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/linalgplus',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
