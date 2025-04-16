2e108d3d1bc1ba6a20e86eaef6ee3c6fb0758a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:24 +0000

    Add package willphanpy/0.2.0/willphanpy-0.2.0-py3-none-any.whl

---
+++ packages/willphanpy/willphanpy-0.2.0-py3-none-any.whl/willphanpy/__init__.py
@@ -0,0 +1,8 @@
+
+from .Algorithms.Wpya import Wpya
+from .DataStructure.Wpyd import Wpyd
+from .Aerodynamics.Wpyaero import Wpyaero
+from .MachineLearning.Wpyml import Wpyml
+from .Testing.TeseCase import TestCase
+
+__all__ = ["Wpya", "Wpyd", "Wpyaero", "Wpyml", "TestCase"]
