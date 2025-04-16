c7f129121cd61173c79808949961a0b5fce824b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:04:50 +0000

    Add package sayn/0.6.15/sayn-0.6.15-py3-none-any.whl

---
+++ packages/sayn/sayn-0.6.15-py3-none-any.whl/sayn/__init__.py
@@ -0,0 +1,11 @@
+# from importlib.metadata import version
+#
+# try:
+#     __version__ = version(__name__)
+# except:
+#     pass
+
+__version__ = "0.6.15"
+
+
+from .tasks.python import PythonTask, task
