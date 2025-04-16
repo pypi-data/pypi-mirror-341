cd0e790e914487257ed7c8fda937d3ced0f4b158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 14:04:48 +0000

    Add package sbcli-pool/1.0.2/sbcli_pool-1.0.2.tar.gz

---
+++ packages/sbcli-pool/sbcli_pool-1.0.2.tar.gz/sbcli_pool-1.0.2/simplyblock_core/scripts/clean_local_storage_deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo docker rm spdk --force
+sudo docker rm spdk_proxy --force
+sudo docker rm SNodeAPI --force
+sudo docker rm CachingNodeAPI --force
+sudo docker stack rm app
+sudo docker swarm leave --force
+sudo docker container prune -f
+#sudo docker image prune -a -f
+sudo docker volume prune -a -f
+sudo service docker restart
+echo "Done"
+sudo rm -rf /etc/foundationdb/data/
