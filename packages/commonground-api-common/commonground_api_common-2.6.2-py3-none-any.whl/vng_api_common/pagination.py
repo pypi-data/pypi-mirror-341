60f417bd32da05a58d571a6a8773e795cff3e040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:04:50 +0000

    Add package commonground-api-common/2.6.2/commonground_api_common-2.6.2-py3-none-any.whl

---
+++ packages/commonground-api-common/commonground_api_common-2.6.2-py3-none-any.whl/vng_api_common/pagination.py
@@ -0,0 +1,18 @@
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework.pagination import PageNumberPagination
+
+# These strings are defined by rest_framework.pagination, but never translated
+FORCE_TRANSLATION_STRINGS = [
+    _("A page number within the paginated result set."),
+    _("Number of results to return per page."),
+]
+
+
+class DynamicPageSizeMixin:
+    page_size = 100
+    page_size_query_param = "pageSize"
+    max_page_size = 500
+
+
+class DynamicPageSizePagination(DynamicPageSizeMixin, PageNumberPagination): ...
