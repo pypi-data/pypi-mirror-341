d4ca9d0903bcb35a660612655c02e6964a448564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:25 +0000

    Add package py-thanos-snap/0.1.0/py_thanos_snap-0.1.0-py3-none-any.whl

---
+++ packages/py-thanos-snap/py_thanos_snap-0.1.0-py3-none-any.whl/thanospy/__init__.py
@@ -0,0 +1,15 @@
+# thanospy/__init__.py
+"""
+ThanosPy: Perfectly balanced data structures... as all things should be.
+
+Provides a function to randomly remove half the content from various
+Python built-in types.
+"""
+from .core import snap
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "snap",
+    "__version__",
+]
