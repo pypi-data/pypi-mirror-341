b137a4bfd55ca8616907822b85cbc0448c3f0077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:33 +0000

    Add package flexget/3.15.33/flexget-3.15.33.tar.gz

---
+++ packages/flexget/flexget-3.15.33.tar.gz/flexget-3.15.33/flexget/__init__.py
@@ -0,0 +1,79 @@
+import sys
+from typing import TYPE_CHECKING, Optional
+
+# __version__ import need to be first in order to avoid circular import within logger
+from ._version import __version__  # noqa: F401
+
+# isort: split
+from flexget import log
+from flexget.manager import Manager
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
+def main(args: Optional['Sequence[str]'] = None):
+    """Execute as the main entry point for Command Line Interface."""
+    if args is None:
+        args = sys.argv[1:]
+    try:
+        log.initialize()
+
+        try:
+            manager = Manager(args)
+        except (OSError, ValueError) as e:
+            options = Manager.parse_initial_options(args)
+            log.start(level=options.loglevel, to_file=False)
+            if _is_debug():
+                import traceback
+
+                traceback.print_exc()
+            else:
+                print(f'Could not instantiate manager: {e}', file=sys.stderr)
+            sys.exit(1)
+        else:
+            log.start(
+                manager.log_filename,
+                manager.options.loglevel,
+                to_file=manager.check_ipc_info() is None,
+                to_console=not manager.options.cron,
+            )
+
+        try:
+            if manager.options.profile:
+                try:
+                    import cProfile as profile  # noqa: N813
+                except ImportError:
+                    import profile
+                profile.runctx(
+                    'manager.start()',
+                    globals(),
+                    locals(),
+                    str(manager.config_base / manager.options.profile),
+                )
+            else:
+                manager.start()
+        except (OSError, ValueError) as e:
+            if _is_debug():
+                import traceback
+
+                traceback.print_exc()
+            else:
+                print(f'Could not start manager: {e}', file=sys.stderr)
+
+            sys.exit(1)
+    except KeyboardInterrupt:
+        if _is_debug():
+            import traceback
+
+            traceback.print_exc()
+
+        print('Killed with keyboard interrupt.', file=sys.stderr)
+        sys.exit(1)
+
+
+def _is_debug() -> bool:
+    return any(
+        arg in ['debug', '--debug', '--loglevel=trace', '--loglevel=debug']
+        for arg in [a.lower() for a in sys.argv]
+    )
