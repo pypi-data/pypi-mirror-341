7b72e3fafaa4c37d07c2e9ab3d0ab759e88fc838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:17 +0000

    Add package pdfplucker/0.3.6/pdfplucker-0.3.6.tar.gz

---
+++ packages/pdfplucker/pdfplucker-0.3.6.tar.gz/pdfplucker-0.3.6/pdfplucker/__init__.py
@@ -0,0 +1,16 @@
+"""
+pdfplucker - a powerful wrapper for the Docling library
+"""
+
+__version__ = "0.3.6"
+
+from pdfplucker.processor import process_batch, process_pdf, create_converter
+from pdfplucker.utils import format_result, link_subtitles
+
+__all__ = [
+    "process_batch",
+    "process_pdf",
+    "create_converter",
+    "format_result",
+    "link_subtitles"
+]
