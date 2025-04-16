59616538d50d8fa5f0e146ea26138c8e1cc9d98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:19:22 +0000

    Add package luxonis-train/0.3.5/luxonis_train-0.3.5.tar.gz

---
+++ packages/luxonis-train/luxonis_train-0.3.5.tar.gz/luxonis_train-0.3.5/tests/integration/test_cli_commands.py
@@ -0,0 +1,88 @@
+import os
+from collections.abc import Callable
+from pathlib import Path
+
+import pytest
+from luxonis_ml.data import LuxonisDataset
+from luxonis_ml.typing import Kwargs
+from luxonis_ml.utils import environ
+
+from luxonis_train.__main__ import (
+    archive,
+    export,
+    inspect,
+    train,
+)
+from luxonis_train.__main__ import test as _test
+
+ONNX_PATH = Path("tests/integration/client_commands_test_model.onnx")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prepare():
+    os.environ["LUXONISML_BASE_PATH"] = str(environ.LUXONISML_BASE_PATH)
+    yield
+    ONNX_PATH.unlink(missing_ok=True)
+
+
+@pytest.mark.parametrize(
+    ("command", "kwargs"),
+    [
+        (train, {"config": "tests/configs/config_simple.yaml"}),
+        (
+            _test,
+            {
+                "config": "tests/configs/config_simple.yaml",
+                "view": "val",
+            },
+        ),
+        (
+            export,
+            {
+                "config": "tests/configs/config_simple.yaml",
+                "save_path": ONNX_PATH.parent,
+            },
+        ),
+        (
+            archive,
+            {
+                "config": "tests/configs/config_simple.yaml",
+                "executable": ONNX_PATH,
+            },
+        ),
+    ],
+)
+def test_cli_command_success(
+    command: Callable, kwargs: Kwargs, coco_dataset: LuxonisDataset
+) -> None:
+    command(["loader.params.dataset_name", coco_dataset.identifier], **kwargs)
+
+
+@pytest.mark.parametrize(
+    ("command", "kwargs"),
+    [
+        (train, {"config": "nonexistent.yaml"}),
+        (
+            _test,
+            {"config": "tests/configs/config_simple.yaml", "view": "invalid"},
+        ),
+        (export, {"config": "nonexistent.yaml"}),
+        (
+            inspect,
+            {
+                "config": "nonexistent.yaml",
+                "view": "train",
+                "size_multiplier": -1.0,
+            },
+        ),
+        (archive, {"config": "nonexistent.yaml"}),
+    ],
+)
+def test_cli_command_failure(
+    command: Callable, kwargs: Kwargs, coco_dataset: LuxonisDataset
+) -> None:
+    with pytest.raises(Exception):  # noqa: PT011
+        command(
+            ["loader.params.dataset_name", coco_dataset.identifier],
+            **kwargs,
+        )
