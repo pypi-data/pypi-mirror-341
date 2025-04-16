5167d542ffc8f6c9729a538b1c50c616f816251a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:05:38 +0000

    Add package chatbot-session-flow/0.1.12/chatbot_session_flow-0.1.12.tar.gz

---
+++ packages/chatbot-session-flow/chatbot_session_flow-0.1.12.tar.gz/chatbot_session_flow-0.1.12/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chatbot_session_flow",  # Unique package name
+    version="0.1.12",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Django>=4.1.2"
+    ],
+    author="Dennis Kamau",
+    author_email="kamadennis05@gmail.com",
+    description="A django Whatsapp chatbot session manager",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/DK-denno/whatsapp-chatbot.git",  # Your repository
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
