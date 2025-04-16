562bb48a2185087fafe5496b19191d645bc16ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:05:20 +0000

    Add package numpyUtilsUpdated/0.0.3/numpyutilsupdated-0.0.3-py3-none-any.whl

---
+++ packages/numpyUtilsUpdated/numpyutilsupdated-0.0.3-py3-none-any.whl/numpyUtilsUpdated/ai/__init__.py
@@ -0,0 +1,19 @@
+files = '''
+ AlphaBetaPr 
+ BFS_8pz 
+ DFS_8pz 
+ DLimitS_8pz 
+ GBFS 
+ GraphColor 
+ Minimax 
+ Prologs 
+ ticTacToe 
+ UCS 
+ VaccumCleaner 
+ waterJug 
+ Wumpus 
+
+'''
+
+print(f"[ai] Modules: {files}")
+print("example usage: `import numpyUtilsNani.ai.{Module}.getcode()`....")
