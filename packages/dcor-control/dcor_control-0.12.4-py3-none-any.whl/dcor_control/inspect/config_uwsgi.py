5afcedccceb6fd47b0c55c91715c2d4d63df62f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:05:01 +0000

    Add package dcor-control/0.12.4/dcor_control-0.12.4-py3-none-any.whl

---
+++ packages/dcor-control/dcor_control-0.12.4-py3-none-any.whl/dcor_control/inspect/config_uwsgi.py
@@ -0,0 +1,40 @@
+from dcor_shared.paths import get_uwsgi_config_path
+
+from .common import ask
+
+
+def check_uwsgi(harakiri, autocorrect=False):
+    """Set harakiri timeout of uwsgi (important for data upload)
+
+    Parameters
+    ----------
+    harakiri: int
+        uwsgi timeout in minutes
+    """
+    did_something = 0
+    path_uwsgi = get_uwsgi_config_path()
+    with open(path_uwsgi) as fd:
+        lines = fd.readlines()
+    for ii, line in enumerate(lines):
+        data = line.split("=", 1)
+        if len(data) < 2:
+            continue
+        else:
+            key, value = data
+            key = key.strip()
+        if key == "harakiri":
+            value = int(value)
+            if value != harakiri:
+                if autocorrect:
+                    change = True
+                    print("Setting UWSGI harakiri to {} min".format(harakiri))
+                else:
+                    change = ask(
+                        "UWSGI timeout should be '{}' min".format(harakiri)
+                        + ", but is '{}' min".format(value))
+                if change:
+                    did_something += 1
+                    lines[ii] = line.replace(str(value), str(harakiri))
+                    with open(path_uwsgi, "w") as fd:
+                        fd.writelines(lines)
+    return did_something
