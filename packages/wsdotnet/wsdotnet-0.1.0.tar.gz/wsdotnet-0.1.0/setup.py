b359b5340bb41efd097cceb4a31a4b7ff8bd593c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:27 +0000

    Add package wsdotnet/0.1.0/wsdotnet-0.1.0.tar.gz

---
+++ packages/wsdotnet/wsdotnet-0.1.0.tar.gz/wsdotnet-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wsdotnet',                     # Your package name
+    version='0.1.0',                    # Version
+    description='Define a simple service like Converting Rs into Dollar and Call it from different platform like JAVA and .NET',
+    author='Aryan Bhagwat',
+    author_email='legendasur531@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
