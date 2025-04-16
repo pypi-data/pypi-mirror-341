b27169bff7333287e32a02df451b09da87b74925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:19:13 +0000

    Add package artistools/2025.4.15/artistools-2025.4.15-cp311-abi3-macosx_13_0_arm64.whl

---
+++ packages/artistools/artistools-2025.4.15-cp311-abi3-macosx_13_0_arm64.whl/artistools/nonthermal/__init__.py
@@ -0,0 +1,7 @@
+"""Artistools - spectra related functions."""
+
+__all__ = ["plot"]
+
+from artistools.nonthermal import solvespencerfanocmd as solvespencerfanocmd
+from artistools.nonthermal.plotnonthermal import addargs as addargs
+from artistools.nonthermal.plotnonthermal import main as plot
