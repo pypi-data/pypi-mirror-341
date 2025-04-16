588f3a81dbce94f4ac28a514fc9ebb303c3972e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:04:52 +0000

    Add package odoo-addon-web-widget-remote-measure/15.0.1.1.0.2/odoo_addon_web_widget_remote_measure-15.0.1.1.0.2-py3-none-any.whl

---
+++ packages/odoo-addon-web-widget-remote-measure/odoo_addon_web_widget_remote_measure-15.0.1.1.0.2-py3-none-any.whl/odoo/addons/web_widget_remote_measure/models/res_users.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    remote_measure_device_id = fields.Many2one(
+        comodel_name="remote.measure.device",
+        help="Default remote measure device for this user",
+    )
+
+    def action_close_remote_device_wizard(self):
+        return {
+            "type": "ir.actions.act_window_close",
+        }
