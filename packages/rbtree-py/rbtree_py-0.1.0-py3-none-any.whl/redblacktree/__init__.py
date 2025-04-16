6790fa7b0f45963f8fb350666b51247a77582ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:14 +0000

    Add package rbtree-py/0.1.0/rbtree_py-0.1.0-py3-none-any.whl

---
+++ packages/rbtree-py/rbtree_py-0.1.0-py3-none-any.whl/redblacktree/__init__.py
@@ -0,0 +1,15 @@
+# redblacktree/__init__.py
+"""
+A Python implementation of a Red-Black Tree data structure.
+"""
+from .tree import RedBlackTree, RBNode, RED, BLACK
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "RedBlackTree",
+    "RBNode",
+    "RED",
+    "BLACK",
+    "__version__",
+]
