1b5f43347424dc78c9cb391f4d472816344648f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:06:21 +0000

    Add package dash-mantine-components/1.1.2rc1/dash_mantine_components-1.1.2rc1-py3-none-any.whl

---
+++ packages/dash-mantine-components/dash_mantine_components-1.1.2rc1-py3-none-any.whl/dash_mantine_components/styles.py
@@ -0,0 +1,17 @@
+DATES = "https://unpkg.com/@mantine/dates@7.17.4/styles.css"
+CODE_HIGHLIGHT = "https://unpkg.com/@mantine/code-highlight@7.17.4/styles.css"
+CHARTS = "https://unpkg.com/@mantine/charts@7.17.4/styles.css"
+CAROUSEL = "https://unpkg.com/@mantine/carousel@7.17.4/styles.css"
+NOTIFICATIONS = "https://unpkg.com/@mantine/notifications@7.17.4/styles.css"
+NPROGRESS = "https://unpkg.com/@mantine/nprogress@7.17.4/styles.css"
+RICH_TEXT_EDITOR = "https://unpkg.com/@mantine/tiptap@7.17.4/styles.css"
+
+ALL = [
+    DATES,
+    CODE_HIGHLIGHT,
+    CHARTS,
+    CAROUSEL,
+    NOTIFICATIONS,
+    NPROGRESS,
+    RICH_TEXT_EDITOR,
+]
