e22b6db75dd3a5f4c9bfcc5bc4ed4725f6d584e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:05:14 +0000

    Add package pulumi-easy/0.16.0/pulumi_easy-0.16.0.tar.gz

---
+++ packages/pulumi-easy/pulumi_easy-0.16.0.tar.gz/pulumi_easy-0.16.0/pulumi_easy/aws/ec2/__init__.py
@@ -0,0 +1,7 @@
+"""EC2 module for Pulumi Easy"""
+
+# Re-export EC2Manager class for direct imports
+from .ec2 import EC2Manager
+from .ec2_ubuntu import EC2Ubuntu
+from .ec2_al import EC2AL
+from .vpc import EC2VPC
