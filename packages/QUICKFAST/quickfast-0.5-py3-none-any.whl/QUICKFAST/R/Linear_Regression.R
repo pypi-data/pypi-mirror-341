18eb989fc39c4eba55cd5c2ad6130243940e5179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:04:45 +0000

    Add package QUICKFAST/0.5/quickfast-0.5-py3-none-any.whl

---
+++ packages/QUICKFAST/quickfast-0.5-py3-none-any.whl/QUICKFAST/R/Linear_Regression.R
@@ -0,0 +1,35 @@
+X<-c(152,156,159,163,165)
+Y<-c(63,68,59,71,64)
+X
+Y
+relationYX<-lm(Y~X)
+print(relationYX)
+
+relationXY<-lm(X~Y)
+print(relationXY)
+
+summaryXY=summary(relationXY)
+print(summaryXY)
+
+summaryYX=summary(relationYX)
+print(summaryYX)
+
+ print(summaryYX)
+
+a=data.frame(X=173)
+result=predict(relationYX,a)
+print(paste("Weight of the student when Height=173 is ",result," kg"))
+b=data.frame(Y=70)
+result1=predict(relationXY,b)
+
+print(paste("Height of the student when weight=70 is ",result1," cm"))
+plot(Y,X,col="blue",main="Height and Weight of Students",xlab="Weight",ylab="Height",pch=15,abline(lm(X~Y)))
+
+
+
+a=mtcars[c("cyl","wt","am","gear")]
+d=head(a)
+d
+b<-glm(formula=am~cyl+wt,data=a,family=binomial)
+c<- summary(b)
+c
