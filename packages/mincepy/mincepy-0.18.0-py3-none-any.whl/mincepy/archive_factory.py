20986c03f2b1f72f1b5d7217b346c218ada84b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:24 +0000

    Add package mincepy/0.18.0/mincepy-0.18.0-py3-none-any.whl

---
+++ packages/mincepy/mincepy-0.18.0-py3-none-any.whl/mincepy/archive_factory.py
@@ -0,0 +1,39 @@
+import logging
+import os
+from typing import TYPE_CHECKING, Optional, Union
+from urllib import parse
+
+from . import mongo
+
+if TYPE_CHECKING:
+    import mincepy
+
+__all__ = (
+    "create_archive",
+    "DEFAULT_ARCHIVE_URI",
+    "ENV_ARCHIVE_URI",
+    "default_archive_uri",
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_ARCHIVE_URI = "mongodb://localhost/mincepy"
+ENV_ARCHIVE_URI = "MINCEPY_ARCHIVE"
+
+
+def default_archive_uri() -> Optional[str]:
+    """Returns the default archive URI.  This is currently being taken from the environmental
+    MINCEPY_ARCHIVE, however it may chance to include a config file in the future."""
+    return os.environ.get(ENV_ARCHIVE_URI, DEFAULT_ARCHIVE_URI)
+
+
+def create_archive(uri: Union[str, parse.ParseResult], connect_timeout=30000) -> "mincepy.Archive":
+    """Create an archive type based on a URI string
+
+    :param uri: the specification of where to connect to
+    :param connect_timeout: a connection timeout (in milliseconds)
+    """
+    archive = mongo.connect(uri, timeout=connect_timeout)
+
+    _LOGGER.info("Connected to archive with uri: %s", uri)
+    return archive
---
+++ packages/mincepy/mincepy-0.18.0-py3-none-any.whl/mincepy/process.py
@@ -0,0 +1,41 @@
+import contextlib
+import uuid
+
+from . import base_savable, tracking
+
+__all__ = ("Process",)
+
+
+class Process(
+    base_savable.SimpleSavable, type_id=uuid.UUID("bcf03171-a1f1-49c7-b890-b7f9d9f9e5a2")
+):
+    STACK = []
+    ATTRS = "_name", "_running"
+
+    def __init__(self, name: str):
+        super().__init__()
+        self._name = name
+        self._running = 0
+
+    def __eq__(self, other):
+        if not isinstance(other, Process):
+            return False
+
+        return self.name == other.name
+
+    @property
+    def is_running(self):
+        return self._running != 0
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @contextlib.contextmanager
+    def running(self):
+        try:
+            self._running += 1
+            with tracking.track(self):
+                yield
+        finally:
+            self._running -= 1
