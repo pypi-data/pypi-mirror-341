0ac71b3bcda05d6390abed9936ed2b71ddcb7aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:05:04 +0000

    Add package encommon/0.22.10/encommon-0.22.10.tar.gz

---
+++ packages/encommon/encommon-0.22.10.tar.gz/encommon-0.22.10/encommon/types/types.py
@@ -0,0 +1,20 @@
+"""
+Functions and routines associated with Enasis Network Common Library.
+
+This file is part of Enasis Network software eco-system. Distribution
+is permitted, for more information consult the project license file.
+"""
+
+
+
+from typing import Any
+
+
+
+DictStrAny = dict[str, Any]
+
+
+
+NCTrue = True
+NCFalse = False
+NCNone = None
