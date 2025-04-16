c348f05fbcf4e490f931e4bd299ce56cc3a3bef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 14:04:56 +0000

    Add package sindre/2025.4.15/sindre-2025.4.15-py3-none-any.whl

---
+++ packages/sindre/sindre-2025.4.15-py3-none-any.whl/sindre/utils3d/__init__.py
@@ -0,0 +1,6 @@
+from sindre.utils3d.augment import *
+from sindre.utils3d.dental_tools import *
+from sindre.utils3d.mesh import *
+from sindre.utils3d.algorithm import *
+from sindre.utils3d.vedo_tools import *
+ 
