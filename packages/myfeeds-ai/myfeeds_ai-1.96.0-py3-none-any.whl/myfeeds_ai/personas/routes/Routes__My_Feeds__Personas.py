f0d8e7ad43b289ed487b6f7d5ecc4e21c9e7c87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:41 +0000

    Add package myfeeds-ai/1.96.0/myfeeds_ai-1.96.0-py3-none-any.whl

---
+++ packages/myfeeds-ai/myfeeds_ai-1.96.0-py3-none-any.whl/myfeeds_ai/personas/routes/Routes__My_Feeds__Personas.py
@@ -0,0 +1,44 @@
+from fastapi                                                                        import Response
+from osbot_fast_api.api.Fast_API_Routes                                             import Fast_API_Routes
+from starlette                                                                      import status
+from myfeeds_ai.data_feeds.Data_Feeds__S3__Key_Generator                            import S3_Key__File__Content_Type
+from myfeeds_ai.personas.actions.My_Feeds__Persona                                  import My_Feeds__Persona
+from myfeeds_ai.personas.schemas.Schema__Persona__Types                             import Schema__Persona__Types
+
+ROUTE_PATH__PERSONAS = 'personas'
+
+ROUTES_PATHS__MY_FEEDS__PERSONAS = [f'/{ROUTE_PATH__PERSONAS}/persona'                      ,
+                                    f'/{ROUTE_PATH__PERSONAS}/persona-png'                  ,
+                                    f'/{ROUTE_PATH__PERSONAS}/persona-tree'                 ]
+
+class Routes__My_Feeds__Personas(Fast_API_Routes):
+    tag          : str = ROUTE_PATH__PERSONAS
+
+    def persona(self, persona_type: Schema__Persona__Types):
+        with My_Feeds__Persona(persona_type=persona_type) as _:
+            if _.exists():
+                return _.data()
+            else:
+                return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+
+    def persona_png(self, persona_type: Schema__Persona__Types):
+        png_bytes = My_Feeds__Persona(persona_type=persona_type).persona__entities__png()
+        if png_bytes:
+            return Response(content=png_bytes, media_type=str(S3_Key__File__Content_Type.PNG))
+        else:
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    def persona_tree(self, persona_type: Schema__Persona__Types):
+        tree_values = My_Feeds__Persona(persona_type=persona_type).persona__entities__tree_values()
+        if tree_values:
+            return Response(content=tree_values, media_type=str(S3_Key__File__Content_Type.TXT))
+        else:
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+    def setup_routes(self):
+        self.add_route_get   (self.persona                      )
+        self.add_route_get   (self.persona_png                  )
+        self.add_route_get   (self.persona_tree                 )
