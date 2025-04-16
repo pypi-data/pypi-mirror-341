290ca21d64df7177ebb21f9a02b51e4dd3e6e4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:32 +0000

    Add package pytempl-icons/0.2.1/pytempl_icons-0.2.1-py3-none-any.whl

---
+++ packages/pytempl-icons/pytempl_icons-0.2.1-py3-none-any.whl/pytempl_icons/__init__.py
@@ -0,0 +1,51 @@
+from ._base import BaseSVGIconElement, SVGIconAttributes
+from .abstract import (
+    CheckCircleBigIcon,
+    CheckIcon,
+    CirclePlusIcon,
+    CrossIcon,
+    DotFilledIcon,
+    EllipsisIcon,
+    ExclamationTriangleIcon,
+    HamburgerMenuIcon,
+    LoaderCircleIcon,
+    PlusIcon,
+    RefreshCWIcon,
+)
+from .arrows import (
+    ArrowLeftIcon,
+    ArrowRightIcon,
+    ChevronDownIcon,
+    ChevronLeftIcon,
+    ChevronRightIcon,
+    ChevronUpIcon,
+)
+from .objects import BanknoteIcon, EyeIcon, EyeOffIcon, PencilIcon, TrashIcon
+
+__version__ = "0.2.1"
+__all__ = [
+    "ArrowRightIcon",
+    "ArrowLeftIcon",
+    "BanknoteIcon",
+    "BaseSVGIconElement",
+    "CheckCircleBigIcon",
+    "CheckIcon",
+    "ChevronDownIcon",
+    "ChevronLeftIcon",
+    "ChevronRightIcon",
+    "ChevronUpIcon",
+    "CirclePlusIcon",
+    "CrossIcon",
+    "DotFilledIcon",
+    "EllipsisIcon",
+    "ExclamationTriangleIcon",
+    "EyeIcon",
+    "EyeOffIcon",
+    "HamburgerMenuIcon",
+    "LoaderCircleIcon",
+    "PencilIcon",
+    "PlusIcon",
+    "RefreshCWIcon",
+    "SVGIconAttributes",
+    "TrashIcon",
+]
