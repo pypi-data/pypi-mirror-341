741ef628ed31441bcd864494c3867e8a34223845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:06:00 +0000

    Add package configserver-py/1.0.3/configserver_py-1.0.3.tar.gz

---
+++ packages/configserver-py/configserver_py-1.0.3.tar.gz/configserver_py-1.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="configserver-py",
+    version="1.0.3",
+    description="A Python library for consuming Spring Cloud Config Server and managing configurations.",
+    author="Luis Martinez",
+    author_email="linuxlondo1211@gmail.com",
+    url="https://github.com/your-repo/backstage-lib",
+    packages=find_packages(),
+    install_requires=[
+        "pyyaml",
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
