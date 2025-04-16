e2c533e9dbe87fe4aca56e05fe3b9bc68f04ec58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:04:50 +0000

    Add package sculptor/0.2.0/sculptor-0.2.0.tar.gz

---
+++ packages/sculptor/sculptor-0.2.0.tar.gz/sculptor-0.2.0/src/sculptor/__init__.py
@@ -0,0 +1,9 @@
+raise ImportError(
+    "\n"
+    "The 'sculptor' package has been renamed to 'sculpt'.\n"
+    "Please uninstall the old package and install the new one:\n\n"
+    "    pip uninstall sculptor\n"
+    "    pip install sculpt\n\n"
+    "Then update your imports:\n\n"
+    "    from sculpt import ...\n"
+)
