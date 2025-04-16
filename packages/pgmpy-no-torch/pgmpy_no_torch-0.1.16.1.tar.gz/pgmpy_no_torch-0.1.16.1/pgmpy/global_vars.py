de2fae1a4ccaf4b3a0cdf04bb179c89de8619624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:28 +0000

    Add package pgmpy-no-torch/0.1.16.1/pgmpy_no_torch-0.1.16.1.tar.gz

---
+++ packages/pgmpy-no-torch/pgmpy_no_torch-0.1.16.1.tar.gz/pgmpy_no_torch-0.1.16.1/pgmpy/global_vars.py
@@ -0,0 +1,28 @@
+# TODO: This variables being set in this file should move to setup.py
+
+torch = None
+device = None
+dtype = None
+
+
+# This module initializes flags for optional dependencies
+try:
+    import pandas
+
+    HAS_PANDAS = True
+except ImportError:  # pragma: no cover
+    HAS_PANDAS = False
+    pandas = None
+
+
+# Set a global variable whether to show progress bar or not.
+SHOW_PROGRESS = True
+
+
+def no_progress():
+    """
+    If called sets the global variable `SHOW_PROGRESS` to False resulting in no
+    progress bars anywhere.
+    """
+    global SHOW_PROGRESS
+    SHOW_PROGRESS = False
