c78de31e24c2f92feb928aba8a81a407d13edf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:19:30 +0000

    Add package textura/1.0.0/textura-1.0.0-py3-none-any.whl

---
+++ packages/textura/textura-1.0.0-py3-none-any.whl/textura/__init__.py
@@ -0,0 +1,21 @@
+from .textura import (
+    Fore,
+    Back,
+    Style,
+    txtra_print,
+    txtra_format,
+    option,
+    colors_list,
+    PrintError,
+)
+
+__all__ = [
+    "Fore",
+    "Back",
+    "Style",
+    "txtra_print",
+    "txtra_format",
+    "option",
+    "colors_list",
+    "PrintError",
+]
