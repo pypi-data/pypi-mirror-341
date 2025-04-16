e9f6ed3f545dbfb404584a1917e058a0fffd10f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:05:20 +0000

    Add package textcolor-v2/0.0.1/textcolor_v2-0.0.1.tar.gz

---
+++ packages/textcolor-v2/textcolor_v2-0.0.1.tar.gz/textcolor_v2-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='textcolor_v2',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+    include_package_data=True,
+)
