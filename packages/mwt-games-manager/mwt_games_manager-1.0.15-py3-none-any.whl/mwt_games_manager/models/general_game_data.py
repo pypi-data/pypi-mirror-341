52b1a4a54d5287be87b12f1fd85310efe096b907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:05:44 +0000

    Add package mwt-games-manager/1.0.15/mwt_games_manager-1.0.15-py3-none-any.whl

---
+++ packages/mwt-games-manager/mwt_games_manager-1.0.15-py3-none-any.whl/mwt_games_manager/models/general_game_data.py
@@ -0,0 +1,7 @@
+class GeneralGameData:
+    def __init__(self, game_name="", score=0, date_started=None, wins=0, loses=0):
+        self.game_name = game_name
+        self.score = score
+        self.date_started = date_started
+        self.wins = wins
+        self.loses = loses
