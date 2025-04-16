46875c508fc16088b94580d939d33d9de89036d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:05:39 +0000

    Add package SG21038Uno/1.0/sg21038uno-1.0.tar.gz

---
+++ packages/SG21038Uno/sg21038uno-1.0.tar.gz/sg21038uno-1.0/README.md
@@ -0,0 +1,21 @@
+#  Libreria Ecuaciones
+
+Esta es una libreria para resolver sistemas de ecuaciones lineales y no lineales
+
+## Metodos incluidos
+
+### Lineales:
+- Gauss
+- Gauss-Jordan
+- Cramer
+- LU
+- Jacobi
+- Gauss-Seidel
+
+### No lineales:
+- Biseccion
+
+## Instalación
+
+```bash
+pip install SG21038UNO
---
+++ packages/SG21038Uno/sg21038uno-1.0.tar.gz/sg21038uno-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SG21038UNO',
+    version='1.0',
+    description='Libreria  para resolver ecuaciones lineales y no lineales',
+    author='Katherine Gámez',
+    author_email='sg21038@ues.edu.sv',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    python_requires='>=3.6',
+)
