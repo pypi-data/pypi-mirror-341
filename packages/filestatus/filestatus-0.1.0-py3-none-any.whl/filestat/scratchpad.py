3a6cb74be8b8d41b10f9f7428e0dedac6ced737b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:06:03 +0000

    Add package filestatus/0.1.0/filestatus-0.1.0-py3-none-any.whl

---
+++ packages/filestatus/filestatus-0.1.0-py3-none-any.whl/filestat/scratchpad.py
@@ -0,0 +1,5 @@
+import cli
+print(cli.num_letters("filestat/src/filestat/text.txt"))
+print(cli.num_bytes("filestat/src/filestat/text.txt"))
+print(cli.num_lines("filestat/src/filestat/text.txt")) 
+print(cli.num_words("filestat/src/filestat/text.txt"))
