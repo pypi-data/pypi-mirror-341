b016f384bd56dcae50a35aa2967ad55f305ea9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:19:12 +0000

    Add package plurally/0.3.70/plurally-0.3.70.tar.gz

---
+++ packages/plurally/plurally-0.3.70.tar.gz/plurally-0.3.70/plurally/models/action/ai.py
@@ -0,0 +1,6 @@
+from plurally.models.action.indexer import Indexer  # noqa: F401
+from plurally.models.action.instruct import Instruct  # noqa: F401
+from plurally.models.action.transcript import Transcript  # noqa: F401
+from plurally.models.swagger import Swagger  # noqa: F401
+
+__all__ = ["Indexer", "Instruct", "Transcript", "Swagger"]
