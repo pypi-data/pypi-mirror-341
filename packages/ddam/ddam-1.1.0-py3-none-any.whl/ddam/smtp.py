03a74cef702a7fe891bc7ab292a74edea584e1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 07:04:32 +0000

    Add package ddam/1.1.0/ddam-1.1.0-py3-none-any.whl

---
+++ packages/ddam/ddam-1.1.0-py3-none-any.whl/ddam/smtp.py
@@ -0,0 +1,39 @@
+import logging
+import smtplib
+from email.mime.text import MIMEText
+
+logger = logging.getLogger(__name__)
+
+
+class Mailer:
+    def __init__(
+        self,
+        smtp_host: str,
+        smtp_port: int,
+        ssl: bool,
+        email_from: str,
+        recipients: list[str],
+    ):
+        self.smtp_host = smtp_host
+        self.smtp_port = smtp_port
+        self.ssl = ssl
+        self.email_from = email_from
+        self.recipients = recipients
+
+    def send(self, subject: str, body: str):
+        smtp_cls: type[smtplib.SMTP_SSL] | type[smtplib.SMTP]
+
+        if self.ssl:
+            smtp_cls = smtplib.SMTP_SSL
+        else:
+            smtp_cls = smtplib.SMTP
+
+        message = MIMEText(body)
+        message["From"] = self.email_from
+        message["To"] = ", ".join(self.recipients)
+        message["Subject"] = subject
+
+        logger.debug("Sending email")
+        with smtp_cls(self.smtp_host, port=self.smtp_port) as smtp:
+            smtp.sendmail(self.email_from, self.recipients, message.as_bytes())
+        logger.debug("Email sent")
