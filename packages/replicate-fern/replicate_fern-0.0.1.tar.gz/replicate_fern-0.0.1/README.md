2a762bc74f278689827cf25e69572263a2320944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:04:35 +0000

    Add package replicate-fern/0.0.1/replicate_fern-0.0.1.tar.gz

---
+++ packages/replicate-fern/replicate_fern-0.0.1.tar.gz/replicate_fern-0.0.1/README.md
@@ -0,0 +1,3 @@
+# replicate-fern
+
+This is a placeholder (stub) package to reserve the `replicate-fern` namespace on PyPI.
---
+++ packages/replicate-fern/replicate_fern-0.0.1.tar.gz/replicate_fern-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="replicate-fern",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Stub package to reserve the 'replicate-fern' name.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="you@example.com",
+    url="https://github.com/yourname/replicate-fern",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+    ],
+    python_requires=">=3.6",
+)
