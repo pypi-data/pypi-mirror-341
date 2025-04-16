8365f8271662b002b250c9b4b05d6f6766d36259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:18:49 +0000

    Add package tml-gender-guess/0.1.10/tml_gender_guess-0.1.10.tar.gz

---
+++ packages/tml-gender-guess/tml_gender_guess-0.1.10.tar.gz/tml_gender_guess-0.1.10/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tml_gender_guess",
+    version="0.1.10",
+    description="Gender classification pipeline based on names",
+    author="José Ángel Morell",
+    packages=find_packages(include=["gender_guess", "gender_guess.*"]),
+    include_package_data=True,
+    install_requires=[
+        "pandas>=2.2.3",
+        "tqdm>=4.67.1",
+        "scikit-learn>=1.6.1",
+        "torch>=2.6.0",
+        "transformers>=4.51.1",
+        "uvicorn>=0.34.1",
+        "nltk>=3.9.1",
+        "requests>=2.32.3",
+        "regex>=2024.11.6",
+        "Unidecode>=1.3.2",
+        "nameparser>=1.0.6",
+       
+        "typing-inspection>=0.4.0",
+        "wordsegment>=1.3.1",
+        "wordninja>=2.0.0",
+    ],
+    package_data={
+        "gender_guess": ["nameLists/*.csv", "nameLists/*.dict"]
+    },
+)
+
