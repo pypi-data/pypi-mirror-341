bd4d6c813262fed637b196ef654404ae3587b4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:30 +0000

    Add package sentence-transformers/4.1.0/sentence_transformers-4.1.0.tar.gz

---
+++ packages/sentence-transformers/sentence_transformers-4.1.0.tar.gz/sentence_transformers-4.1.0/tests/test_multi_process.py
@@ -0,0 +1,43 @@
+"""
+Computes embeddings
+"""
+
+from __future__ import annotations
+
+import numpy as np
+import pytest
+
+from sentence_transformers import SentenceTransformer
+
+
+@pytest.mark.skip(
+    "This test fails if optimum.intel.openvino is imported, because openvinotoolkit/nncf "
+    "patches torch._C._nn.gelu in a way that breaks pickling."
+)
+@pytest.mark.parametrize("normalize_embeddings", (False, True))
+@pytest.mark.parametrize("prompt_name", (None, "retrieval"))
+def test_encode_multi_process(
+    stsb_bert_tiny_model: SentenceTransformer, normalize_embeddings: bool, prompt_name: str | None
+) -> None:
+    model = stsb_bert_tiny_model
+    model.prompts = {"retrieval": "Represent this sentence for searching relevant passages: "}
+    sentences = [f"This is sentence {i}" for i in range(40)]
+
+    # Start the multi-process pool on e.g. two CPU devices & compute the embeddings using the pool
+    pool = model.start_multi_process_pool(["cpu", "cpu"])
+    emb = model.encode_multi_process(
+        sentences, pool, chunk_size=10, normalize_embeddings=normalize_embeddings, prompt_name=prompt_name
+    )
+    model.stop_multi_process_pool(pool)
+    assert emb.shape == (len(sentences), 128)
+
+    # Make sure the embeddings aren't just all 0
+    assert emb.sum() != 0.0
+
+    # Compare against normal embeddings
+    emb_normal = model.encode(sentences, normalize_embeddings=normalize_embeddings, prompt_name=prompt_name)
+    diff = np.max(np.abs(emb - emb_normal))
+    assert diff < 0.001
+
+    # Ensure that after normalizing, the means are all almost 0, and otherwise not
+    assert np.all(np.abs(emb.mean(1)) < 0.01) == normalize_embeddings
