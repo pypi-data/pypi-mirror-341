7714f809c948ce1bc1bebf6e282433975d3fcfca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:04:50 +0000

    Add package stpp-tplot/0.1.1/stpp_tplot-0.1.1.tar.gz

---
+++ packages/stpp-tplot/stpp_tplot-0.1.1.tar.gz/stpp_tplot-0.1.1/README.md
@@ -0,0 +1,17 @@
+# stpp_tplot
+
+Simple time series plotting library based on pyspedas and matplotlib.
+
+## Overview
+
+`stpp_tplot` is a Python library that provides convenient functions for plotting time series data, especially for space physics data processed by `pyspedas`. It simplifies the creation of publication-quality plots with features like:
+
+* Multiple panel plots with shared x-axis
+* Spectrogram plots with colorbar
+* Orbit parameter labels panel
+* Customizable plot options
+
+## Installation
+
+```bash
+pip install stpp_tplot
---
+++ packages/stpp-tplot/stpp_tplot-0.1.1.tar.gz/stpp_tplot-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import stpp_tplot
+
+setup(
+    name='stpp_tplot',  # パッケージ名 (pip install で使う名前)
+    version=stpp_tplot.__version__,  # バージョン (Semantic Versioning に従うことを推奨)
+    packages=find_packages(where='stpp_tplot'),  # パッケージを自動検出 (stpp_tplotディレクトリ内)
+    package_dir={'': 'stpp_tplot'}, # パッケージのルートディレクトリを指定
+    install_requires=[  # 依存するライブラリ
+        'matplotlib==3.10.1',
+        'pyspedas==1.7.19',
+        'pytz==2025.2',
+        'numpy==2.2.4',
+    ],
+    author='Kohki Tachi',  # あなたの名前 (作者名)
+    author_email='ktachiresearch@gmail.com',  # あなたのメールアドレス (作者メールアドレス)
+    description='Simple time series plotting library based on pyspedas and matplotlib.',  # 簡単な説明
+    long_description=open('README.md').read(),  # 詳細な説明 (README.md を読み込む)
+    long_description_content_type='text/markdown',  # README.md の形式
+    url='https://github.com/JackKTachi/stpp_tplot',  # ライブラリのURL (GitHubリポジトリなど)
+    license='MIT',  # ライセンス (例: MIT License)
+    classifiers=[  # PyPIでの分類 (任意だが推奨)
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research', # 対象ユーザー
+        'Topic :: Scientific/Engineering :: Visualization', # トピック
+    ],
+)
