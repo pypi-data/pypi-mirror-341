65c2cd694180cb252f01a3ff87469eae2907da63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 14:04:48 +0000

    Add package pybaselib/0.7.4/pybaselib-0.7.4.tar.gz

---
+++ packages/pybaselib/pybaselib-0.7.4.tar.gz/pybaselib-0.7.4/pybaselib/utils/pythrow/exceptions.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# @Author: maoyongfan
+# @email: maoyongfan@163.com
+# @Date: 2025/1/6 17:32
+import logging
+
+class GenErr(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class BadValue(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ParameterError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class UnimplementedFunctionality(Exception):
+    """
+        此异常需要提交issue future
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DeviceError(Exception):
+    """
+        此异常需要告警
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class BugException(Exception):
+    """
+        此异常需要告警
+    """
+
+    def __init__(self, message):
+        self.message = message
+        logging.error(self.message)
+        super().__init__(self.message)
+
+class UDPNonResponse(Exception):
+    """
+        UDP Server 无响应
+    """
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
