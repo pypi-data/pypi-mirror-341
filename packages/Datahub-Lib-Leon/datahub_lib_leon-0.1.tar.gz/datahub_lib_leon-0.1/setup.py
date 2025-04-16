27c36fbdad84f91e88c14e95f3d1e41448c8f198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:18:51 +0000

    Add package Datahub-Lib-Leon/0.1/datahub_lib_leon-0.1.tar.gz

---
+++ packages/Datahub-Lib-Leon/datahub_lib_leon-0.1.tar.gz/datahub_lib_leon-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+ name='Datahub_Lib_Leon',
+ version='0.1',
+ packages=find_packages(),
+ include_package_data=True,
+ package_data={
+ 'Datahub_Lib': ['data/data_hub_lib.py'],
+ },
+ )
+
