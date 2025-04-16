8e62cc11485bd8dad9705c276179bfec9299537d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:30 +0000

    Add package gh-views/2.0.0/gh-views-2.0.0.tar.gz

---
+++ packages/gh-views/gh-views-2.0.0.tar.gz/gh-views-2.0.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='gh-views',
+    version="2.0.0",
+    author='@readwithai',
+    long_description_content_type='text/markdown',
+    author_email='talwrii@gmail.com',
+    description='',
+    license='BSD',
+    keywords='github, statistics',
+    url='https://github.com/talwrii/gh-views',
+    packages=["gh_views"],
+    install_requires=["pytz"],
+    long_description=open('README.md').read(),
+    entry_points={
+        'console_scripts': ['gh-views=gh_views.main:main']
+    }
+)
