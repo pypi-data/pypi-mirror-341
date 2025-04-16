b35b4bace1ed97d29ab16380c6d6394fe87f5699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:05:18 +0000

    Add package makehuman/0.1/makehuman-0.1.tar.gz

---
+++ packages/makehuman/makehuman-0.1.tar.gz/makehuman-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="makehuman",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["numpy>=1.17.4", "PyQt5>=5.12.8", "PyOpenGL>=3.1.0"],
+)
