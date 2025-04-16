de9ea2db36287eba7b09d6c320c0dd4f94ee5ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:06:30 +0000

    Add package galaxygradtemp/0.0.14/galaxygradtemp-0.0.14.tar.gz

---
+++ packages/galaxygradtemp/galaxygradtemp-0.0.14.tar.gz/galaxygradtemp-0.0.14/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.14"
+DESCRIPTION = "Diffusion model for galaxy generation"
+LONG_DESCRIPTION = "Contains 4 generative diffusion models ScoreNet32 and ScoreNet64 for both the HSC and ZTF surveys. These are used to return the gradients of an arbitrary image with respect to a prior distribution of individual artifact free galaxy models."
+
+# Setting up
+setup(
+    name="galaxygradtemp",
+    version=VERSION,
+    author="Matt Sampson",
+    author_email="matt.sampson@princeton.edu",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["jax", "equinox", "einops", "huggingface-hub"],
+    keywords=["python", "diffusion"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
