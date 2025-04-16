1a82857009601186d5b81a89a01caee43fcc851f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:35 +0000

    Add package cheutils/2.8.5/cheutils-2.8.5.tar.gz

---
+++ packages/cheutils/cheutils-2.8.5.tar.gz/cheutils-2.8.5/src/cheutils/feature_generation/__init__.py
@@ -0,0 +1,7 @@
+from cheutils.feature_generation.date_features import DateFeaturesAugmenter
+from cheutils.feature_generation.geospatial_features import GeohashAugmenter
+from cheutils.feature_generation.cos_sine_features import PeriodicFeaturesAugmenter
+from cheutils.feature_generation.feature_trends import FeatureTrendsAugmenter
+from cheutils.feature_generation.extreme_states import ExtremeStateAugmenter
+from cheutils.feature_generation.pct_change_features import PctChangeInterceptor
+from cheutils.feature_generation.interaction_features import InteractionFeaturesInterceptor
