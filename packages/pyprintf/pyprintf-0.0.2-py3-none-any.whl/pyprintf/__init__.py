2357353de47f3673d81ae497960aad466d6471b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 07:04:38 +0000

    Add package pyprintf/0.0.2/pyprintf-0.0.2-py3-none-any.whl

---
+++ packages/pyprintf/pyprintf-0.0.2-py3-none-any.whl/pyprintf/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .core import (
+    sprintf,
+    vsprintf,
+    config,
+    PyPrintfConfig
+)
+
+__all__ = ['sprintf', 'vsprintf', 'config', 'PyPrintfConfig']
+__version__ = '0.0.2'
