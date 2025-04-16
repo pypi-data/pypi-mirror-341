c93ea09ab6f14346a66044e6fb3f950fa28f387d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 07:04:40 +0000

    Add package vellum-ai/0.14.38/vellum_ai-0.14.38.tar.gz

---
+++ packages/vellum-ai/vellum_ai-0.14.38.tar.gz/vellum_ai-0.14.38/ee/vellum_ee/workflows/display/workflows/get_vellum_workflow_display_class.py
@@ -0,0 +1,33 @@
+from typing import Optional, Type
+
+from vellum.workflows.types.generics import WorkflowType
+from vellum_ee.workflows.display.types import WorkflowDisplayContext, WorkflowDisplayType
+
+
+def get_workflow_display(
+    *,
+    base_display_class: Type[WorkflowDisplayType],
+    workflow_class: Type[WorkflowType],
+    root_workflow_class: Optional[Type[WorkflowType]] = None,
+    parent_display_context: Optional[WorkflowDisplayContext] = None,
+    dry_run: bool = False,
+) -> WorkflowDisplayType:
+    try:
+        workflow_display_class = base_display_class.get_from_workflow_display_registry(workflow_class)
+    except KeyError:
+        try:
+            return get_workflow_display(
+                base_display_class=base_display_class,
+                workflow_class=workflow_class.__bases__[0],
+                root_workflow_class=workflow_class if root_workflow_class is None else root_workflow_class,
+                parent_display_context=parent_display_context,
+                dry_run=dry_run,
+            )
+        except IndexError:
+            return base_display_class(workflow_class)
+
+    return workflow_display_class(  # type: ignore[return-value]
+        workflow_class,
+        parent_display_context=parent_display_context,
+        dry_run=dry_run,
+    )
