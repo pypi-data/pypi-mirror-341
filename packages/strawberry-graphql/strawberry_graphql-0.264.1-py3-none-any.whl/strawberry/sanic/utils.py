2074e969dfc42762ff641c14ecedc202c7947eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:27 +0000

    Add package strawberry-graphql/0.264.1/strawberry_graphql-0.264.1-py3-none-any.whl

---
+++ packages/strawberry-graphql/strawberry_graphql-0.264.1-py3-none-any.whl/strawberry/sanic/utils.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from io import BytesIO
+from typing import TYPE_CHECKING, Any, Optional, Union, cast
+
+if TYPE_CHECKING:
+    from sanic.request import File, Request
+
+
+def convert_request_to_files_dict(request: Request) -> dict[str, Any]:
+    """Converts the request.files dictionary to a dictionary of BytesIO objects.
+
+    `request.files` has the following format, even if only a single file is uploaded:
+
+    ```python
+    {
+        "textFile": [
+            sanic.request.File(type="text/plain", body=b"strawberry", name="textFile.txt")
+        ]
+    }
+    ```
+
+    Note that the dictionary entries are lists.
+    """
+    request_files = cast("Optional[dict[str, list[File]]]", request.files)
+
+    if not request_files:
+        return {}
+
+    files_dict: dict[str, Union[BytesIO, list[BytesIO]]] = {}
+
+    for field_name, file_list in request_files.items():
+        assert len(file_list) == 1
+
+        files_dict[field_name] = BytesIO(file_list[0].body)
+
+    return files_dict
+
+
+__all__ = ["convert_request_to_files_dict"]
