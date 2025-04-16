e94db1155db97740aabb41f2a0d39bcca5f4647a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:39 +0000

    Add package ms2rescore/3.1.5/ms2rescore-3.1.5-py3-none-any.whl

---
+++ packages/ms2rescore/ms2rescore-3.1.5-py3-none-any.whl/ms2rescore/__init__.py
@@ -0,0 +1,16 @@
+"""MS²Rescore: Sensitive PSM rescoring with predicted MS² peak intensities and RTs."""
+
+__version__ = "3.1.5"
+
+from warnings import filterwarnings
+
+# mzmlb is not used, so hdf5plugin is not needed
+filterwarnings(
+    "ignore",
+    message="hdf5plugin is missing",
+    category=UserWarning,
+    module="psims.mzmlb",
+)
+
+from ms2rescore.config_parser import parse_configurations  # noqa: F401 E402
+from ms2rescore.core import rescore  # noqa: F401 E402
