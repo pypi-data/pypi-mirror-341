cf2865829b5a283176be2d1fd798a07a249ebe42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:05:08 +0000

    Add package mypackage-anas/0.0.1/mypackage_anas-0.0.1.tar.gz

---
+++ packages/mypackage-anas/mypackage_anas-0.0.1.tar.gz/mypackage_anas-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(
+    name="mypackage_anas",
+    version="0.0.1",
+    author="Mohamedanas",
+    author_email="anous17101005@gmail.com",
+    packages=find_packages(),
+    
+)
