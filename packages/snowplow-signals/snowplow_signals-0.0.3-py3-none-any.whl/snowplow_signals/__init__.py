2b357d42ba3196306e95a918affe57544dd20a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:28 +0000

    Add package snowplow-signals/0.0.3/snowplow_signals-0.0.3-py3-none-any.whl

---
+++ packages/snowplow-signals/snowplow_signals-0.0.3-py3-none-any.whl/snowplow_signals/__init__.py
@@ -0,0 +1,28 @@
+from snowplow_signals.models import (
+    View,
+    Service,
+    Attribute,
+    Criteria,
+    Criterion,
+    Entity,
+    BatchSource,
+    Field,
+    LinkEntity,
+    Event,
+)
+from snowplow_signals.signals import Signals
+from .definitions import user_entity, session_entity
+
+Signals
+View
+Service
+Attribute
+Criteria
+Criterion
+Field
+BatchSource
+Entity
+LinkEntity
+Event
+user_entity
+session_entity
