188ff92120b1896dea95fae1670237011dcde47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:24 +0000

    Add package trailml/1.2.1/trailml-1.2.1.tar.gz

---
+++ packages/trailml/trailml-1.2.1.tar.gz/trailml-1.2.1/trail/userconfig/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+from trail.libconfig import libconfig
+from trail.userconfig.config import MainConfig
+from trail.userconfig.init import init_environment
+
+_userconfig = None
+
+
+def userconfig():
+    global _userconfig
+
+    if not _userconfig:
+        primary_config_path = os.path.expanduser(libconfig.PRIMARY_USER_CONFIG_PATH)
+        if os.path.isfile(primary_config_path):
+            _userconfig = MainConfig.from_file(primary_config_path)
+        else:
+            # initialize environment
+            init_environment()
+
+            return userconfig()
+
+    return _userconfig
