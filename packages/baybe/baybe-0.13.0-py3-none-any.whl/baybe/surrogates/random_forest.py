0b25bc1d3a69dce056a869b2bb243556a3de586d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 17:04:53 +0000

    Add package baybe/0.13.0/baybe-0.13.0-py3-none-any.whl

---
+++ packages/baybe/baybe-0.13.0-py3-none-any.whl/baybe/surrogates/random_forest.py
@@ -0,0 +1,110 @@
+"""Random forest surrogates."""
+
+from __future__ import annotations
+
+from collections.abc import Collection
+from typing import TYPE_CHECKING, Any, ClassVar, Protocol
+
+import numpy as np
+from attrs import define, field
+from sklearn.ensemble import RandomForestRegressor
+from typing_extensions import override
+
+from baybe.parameters.base import Parameter
+from baybe.surrogates.base import Surrogate
+from baybe.surrogates.utils import batchify_ensemble_predictor, catch_constant_targets
+from baybe.surrogates.validation import get_model_params_validator
+from baybe.utils.conversion import to_string
+
+if TYPE_CHECKING:
+    from botorch.models.ensemble import EnsemblePosterior
+    from botorch.models.transforms.input import InputTransform
+    from botorch.models.transforms.outcome import OutcomeTransform
+    from torch import Tensor
+
+
+class _Predictor(Protocol):
+    """A basic predictor."""
+
+    def predict(self, x: np.ndarray, /) -> np.ndarray: ...
+
+
+@catch_constant_targets
+@define
+class RandomForestSurrogate(Surrogate):
+    """A random forest surrogate model."""
+
+    supports_transfer_learning: ClassVar[bool] = False
+    # See base class.
+
+    model_params: dict[str, Any] = field(
+        factory=dict,
+        converter=dict,
+        validator=get_model_params_validator(RandomForestRegressor.__init__),
+    )
+    """Optional model parameter that will be passed to the surrogate constructor."""
+
+    _model: RandomForestRegressor | None = field(init=False, default=None, eq=False)
+    """The actual model."""
+
+    @override
+    @staticmethod
+    def _make_parameter_scaler_factory(
+        parameter: Parameter,
+    ) -> type[InputTransform] | None:
+        # Tree-like models do not require any input scaling
+        return None
+
+    @override
+    @staticmethod
+    def _make_target_scaler_factory() -> type[OutcomeTransform] | None:
+        # Tree-like models do not require any output scaling
+        return None
+
+    @override
+    def _posterior(self, candidates_comp_scaled: Tensor, /) -> EnsemblePosterior:
+        from botorch.models.ensemble import EnsemblePosterior
+
+        @batchify_ensemble_predictor
+        def predict(candidates_comp_scaled: Tensor) -> Tensor:
+            """Make the end-to-end ensemble prediction."""
+            import torch
+
+            # FIXME[typing]: It seems there is currently no better way to inform the
+            #   type checker that the attribute is available at the time of the
+            #   function call
+            assert self._model is not None
+
+            return torch.from_numpy(
+                self._predict_ensemble(
+                    self._model.estimators_, candidates_comp_scaled.numpy()
+                )
+            )
+
+        return EnsemblePosterior(predict(candidates_comp_scaled).unsqueeze(-1))
+
+    @staticmethod
+    def _predict_ensemble(
+        predictors: Collection[_Predictor], candidates: np.ndarray
+    ) -> np.ndarray:
+        """Evaluate an ensemble of predictors on a given candidate set."""
+        # Extract shapes
+        n_candidates = len(candidates)
+        n_estimators = len(predictors)
+
+        # Evaluate all trees
+        predictions = np.zeros((n_estimators, n_candidates))
+        for p, predictor in enumerate(predictors):
+            predictions[p] = predictor.predict(candidates)
+
+        return predictions
+
+    @override
+    def _fit(self, train_x: Tensor, train_y: Tensor) -> None:
+        self._model = RandomForestRegressor(**(self.model_params))
+        self._model.fit(train_x.numpy(), train_y.numpy().ravel())
+
+    @override
+    def __str__(self) -> str:
+        fields = [to_string("Model Params", self.model_params, single_line=True)]
+        return to_string(super().__str__(), *fields)
