9321037addb0fdbc01ec0b5af56820a883b52cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 14:05:04 +0000

    Add package codex-sdk/0.1.0a15/codex_sdk-0.1.0a15-py3-none-any.whl

---
+++ packages/codex-sdk/codex_sdk-0.1.0a15-py3-none-any.whl/codex/types/projects/cluster_list_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["ClusterListParams"]
+
+
+class ClusterListParams(TypedDict, total=False):
+    limit: int
+
+    offset: int
+
+    order: Literal["asc", "desc"]
+
+    sort: Optional[Literal["created_at", "answered_at", "cluster_frequency_count", "custom_rank"]]
+
+    states: List[Literal["unanswered", "draft", "published", "published_with_draft"]]
