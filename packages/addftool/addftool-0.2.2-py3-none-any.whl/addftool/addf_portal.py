28a97c82402a8f2b576d357507e719fd4a73166a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:18:54 +0000

    Add package addftool/0.2.2/addftool-0.2.2-py3-none-any.whl

---
+++ packages/addftool/addftool-0.2.2-py3-none-any.whl/addftool/addf_portal.py
@@ -0,0 +1,35 @@
+import argparse
+from addftool.process import add_killer_args, killer_main
+from addftool.sync import add_sync_args, sync_main
+from addftool.deploy import add_deploy_args, deploy_main
+from addftool.broadcast_folder import add_broadcast_folder_args, broadcast_folder_main
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Addf's tool")
+
+    subparsers = parser.add_subparsers(dest='command', help='Sub-command help')
+    add_killer_args(subparsers)
+    add_sync_args(subparsers)
+    add_deploy_args(subparsers)
+    add_broadcast_folder_args(subparsers)
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    if args.command == "kill":
+        killer_main(args)
+    elif args.command == "sync":
+        sync_main(args)
+    elif args.command == "deploy":
+        deploy_main(args)
+    elif args.command == "broadcast-folder":
+        broadcast_folder_main(args)
+    else:
+        print("Unknown command: ", args.command)
+
+
+if __name__ == "__main__":
+    main()
