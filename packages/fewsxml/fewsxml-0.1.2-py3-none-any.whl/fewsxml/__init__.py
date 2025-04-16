be8ed90754be23de2a5fc3878541befc8a137803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:19:35 +0000

    Add package fewsxml/0.1.2/fewsxml-0.1.2-py3-none-any.whl

---
+++ packages/fewsxml/fewsxml-0.1.2-py3-none-any.whl/fewsxml/__init__.py
@@ -0,0 +1,12 @@
+from fewsxml.fewsxml import read_xml, write_xml
+from fewsxml.schema import FXData, FXTimeseries
+
+__version__ = "0.1.2"
+__author__ = "Farid Alavi"
+
+__all__ = [
+    "read_xml",
+    "write_xml",
+    "FXData",
+    "FXTimeseries"
+]
