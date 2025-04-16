530aacf34b47bd8f6450f7bd80b46a048f0fe869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 14:05:06 +0000

    Add package stax-cx-automation-sdk/0.1.10/stax_cx_automation_sdk-0.1.10.tar.gz

---
+++ packages/stax-cx-automation-sdk/stax_cx_automation_sdk-0.1.10.tar.gz/stax_cx_automation_sdk-0.1.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='stax_cx_automation_sdk',
+    version='0.1.10',
+    url='https://bitbucket.org/staxai/cx-automation-sdk',
+    author='Stax.ai, Inc. <https://stax.ai>',
+    author_email='naru@stax.ai',
+    description='Stax.ai CX Automation SDK',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    license='Proprietary',
+    python_requires='>=3.6',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=['retry_requests', 'functions_framework', 'js2py', 'pymongo']
+)
