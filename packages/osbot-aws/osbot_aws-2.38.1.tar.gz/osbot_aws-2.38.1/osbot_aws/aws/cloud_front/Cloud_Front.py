505353434d149fe2e69c199e44afc76543cdb019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:32 +0000

    Add package osbot-aws/2.38.1/osbot_aws-2.38.1.tar.gz

---
+++ packages/osbot-aws/osbot_aws-2.38.1.tar.gz/osbot_aws-2.38.1/osbot_aws/aws/cloud_front/Cloud_Front.py
@@ -0,0 +1,32 @@
+import time
+from typing                                     import List
+
+from osbot_utils.type_safe.decorators.type_safe import type_safe
+
+from osbot_aws.apis.Session                     import Session
+from osbot_utils.base_classes.Kwargs_To_Self    import Kwargs_To_Self
+
+
+
+class Cloud_Front(Kwargs_To_Self):
+
+    def client(self):
+        return Session().client('cloudfront',region_name='us-east-1')
+
+
+    def distributions(self):
+        response = self.client().list_distributions()       # todo: add support for when not all values are returned
+        items = response.get('DistributionList', {}).get('Items', [])
+        return items
+
+    def invalidate_path(self, distribution_id: str, paths: str):
+        return self.invalidate_paths(distribution_id, [paths])
+
+    @type_safe
+    def invalidate_paths(self, distribution_id:str, paths: List[str]):
+        kwargs = dict(DistributionId    = distribution_id,
+                      InvalidationBatch = { 'Paths'          : { 'Quantity': len(paths)            ,
+                                                                 'Items'   : paths                 },
+                                            'CallerReference': f'invalidation-{int(time.time()) }' })  # Unique reference
+        response = self.client().create_invalidation(**kwargs)
+        return response
