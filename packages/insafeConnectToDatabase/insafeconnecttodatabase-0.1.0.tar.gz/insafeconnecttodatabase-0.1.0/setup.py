c3664cbe1b9f1474354d33b0bde1aeac2d923b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:05:19 +0000

    Add package insafeConnectToDatabase/0.1.0/insafeconnecttodatabase-0.1.0.tar.gz

---
+++ packages/insafeConnectToDatabase/insafeconnecttodatabase-0.1.0.tar.gz/insafeconnecttodatabase-0.1.0/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="insafeConnectToDatabase",  # Name of your package
+    version="0.1.0",  # Version of your package
+    description="A utility to determine the type of permit based on ID",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="khaled.jabari",
+    author_email="khaled.jabari@cntxt.com",
+    url="https://github.com",  # Replace with your repo link
+    packages=find_packages(),  # Automatically find all packages
+    install_requires=[
+        "pydantic>=1.10.0",  # Add any dependencies here
+        "psycopg2",  # Add psycopg2 dependency
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
