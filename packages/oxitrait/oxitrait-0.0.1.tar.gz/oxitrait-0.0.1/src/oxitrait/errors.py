98f4d86910907efd0f9984763e2d64eef6d225b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 05:04:33 +0000

    Add package oxitrait/0.0.1/oxitrait-0.0.1.tar.gz

---
+++ packages/oxitrait/oxitrait-0.0.1.tar.gz/oxitrait-0.0.1/src/oxitrait/errors.py
@@ -0,0 +1,22 @@
+class OxitraitError(RuntimeError):
+    pass
+
+
+class DisallowedInitError(OxiTraitError):
+    pass
+
+
+class NonMethodAttrError(OxiTraitError):
+    pass
+
+
+class MultipleImplementationError(OxiTraitError):
+    pass
+
+
+class InheritanceError(OxiTraitError):
+    pass
+
+
+class NamingConventionError(OxiTraitError):
+    pass
