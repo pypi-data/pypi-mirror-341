a7b6a439ea9cb59d2cc744824112857aa701aa7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 15:05:30 +0000

    Add package blitzkit/0.0.0/blitzkit-0.0.0-py3-none-any.whl

---
+++ packages/blitzkit/blitzkit-0.0.0-py3-none-any.whl/bin/blitz.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+from blitzkit.cli import main
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+if __name__ == "__main__":
+    main()
