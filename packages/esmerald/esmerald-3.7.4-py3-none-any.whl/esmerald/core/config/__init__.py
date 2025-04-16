5cd1cb04670409c6baeff9ad7df17aea85437a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 04:04:38 +0000

    Add package esmerald/3.7.4/esmerald-3.7.4-py3-none-any.whl

---
+++ packages/esmerald/esmerald-3.7.4-py3-none-any.whl/esmerald/core/config/__init__.py
@@ -0,0 +1,15 @@
+from .asyncexit import AsyncExitConfig
+from .cors import CORSConfig
+from .csrf import CSRFConfig
+from .openapi import OpenAPIConfig
+from .session import SessionConfig
+from .static_files import StaticFilesConfig
+
+__all__ = [
+    "AsyncExitConfig",
+    "CORSConfig",
+    "CSRFConfig",
+    "OpenAPIConfig",
+    "SessionConfig",
+    "StaticFilesConfig",
+]
---
+++ packages/esmerald/esmerald-3.7.4-py3-none-any.whl/esmerald/logging.py
@@ -0,0 +1,22 @@
+import logging
+
+from loguru import logger as loguru_logger
+
+
+class InterceptHandler(logging.Handler):  # pragma: no cover
+    def emit(self, record: logging.LogRecord) -> None:
+        level: str
+        try:
+            level = loguru_logger.level(record.levelname).name
+        except ValueError:
+            level = str(record.levelno)
+
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        loguru_logger.opt(depth=depth, exception=record.exc_info).log(
+            level,
+            record.getMessage(),
+        )
