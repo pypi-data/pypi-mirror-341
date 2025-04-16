d74815ff451ffcad17e7455ddbfde4412a1eb766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 03:19:33 +0000

    Add package jaxFMM/0.0.2/jaxfmm-0.0.2.tar.gz

---
+++ packages/jaxFMM/jaxfmm-0.0.2.tar.gz/jaxfmm-0.0.2/demos/unitcube.py
@@ -0,0 +1,20 @@
+import jax.numpy as jnp
+from jax import random
+from jaxfmm import *
+
+print("Generating sample points and charges.")
+N = 2**15
+key = random.key(124)
+pts = random.uniform(key,(N,3))
+chrgs = random.uniform(key,N)
+
+print("Generating FMM hierarchy.")
+tree_info = gen_hierarchy(pts)
+
+print("Compiling and computing FMM potential.")
+pot_FMM = eval_potential(*tree_info,chrgs)
+
+print("Compiling and computing analytic potential.")
+pot_dir = eval_potential_direct(pts,chrgs)
+
+print("FMM normwise relative error: %.2e"%(jnp.linalg.norm(pot_dir-pot_FMM)/jnp.linalg.norm(pot_dir)))
