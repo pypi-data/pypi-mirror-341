4a19aafc3f65ba127234264abf1819e96a58cb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:04:59 +0000

    Add package django-admin-collaborator/0.2.6/django_admin_collaborator-0.2.6-py3-none-any.whl

---
+++ packages/django-admin-collaborator/django_admin_collaborator-0.2.6-py3-none-any.whl/django_admin_collaborator/defaults.py
@@ -0,0 +1,8 @@
+DEFAULT_ADMIN_COLLABORATOR_OPTIONS = {
+    "editor_mode_text": "You are in editor mode.",
+    "viewer_mode_text": "This page is being edited by {editor_name}. You cannot make changes until they leave.",
+    "claiming_editor_text": "The editor has left. The page will refresh shortly to allow editing.",
+    "avatar_field": None 
+}
+ADMIN_COLLABORATOR_ADMIN_URL = "admin"
+
