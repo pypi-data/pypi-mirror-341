d560587b375aec2ed8830f79d484fa8b058c4285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:06:14 +0000

    Add package pybaseballstats/0.3.4/pybaseballstats-0.3.4.tar.gz

---
+++ packages/pybaseballstats/pybaseballstats-0.3.4.tar.gz/pybaseballstats-0.3.4/src/pybaseballstats/statcast.py
@@ -0,0 +1,31 @@
+import asyncio
+from typing import Literal
+
+import nest_asyncio
+import pandas as pd
+import polars as pl
+
+from .utils.statcast_utils import (
+    _statcast_date_range_helper,
+)
+
+# Apply nest_asyncio to allow nested event loops
+nest_asyncio.apply()
+
+
+# TODO: usage docs
+def statcast_date_range_pitch_by_pitch(
+    start_date: str,
+    end_date: str,
+    perspective: Literal["pitcher", "batter"] = "pitcher",
+    return_pandas: bool = False,
+) -> pl.LazyFrame | pd.DataFrame:
+    async def async_statcast():
+        return await _statcast_date_range_helper(
+            start_date, end_date, perspective, return_pandas
+        )
+
+    return asyncio.run(async_statcast())
+
+
+# TODO: SINGLEPLAYER function
