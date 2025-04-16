c86f26a0109f01d5ce6ed5f50d149b8191d4b979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 14:05:14 +0000

    Add package PyThermoDB/1.9.2/pythermodb-1.9.2-py3-none-any.whl

---
+++ packages/PyThermoDB/pythermodb-1.9.2-py3-none-any.whl/pyThermoDB/config/constants.py
@@ -0,0 +1,6 @@
+# pyThermoDB version
+__version__ = "1.9.2"
+# author
+__author__ = "Sina Gilassi"
+# description
+__description__ = "PyThermoDB is a lightweight and user-friendly Python package designed to provide quick access to essential thermodynamic data."
