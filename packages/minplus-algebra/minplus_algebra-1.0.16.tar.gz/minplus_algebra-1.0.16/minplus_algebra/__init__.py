e6353189c73d891e8535c3e2141a82acc3839c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 06:06:24 +0000

    Add package minplus-algebra/1.0.16/minplus_algebra-1.0.16.tar.gz

---
+++ packages/minplus-algebra/minplus_algebra-1.0.16.tar.gz/minplus_algebra-1.0.16/minplus_algebra/__init__.py
@@ -0,0 +1,14 @@
+"""
+:authors: zaithevalex
+:license: Apache License, Version 2.0, see LICENSE file
+
+:copyright: (c) 2025 zaithevalex
+"""
+
+from .curves import *
+from .operators import *
+from .operators_test import *
+
+__author__ = 'zaithevalex'
+__version__ = '1.0.15'
+__email__ = 'zaithevalex@gmail.com'
