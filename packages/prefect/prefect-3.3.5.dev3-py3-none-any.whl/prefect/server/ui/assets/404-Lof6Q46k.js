008b406ded8f512579411f48abe97768ae037f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:04:55 +0000

    Add package prefect/3.3.5.dev3/prefect-3.3.5.dev3-py3-none-any.whl

---
+++ packages/prefect/prefect-3.3.5.dev3-py3-none-any.whl/prefect/server/ui/assets/404-Lof6Q46k.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,p as t,dB as r}from"./index-CUm6gmtO.js";import{u as s}from"./usePageTitle-BEMSydIZ.js";const m=e({__name:"404",setup(n){return s("Page Not Found"),(c,p)=>(a(),o(t(r)))}});export{m as default};
+//# sourceMappingURL=404-Lof6Q46k.js.map
---
+++ packages/prefect/prefect-3.3.5.dev3-py3-none-any.whl/prefect/server/ui/assets/useCan-BactzY1Z.js
@@ -0,0 +1,2 @@
+import{cC as n}from"./index-CUm6gmtO.js";const c=Symbol("canInjectionKey");function a(){return n(c)}export{c,a as u};
+//# sourceMappingURL=useCan-BactzY1Z.js.map
