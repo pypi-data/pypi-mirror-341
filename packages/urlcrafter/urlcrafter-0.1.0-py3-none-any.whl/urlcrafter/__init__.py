51790dfc17b8a3c254152acdc9286126d1eee38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 07:04:52 +0000

    Add package urlcrafter/0.1.0/urlcrafter-0.1.0-py3-none-any.whl

---
+++ packages/urlcrafter/urlcrafter-0.1.0-py3-none-any.whl/urlcrafter/__init__.py
@@ -0,0 +1,10 @@
+"""
+URLCrafter - A human-friendly library for building and manipulating URLs
+Created by Madhav Panchal (2025)
+"""
+
+from urlcrafter.url import URL
+
+__version__ = "0.1.0"
+__all__ = ["URL"]
+__author__ = "Madhav Panchal"
