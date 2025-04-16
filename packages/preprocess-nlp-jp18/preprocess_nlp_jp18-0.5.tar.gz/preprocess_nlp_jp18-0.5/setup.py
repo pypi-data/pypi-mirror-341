afc7466e918505e70db45436395f25514033189d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-16 16:05:18 +0000

    Add package preprocess-nlp-jp18/0.5/preprocess_nlp_jp18-0.5.tar.gz

---
+++ packages/preprocess-nlp-jp18/preprocess_nlp_jp18-0.5.tar.gz/preprocess_nlp_jp18-0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('readme.md') as fp:
+    long_description = fp.read()
+
+with open('requirements.txt') as fp:
+    requirements = fp.read().splitlines()
+
+setuptools.setup(
+    name='preprocess_nlp_jp18',
+    include_package_data=True,
+    version='0.5',
+    author='Jayesh Padiya',
+    description='This is a text preprocessing package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.7',
+    install_requires=requirements,
+)
