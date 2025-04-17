3da7b1e5cb85189ee337883f1ec924020f1c4a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:05:14 +0000

    Add package butterrand/0.1.1/butterrand-0.1.1.tar.gz

---
+++ packages/butterrand/butterrand-0.1.1.tar.gz/butterrand-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='butterrand',
+    version='0.1.1',
+    description='Random number generator based on butterfly effect and chaotic maps',
+    author='Bùi Phong Phú',
+    author_email='omerasutvail@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+    license='MIT',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
