d3f8a1de9a2fe9ee76106249f3efd01d0cb2365b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 14:05:24 +0000

    Add package lenskit/2025.3.0a2.post2/lenskit-2025.3.0a2.post2-cp313-cp313-macosx_10_13_x86_64.whl

---
+++ packages/lenskit/lenskit-2025.3.0a2.post2-cp313-cp313-macosx_10_13_x86_64.whl/lenskit/logging/processors.py
@@ -0,0 +1,65 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University
+# Copyright (C) 2023-2025 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+"""
+LensKit logging processors and converters.
+"""
+
+import multiprocessing as mp
+from collections import deque
+from datetime import datetime
+from typing import Any
+
+import structlog
+from structlog.typing import EventDict
+
+LOGGED_ERRORS = deque([], 5)
+
+
+def remove_internal(logger: Any, method: str, event_dict: EventDict) -> EventDict:
+    """
+    Filter out “internal” attrs (beginning with ``_``) for console logging.
+    """
+
+    to_del = [k for k in event_dict.keys() if k.startswith("_")]
+    for k in to_del:
+        del event_dict[k]
+
+    return event_dict
+
+
+def format_timestamp(logger: Any, method: str, event_dict: EventDict) -> EventDict:
+    """
+    Reformat UNIX timestamps.
+    """
+
+    if "timestamp" in event_dict:
+        stamp = datetime.fromtimestamp(event_dict["timestamp"])
+        event_dict = dict(event_dict)
+        event_dict["timestamp"] = stamp.isoformat(timespec="seconds")
+        return event_dict
+    else:
+        return event_dict
+
+
+def add_process_info(logger: Any, method: str, event_dict: EventDict) -> EventDict:
+    """
+    Add process info if it does not exist.
+    """
+
+    proc = mp.current_process()
+    if "pid" not in event_dict:
+        event_dict["pid"] = proc.pid
+    if "pname" not in event_dict:
+        event_dict["pname"] = proc.name
+
+    return event_dict
+
+
+def log_warning(message, category, filename, lineno, file=None, line=None):
+    log = structlog.stdlib.get_logger("lenskit")
+    log = log.bind(category=category.__name__, file=filename, lineno=lineno)
+    log.warning(str(message))
---
+++ packages/lenskit/lenskit-2025.3.0a2.post2-cp313-cp313-macosx_10_13_x86_64.whl/lenskit/logging/progress/_base.py
@@ -0,0 +1,51 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University
+# Copyright (C) 2023-2025 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from typing import Any
+
+
+class Progress:
+    """
+    Base class for progress reporting.  The default implementations do nothing.
+    """
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        pass
+
+    def update(
+        self,
+        advance: int = 1,
+        completed: int | None = None,
+        total: int | None = None,
+        **kwargs: float | int | str,
+    ):
+        """
+        Update the progress bar.
+
+        Args:
+            advance:
+                The amount to advance by.
+            completed:
+                The number completed; this overrides ``advance``.
+            total:
+                A new total, to update the progress bar total.
+        """
+        pass
+
+    def finish(self):
+        """
+        Finish and clean up this progress bar.  If the progresss bar is used as
+        a context manager, this is automatically called on context exit.
+        """
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.finish()
