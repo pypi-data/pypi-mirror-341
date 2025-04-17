051856143d7d93fc1022d5fb05db53042b07068b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 08:06:35 +0000

    Add package pycoral-pi5/2.0.0/pycoral_pi5-2.0.0.tar.gz

---
+++ packages/pycoral-pi5/pycoral_pi5-2.0.0.tar.gz/pycoral_pi5-2.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pycoral-pi5',
+    version='2.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.26.0',
+        'Pillow',
+    ],
+    python_requires='>=3.11',
+    description='PyCoral built for Raspberry Pi 5 with Python 3.11',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Pierre Gode',
+    author_email='you@example.com',
+    url='https://github.com/PierreGode/pycoral-pi5',
+    license='Apache-2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: POSIX :: Linux',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    include_package_data=True,
+)
