c9b8067e038133ffd47e697d0dfe68a85ce7fd8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 15:05:40 +0000

    Add package heapprint/0.0.2/heapprint-0.0.2.tar.gz

---
+++ packages/heapprint/heapprint-0.0.2.tar.gz/heapprint-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="heapprint",
+    version="0.0.2",
+    packages=find_packages(),
+    author="Jasur Omanov",
+    description="Heap (ikkilik daraxt) tuzilishini vizual ko'rsatish uchun Python kutubxonasi",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+)
