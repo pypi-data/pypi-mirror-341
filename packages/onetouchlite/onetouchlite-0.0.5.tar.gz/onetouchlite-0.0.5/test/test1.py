1975a0e28cce5adbba195e24697563d1d88085a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:52 +0000

    Add package onetouchlite/0.0.5/onetouchlite-0.0.5.tar.gz

---
+++ packages/onetouchlite/onetouchlite-0.0.5.tar.gz/onetouchlite-0.0.5/test/test1.py
@@ -0,0 +1,14 @@
+import onetouchlite.tools as tools
+
+
+tools.prints(1, style='color:bright_blue;', display=True)
+
+tools.prints(1, style='color:bright_blue;bold:True', display=True)
+
+tools.prints(1, style='color:bright_blue;bold:False', display=True)
+
+tools.printc(2, color="bright_blue", display=True)
+
+tools.printc(2, color="bright_blue", display=True)
+
+tools.printc(2, color="bright_blue", display=True)
