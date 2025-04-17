276d74dd4f824c2587a8ccf6c2cee46311e70339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 17:06:16 +0000

    Add package emu-mps/2.0.1/emu_mps-2.0.1.tar.gz

---
+++ packages/emu-mps/emu_mps-2.0.1.tar.gz/emu_mps-2.0.1/emu_mps/optimatrix/__init__.py
@@ -0,0 +1,9 @@
+from .optimiser import minimize_bandwidth
+from .permutations import permute_list, permute_matrix, invert_permutation
+
+__all__ = [
+    "minimize_bandwidth",
+    "permute_list",
+    "permute_matrix",
+    "invert_permutation",
+]
