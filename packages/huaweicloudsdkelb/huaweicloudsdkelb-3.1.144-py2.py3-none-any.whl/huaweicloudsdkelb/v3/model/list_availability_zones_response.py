8512a8338545db24100b1e1c57b6db5630c492fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 16:05:46 +0000

    Add package huaweicloudsdkelb/3.1.144/huaweicloudsdkelb-3.1.144-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkelb/huaweicloudsdkelb-3.1.144-py2.py3-none-any.whl/huaweicloudsdkelb/v3/model/list_availability_zones_response.py
@@ -0,0 +1,174 @@
+# coding: utf-8
+
+import six
+
+from huaweicloudsdkcore.sdk_response import SdkResponse
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class ListAvailabilityZonesResponse(SdkResponse):
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'request_id': 'str',
+        'availability_zones': 'list[list[AvailabilityZone]]',
+        'spec_code': 'str'
+    }
+
+    attribute_map = {
+        'request_id': 'request_id',
+        'availability_zones': 'availability_zones',
+        'spec_code': 'spec_code'
+    }
+
+    def __init__(self, request_id=None, availability_zones=None, spec_code=None):
+        r"""ListAvailabilityZonesResponse
+
+        The model defined in huaweicloud sdk
+
+        :param request_id: 参数解释：请求ID。  注：自动生成。
+        :type request_id: str
+        :param availability_zones: 参数解释：返回创建LB时可使用的可用区集合列表。如：[[az1,az2],[az2,az3]] ,则在创建LB时，只能从其中的一个子列表中选择一个或多个可用区，不能跨列表选择。在上述例子中，不能选择az1和az3。
+        :type availability_zones: list[list[AvailabilityZone]]
+        :param spec_code: 参数解释：可用区的产品编码，仅边缘场景有效。
+        :type spec_code: str
+        """
+        
+        super(ListAvailabilityZonesResponse, self).__init__()
+
+        self._request_id = None
+        self._availability_zones = None
+        self._spec_code = None
+        self.discriminator = None
+
+        if request_id is not None:
+            self.request_id = request_id
+        if availability_zones is not None:
+            self.availability_zones = availability_zones
+        if spec_code is not None:
+            self.spec_code = spec_code
+
+    @property
+    def request_id(self):
+        r"""Gets the request_id of this ListAvailabilityZonesResponse.
+
+        参数解释：请求ID。  注：自动生成。
+
+        :return: The request_id of this ListAvailabilityZonesResponse.
+        :rtype: str
+        """
+        return self._request_id
+
+    @request_id.setter
+    def request_id(self, request_id):
+        r"""Sets the request_id of this ListAvailabilityZonesResponse.
+
+        参数解释：请求ID。  注：自动生成。
+
+        :param request_id: The request_id of this ListAvailabilityZonesResponse.
+        :type request_id: str
+        """
+        self._request_id = request_id
+
+    @property
+    def availability_zones(self):
+        r"""Gets the availability_zones of this ListAvailabilityZonesResponse.
+
+        参数解释：返回创建LB时可使用的可用区集合列表。如：[[az1,az2],[az2,az3]] ,则在创建LB时，只能从其中的一个子列表中选择一个或多个可用区，不能跨列表选择。在上述例子中，不能选择az1和az3。
+
+        :return: The availability_zones of this ListAvailabilityZonesResponse.
+        :rtype: list[list[AvailabilityZone]]
+        """
+        return self._availability_zones
+
+    @availability_zones.setter
+    def availability_zones(self, availability_zones):
+        r"""Sets the availability_zones of this ListAvailabilityZonesResponse.
+
+        参数解释：返回创建LB时可使用的可用区集合列表。如：[[az1,az2],[az2,az3]] ,则在创建LB时，只能从其中的一个子列表中选择一个或多个可用区，不能跨列表选择。在上述例子中，不能选择az1和az3。
+
+        :param availability_zones: The availability_zones of this ListAvailabilityZonesResponse.
+        :type availability_zones: list[list[AvailabilityZone]]
+        """
+        self._availability_zones = availability_zones
+
+    @property
+    def spec_code(self):
+        r"""Gets the spec_code of this ListAvailabilityZonesResponse.
+
+        参数解释：可用区的产品编码，仅边缘场景有效。
+
+        :return: The spec_code of this ListAvailabilityZonesResponse.
+        :rtype: str
+        """
+        return self._spec_code
+
+    @spec_code.setter
+    def spec_code(self, spec_code):
+        r"""Sets the spec_code of this ListAvailabilityZonesResponse.
+
+        参数解释：可用区的产品编码，仅边缘场景有效。
+
+        :param spec_code: The spec_code of this ListAvailabilityZonesResponse.
+        :type spec_code: str
+        """
+        self._spec_code = spec_code
+
+    def to_dict(self):
+        """Returns the model properties as a dict"""
+        result = {}
+
+        for attr, _ in six.iteritems(self.openapi_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        if six.PY2:
+            import sys
+            reload(sys)
+            sys.setdefaultencoding("utf-8")
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ListAvailabilityZonesResponse):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkelb/huaweicloudsdkelb-3.1.144-py2.py3-none-any.whl/huaweicloudsdkelb/v3/model/member_status.py
@@ -0,0 +1,167 @@
+# coding: utf-8
+
+import six
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class MemberStatus:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'listener_id': 'str',
+        'operating_status': 'str',
+        'reason': 'MemberHealthCheckFailedReason'
+    }
+
+    attribute_map = {
+        'listener_id': 'listener_id',
+        'operating_status': 'operating_status',
+        'reason': 'reason'
+    }
+
+    def __init__(self, listener_id=None, operating_status=None, reason=None):
+        r"""MemberStatus
+
+        The model defined in huaweicloud sdk
+
+        :param listener_id: 参数解释：监听器ID
+        :type listener_id: str
+        :param operating_status: 参数解释：后端服务器的健康状态。  取值范围： - ONLINE：后端服务器正常。 - NO_MONITOR：后端服务器所在的服务器组没有健康检查器。 - OFFLINE：后端服务器关联的ECS服务器不存在或已关机。
+        :type operating_status: str
+        :param reason: 
+        :type reason: :class:`huaweicloudsdkelb.v3.MemberHealthCheckFailedReason`
+        """
+        
+        
+
+        self._listener_id = None
+        self._operating_status = None
+        self._reason = None
+        self.discriminator = None
+
+        self.listener_id = listener_id
+        self.operating_status = operating_status
+        if reason is not None:
+            self.reason = reason
+
+    @property
+    def listener_id(self):
+        r"""Gets the listener_id of this MemberStatus.
+
+        参数解释：监听器ID
+
+        :return: The listener_id of this MemberStatus.
+        :rtype: str
+        """
+        return self._listener_id
+
+    @listener_id.setter
+    def listener_id(self, listener_id):
+        r"""Sets the listener_id of this MemberStatus.
+
+        参数解释：监听器ID
+
+        :param listener_id: The listener_id of this MemberStatus.
+        :type listener_id: str
+        """
+        self._listener_id = listener_id
+
+    @property
+    def operating_status(self):
+        r"""Gets the operating_status of this MemberStatus.
+
+        参数解释：后端服务器的健康状态。  取值范围： - ONLINE：后端服务器正常。 - NO_MONITOR：后端服务器所在的服务器组没有健康检查器。 - OFFLINE：后端服务器关联的ECS服务器不存在或已关机。
+
+        :return: The operating_status of this MemberStatus.
+        :rtype: str
+        """
+        return self._operating_status
+
+    @operating_status.setter
+    def operating_status(self, operating_status):
+        r"""Sets the operating_status of this MemberStatus.
+
+        参数解释：后端服务器的健康状态。  取值范围： - ONLINE：后端服务器正常。 - NO_MONITOR：后端服务器所在的服务器组没有健康检查器。 - OFFLINE：后端服务器关联的ECS服务器不存在或已关机。
+
+        :param operating_status: The operating_status of this MemberStatus.
+        :type operating_status: str
+        """
+        self._operating_status = operating_status
+
+    @property
+    def reason(self):
+        r"""Gets the reason of this MemberStatus.
+
+        :return: The reason of this MemberStatus.
+        :rtype: :class:`huaweicloudsdkelb.v3.MemberHealthCheckFailedReason`
+        """
+        return self._reason
+
+    @reason.setter
+    def reason(self, reason):
+        r"""Sets the reason of this MemberStatus.
+
+        :param reason: The reason of this MemberStatus.
+        :type reason: :class:`huaweicloudsdkelb.v3.MemberHealthCheckFailedReason`
+        """
+        self._reason = reason
+
+    def to_dict(self):
+        """Returns the model properties as a dict"""
+        result = {}
+
+        for attr, _ in six.iteritems(self.openapi_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        if six.PY2:
+            import sys
+            reload(sys)
+            sys.setdefaultencoding("utf-8")
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, MemberStatus):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
