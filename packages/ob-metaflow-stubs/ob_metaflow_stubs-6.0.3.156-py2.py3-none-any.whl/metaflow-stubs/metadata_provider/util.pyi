6b165cfb5534c5a23d6381cdd6cbf32c891ce284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 08:06:08 +0000

    Add package ob-metaflow-stubs/6.0.3.156/ob_metaflow_stubs-6.0.3.156-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.156-py2.py3-none-any.whl/metaflow-stubs/metadata_provider/util.pyi
@@ -0,0 +1,19 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.7.2+obcheckpoint(0.1.9);ob(v1)                                                    #
+# Generated on 2025-04-16T21:50:24.063360                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def copy_tree(src, dst, update = False):
+    ...
+
+def sync_local_metadata_to_datastore(metadata_local_dir, task_ds):
+    ...
+
+def sync_local_metadata_from_datastore(metadata_local_dir, task_ds):
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.156-py2.py3-none-any.whl/metaflow-stubs/plugins/aws/step_functions/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.7.2+obcheckpoint(0.1.9);ob(v1)                                                    #
+# Generated on 2025-04-16T21:50:24.095417                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import schedule_decorator as schedule_decorator
+from . import step_functions_deployer as step_functions_deployer
+
