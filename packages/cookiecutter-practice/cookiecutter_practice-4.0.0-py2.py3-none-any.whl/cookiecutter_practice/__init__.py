cc60f92674e49429e0a43458f55fbc7185208cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 07:05:10 +0000

    Add package cookiecutter-practice/4.0.0/cookiecutter_practice-4.0.0-py2.py3-none-any.whl

---
+++ packages/cookiecutter-practice/cookiecutter_practice-4.0.0-py2.py3-none-any.whl/cookiecutter_practice/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for CookieCutter Practice."""
+
+__author__ = """Martin Arrogante"""
+__email__ = "arrogantemartin@gmail.com"
+__version__ = "4.0.0"
+
+from cookiecutter_practice import FoliumMap, LeafMap
