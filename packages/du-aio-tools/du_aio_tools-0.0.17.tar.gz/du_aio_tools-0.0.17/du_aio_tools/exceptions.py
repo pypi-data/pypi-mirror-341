dc19ae084111eb82c410983c51bdc0732e1e9ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 15:05:48 +0000

    Add package du-aio-tools/0.0.17/du_aio_tools-0.0.17.tar.gz

---
+++ packages/du-aio-tools/du_aio_tools-0.0.17.tar.gz/du_aio_tools-0.0.17/du_aio_tools/exceptions.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @time: 2022/12/6 9:27
+# @author: Dyz
+# @file: exceptions.py.py
+# @software: PyCharm
+class HTTPError(BaseException):
+    """
+    Http exception is raised when
+    """
