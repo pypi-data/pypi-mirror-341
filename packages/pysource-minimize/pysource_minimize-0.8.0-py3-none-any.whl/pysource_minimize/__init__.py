ec4dbd1f7cc551041e6005a334da7208af3a9e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:18:52 +0000

    Add package pysource-minimize/0.8.0/pysource_minimize-0.8.0-py3-none-any.whl

---
+++ packages/pysource-minimize/pysource_minimize-0.8.0-py3-none-any.whl/pysource_minimize/__init__.py
@@ -0,0 +1,9 @@
+from ._minimize import CouldNotMinimize
+from ._minimize import minimize
+from ._minimize_base import StopMinimization
+
+
+__all__ = ("minimize", "CouldNotMinimize", "StopMinimization")
+
+
+version = "0.8.0"
