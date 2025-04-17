c46b957ef5a90bc00a45cc9b39e8231bce6044b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:48 +0000

    Add package clusterir/0.1.1/clusterir-0.1.1-py3-none-any.whl

---
+++ packages/clusterir/clusterir-0.1.1-py3-none-any.whl/clusterir/__init__.py
@@ -0,0 +1,33 @@
+def addition(numbers: list) -> float:
+    return sum(numbers)
+def show_code():
+    code = '''
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+documents = [
+    "This is the first document.",
+    "This document is the second document.",
+    "And this is the third one.",
+    "Is this the first document?",
+    "This is the last document."
+]
+
+last_document = documents[-1]
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
+
+k = 2
+
+if tfidf_matrix.shape[0] >= k:
+    kmeans = KMeans(n_clusters=k, random_state=42, n_init=10)
+
+    clusters = kmeans.fit_predict(tfidf_matrix)
+    silhouette_score_value = silhouette_score(tfidf_matrix, clusters)
+    print("Silhouette Score:", silhouette_score_value)
+else:
+    print("Error: Number of clusters exceeds the number of documents.")
+    '''
+    print(code)
