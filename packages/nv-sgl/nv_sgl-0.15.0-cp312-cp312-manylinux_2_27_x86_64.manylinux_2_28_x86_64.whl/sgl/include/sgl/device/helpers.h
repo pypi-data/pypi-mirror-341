68a8e9d792fa285a343e455fcd62b25a4ef1ec8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:19:01 +0000

    Add package nv-sgl/0.15.0/nv_sgl-0.15.0-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/nv-sgl/nv_sgl-0.15.0-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/sgl/include/sgl/device/helpers.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "sgl/core/error.h"
+
+#include <slang-rhi.h>
+
+
+namespace sgl {
+SGL_API std::string build_slang_failed_message(const char* call, SlangResult result);
+}
+
+#define SLANG_CALL(call)                                                                                               \
+    {                                                                                                                  \
+        SlangResult result_ = call;                                                                                    \
+        if (SLANG_FAILED(result_)) {                                                                                   \
+            SGL_THROW(build_slang_failed_message(#call, result_));                                                     \
+        }                                                                                                              \
+    }
