e59033e76b214c46f3f61f767b079494aed3d939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:48 +0000

    Add package clusterir/0.1.1/clusterir-0.1.1.tar.gz

---
+++ packages/clusterir/clusterir-0.1.1.tar.gz/clusterir-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clusterir',                     # Your package name
+    version='0.1.1',                    # Version
+    description='''Clustering for Information Retrieval
+    ● Implement a clustering algorithm (e.g., K-means or hierarchical clustering).
+    ● Apply the clustering algorithm to a set of documents and evaluate the clustering results.''',
+    author='Aryan Bhagwat',
+    author_email='legendasur531@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
