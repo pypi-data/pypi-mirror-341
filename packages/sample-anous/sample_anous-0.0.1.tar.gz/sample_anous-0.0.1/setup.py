6fd310b4bd593434e2d13618351c65c506b4b0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 15:05:14 +0000

    Add package sample-anous/0.0.1/sample_anous-0.0.1.tar.gz

---
+++ packages/sample-anous/sample_anous-0.0.1.tar.gz/sample_anous-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+name="sample_anous",
+version="0.0.1",
+author="Mohamed Anous",
+author_email="anous17101005@gmail.com",
+packages=find_packages(),
+)
