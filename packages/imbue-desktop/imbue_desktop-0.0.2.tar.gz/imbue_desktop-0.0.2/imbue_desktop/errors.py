f04aed8e83d4d6661ca8e6263ae6c35d4e085384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:05 +0000

    Add package imbue-desktop/0.0.2/imbue_desktop-0.0.2.tar.gz

---
+++ packages/imbue-desktop/imbue_desktop-0.0.2.tar.gz/imbue_desktop-0.0.2/imbue_desktop/errors.py
@@ -0,0 +1,13 @@
+class SwitchError(Exception):
+    def __init__(self, unmatched_case: object) -> None:
+        super().__init__(f"Failed to match: {unmatched_case}")
+
+
+class NoAPIKeyError(Exception):
+    def __init__(self) -> None:
+        super().__init__("No API key provided and no API key found in ~/.imbue.toml")
+
+
+class NoURLError(Exception):
+    def __init__(self) -> None:
+        super().__init__("No URL provided and no URL found in ~/.imbue.toml")
