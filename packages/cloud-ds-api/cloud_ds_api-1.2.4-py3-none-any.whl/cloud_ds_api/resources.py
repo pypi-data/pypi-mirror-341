1ce00d083fee4dbfb3f887c95e8819dcdf79dd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:06:06 +0000

    Add package cloud-ds-api/1.2.4/cloud_ds_api-1.2.4-py3-none-any.whl

---
+++ packages/cloud-ds-api/cloud_ds_api-1.2.4-py3-none-any.whl/cloud_ds_api/resources.py
@@ -0,0 +1,53 @@
+TOP_DOMAINS = [
+    "google.com", "youtube.com", "facebook.com", "twitter.com", "instagram.com",
+    "wikipedia.org", "amazon.com", "yahoo.com", "reddit.com", "linkedin.com",
+    "netflix.com", "tiktok.com", "whatsapp.com", "microsoft.com", "apple.com",
+    "office.com", "bing.com", "live.com", "pinterest.com", "openxta.com",
+    "imdb.com", "fandom.com", "ebay.com", "cnn.com", "espn.com",
+    "bbc.com", "nytimes.com", "paypal.com", "quora.com", "tumblr.com",
+    "stackoverflow.com", "github.com", "medium.com", "walmart.com", "x.com",
+    "vk.com", "roblox.com", "dropbox.com", "twitch.tv", "etsy.com",
+    "adobe.com", "ok.ru", "amazon.in", "indeed.com", "weather.com",
+    "booking.com", "yandex.ru", "chase.com", "msn.com", "spotify.com",
+    "duckduckgo.com", "cdc.gov", "aol.com", "hulu.com", "salesforce.com",
+    "canva.com", "zillow.com", "target.com", "coursera.org", "craigslist.org",
+    "discord.com", "slack.com", "openai.com", "medium.com", "airbnb.com",
+    "bilibili.com", "mail.ru", "forbes.com", "archive.org", "kick.com",
+    "intuit.com", "icloud.com", "weibo.com", "cnet.com", "washingtonpost.com",
+    "foxnews.com", "bbc.co.uk", "nih.gov", "nasa.gov", "healthline.com",
+    "reuters.com", "nbcnews.com", "usps.com", "ycombinator.com", "udemy.com",
+    "aliexpress.com", "flipkart.com", "hotmail.com", "bankofamerica.com", "capitalone.com",
+    "icicibank.com", "stackoverflow.blog", "news.google.com", "theguardian.com", "medium.org",
+    "quizlet.com", "npr.org", "wayfair.com", "doorDash.com", "foodnetwork.com",
+    "telegram.org", "wordpress.com", "yelp.com", "cloudflare.com", "azure.com"
+]
+
+
+TOP_SUBS = [
+    "mail", "news", "drive", "calendar", "accounts",
+    "maps", "translate", "docs", "photos", "play",
+    "video", "support", "login", "outlook", "account",
+    "teams", "office", "onedrive", "portal", "developer",
+    "finance", "sports", "answers", "en", "es",
+    "de", "fr", "ja", "shop", "old",
+    "np", "about", "ads", "chat", "platform",
+    "help", "www", "music", "tv", "apps",
+    "radar", "learn", "azure", "cloud", "creativecloud",
+    "id", "m", "media", "jobs", "research",
+    "proton", "calendar", "vpn", "web", "business",
+    "faq", "blog", "careers", "policies", "about",
+    "scholar", "classroom", "marketingplatform", "tagmanager", "admin",
+    "console", "developers", "m", "l", "core",
+    "t", "ads", "mobile", "pbs", "cdn",
+    "support", "store", "forum", "community", "services",
+    "static", "api", "blog", "newsroom", "login",
+    "status", "home", "dashboard", "cdn", "edge",
+    "media", "video", "auth", "secure", "game",
+    "files", "resources", "billing", "my", "search"
+]
+
+
+VERIFIED_PKGS = {'daf1a4224e1fbeef180bbd1a53c80f3c086abed42f67524c8cc42742a2556cc2': '3.3.6',
+                 'c0813c4bdf41d8c262f93bc6e10718793844c50ea24e01ab5601b351c4d1a3a3': '1.1.6',
+                 '35b91f85da19873f4107afe74477dbf9b4f02963df31c317636ccfbd86de9436': '1.2.7',
+                 '34b65d1c31bd0f902a615700dd7654a77f70307bfa643bb3578418912349f8e6': '0.0.6'}
