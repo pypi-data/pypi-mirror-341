44727dbd376727109db663d3201d2db335096f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:18:43 +0000

    Add package grz-cli/0.4.0/grz_cli-0.4.0.tar.gz

---
+++ packages/grz-cli/grz_cli-0.4.0.tar.gz/grz_cli-0.4.0/src/grz_cli/cli.py
@@ -0,0 +1,331 @@
+"""
+CLI module for handling command-line interface operations.
+"""
+
+import importlib
+import logging
+import logging.config
+import sys
+from os import PathLike, sched_getaffinity
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import click
+import grz_pydantic_models.submission.metadata
+import platformdirs
+import yaml
+
+from .constants import PACKAGE_ROOT
+from .logging_setup import add_filelogger
+from .models.config import ConfigModel
+from .parser import Worker
+
+log = logging.getLogger(PACKAGE_ROOT + ".cli")
+
+DEFAULT_CONFIG_PATH = Path(platformdirs.user_config_dir("grz-cli")) / "config.yaml"
+
+# Aliases for path types for click options
+# Naming convention: {DIR,FILE}_{Read,Write}_{Exists,Create}
+DIR_R_E = click.Path(
+    exists=True,
+    file_okay=False,
+    dir_okay=True,
+    readable=True,
+    writable=False,
+    resolve_path=True,
+)
+DIR_RW_E = click.Path(
+    exists=True,
+    file_okay=False,
+    dir_okay=True,
+    readable=True,
+    writable=True,
+    resolve_path=True,
+)
+DIR_RW_C = click.Path(
+    exists=False,
+    file_okay=False,
+    dir_okay=True,
+    readable=True,
+    writable=True,
+    resolve_path=True,
+)
+FILE_R_E = click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True)
+
+submission_dir = click.option(
+    "--submission-dir",
+    metavar="PATH",
+    type=DIR_R_E,
+    required=True,
+    help="Path to the submission directory containing 'metadata/', 'files/', 'encrypted_files/' and 'logs/' directories",
+)
+
+config_file = click.option(
+    "--config-file",
+    metavar="STRING",
+    type=FILE_R_E,
+    required=False,
+    default=DEFAULT_CONFIG_PATH,
+    help="Path to config file",
+)
+
+threads = click.option(
+    "--threads",
+    default=min(len(sched_getaffinity(0)), 4),
+    type=int,
+    show_default=True,
+    help="Number of threads to use for parallel operations",
+)
+
+submission_id = click.option(
+    "--submission-id",
+    required=True,
+    type=str,
+    metavar="STRING",
+    help="S3 submission ID",
+)
+
+output_dir = click.option(
+    "--output-dir",
+    metavar="PATH",
+    type=DIR_RW_E,
+    required=True,
+    default=None,
+    help="Path to the target submission output directory",
+)
+
+
+class OrderedGroup(click.Group):
+    """
+    A click Group that keeps track of the order in which commands are added.
+    """
+
+    def list_commands(self, ctx):
+        """Return the list of commands in the order they were added."""
+        return list(self.commands.keys())
+
+
+@click.group(
+    cls=OrderedGroup,
+    help="Validate, encrypt, decrypt and upload submissions to a GRZ/GDC.",
+)
+@click.version_option(
+    version=importlib.metadata.version("grz-cli"),
+    prog_name="grz-cli",
+    message=f"%(prog)s v%(version)s (metadata schema versions: {', '.join(grz_pydantic_models.submission.metadata.get_supported_versions())})",
+)
+@click.option("--log-file", metavar="FILE", type=str, help="Path to log file")
+@click.option(
+    "--log-level",
+    default="INFO",
+    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
+    help="Set the log level (default: INFO)",
+)
+def cli(log_file: str | None = None, log_level: str = "INFO"):
+    """
+    Command-line interface function for setting up logging.
+
+    :param log_file: Path to the log file. If provided, a file logger will be added.
+    :param log_level: Log level for the logger. It should be one of the following:
+                       DEBUG, INFO, WARNING, ERROR, CRITICAL.
+    """
+    if log_file:
+        add_filelogger(
+            log_file,
+            log_level.upper(),
+        )  # Add file logger
+
+    # show only time and log level in STDOUT
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+
+    # set the log level for this package
+    logging.getLogger(PACKAGE_ROOT).setLevel(log_level.upper())
+
+    log.debug("Logging setup complete.")
+
+
+@cli.command()
+@submission_dir
+def validate(submission_dir):
+    """
+    Validate the submission.
+
+    This validates the submission by checking its checksums, as well as performing basic sanity checks on the supplied metadata.
+    Must be executed before calling `encrypt` and `upload`.
+    """
+    log.info("Starting validation...")
+
+    submission_dir = Path(submission_dir)
+
+    worker_inst = Worker(
+        metadata_dir=submission_dir / "metadata",
+        files_dir=submission_dir / "files",
+        log_dir=submission_dir / "logs",
+        encrypted_files_dir=submission_dir / "encrypted_files",
+    )
+    worker_inst.validate()
+
+    log.info("Validation finished!")
+
+
+@cli.command()
+@submission_dir
+@config_file
+def encrypt(
+    submission_dir,
+    config_file,
+):
+    """
+    Encrypt a submission.
+
+    Encryption is done with the recipient's public key.
+    Sub-folders 'encrypted_files' and 'logs' are created within the submission directory.
+    """
+    config = read_config(config_file)
+
+    submitter_privkey_path = config.submitter_private_key_path
+    if submitter_privkey_path == "":
+        submitter_privkey_path = None
+
+    log.info("Starting encryption...")
+
+    submission_dir = Path(submission_dir)
+
+    worker_inst = Worker(
+        metadata_dir=submission_dir / "metadata",
+        files_dir=submission_dir / "files",
+        log_dir=submission_dir / "logs",
+        encrypted_files_dir=submission_dir / "encrypted_files",
+    )
+    if config.grz_public_key:
+        with NamedTemporaryFile("w") as f:
+            f.write(config.grz_public_key)
+            f.flush()
+            worker_inst.encrypt(
+                f.name,
+                submitter_private_key_path=submitter_privkey_path,
+            )
+    else:
+        worker_inst.encrypt(
+            config.grz_public_key_path,
+            submitter_private_key_path=submitter_privkey_path,
+        )
+
+    log.info("Encryption successful!")
+
+
+@cli.command()
+@submission_dir
+@config_file
+def decrypt(
+    submission_dir,
+    config_file,
+):
+    """
+    Decrypt a submission.
+
+    Decrypting a submission requires the _private_ key of the original recipient.
+    """
+    config = read_config(config_file)
+
+    grz_privkey_path = config.grz_private_key_path
+    if not grz_privkey_path:
+        log.error("GRZ private key path is required for decryption.")
+        sys.exit(1)
+
+    log.info("Starting decryption...")
+
+    submission_dir = Path(submission_dir)
+
+    worker_inst = Worker(
+        metadata_dir=submission_dir / "metadata",
+        files_dir=submission_dir / "files",
+        log_dir=submission_dir / "logs",
+        encrypted_files_dir=submission_dir / "encrypted_files",
+    )
+    worker_inst.decrypt(grz_privkey_path)
+
+    log.info("Encryption successful!")
+
+
+@cli.command()
+@submission_dir
+@config_file
+@threads
+def upload(
+    submission_dir,
+    config_file,
+    threads,
+):
+    """
+    Upload a submission to a GRZ/GDC.
+    """
+    config = read_config(config_file)
+
+    log.info("Starting upload...")
+
+    submission_dir = Path(submission_dir)
+
+    worker_inst = Worker(
+        metadata_dir=submission_dir / "metadata",
+        files_dir=submission_dir / "files",
+        log_dir=submission_dir / "logs",
+        encrypted_files_dir=submission_dir / "encrypted_files",
+        threads=threads,
+    )
+    # output the generated submission ID
+    print(worker_inst.upload(config))
+
+    log.info("Upload finished!")
+
+
+@cli.command()
+@submission_id
+@output_dir
+@config_file
+@threads
+def download(
+    submission_id,
+    output_dir,
+    config_file,
+    threads,
+):
+    """
+    Download a submission from a GRZ.
+
+    Downloaded metadata is stored within the `metadata` sub-folder of the submission output directory.
+    Downloaded files are stored within the `encrypted_files` sub-folder of the submission output directory.
+    """
+    config = read_config(config_file)
+
+    log.info("Starting download...")
+
+    submission_dir_path = Path(output_dir)
+    if not submission_dir_path.is_dir():
+        log.debug("Creating submission directory %s", submission_dir_path)
+        submission_dir_path.mkdir(mode=0o770, parents=False, exist_ok=False)
+
+    worker_inst = Worker(
+        metadata_dir=submission_dir_path / "metadata",
+        files_dir=submission_dir_path / "files",
+        log_dir=submission_dir_path / "logs",
+        encrypted_files_dir=submission_dir_path / "encrypted_files",
+        threads=threads,
+    )
+    worker_inst.download(config, submission_id)
+
+    log.info("Download finished!")
+
+
+def read_config(config_path: str | PathLike) -> ConfigModel:
+    """Reads the configuration file and validates it against the schema."""
+    with open(config_path, encoding="utf-8") as f:
+        config = ConfigModel(**yaml.safe_load(f))
+
+    return config
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/grz-cli/grz_cli-0.4.0.tar.gz/grz_cli-0.4.0/src/grz_cli/upload.py
@@ -0,0 +1,236 @@
+"""Module for uploading encrypted submissions to a remote storage"""
+
+from __future__ import annotations
+
+import abc
+import logging
+import math
+import re
+from os import PathLike
+from os.path import getsize
+from pathlib import Path
+from typing import TYPE_CHECKING, override
+
+import boto3  # type: ignore[import-untyped]
+import botocore.handlers  # type: ignore[import-untyped]
+from boto3 import client as boto3_client  # type: ignore[import-untyped]
+from boto3.s3.transfer import S3Transfer, TransferConfig  # type: ignore[import-untyped]
+from botocore.config import Config as Boto3Config  # type: ignore[import-untyped]
+from tqdm.auto import tqdm
+
+from .models.config import ConfigModel
+from .progress_logging import FileProgressLogger
+from .states import UploadState
+
+MULTIPART_THRESHOLD = 8 * 1024 * 1024  # 8MiB, boto3 default
+MULTIPART_CHUNKSIZE = 8 * 1024 * 1024  # 8MiB, boto3 default
+MULTIPART_MAX_CHUNKS = 1000  # CEPH S3 limit, AWS limit is 10000
+
+if TYPE_CHECKING:
+    from .parser import EncryptedSubmission
+
+log = logging.getLogger(__name__)
+
+# see discussion: https://github.com/boto/boto3/discussions/4251 for acception bucketnames with : in the name
+botocore.handlers.VALID_BUCKET = re.compile(r"^[:a-zA-Z0-9.\-_]{1,255}$")  # type: ignore[import-untyped]
+
+
+class UploadError(Exception):
+    """Exception raised when an upload fails"""
+
+    pass
+
+
+class UploadWorker(metaclass=abc.ABCMeta):
+    """Worker baseclass for uploading encrypted submissions"""
+
+    @abc.abstractmethod
+    def upload(self, encrypted_submission: EncryptedSubmission):
+        """
+        Upload an encrypted submission
+
+        :param encrypted_submission: The encrypted submission to upload
+        :raises UploadError: when the upload failed
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def upload_file(self, local_file_path: str | PathLike, s3_object_id: str):
+        """
+        Upload a single file to the specified object ID
+        :param local_file_path: Path to the file to upload
+        :param s3_object_id: Remote S3 object ID under which the file should be stored
+        """
+        raise NotImplementedError()
+
+
+class S3BotoUploadWorker(UploadWorker):
+    """Implementation of an upload worker using boto3 for S3"""
+
+    __log = log.getChild("S3BotoUploadWorker")
+
+    def __init__(
+        self,
+        config: ConfigModel,
+        status_file_path: str | PathLike,
+        threads: int = 1,
+    ):
+        """
+        An upload manager for S3 storage
+
+        :param config: instance of `ConfigModel`
+        :param status_file_path: file for storing upload state. Can be used for e.g. resumable uploads.
+        """
+        super().__init__()
+
+        self._status_file_path = Path(status_file_path)
+        self._config = config
+        self._threads = threads
+
+        self._init_s3_client()
+
+    def _init_s3_client(self):
+        # if user specifies empty strings, this might be an issue
+        def empty_str_to_none(string: str | None) -> str | None:
+            if string == "" or string is None:
+                return None
+            else:
+                return string
+
+        # configure proxies if proxy_url is defined
+        proxy_url = self._config.s3_options.proxy_url
+        config = Boto3Config(
+            proxies={"http": str(proxy_url), "https": str(proxy_url)} if proxy_url is not None else None,
+            request_checksum_calculation=self._config.s3_options.request_checksum_calculation,
+        )
+
+        # Initialize S3 client for uploading
+        self._s3_client: boto3.session.Session.client = boto3_client(
+            service_name="s3",
+            region_name=empty_str_to_none(self._config.s3_options.region_name),
+            api_version=empty_str_to_none(self._config.s3_options.api_version),
+            use_ssl=self._config.s3_options.use_ssl,
+            endpoint_url=empty_str_to_none(str(self._config.s3_options.endpoint_url)),
+            aws_access_key_id=empty_str_to_none(self._config.s3_options.access_key),
+            aws_secret_access_key=empty_str_to_none(self._config.s3_options.secret),
+            aws_session_token=empty_str_to_none(self._config.s3_options.session_token),
+            config=config,
+        )
+
+    @override
+    def upload_file(self, local_file_path, s3_object_id):
+        """
+        Upload a single file to the specified object ID
+        :param local_file_path: Path to the file to upload
+        :param s3_object_id: Remote S3 object ID under which the file should be stored
+        """
+        self.__log.info(f"Uploading {local_file_path} to {s3_object_id}...")
+
+        filesize = getsize(local_file_path)
+
+        chunksize = (
+            math.ceil(filesize / MULTIPART_MAX_CHUNKS)
+            if filesize / MULTIPART_CHUNKSIZE > MULTIPART_MAX_CHUNKS
+            else MULTIPART_CHUNKSIZE
+        )
+        self.__log.debug(
+            f"Using a chunksize of: {chunksize / 1024**2}MiB, results in {math.ceil(filesize / chunksize)} chunk(s)"
+        )
+
+        config = TransferConfig(
+            multipart_threshold=MULTIPART_THRESHOLD,
+            multipart_chunksize=chunksize,
+            max_concurrency=self._threads,
+        )
+
+        transfer = S3Transfer(self._s3_client, config)
+        progress_bar = tqdm(total=filesize, unit="B", unit_scale=True, unit_divisor=1024)
+        transfer.upload_file(
+            local_file_path,
+            self._config.s3_options.bucket,
+            s3_object_id,
+            callback=lambda bytes_transferred: progress_bar.update(bytes_transferred),
+        )
+
+    @override
+    def upload(self, encrypted_submission: EncryptedSubmission):
+        """
+        Upload an encrypted submission
+        :param encrypted_submission: The encrypted submission to upload
+        """
+        progress_logger = FileProgressLogger[UploadState](self._status_file_path)
+        metadata_file_path, metadata_s3_object_id = encrypted_submission.get_metadata_file_path_and_object_id()
+        files_to_upload = encrypted_submission.get_encrypted_files_and_object_id()
+        files_to_upload[metadata_file_path] = metadata_s3_object_id
+
+        for file_path in files_to_upload:
+            if not Path(file_path).exists():
+                raise UploadError(f"File {file_path} does not exist")
+
+        for file_path, file_metadata in encrypted_submission.encrypted_files.items():
+            logged_state = progress_logger.get_state(file_path, file_metadata)
+            self.__log.debug("state for %s: %s", file_path, logged_state)
+
+            s3_object_id = files_to_upload[file_path]
+
+            if (logged_state is None) or not logged_state.get("upload_successful", False):
+                self.__log.info(
+                    "Uploading file: '%s' -> '%s'",
+                    str(file_path),
+                    str(s3_object_id),
+                )
+
+                try:
+                    self.upload_file(file_path, s3_object_id)
+
+                    self.__log.info(f"Upload complete for {str(file_path)}. ")
+                    progress_logger.set_state(
+                        file_path,
+                        file_metadata,
+                        state=UploadState(upload_successful=True),
+                    )
+                except Exception as e:
+                    self.__log.error("Upload failed for '%s'", str(file_path))
+
+                    progress_logger.set_state(
+                        file_path,
+                        file_metadata,
+                        state=UploadState(upload_successful=False, errors=[str(e)]),
+                    )
+
+                    raise e
+            else:
+                self.__log.info(
+                    "File '%s' already uploaded (at '%s')",
+                    str(file_path),
+                    str(s3_object_id),
+                )
+
+        # finally upload the metadata.json file, unconditionally:
+        try:
+            self.upload_file(metadata_file_path, metadata_s3_object_id)
+            self.__log.info(f"Upload complete for {str(metadata_file_path)}. ")
+        except Exception as e:
+            self.__log.error("Upload failed for '%s'", str(metadata_file_path))
+            raise e
+
+    def _check_for_completed_submission(self, s3_object_id: str) -> bool:
+        try:
+            return s3_object_id in self._list_keys(
+                self._config.s3_options.bucket, prefix=str(Path(s3_object_id).parent)
+            )
+        except Exception as e:
+            self.__log.warning(
+                "Exception occured during check for completed submission; assuming submission is incomplete.",
+                exc_info=e,
+            )
+            return False
+
+    # https://stackoverflow.com/a/54014862
+    def _list_keys(self, bucket_name, prefix="/", delimiter="/", start_after=""):
+        s3_paginator = self._s3_client.get_paginator("list_objects_v2")
+        prefix = prefix.lstrip(delimiter)
+        start_after = (start_after or prefix) if prefix.endswith(delimiter) else start_after
+        for page in s3_paginator.paginate(Bucket=bucket_name, Prefix=prefix, StartAfter=start_after):
+            for content in page.get("Contents", ()):
+                yield content["Key"]
