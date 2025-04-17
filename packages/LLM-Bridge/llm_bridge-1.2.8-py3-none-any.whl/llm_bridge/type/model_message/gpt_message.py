645f7e175a26f4644a5bff989218ed79fe48a81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:29 +0000

    Add package LLM-Bridge/1.2.8/llm_bridge-1.2.8-py3-none-any.whl

---
+++ packages/LLM-Bridge/llm_bridge-1.2.8-py3-none-any.whl/llm_bridge/type/model_message/gpt_message.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from openai.types.chat import (
+    ChatCompletionContentPartTextParam,
+    ChatCompletionContentPartImageParam, \
+    ChatCompletionContentPartInputAudioParam
+)
+
+from llm_bridge.type.message import Role
+
+
+@dataclass
+class GptMessage:
+    role: Role
+    content: list[
+        ChatCompletionContentPartTextParam |
+        ChatCompletionContentPartImageParam |
+        ChatCompletionContentPartInputAudioParam
+    ]
