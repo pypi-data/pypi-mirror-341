813fc780a02ee651c4fc1aecf49fb0ae5d82c17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:19:18 +0000

    Add package tortoise-serializer/1.4.1/tortoise_serializer-1.4.1-py3-none-any.whl

---
+++ packages/tortoise-serializer/tortoise_serializer-1.4.1-py3-none-any.whl/tortoise_serializer/__init__.py
@@ -0,0 +1,16 @@
+from .serializers import (
+    ModelSerializer,
+    Serializer,
+    require_permission_or_unset,
+)
+from .types import ContextType, Unset, UnsetType
+
+__all__ = [
+    "ContextType",
+    "ModelSerializer",
+    "Serializer",
+    "ModelSerializer",
+    "Unset",
+    "UnsetType",
+    "require_permission_or_unset",
+]
