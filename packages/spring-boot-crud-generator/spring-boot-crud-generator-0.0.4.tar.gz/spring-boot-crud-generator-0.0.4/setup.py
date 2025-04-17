6665017eaa242ffb5e10defc3adde7fca8b25944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:05:14 +0000

    Add package spring-boot-crud-generator/0.0.4/spring-boot-crud-generator-0.0.4.tar.gz

---
+++ packages/spring-boot-crud-generator/spring-boot-crud-generator-0.0.4.tar.gz/spring-boot-crud-generator-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spring-boot-crud-generator",
+    version="0.0.4",
+    packages=["spring-boot-crud-generator"],
+    entry_points={
+        "console_scripts": [
+            "spring-crud=springcrud.crud:main",
+        ],
+    },
+    install_requires=[],
+    author="wonow",
+    author_email="wonowdaily@gmail.com",
+    description="Spring Boot CRUD 코드 생성기",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/wonowonow/spring-boot-crud-generator",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
