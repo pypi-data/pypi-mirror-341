adcd13ac9bcd594801e151d5b2e7977c7fcf6aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:19:03 +0000

    Add package aa-ledger/0.7.5/aa_ledger-0.7.5-py3-none-any.whl

---
+++ packages/aa-ledger/aa_ledger-0.7.5-py3-none-any.whl/ledger/api/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+from ninja import NinjaAPI
+from ninja.security import django_auth
+
+from django.conf import settings
+
+from ledger.api import ledger
+
+logger = logging.getLogger(__name__)
+
+api = NinjaAPI(
+    title="Geuthur API",
+    version="0.5.0",
+    urls_namespace="ledger:api",
+    auth=django_auth,
+    csrf=True,
+    openapi_url=settings.DEBUG and "/openapi.json" or "",
+)
+
+# Add the ledger endpoints
+ledger.setup(api)
