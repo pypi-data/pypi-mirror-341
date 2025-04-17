0b2826d74bcd30489a0299fa4083896f61b811a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:07 +0000

    Add package mseep-ida-mcp-docs/0.1.0/mseep_ida_mcp_docs-0.1.0.tar.gz

---
+++ packages/mseep-ida-mcp-docs/mseep_ida_mcp_docs-0.1.0.tar.gz/mseep_ida_mcp_docs-0.1.0/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-ida-mcp-docs",
+    version="0.1.0",
+    description="IDA Pro MCP Documentation and Utilities",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires={'python': '^3.8', 'mcp': {'extras': ['cli'], 'version': '^0.1.0'}},
+    keywords=["mseep"] + [],
+)
