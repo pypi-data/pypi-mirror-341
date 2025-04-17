1d1e33672872778965662de5079310aad7175b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:50 +0000

    Add package aiocallback/0.1.2/aiocallback-0.1.2-py3-none-any.whl

---
+++ packages/aiocallback/aiocallback-0.1.2-py3-none-any.whl/aiocallback/__init__.py
@@ -0,0 +1,14 @@
+# ruff: noqa: E402,F401
+from .events import (
+    EventWrapper,
+    SelfEventWrapper,
+    contextevent,
+    event,
+    EventList,
+    EventListMetaclass,
+    subclasscontextevent,
+    subclassevent,
+)
+__author__ = "Vizonex"
+
+from .__version__ import __version__
