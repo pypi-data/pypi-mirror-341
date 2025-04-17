38eddc39e733824707f8c907b7b164993c4e472a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 08:06:28 +0000

    Add package dependency-metrics/0.1.2/dependency_metrics-0.1.2.tar.gz

---
+++ packages/dependency-metrics/dependency_metrics-0.1.2.tar.gz/dependency_metrics-0.1.2/dependency_metrics/package_managers/pip.py
@@ -0,0 +1,36 @@
+import json
+
+import sh
+
+
+def get_outdated_pip_packages():
+    """
+    Return relevant package version info from pip list output
+    """
+    pip_packages = json.loads(Pip.list(outdated=True))
+    cleaned_package_info_list = []
+    for pkg_info in pip_packages:
+        del pkg_info['latest_filetype']
+        cleaned_package_info_list.append(pkg_info)
+    return cleaned_package_info_list
+
+
+def get_total_count_for_pip():
+    """
+    Return total number of installed dependencies
+    """
+    packages = json.loads(Pip.list())
+    return len(packages)
+
+
+class Pip:
+
+    @staticmethod
+    def list(outdated=False):
+        """
+        Equivalent to ``pip list --format json [--outdated]``
+        """
+        args = ["list", "--format", "json"]
+        if outdated:
+            args.append("--outdated")
+        return sh.pip(*args)
