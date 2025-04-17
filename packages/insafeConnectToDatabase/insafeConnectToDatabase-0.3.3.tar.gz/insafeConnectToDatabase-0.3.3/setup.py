69b4ca485399ceff1ce2163ffaa85badd4e2c580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 16:05:28 +0000

    Add package insafeConnectToDatabase/0.3.3/insafeConnectToDatabase-0.3.3.tar.gz

---
+++ packages/insafeConnectToDatabase/insafeConnectToDatabase-0.3.3.tar.gz/insafeConnectToDatabase-0.3.3/setup.py
@@ -0,0 +1,36 @@
+# setup.py
+import shutil
+import os
+from setuptools import setup, find_packages
+
+# Remove previous 'dist' and 'build' directories
+for directory in ['dist', 'build']:
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+
+setup(
+    name="insafeConnectToDatabase",  # Name of your package
+    version="0.3.3",  # Version of your package
+    description="A utility to determine the type of permit based on ID",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="khaled.jabari",
+    author_email="khaled.jabari@cntxt.com",
+    url="https://github.com",  # Replace with your repo link
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),  # Automatically find all packages
+    include_package_data=True,  # Ensures non-Python files are included
+    package_data={
+        'insafeConnectToDatabase': ['cloud-sql-proxy'],  # Include the script
+    },
+    install_requires=[
+        "pydantic>=1.10.0",  # Add any dependencies here
+        "psycopg2",  # Add psycopg2 dependency
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
