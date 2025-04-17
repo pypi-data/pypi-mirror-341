6bde518107ebac88fbf3b82ebc1671631b7dc889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:08 +0000

    Add package compass.api-client/1.0.183/compass_api_client-1.0.183.tar.gz

---
+++ packages/compass.api-client/compass_api_client-1.0.183.tar.gz/compass_api_client-1.0.183/compass/api_client/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from compass.api_client.api.aave_v3_api import AaveV3Api
+from compass.api_client.api.aerodrome_slipstream_api import AerodromeSlipstreamApi
+from compass.api_client.api.others_api import OthersApi
+from compass.api_client.api.uniswap_v3_api import UniswapV3Api
+
