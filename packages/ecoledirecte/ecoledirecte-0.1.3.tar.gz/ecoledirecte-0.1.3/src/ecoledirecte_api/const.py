6540adca63cf551b0db417d761cac7db88a51f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:58 +0000

    Add package ecoledirecte/0.1.3/ecoledirecte-0.1.3.tar.gz

---
+++ packages/ecoledirecte/ecoledirecte-0.1.3.tar.gz/ecoledirecte-0.1.3/src/ecoledirecte_api/const.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+AUGUST = 8
+ED_MFA_REQUIRED = 250
+ED_OK = 200
+APIURL = "https://api.ecoledirecte.com/v3"
+APIVERSION = "4.77.5"
