2133016106749199b6b4e3e4652928fb3b99e5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:05:12 +0000

    Add package egos-helpers/0.3.0/egos_helpers-0.3.0.tar.gz

---
+++ packages/egos-helpers/egos_helpers-0.3.0.tar.gz/egos_helpers-0.3.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" setup.py for pypi """
+
+import setuptools
+from egos_helpers.constants import NAME, BUILD, VERSION
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+version = VERSION
+
+if BUILD:
+    version += f'.{BUILD}'
+
+setuptools.setup(
+    name=NAME,
+    version=version,
+    author="Alex Thomae",
+    author_email="egos-helpers@egos.tech",
+    description="A python library for helper functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT License',
+    license_files=['LICENSE'],
+    url="https://gitlab.com/egos-tech/egos-helpers",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pygelf==0.4.2',
+    ],
+)
