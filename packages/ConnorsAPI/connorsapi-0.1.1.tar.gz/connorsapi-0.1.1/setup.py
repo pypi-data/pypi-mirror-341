76af53c9e7fe5c7a74939612b271ff21f21eea0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 14:05:26 +0000

    Add package ConnorsAPI/0.1.1/connorsapi-0.1.1.tar.gz

---
+++ packages/ConnorsAPI/connorsapi-0.1.1.tar.gz/connorsapi-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ConnorsAPI",  # The name of your package
+    version="0.1.1",
+    packages=find_packages(),  # Automatically finds the 'api_thingy' directory
+    install_requires=[  # Add any external libraries here
+        # "requests",  # Example if you have any dependencies
+    ],
+    description="A short description of your package",
+    author="Connor Daniels",
+    author_email="youremail@example.com",
+    url="https://github.com/Concon6321/API-THINGY",  # Your GitHub URL
+    include_package_data=True,
+)
