bc8cff5f6a8d0d14c878ad85f33f0bf32e33afa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 07:05:41 +0000

    Add package pyeggp/1.0.0/pyeggp-1.0.0.tar.gz

---
+++ packages/pyeggp/pyeggp-1.0.0.tar.gz/pyeggp-1.0.0/test/test_pyeggp.py
@@ -0,0 +1,16 @@
+from pyeggp import pyeggp_run, PyEGGP
+import pandas as pd
+
+output = pyeggp_run("test/data.csv", 100, 100, 10, 3, 0.9, 0.3, "add,sub,mul,div,log", "MSE", 50, 2, -1, 3, 0, "", "")
+
+print(output)
+
+print("Check PyEGGP")
+df = pd.read_csv("test/data.csv")
+Z = df.values
+X = Z[:,:-1]
+y = Z[:,-1]
+
+reg = PyEGGP(100, 100, 10, 3, 0.9, 0.3, "add,sub,mul,div,log", "MSE", 50, 2, -1, 3, 0, "", "")
+reg.fit(X, y)
+print(reg.score(X, y))
