1563e7e23f34662b84cde39bdc787fbefd8c3f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:36 +0000

    Add package act-atmos/2.2.0/act_atmos-2.2.0-py3-none-any.whl

---
+++ packages/act-atmos/act_atmos-2.2.0-py3-none-any.whl/act/discovery/__init__.py
@@ -0,0 +1,33 @@
+"""
+This module contains procedures for exploring and downloading data
+from a variety of web services
+
+"""
+
+import lazy_loader as lazy
+
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__,
+    submodules=[
+        'arm',
+        'asos',
+        'airnow',
+        'cropscape',
+        'improve',
+        'noaapsl',
+        'neon',
+        'surfrad',
+        'nasa',
+    ],
+    submod_attrs={
+        'arm': ['download_arm_data', 'get_arm_doi'],
+        'asos': ['get_asos_data'],
+        'airnow': ['get_airnow_bounded_obs', 'get_airnow_obs', 'get_airnow_forecast'],
+        'cropscape': ['get_crop_type'],
+        'improve': ['get_improve_data'],
+        'noaapsl': ['download_noaa_psl_data'],
+        'neon': ['get_neon_site_products', 'get_neon_product_avail', 'download_neon_data'],
+        'surfrad': ['download_surfrad_data'],
+        'nasa': ["download_mplnet_data", "get_mplnet_meta"],
+    },
+)
