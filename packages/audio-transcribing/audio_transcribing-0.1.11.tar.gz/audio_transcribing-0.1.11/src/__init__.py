434963f416bcb9eb60a06c4f68fa44e94d463d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:37 +0000

    Add package audio-transcribing/0.1.11/audio_transcribing-0.1.11.tar.gz

---
+++ packages/audio-transcribing/audio_transcribing-0.1.11.tar.gz/audio_transcribing-0.1.11/src/__init__.py
@@ -0,0 +1,18 @@
+"""
+Package: audio_transcribing
+
+This package provides classes for transcribing and processing audio content.
+It supports speaker diarization, stopword removal, and transcription using
+different models.
+
+Classes
+-------
+NatashaStopwordsRemover:
+    Handles text processing and stopword removal.
+Transcriber :
+    Class for performing audio transcription and processing tasks.
+"""
+
+from .audio_transcribing import *
+
+__all__ = ["Transcriber", "NatashaStopwordsRemover"]
