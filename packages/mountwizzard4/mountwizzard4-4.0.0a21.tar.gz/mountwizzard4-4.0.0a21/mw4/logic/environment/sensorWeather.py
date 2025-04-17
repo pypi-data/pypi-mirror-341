27e5dfd6febc12e47fc7e03ad1d8bf1c7df1cb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:58 +0000

    Add package mountwizzard4/4.0.0a21/mountwizzard4-4.0.0a21.tar.gz

---
+++ packages/mountwizzard4/mountwizzard4-4.0.0a21.tar.gz/mountwizzard4-4.0.0a21/mw4/logic/environment/sensorWeather.py
@@ -0,0 +1,81 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PySide for python
+#
+# written in python3, (c) 2019-2025 by mworion
+# Licence APL2.0
+#
+###########################################################
+# standard libraries
+import logging
+import platform
+
+# external packages
+
+# local imports
+from base.signalsDevices import Signals
+from logic.environment.sensorWeatherIndi import SensorWeatherIndi
+from logic.environment.sensorWeatherAlpaca import SensorWeatherAlpaca
+
+if platform.system() == "Windows":
+    from logic.environment.sensorWeatherAscom import SensorWeatherAscom
+
+
+class SensorWeather:
+    """ """
+
+    log = logging.getLogger("MW4")
+
+    def __init__(self, app):
+        self.app = app
+        self.threadPool = app.threadPool
+        self.signals = Signals()
+        self.data = {}
+        self.defaultConfig = {"framework": "", "frameworks": {}}
+        self.framework = ""
+        self.run = {
+            "indi": SensorWeatherIndi(self.app, self.signals, self.data),
+            "alpaca": SensorWeatherAlpaca(self.app, self.signals, self.data),
+        }
+
+        if platform.system() == "Windows":
+            self.run["ascom"] = SensorWeatherAscom(self.app, self.signals, self.data)
+
+        for fw in self.run:
+            self.defaultConfig["frameworks"].update({fw: self.run[fw].defaultConfig})
+
+    @property
+    def updateRate(self):
+        return self.run[self.framework].updateRate
+
+    @updateRate.setter
+    def updateRate(self, value):
+        value = int(value)
+        for fw in self.run:
+            self.run[fw].updateRate = value
+
+    @property
+    def loadConfig(self):
+        return self.run[self.framework].loadConfig
+
+    @loadConfig.setter
+    def loadConfig(self, value):
+        value = bool(value)
+        for fw in self.run:
+            self.run[fw].loadConfig = value
+
+    def startCommunication(self) -> None:
+        """ """
+        self.run[self.framework].startCommunication()
+
+    def stopCommunication(self) -> None:
+        """ """
+        self.run[self.framework].stopCommunication()
---
+++ packages/mountwizzard4/mountwizzard4-4.0.0a21.tar.gz/mountwizzard4-4.0.0a21/mw4/logic/focuser/focuserAlpaca.py
@@ -0,0 +1,51 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PySide for python
+#
+# written in python3, (c) 2019-2025 by mworion
+# Licence APL2.0
+#
+###########################################################
+# standard libraries
+
+# external packages
+
+# local imports
+from base.alpacaClass import AlpacaClass
+
+
+class FocuserAlpaca(AlpacaClass):
+    """ """
+
+    def __init__(self, app=None, signals=None, data=None):
+        super().__init__(app=app, data=data)
+        self.signals = signals
+        self.data = data
+
+    def workerPollData(self) -> None:
+        """ """
+        if not self.deviceConnected:
+            return
+        self.getAndStoreAlpacaProperty(
+            "position", "ABS_FOCUS_POSITION.FOCUS_ABSOLUTE_POSITION"
+        )
+
+    def move(self, position: int) -> None:
+        """ """
+        if not self.deviceConnected:
+            return
+        self.setAlpacaProperty("move", Position=position)
+
+    def halt(self) -> None:
+        """ """
+        if not self.deviceConnected:
+            return
+        self.getAlpacaProperty("halt")
