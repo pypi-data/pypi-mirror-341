8f51daa7e83ef4c394ae2f89f425de3137c8eaa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:58 +0000

    Add package extract-thinker/0.1.12/extract_thinker-0.1.12-py3-none-any.whl

---
+++ packages/extract-thinker/extract_thinker-0.1.12-py3-none-any.whl/extract_thinker/models/classification.py
@@ -0,0 +1,36 @@
+from typing import Any, Optional, Type
+from pydantic import BaseModel, field_validator
+from extract_thinker.models.contract import Contract
+import os
+
+class Classification(BaseModel):
+    name: str
+    description: str
+    contract: Optional[Type] = None
+    extraction_contract: Optional[Type] = None
+    image: Optional[str] = None
+    extractor: Optional[Any] = None
+
+    @field_validator('contract', mode='before')
+    def validate_contract(cls, v):
+        if v is not None:
+            if not isinstance(v, type):
+                raise ValueError('contract must be a type')
+            if not issubclass(v, Contract):
+                raise ValueError('contract must be a subclass of Contract')
+        return v
+
+    @field_validator('extraction_contract', mode='before')
+    def validate_extraction_contract(cls, v):
+        if v is not None:
+            if not isinstance(v, type):
+                raise ValueError('extraction_contract must be a type')
+            if not issubclass(v, Contract):
+                raise ValueError('extraction_contract must be a subclass of Contract')
+        return v
+
+    def set_image(self, image_path: str):
+        if os.path.isfile(image_path):
+            self.image = image_path
+        else:
+            raise ValueError(f"The provided string '{image_path}' is not a valid file path.")
