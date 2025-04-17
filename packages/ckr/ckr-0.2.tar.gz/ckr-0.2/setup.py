6cb78bb0df3285ef8144cd48cc86bb788a0a5a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:18:46 +0000

    Add package ckr/0.2/ckr-0.2.tar.gz

---
+++ packages/ckr/ckr-0.2.tar.gz/ckr-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ckr',
+    version='0.2',
+    packages=find_packages(),
+    author='Joker',
+    author_email='jokerspubg993@gmail.com',
+    description='Simple',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
