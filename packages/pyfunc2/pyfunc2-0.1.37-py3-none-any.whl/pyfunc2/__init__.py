84abd3b51724e60c507b2e8b8705d91784c17f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:19:11 +0000

    Add package pyfunc2/0.1.37/pyfunc2-0.1.37-py3-none-any.whl

---
+++ packages/pyfunc2/pyfunc2-0.1.37-py3-none-any.whl/pyfunc2/__init__.py
@@ -0,0 +1,17 @@
+# Auto-generated __init__.py
+
+# Version of the pyfunc2 package
+import sys
+sys.path.append('../')
+from ._version import __version__
+
+# Import necessary modules and functions here
+from .all_lasts import all_lasts
+from .email_update import email_update
+from .img_to_base64 import img_to_base64
+from .lasts import lasts
+from .to_lower_case import convert_char
+from .to_lower_case import to_lower_case
+
+# Public API of the package
+__all__ = [all_lasts, email_update, img_to_base64, lasts, convert_char, to_lower_case]
