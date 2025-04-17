d32910c9739ae79294b833392befb218c66237a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 14:05:12 +0000

    Add package dumbpw/1.0.0/dumbpw-1.0.0.tar.gz

---
+++ packages/dumbpw/dumbpw-1.0.0.tar.gz/dumbpw-1.0.0/dumbpw/constants.py
@@ -0,0 +1,18 @@
+"""Provide constants and hard-coded values."""
+
+import string
+
+import deal
+
+
+deal.activate()
+deal.module_load(deal.pure)
+
+PASSWORD_LENGTH_MIN = 5
+PASSWORD_LENGTH_MAX = 64
+
+DEFAULT_DIGITS = string.digits
+DEFAULT_EXTRAS = string.punctuation
+DEFAULT_LOWERS = string.ascii_lowercase
+DEFAULT_UPPERS = string.ascii_uppercase
+DEFAULT_BLOCKS = """'";"""
