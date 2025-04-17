da11e2989b666704868c84765e9ca3fbf74ce7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 14:04:43 +0000

    Add package odoo14-addon-ssi-account-receivable-penalty/14.0.1.0.1/odoo14_addon_ssi_account_receivable_penalty-14.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-account-receivable-penalty/odoo14_addon_ssi_account_receivable_penalty-14.0.1.0.1-py3-none-any.whl/odoo/addons/ssi_account_receivable_penalty/models/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from . import (
+    account_receivable_penalty_type,
+    account_receivable_penalty_computation,
+    account_receivable_penalty_tax,
+    account_receivable_penalty,
+)
