01a02ec94ff67ca5fc924445cb4981d413af7b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 08:06:12 +0000

    Add package pyquibbler-labextension/0.3.6/pyquibbler_labextension-0.3.6-py3-none-any.whl

---
+++ packages/pyquibbler-labextension/pyquibbler_labextension-0.3.6-py3-none-any.whl/pyquibbler_labextension/_version.py
@@ -0,0 +1,24 @@
+import json
+from pathlib import Path
+
+__all__ = ["__version__"]
+HERE = Path(__file__).parent.resolve()
+
+
+def _fetchVersion():
+    for settings in HERE.rglob("package.json"):
+        try:
+            with settings.open() as f:
+                version = json.load(f)["version"]
+                return (
+                    version.replace("-alpha.", "a")
+                    .replace("-beta.", "b")
+                    .replace("-rc.", "rc")
+                )
+        except FileNotFoundError:
+            pass
+
+    raise FileNotFoundError(f"Could not find package.json under dir {HERE!s}")
+
+
+__version__ = _fetchVersion()
