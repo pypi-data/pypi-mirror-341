d13dd005b9b2b1fe3f88ee1c25615ea90fd1771b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:18:52 +0000

    Add package crunch-uml/0.2.21/crunch_uml-0.2.21-py3-none-any.whl

---
+++ packages/crunch-uml/crunch_uml-0.2.21-py3-none-any.whl/crunch_uml/renderers/jinja2renderer.py
@@ -0,0 +1,260 @@
+import html
+import json
+import logging
+import os
+import re
+
+import inflection
+import validators
+from charset_normalizer import from_bytes
+from jinja2 import Environment, FileSystemLoader
+
+import crunch_uml.schema as sch
+from crunch_uml import const, db, util
+from crunch_uml.exceptions import CrunchException
+from crunch_uml.renderers.renderer import ClassRenderer, ModelRenderer, RendererRegistry
+
+logger = logging.getLogger()
+
+
+@RendererRegistry.register(
+    "jinja2",
+    descr="Renderer that uses Jinja2 to renders one file per model in the database, "
+    + "where a model is a package that includes at least one Class. "
+    + ' Needs parameter "output_jinja2_template" and "output_jinja2_templatedir".',
+)
+class Jinja2Renderer(ModelRenderer):
+    """
+    Renders all model packages using jinja2 and a template.
+    A model package is a package with at least 1 class inside
+    """
+
+    templatedir = None
+    template = None
+    enforce_output_package_ids = False
+
+    def getTemplateAndDir(self, args):  # sourcery skip: raise-specific-error
+        # get templatedir to be used
+        if self.templatedir is not None:
+            templatedir = self.templatedir
+        elif args.output_jinja2_templatedir is not None:
+            templatedir = args.output_jinja2_templatedir
+        else:
+            # Use the virtual environment's template directory if no templatedir is provided
+            templatedir = util.find_module_path("crunch_uml")
+            if templatedir:
+                templatedir = os.path.join(templatedir, "templates")
+            if not os.path.isdir(templatedir):
+                templatedir = const.TEMPLATE_DIR
+
+        if not os.path.isdir(templatedir):
+            msg = f"Template directory with value {templatedir} does not exist, exiting"
+            logger.error(msg)
+            raise CrunchException(msg)
+        logger.debug(f"Rendering with templatedir {templatedir}")
+
+        # get template to be used
+        if self.template is not None:
+            template = self.template
+        elif args.output_jinja2_template is not None:
+            template = args.output_jinja2_template
+        else:
+            msg = "No template provided voor Jinja2, exiting"
+            logger.error(msg)
+            raise CrunchException(msg)
+        logger.debug(f"Rendering with template {template}")
+        return template, templatedir
+
+    def addFilters(self, env):
+        def fix_and_format(s):
+            if isinstance(s, bytes):
+                result = from_bytes(s).best()
+                if result:
+                    s = str(result)
+                else:
+                    return ""
+            elif not isinstance(s, str):
+                return ""
+
+            s = html.unescape(s)
+
+            def normalize_bullet(line):
+                # Check op opsommingsteken na eventuele inspringing
+                match = re.match(r"^(\s*)([-■•*·●◦‣›»▪–—])(\s+)(.*)", line)
+                if match:
+                    indent, _, spacing, rest = match.groups()
+                    return f"{indent}•{spacing}{rest}"
+                return line
+
+            lines = s.strip().splitlines()
+            lines = [normalize_bullet(line.rstrip()) for line in lines if line.strip() != ""]
+            return "<br>".join(lines)
+
+        # Voeg het inflection filter toe
+        env.filters["snake_case"] = lambda s: (inflection.underscore(s.replace(" ", "")) if isinstance(s, str) else "")
+        env.filters["pascal_case"] = lambda s: (inflection.camelize(s.replace(" ", "")) if isinstance(s, str) else "")
+        env.filters["camel_case"] = lambda s: (
+            inflection.camelize(s.replace(" ", ""), False) if isinstance(s, str) else ""
+        )
+        env.filters["pythonize"] = lambda s: (s.replace(" ", "").replace("-", "_") if isinstance(s, str) else "")
+        env.filters["md_newline"] = lambda s: (
+            s.replace("\n", "\n> ").replace("\r\n", "\r\n> ") if isinstance(s, str) else ""
+        )
+        env.filters["del_newline"] = lambda s: (s.replace("\n", " ").replace("\r\n", " ") if isinstance(s, str) else "")
+        env.filters["set_url"] = lambda s: f"[{s}]({s})" if validators.url(s) else s
+        env.filters["reject_method"] = lambda iterable, method_name: [
+            item for item in iterable if not getattr(item, method_name)()
+        ]
+        env.filters["sort_order"] = lambda rels: sorted(
+            rels if rels else [], key=lambda rel: float('inf') if rel.order is None else rel.order
+        )
+        env.filters["mdonize"] = lambda s: (
+            "<br>".join(line.strip() for line in s.strip().splitlines()) if isinstance(s, str) else ""
+        )
+        env.filters["fix_and_format"] = fix_and_format
+
+    def getFilename(self, inputfilename, extension, uml_generic):
+        return f"{inputfilename}_{uml_generic.name}{extension}"
+
+    def render(self, args, schema: sch.Schema):
+        # setup output filename
+        filename, extension = os.path.splitext(args.outputfile)
+
+        # get template and templatedir
+        template, templatedir = self.getTemplateAndDir(args)
+
+        # sourcery skip: raise-specific-error
+        # Settup environment for rendering using Jinja2
+        file_loader = FileSystemLoader(templatedir)
+        env = Environment(loader=file_loader)
+        self.addFilters(env)
+
+        # Check to see if a list of Package ids is provided
+        # if self.enforce_output_package_ids and args.output_package_ids is None:
+        #    msg = "Usage of parameter --output_package_ids is enforced for this renderer. Not provided, exiting."
+        #    logger.error(msg)
+        #    raise CrunchException(msg)
+
+        # Get list of packages that are to be rendered
+        models = self.getModels(args, schema)
+        if len(models) is None:
+            msg = "Cannot render output: packages do not exist"
+            logger.error(msg)
+            raise CrunchException(msg)
+
+        # Render all packages that are named
+        for index, package in enumerate(models):
+            # Render output
+            template = env.get_template(template)
+            output = template.render(package=package, args=args)
+
+            outputfilename = (
+                self.getFilename(filename, extension, package)
+                if package.name is not None
+                else f"{filename}_{index}{extension}"
+            )
+            with open(outputfilename, "w") as file:
+                file.write(output)
+
+
+@RendererRegistry.register(
+    "ggm_md",
+    descr="Renderer renders one basic markdown file per model in the database, "
+    + "where a model is a package that includes at least one Class. ",
+)
+class GGM_MDRenderer(Jinja2Renderer):
+    template = "ggm_markdown.j2"  # type: ignore
+    enforce_output_package_ids = True  # Enforce list of Package ids
+
+
+def getJSONDatatype(
+    self,  # "koppel_{{ associatie.name | snake_case }}_{{ associatie.id}}"
+):
+    if self.primitive is not None:
+        if str(self.primitive).lower().startswith("bool"):
+            return '"type": "boolean"'
+        elif str(self.primitive).lower().startswith("int"):
+            return '"type": "integer"'
+        elif str(self.primitive).lower().startswith("bedrag"):
+            return '"$ref": "#/$defs/bedrag"'  # Needs to be defined in Jinja2 template
+        elif "mail" in str(self.primitive).lower():
+            return '"$ref": "#/$defs/email"'  # Needs to be defined in Jinja2 template
+        elif str(self.primitive).lower() in ["tijd", "time"]:
+            return '"$ref": "#/$defs/tijd"'  # Needs to be defined in Jinja2 template
+        elif str(self.primitive).lower() in ["datum", "date"]:
+            return '"$ref": "#/$defs/datum"'  # Needs to be defined in Jinja2 template
+        elif str(self.primitive).lower() in ["datumtijd", "datetime"]:
+            return '"$ref": "#/$defs/datum-tijd"'  # Needs to be defined in Jinja2 template
+        else:
+            return '"type": "string"'
+    elif self.enumeration is not None:
+        return f'"$ref": "#/$defs/{self.enumeration.name}"'  # Needs to be defined in Jinja2 template
+    elif self.type_class is not None:
+        return f'"$ref": "#/$defs/{self.type_class.name}"'  # Needs to be defined in Jinja2 template
+    else:
+        return '"type": "string"'
+
+
+def getVerplichteAttributen(self):
+    set_verplicht = {attr.name for attr in self.attributes if attr.verplicht}
+    set_verplichr_rel = {
+        (
+            assoc.src_role.lower()
+            if assoc.src_role is not None and assoc.src_role != ""
+            else assoc.dst_class.name.lower()
+        )
+        for assoc in self.uitgaande_associaties
+        if assoc.isEnkelvoudig(dst=True) and assoc.isVerplicht(dst=True) and assoc.dst_class.name is not None
+    }
+    return list(set_verplicht.union(set_verplichr_rel))
+
+
+@RendererRegistry.register(
+    "json_schema",
+    descr="Renderer renders a JSON schema from a single package. ",
+)
+class JSON_SchemaRenderer(Jinja2Renderer, ClassRenderer):
+    template = "json_schema.j2"  # type: ignore
+    enforce_output_package_ids = True  # Enforce list of Package ids
+
+    def render(self, args, schema: sch.Schema):
+        try:
+            # Add mthod to ger Correct JSON Datatypes from class instance
+            db.Attribute.getJSONDatatype = getJSONDatatype  # No error!
+            db.Class.getVerplichteAttributen = getVerplichteAttributen  # No error!
+            # setup output filename
+            filename, extension = os.path.splitext(args.outputfile)
+
+            # get template and templatedir
+            template, templatedir = self.getTemplateAndDir(args)
+
+            # sourcery skip: raise-specific-error
+            # Settup environment for rendering using Jinja2
+            file_loader = FileSystemLoader(templatedir)
+            env = Environment(loader=file_loader)
+            self.addFilters(env)
+
+            # Get list of packages that are to be rendered
+            clazz = self.getClass(args, schema)
+
+            # Render output
+            template = env.get_template(template)
+            output = template.render(clazz=clazz, args=args)
+
+            # JSON-gegevens formatteren
+            try:
+                # Stap 1: Laad de JSON-string in een Python dictionary
+                data = json.loads(output)
+                formatted_json = json.dumps(data, indent=4)
+            except Exception as ex:
+                formatted_json = output
+                logger.warning(f"Exception Could not format JSON data with message {ex}")
+
+            outputfilename = (
+                self.getFilename(filename, extension, clazz) if clazz.name is not None else f"{filename}{extension}"
+            )
+            with open(outputfilename, "w") as file:
+                file.write(formatted_json)
+        finally:
+            del db.Attribute.getJSONDatatype  # No error!
+            del db.Class.getVerplichteAttributen  # No error!
---
+++ packages/crunch-uml/crunch_uml-0.2.21-py3-none-any.whl/crunch_uml/renderers/pandasrenderer.py
@@ -0,0 +1,268 @@
+import json
+import logging
+import os
+
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.hybrid import hybrid_property
+
+import crunch_uml.schema as sch
+from crunch_uml import const, db, lang, util
+from crunch_uml.renderers.renderer import Renderer, RendererRegistry
+
+logger = logging.getLogger()
+
+
+def object_as_dict(obj, session):
+    """
+    Converteert een SQLAlchemy-modelobject naar een dictionary, inclusief hybride attributen en kolommen.
+    :param obj: Het SQLAlchemy-object.
+    :return: Dictionary met alle kolom- en hybride attributen.
+    """
+    # Haal kolom-attributen op
+    col_attrs = {attr.key for attr in sqlalchemy.inspect(obj).mapper.column_attrs}
+    hyb_attrs = {
+        attr
+        for attr in dir(obj.__class__)
+        if hasattr(getattr(obj.__class__, attr), 'descriptor')
+        and isinstance(getattr(obj.__class__, attr).descriptor, hybrid_property)
+    }
+    attrs = col_attrs | hyb_attrs
+
+    dict_obj = {key: getattr(obj, key) for key in dir(obj) if key in attrs and hasattr(obj, key)}
+
+    # Dirty hack, but only way: set package domain name to domain_iv3
+    if const.COLUMN_DOMEIN_IV3 in attrs and (isinstance(obj, db.Class) or isinstance(obj, db.Enumeratie)):
+        package = (
+            session.query(db.Package)
+            .filter(db.Package.id == obj.package_id, db.Package.schema_id == obj.schema_id)
+            .one_or_none()
+        )
+        dict_obj[const.COLUMN_DOMEIN_IV3] = package.domain_name if package is not None else obj.domein_iv3
+    dict_obj[const.COLUMN_DOMEIN_GGM_UML_TYPE] = obj.__class__.__name__
+    dict_obj[const.COLUMN_DOMEIN_DATUM_TIJD_EXPORT] = util.current_time_export()
+
+    return dict_obj
+
+
+@RendererRegistry.register(
+    "json",
+    descr="Renders JSON document where each element corresponds to one of the tables in the datamodel.",
+)
+class JSONRenderer(Renderer):
+    def get_record_type(self):
+        return const.RECORD_TYPE_RECORD
+
+    def get_all_data(self, args, schema: sch.Schema, empty_values=True):
+        # Retrieve all models dynamically
+        base = db.Base
+        models = base.metadata.tables
+        session = schema.get_session()
+        all_data = {}
+
+        # Define the list of column names to include in the output
+        # If this list is empty, all columns will be included
+        included_columns = self.get_included_columns(args)
+
+        for table_name, table in models.items():
+            # Model class associated with the table
+            model = base.model_lookup_by_table_name(table_name)
+            if (
+                not model or self.get_record_type() == const.RECORD_TYPE_INDEXED and "id" not in model.__table__.columns
+            ):  # In case of a junction table
+                continue
+
+            records = session.query(model).filter(model.schema_id == schema.schema_id).all()
+            data = [object_as_dict(record, session) for record in records]
+
+            # Filter columns based on included_columns, unless included_columns is empty
+            filtered_data = []
+            for record in data:
+                if included_columns and len(included_columns) > 0:
+                    filtered_record = {
+                        key: value
+                        for key, value in record.items()
+                        if key in included_columns and (empty_values or value is not None)
+                    }
+                    filtered_record = util.reorder_dict(filtered_record, included_columns)
+                else:
+                    filtered_record = record  # Include all columns if included_columns is empty
+                if len(filtered_record) > 0:
+                    filtered_data.append(
+                        filtered_record
+                        if self.get_record_type() == const.RECORD_TYPE_RECORD
+                        else {record["id"]: filtered_record}
+                    )
+
+            all_data[table_name] = filtered_data
+        return all_data
+
+    def rename_keys(self, input_dict, key_mapper):
+        """
+        Hernoem keys in een dictionary (inclusief geneste dictionaries) volgens een gegeven mapper.
+        :param input_dict: De originele dictionary waarvan de keys moeten worden hernoemd.
+        :param key_mapper: Een dictionary waarin oude keys worden gekoppeld aan nieuwe keys.
+        :return: Een nieuwe dictionary met hernoemde keys.
+        """
+        if key_mapper is None or len(key_mapper) == 0:
+            return input_dict
+
+        renamed_dict = {}
+        for k, v in input_dict.items():
+            # Hernoem de key volgens de mapper
+            new_key = key_mapper.get(k, k)
+
+            # Controleer of de waarde een geneste dictionary is
+            if isinstance(v, dict):
+                # Roep de functie recursief aan voor de geneste dictionary
+                renamed_dict[new_key] = self.rename_keys(v, key_mapper)
+            elif isinstance(v, list):
+                # Als de waarde een lijst is, controleer of elementen dictionaries zijn
+                renamed_dict[new_key] = [
+                    self.rename_keys(item, key_mapper) if isinstance(item, dict) else item for item in v
+                ]
+            else:
+                # Anders gebruik de originele waarde
+                renamed_dict[new_key] = v
+        return renamed_dict
+
+    def render(self, args, schema: sch.Schema):
+        all_data = self.get_all_data(args, schema)
+        all_data = self.rename_keys(all_data, json.loads(args.mapper))
+        with open(args.outputfile, "w") as json_file:
+            json.dump(all_data, json_file, default=str, indent=4, sort_keys=True)
+
+
+@RendererRegistry.register(
+    "i18n",
+    descr=(
+        "Renders a i18n file containing all tables with keys to the translatable fields"
+        f" ({const.LANGUAGE_TRANSLATE_FIELDS}) Also translates to a specified language."
+    ),
+)
+class I18nRenderer(JSONRenderer):
+    def get_included_columns(self, args):
+        # Define the list of column names to include in the output
+        # If this list is empty, all columns will be included
+        return const.LANGUAGE_TRANSLATE_FIELDS
+
+    def get_record_type(self):
+        return const.RECORD_TYPE_INDEXED
+
+    def translate_data(self, data, to_language, from_language="auto", update_i18n=True, original_i18n={}):
+        logger.info(
+            f"Starting Translating data to language '{to_language}'. This may take a while:"
+            f" {util.count_dict_elements(data)} entries..."
+        )
+        translated_data = {}
+        for section, entries in data.items():
+            logger.info(f"Translating section {section}...")
+            translated_data[section] = []
+            original_i18n_section = original_i18n.get(to_language, {}).get(section, [])
+            for entry in entries:
+                translated_record = {}
+                for key, record in entry.items():
+                    original_record = [record for record in original_i18n_section if key in record]
+                    original_record = original_record[0] if len(original_record) > 0 else {}
+                    original_record = original_record.get(key, {})
+                    for field, value in record.items():
+                        if not isinstance(value, str):
+                            next
+                        if not (util.is_empty_or_none(value)):
+                            if update_i18n:
+                                # Check if the field is already translated
+                                translated_value = original_record.get(field, None)
+                                if translated_value is None:
+                                    # Translate the value
+                                    translated_value = lang.translate(
+                                        value,
+                                        to_language=to_language,
+                                        from_language=from_language,
+                                        max_retries=1,
+                                    )
+                                translated_record[field] = translated_value
+                            else:
+                                translated_record[field] = lang.translate(  #
+                                    value,
+                                    to_language=to_language,
+                                    from_language=from_language,
+                                    max_retries=1,
+                                )
+                translated_data[section].append({key: translated_record})
+
+        logger.info(f"Finished translating data to language '{to_language}'.")
+        return translated_data
+
+    def render(self, args, schema: sch.Schema):
+
+        logger.info(f"Starting rendering i18n file {args.outputfile}...")
+
+        # Initialize the i18n structure
+        i18n_data = {}
+        if os.path.exists(args.outputfile):
+            # If the file exists, check if it's a valid JSON (i18n) file and load it
+            with open(args.outputfile, "r", encoding="utf-8") as json_file:
+                try:
+                    i18n_data = json.load(json_file)
+                except json.JSONDecodeError:
+                    raise ValueError(f"The file {args.outputfile} is not a valid JSON file.")
+
+            if not isinstance(i18n_data, dict):
+                raise ValueError(f"The file {args.outputfile} does not contain a valid i18n structure.")
+
+        # Retrieve all data
+        all_data = self.get_all_data(args, schema, empty_values=False)
+        if args.translate:
+            all_data = self.translate_data(
+                all_data, args.language, from_language=args.from_language, update_i18n=True, original_i18n=i18n_data
+            )
+
+        # Update the i18n data with the new language entry
+        i18n_data[args.language] = all_data
+
+        # Map fields
+        i18n_data = self.rename_keys(i18n_data, json.loads(args.mapper))
+
+        # Controleer of het bestand al bestaat
+        if not os.path.exists(args.outputfile):
+            logger.info(f"Vertaalbestand {args.outputfile} bestaat niet, maak een nieuw bestand aan...")
+
+        # Write the updated i18n data back to the file
+        with open(args.outputfile, "w", encoding="utf-8") as json_file:
+            json.dump(i18n_data, json_file, ensure_ascii=False, indent=4, default=str)
+
+        logger.info(f"Rendering i18n file {args.outputfile} success")
+
+
+@RendererRegistry.register(
+    "csv",
+    descr="Renders multiple CSV files where each file corresponds to one of the tables in the datamodel.",
+)
+class CSVRenderer(Renderer):
+    def render(self, args, schema: sch.Schema):
+        # Retrieve all models dynamically
+        base = db.Base
+        models = base.metadata.tables
+        session = schema.get_session()
+        entity_name = args.entity_name
+
+        for table_name, table in models.items():
+            # Model class associated with the table
+            model = base.model_lookup_by_table_name(table_name)
+            if not model:  # In geval van koppeltabel
+                continue
+
+            # check of entity_name is gegeven en niet gelijk is aan table_name
+            if entity_name is not None and entity_name != table_name:
+                continue
+
+            # Retrieve data
+            records = session.query(model).filter(model.schema_id == schema.schema_id).all()
+            df = pd.DataFrame([object_as_dict(record, session) for record in records])
+
+            # Map columns
+            mapper = json.loads(args.mapper)
+            if mapper is not None and len(mapper) > 0:
+                df = df.rename(columns=mapper)
+
+            df.to_csv(f"{args.outputfile}_{table_name}.csv", index=False)
