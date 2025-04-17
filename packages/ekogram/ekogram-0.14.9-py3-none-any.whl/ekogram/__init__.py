b671b1634343ec88c753cee8c9ff1c6ef79bb9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 07:05:42 +0000

    Add package ekogram/0.14.9/ekogram-0.14.9-py3-none-any.whl

---
+++ packages/ekogram/ekogram-0.14.9-py3-none-any.whl/ekogram/__init__.py
@@ -0,0 +1,2 @@
+from .ekogram import Bot, Markup, InputFile, InputMediaAudio, InputMediaDocument, InputMediaVideo, InputMediaAnimation, InputMediaPhoto, MessageEntity, InlineQuery, Pollinations, ChatGPT
+__all__ = ['Bot', 'Markup', 'InputFile', 'InputMediaAnimation', 'InputMediaPhoto', 'InputMediaVideo', 'InputMediaAudio', 'InputMediaDocument', 'MessageEntity', 'InlineQuery', 'Pollinations', 'ChatGPT']
