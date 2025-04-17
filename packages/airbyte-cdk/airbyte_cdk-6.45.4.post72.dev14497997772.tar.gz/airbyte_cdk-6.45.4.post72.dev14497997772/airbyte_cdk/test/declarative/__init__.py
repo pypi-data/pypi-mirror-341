049b29bde2165cc93bf47108a6ec97e874229619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:34 +0000

    Add package airbyte-cdk/6.45.4.post72.dev14497997772/airbyte_cdk-6.45.4.post72.dev14497997772.tar.gz

---
+++ packages/airbyte-cdk/airbyte_cdk-6.45.4.post72.dev14497997772.tar.gz/airbyte_cdk-6.45.4.post72.dev14497997772/airbyte_cdk/test/declarative/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+"""Declarative tests framework.
+
+This module provides fixtures and utilities for testing Airbyte sources and destinations
+in a declarative way.
+"""
