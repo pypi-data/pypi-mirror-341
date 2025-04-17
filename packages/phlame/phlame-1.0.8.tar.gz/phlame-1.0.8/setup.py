463baa683fe18b2421c251d0d6a556f85688190d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:36 +0000

    Add package phlame/1.0.8/phlame-1.0.8.tar.gz

---
+++ packages/phlame/phlame-1.0.8.tar.gz/phlame-1.0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='phlame',
+    version='1.0.8',
+    packages=find_packages(),
+    description='Novelty-aware intraspecies profiling of metagenome samples',
+    url='https://github.com/quevan/phlame',
+    author='Evan Qu',
+    author_email='equ@mit.edu',
+    license='MIT',
+    scripts=['bin/phlame'],
+    python_requires='>=3.8, <3.13',
+    install_requires=[
+    'numpy',
+    'pandas',
+    'seaborn',
+    'matplotlib',
+    'biopython',
+    'ete3',
+    'statsmodels',
+    'pytest',
+    ],
+
+)
