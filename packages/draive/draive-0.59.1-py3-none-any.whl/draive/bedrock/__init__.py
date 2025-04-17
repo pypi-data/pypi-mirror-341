dfd4f3a7e0895416fe07307a93c3fe1ee8ef10b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:53 +0000

    Add package draive/0.59.1/draive-0.59.1-py3-none-any.whl

---
+++ packages/draive/draive-0.59.1-py3-none-any.whl/draive/bedrock/__init__.py
@@ -0,0 +1,11 @@
+from draive.bedrock.client import BedrockClient
+from draive.bedrock.config import BedrockChatConfig
+from draive.bedrock.lmm import bedrock_lmm
+from draive.bedrock.types import BedrockException
+
+__all__ = (
+    "BedrockChatConfig",
+    "BedrockClient",
+    "BedrockException",
+    "bedrock_lmm",
+)
---
+++ packages/draive/draive-0.59.1-py3-none-any.whl/draive/bedrock/config.py
@@ -0,0 +1,14 @@
+from collections.abc import Sequence
+
+from haiway import MISSING, Missing, State
+
+__all__ = ("BedrockChatConfig",)
+
+
+class BedrockChatConfig(State):
+    model: str
+    temperature: float = 1.0
+    top_p: float | Missing = MISSING
+    max_tokens: int | Missing = MISSING
+    timeout: float | Missing = MISSING
+    stop_sequences: Sequence[str] | Missing = MISSING
