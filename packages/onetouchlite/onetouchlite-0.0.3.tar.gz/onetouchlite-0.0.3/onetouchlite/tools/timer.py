4bad9cef111acede8a794916a0f8a8d7ab633311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:51 +0000

    Add package onetouchlite/0.0.3/onetouchlite-0.0.3.tar.gz

---
+++ packages/onetouchlite/onetouchlite-0.0.3.tar.gz/onetouchlite-0.0.3/onetouchlite/tools/timer.py
@@ -0,0 +1,80 @@
+import functools
+import os
+import platform
+import time
+import ctypes
+
+
+def timer(func):
+    """
+    装饰器：记录函数的运行时长
+    """
+    @functools.wraps(func)  # 保留原函数的元信息
+    def wrapper(*args, **kwargs):
+        start_time = time.time()  # 记录开始时间
+        result = func(*args, **kwargs)  # 调用原函数
+        end_time = time.time()  # 记录结束时间
+        duration = end_time - start_time  # 计算运行时长
+        print(f"Function {func.__name__} took {duration:.6f} seconds to run.")
+        return result
+    return wrapper
+
+
+def format_time(seconds):
+    return f"{int(seconds // 60):02d}:{int(seconds % 60):02d}"
+
+
+def load_progress_lib():
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../utils"))
+    system = platform.system()
+
+    if system == "Windows":
+        libname = "progress.dll"
+    elif system == "Linux":
+        libname = "libprogress.so"
+    else:
+        raise RuntimeError(f"Unsupported platform: {system}")
+
+    lib_path = os.path.join(base_dir, libname)
+    return ctypes.CDLL(lib_path)
+
+
+class Progress:
+    def __init__(self, total):
+        self.obj = lib.create_progress(total)
+        self.total = total
+
+    def update(self, value):
+        lib.update_progress(self.obj, value)
+
+    def finish(self):
+        lib.finish_progress(self.obj)
+
+
+def pace(it, min_interval=0.05):
+    total = len(it)
+    lib.init_progress(total)
+    last_time = time.perf_counter()
+
+    for idx, val in enumerate(it):
+        yield val
+        now = time.perf_counter()
+        is_last = idx + 1 == total
+        if (now - last_time >= min_interval) or is_last:
+            lib.update_progress(idx + 1)
+            last_time = now
+
+
+lib = load_progress_lib()
+
+# 设置函数参数类型
+lib.init_progress.argtypes = [ctypes.c_int]
+lib.update_progress.argtypes = [ctypes.c_int]
+
+
+# 示例用法
+if __name__ == "__main__":
+    for i in pace(range(10_000_000)):
+        pass
+
+
---
+++ packages/onetouchlite/onetouchlite-0.0.3.tar.gz/onetouchlite-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='onetouchlite',  # 包名
+    version='0.0.3',  # 版本号
+    author='17fine',  # 作者
+    author_email='2756601885@qq.com',  # 作者邮箱
+    description='一个简单的轻量，集成了大多数OneTouch用法的工具包',  # 包的简短描述
+    long_description=open('README.md', encoding="utf8").read(),  # 读取 README.md 文件作为长描述
+    long_description_content_type='text/markdown',  # 长描述的内容类型
+    url='https://gitee.com/SunnyB0y/onetouch',  # 包的官网地址
+    packages=find_packages(),  # 自动查找包内的所有包和子包
+    package_data={
+        'onetouchlite': ['utils/progress.dll', 'utils/progress.so'],  # 包含 C 扩展文件
+    },
+    install_requires=[  # 依赖的包列表
+
+    ],
+    classifiers=[  # 分类信息
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Python版本要求
+)
