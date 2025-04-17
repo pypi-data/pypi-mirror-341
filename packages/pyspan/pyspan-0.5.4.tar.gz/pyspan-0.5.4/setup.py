faab032a5be896cb607268bcff500081ce2fb83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:50 +0000

    Add package pyspan/0.5.4/pyspan-0.5.4.tar.gz

---
+++ packages/pyspan/pyspan-0.5.4.tar.gz/pyspan-0.5.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyspan",
+    version="0.5.4",
+    packages=find_packages(),
+    description="A Python package for efficient data cleaning and preprocessing",
+    long_description=open("README.md", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    author="Noor Surani, Amynah Reimoo",
+    author_email="nsurani@hotmail.com, amynahreimoo@gmail.com",
+    license="MIT",
+    url="https://github.com/pyspan24/pyspan_py.git",
+    install_requires=[
+       "numpy>=1.23.2,<3.0.0",
+       "pandas<=2.2.2",
+       "pyspellchecker==0.8.1",
+       "scikit-learn<=1.6.1",
+       "statsmodels>=0.14.4"
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
