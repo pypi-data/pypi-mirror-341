2f2b5c7f9bddbe55c60462a66d06332129bf3d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 16:05:51 +0000

    Add package peakshaving-analyzer/0.0.3/peakshaving_analyzer-0.0.3-py3-none-any.whl

---
+++ packages/peakshaving-analyzer/peakshaving_analyzer-0.0.3-py3-none-any.whl/peakshaving_analyzer/__init__.py
@@ -0,0 +1,16 @@
+# __init__.py
+
+import logging
+
+from peakshaving_analyzer.config import Config
+from peakshaving_analyzer.DBHandler import DatabaseHandler
+from peakshaving_analyzer.PSA import PeakShavingAnalyzer
+
+logging.basicConfig(level=logging.WARNING)
+
+"""
+PeakShaverAnalyzer package initialization.
+"""
+
+__all__ = ["PeakShavingAnalyzer", "Config", "DatabaseHandler"]
+__version__ = "0.0.3"
