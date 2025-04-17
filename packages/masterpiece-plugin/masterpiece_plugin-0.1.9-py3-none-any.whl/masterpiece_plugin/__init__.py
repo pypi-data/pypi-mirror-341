a9d38592429c0c1d9c1914f580512c7b087d1d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 15:05:20 +0000

    Add package masterpiece-plugin/0.1.9/masterpiece_plugin-0.1.9-py3-none-any.whl

---
+++ packages/masterpiece-plugin/masterpiece_plugin-0.1.9-py3-none-any.whl/masterpiece_plugin/__init__.py
@@ -0,0 +1,13 @@
+"""
+Description
+===========
+
+A simple demonstration plugin designed to extend `masterpiece` applications.
+
+"""
+
+from .hello_world import HelloWorld
+
+__all__ = [
+    "HelloWorld",
+]
