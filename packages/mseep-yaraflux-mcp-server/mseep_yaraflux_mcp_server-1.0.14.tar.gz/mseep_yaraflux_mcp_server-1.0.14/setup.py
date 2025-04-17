bdfa4b5aad323b149cfcf79e7a7dded01ad2d60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:18:42 +0000

    Add package mseep-yaraflux-mcp-server/1.0.14/mseep_yaraflux_mcp_server-1.0.14.tar.gz

---
+++ packages/mseep-yaraflux-mcp-server/mseep_yaraflux_mcp_server-1.0.14.tar.gz/mseep_yaraflux_mcp_server-1.0.14/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""Setup script for YaraFlux MCP Server."""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+    name="mseep-yaraflux_mcp_server",
+    )
