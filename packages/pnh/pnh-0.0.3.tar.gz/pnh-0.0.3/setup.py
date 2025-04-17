383b7f23bd9fa1c9b139e703e8ec93b17744a29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:05:04 +0000

    Add package pnh/0.0.3/pnh-0.0.3.tar.gz

---
+++ packages/pnh/pnh-0.0.3.tar.gz/pnh-0.0.3/setup.py
@@ -0,0 +1,10 @@
+#from distutils.core import setup # type: ignore
+from setuptools import setup, find_packages
+
+setup(name='pnh',
+      version='0.0.3',
+      description='pnh',
+      packages=find_packages(),
+      #package_dir={
+      # 'pnh':'.','utils':'.\utils'},
+     )
