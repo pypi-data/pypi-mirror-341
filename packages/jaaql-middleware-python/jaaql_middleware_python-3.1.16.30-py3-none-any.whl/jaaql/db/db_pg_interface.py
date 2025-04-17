101b2269e8fac1115a34991577165a3be43dda2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:05:09 +0000

    Add package jaaql-middleware-python/3.1.16.30/jaaql_middleware_python-3.1.16.30-py3-none-any.whl

---
+++ packages/jaaql-middleware-python/jaaql_middleware_python-3.1.16.30-py3-none-any.whl/jaaql/db/db_pg_interface.py
@@ -0,0 +1,138 @@
+from psycopg import OperationalError
+from psycopg_pool import ConnectionPool
+
+import traceback
+
+from jaaql.db.db_interface import DBInterface, ECHO__none, CHAR__newline
+from jaaql.exceptions.http_status_exception import *
+from jaaql.exceptions.custom_http_status import CustomHTTPStatus
+from jaaql.constants import USERNAME__jaaql
+
+ERR__connect_db = "Could not create connection to database!"
+
+PGCONN__min_conns = 5
+PGCONN__max_conns = 1
+PGCONN__max_conns_jaaql_user = 10
+
+ALLOWABLE_COMMANDS = ["SELECT ", "INSERT ", "UPDATE ", "DELETE "]
+
+ERR__command_not_allowed = "Command not allowed. Please use one of " + str(ALLOWABLE_COMMANDS)
+
+
+class DBPGInterface(DBInterface):
+
+    HOST_POOL = None
+    HOST_USER = None
+
+    def __init__(self, config, host: str, port: int, db_name: str, username: str, password: str, is_jaaql_user: bool,
+                 dev_mode: bool):
+        super().__init__(config, host, username, dev_mode)
+
+        self.output_query_exceptions = config["DEBUG"]["output_query_exceptions"].lower() == "true"
+
+        # Created connection pool, allowing for 1 connection for this specific user
+        # Allows for the lookup of multiple users at the same time when providing jaaql user
+        try:
+            conn_str = "user=" + username + " password=" + password + " dbname=" + db_name
+            self.is_host_pool = False
+            # Important we don't list the host as this will force a unix socket
+            if host not in ['localhost', '127.0.0.1']:
+                conn_str += " host=" + host
+            else:
+                self.is_host_pool = True
+            self.is_host_pool = True
+            if str(port) != "5432":
+                conn_str += " port=" + str(port)
+
+            self.username = username
+
+            if self.is_host_pool:
+                if DBPGInterface.HOST_POOL is None:
+                    DBPGInterface.HOST_POOL = ConnectionPool(conn_str, min_size=PGCONN__min_conns, max_size=PGCONN__max_conns_jaaql_user,
+                                                             max_lifetime=60 * 30)
+                    DBPGInterface.HOST_USER = self.username
+                self.pg_pool = DBPGInterface.HOST_POOL
+            else:
+                self.pg_pool = ConnectionPool(conn_str, min_size=1, max_size=5)
+        except OperationalError as ex:
+            if "does not exist" in str(ex).split("\"")[-1]:
+                raise HttpStatusException(str(ex), CustomHTTPStatus.DATABASE_NO_EXIST)
+            else:
+                raise HttpStatusException(str(ex))
+
+    def get_conn(self):
+        try:
+            conn = self.pg_pool.getconn()
+            if DBPGInterface.HOST_USER != self.username and self.is_host_pool:
+                with conn.cursor() as cursor:
+                    cursor.execute("SET SESSION AUTHORIZATION '" + self.username + "';")
+                    self.commit(conn)
+            if conn is None:
+                raise Exception
+        except Exception as ex:
+            traceback.print_exc()
+            raise HttpStatusException(ERR__connect_db, HTTPStatus.INTERNAL_SERVER_ERROR)
+
+        return conn
+
+    def put_conn(self, conn):
+        if self.is_host_pool and DBPGInterface.HOST_USER != self.username:
+            with conn.cursor() as cursor:
+                cursor.execute("RESET SESSION AUTHORIZATION;")
+                self.commit(conn)
+        return self.pg_pool.putconn(conn)
+
+    def close(self, force: bool = False):
+        if not self.is_host_pool or force:
+            self.pg_pool.close()
+            print("Closing host pool")
+
+            if self.is_host_pool:
+                DBPGInterface.HOST_POOL = None
+                DBPGInterface.HOST_USER = None
+                print("Wiping host pool")
+
+    def execute_query(self, conn, query, parameters=None):
+        while True:
+            try:
+                with conn.cursor() as cursor:
+                    do_prepare = False
+                    # if DBPGInterface.HOST_USER != self.username and self.username != USERNAME__jaaql:
+                    #     do_prepare = True
+                    #     if not any([query.upper().startswith(ok_command) for ok_command in ALLOWABLE_COMMANDS]):
+                    #         raise HttpStatusException(ERR__command_not_allowed)
+
+                    if parameters is None or len(parameters.keys()) == 0:
+                        cursor.execute(query, prepare=do_prepare)
+                    else:
+                        cursor.execute(query, parameters, prepare=do_prepare)
+                    if cursor.description is None:
+                        return [], []
+                    else:
+                        return [desc[0] for desc in cursor.description], cursor.fetchall()
+            except OperationalError as ex:
+                if ex.sqlstate is None or ex.sqlstate.startswith("08"):
+                    traceback.print_exc()
+                    self.pg_pool.putconn(conn)
+                    self.pg_pool.check()
+                    conn = self.get_conn()
+                else:
+                    if self.output_query_exceptions:
+                        traceback.print_exc()
+                    raise ex
+            except Exception as ex:
+                if self.output_query_exceptions:
+                    traceback.print_exc()
+                raise ex
+
+    def commit(self, conn):
+        conn.commit()
+
+    def rollback(self, conn):
+        conn.rollback()
+
+    def handle_db_error(self, err, echo):
+        err = str(err)
+        if echo != ECHO__none:
+            err += CHAR__newline + echo
+        raise HttpStatusException(str(err), HTTPStatus.BAD_REQUEST)
---
+++ packages/jaaql-middleware-python/jaaql_middleware_python-3.1.16.30-py3-none-any.whl/jaaql/jaaql.py
@@ -0,0 +1,138 @@
+from logging import StreamHandler
+from jaaql.constants import ENVIRON__vault_key, ENVIRON__jaaql_profiling
+import re
+import logging
+import pkgutil
+from jaaql.openapi.swagger_documentation import produce_all_documentation
+from jaaql.email.email_manager_service import create_flask_app as create_email_service_app
+from jaaql.services.script_install import bootup
+from jaaql.utilities.options import *
+import sys
+import threading
+import os
+import jaaql.documentation as documentation
+from jaaql.utilities.utils import load_config, get_base_url
+from jaaql.mvc.controller import JAAQLController
+from jaaql.mvc.model import JAAQLModel
+from jaaql.mvc.controller_interface import JAAQLControllerInterface
+from jaaql.mvc.model_interface import JAAQLModelInterface
+from jaaql.config_constants import *
+
+
+DEFAULT__mfa_label = "test"
+
+WARNING__vault_key_stdin = "MAJOR SECURITY ISSUE! Passing vault key via program arguments is insecure as other progra" \
+                           "ms can see the arguments. Please provide via stdin instead!"
+
+WARNING__audit_off = "Audit trail is off. Logs will still be kept by the internal postgres instance"
+
+
+class SensitiveHandler(StreamHandler):
+
+    PATTERN = r"(?:\?|&|;)([^=]+)=([^\s&|;]+)"
+
+    def emit(self, record):
+        msg = self.format(record)
+        msg = re.sub(self.PATTERN, lambda x: x[0][0:len(x[0]) - len(x[2])] + min(len(x[2]), 5) * '*', msg)
+        if "GET /internal/is_installed HTTP/1.1" not in msg:
+            print(msg)
+
+
+def dir_non_builtins(folder):
+    pkgpath = os.path.dirname(folder.__file__)
+    pre_dirs = [(loader, name) for loader, name, _ in pkgutil.walk_packages([pkgpath])]
+    dirs = []
+    for sub_dir in pre_dirs:
+        if sub_dir[1] in dir(folder):
+            dirs.append(getattr(folder, sub_dir[1]))
+        else:
+            dirs.append(sub_dir[0].find_module(sub_dir[1]).load_module(sub_dir[1]))
+    return [doc for doc in dirs if doc.__name__ != "builtins"]
+
+
+def create_app(is_gunicorn: bool = False, override_config_path: str = None, migration_db_interface=None,
+               migration_project_name: str = None, migration_folder: str = None, supplied_documentation = None,
+               controllers: [JAAQLControllerInterface] = None, models: [JAAQLModelInterface] = None, additional_options: List[Option] = None,
+               start_email_service: bool = False, **options):
+    if controllers is None:
+        controllers = []
+    if models is None:
+        models = []
+    if not isinstance(controllers, list):
+        controllers = [controllers]
+    if not isinstance(models, list):
+        models = [models]
+
+    if supplied_documentation is None:
+        supplied_documentation = []
+    if not isinstance(supplied_documentation, list):
+        supplied_documentation = [supplied_documentation]
+
+    if len(options) == 0 and not is_gunicorn:
+        options = parse_options(sys.argv, False, additional_options=additional_options)
+
+    if not is_gunicorn and start_email_service:
+        threading.Thread(target=create_email_service_app, args=[options.get(OPT_KEY__vault_key), options.get(OPT_KEY__email_credentials)],
+                         daemon=True).start()
+    if not is_gunicorn:
+        threading.Thread(target=bootup, args=[options.get(OPT_KEY__vault_key), False, OPT_KEY__local_install in options], daemon=True).start()
+
+    if OPT_KEY__vault_key in options:
+        print(WARNING__vault_key_stdin, file=sys.stderr)
+        vault_key = options[OPT_KEY__vault_key]
+    elif is_gunicorn:
+        vault_key = os.environ.get(ENVIRON__vault_key)
+    else:
+        vault_key = input("Input vault key: ")
+
+    logging.getLogger("werkzeug").handlers.append(SensitiveHandler())
+
+    config = load_config(is_gunicorn, override_config_path)
+
+    mfa_name = config[CONFIG_KEY__security][CONFIG_KEY_SECURITY__mfa_label]
+    if mfa_name == DEFAULT__mfa_label:
+        print("MFA label is set to default. This isn't a security issue but adds a nice name when added to "
+              "authenticator apps via QR codes. You can change in the config")
+
+    force_mfa = config[CONFIG_KEY__security][CONFIG_KEY_SECURITY__force_mfa] in ("true", "True", True, "TRUE")
+    do_audit = config[CONFIG_KEY__security][CONFIG_KEY_SECURITY__do_audit] in ("true", "True", True, "TRUE")
+    invite_only = config[CONFIG_KEY__security][CONFIG_KEY_SECURITY__invite_only] in ("true", "True", True, "TRUE")
+
+    # The following code sets it "properly" in case it is accessed incorrectly from elsewhere
+    config[CONFIG_KEY__security][CONFIG_KEY_SECURITY__force_mfa] = force_mfa
+    config[CONFIG_KEY__security][CONFIG_KEY_SECURITY__do_audit] = do_audit
+    config[CONFIG_KEY__security][CONFIG_KEY_SECURITY__invite_only] = invite_only
+
+    if not do_audit:
+        print(WARNING__audit_off, file=sys.stderr)
+
+    url = config[CONFIG_KEY__swagger][CONFIG_KEY_SWAGGER__url]
+
+    model = JAAQLModel(config, vault_key, migration_db_interface, migration_project_name, migration_folder,
+                       is_container=is_gunicorn, url=url)
+    controller = JAAQLController(model, is_gunicorn, get_base_url(config, is_gunicorn),
+                                 OPT_KEY__profiling in options or os.environ.get(ENVIRON__jaaql_profiling))
+    controller.create_app()
+
+    for sub_controller in controllers:
+        sub_controller.route(controller)
+
+    for sub_model in models:
+        sub_model.set_model(model)
+
+    doc_modules = dir_non_builtins(documentation)
+    all_docs = []
+    for doc in supplied_documentation:
+        all_docs.extend(dir_non_builtins(doc))
+
+    base_path = None
+    if is_gunicorn:
+        base_path = "www"
+        url += "/api/"
+
+    produce_all_documentation(doc_modules + all_docs, url, is_prod=is_gunicorn, base_path=base_path)
+
+    if is_gunicorn:
+        return controller.app
+    else:
+        return int(config[CONFIG_KEY__server][CONFIG_KEY_SERVER__port]), controller.app
