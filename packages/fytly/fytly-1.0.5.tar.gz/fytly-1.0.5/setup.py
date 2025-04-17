56512468481e5f074573b973d90bcd1cf8a853e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:19:14 +0000

    Add package fytly/1.0.5/fytly-1.0.5.tar.gz

---
+++ packages/fytly/fytly-1.0.5.tar.gz/fytly-1.0.5/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import sys
+import os
+
+
+
+# Read README safely
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="fytly",
+    version="1.0.5",
+    author="chowdeswari seelam",
+    author_email="chowdeswari_seelam@aegletek.com",
+    description='A grading component for keyword-based scoring for resume',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3',
+    packages=find_packages(where="py", include=["py", "py.*"]),
+    package_dir={'': 'py'},
+    license='MIT',
+    install_requires=[
+        'PyPDF2',
+        'python-docx',
+        'scikit-learn',
+        'pyspellchecker',
+        'stanza',
+        'nltk',
+        'rapidfuzz',
+        ],
+
+)
