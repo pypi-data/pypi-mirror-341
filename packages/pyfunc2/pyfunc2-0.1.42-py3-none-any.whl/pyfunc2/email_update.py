83fbe156a2b9cb1bf643decf375bec513754255d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:57 +0000

    Add package pyfunc2/0.1.42/pyfunc2-0.1.42-py3-none-any.whl

---
+++ packages/pyfunc2/pyfunc2-0.1.42-py3-none-any.whl/pyfunc2/email_update.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.path.append('../')
+
+from .email.download_all_attachments_in_inbox import download_all_attachments_in_inbox
+from .config.get_email_path import get_email_path
+
+
+def email_update(emails, storage_root="", limit=3):
+    for email in emails:
+        data_path = get_email_path(email["target"], storage_root)
+        download_all_attachments_in_inbox(email["server"], email["username"], email["password"], data_path, email["source"], limit)
