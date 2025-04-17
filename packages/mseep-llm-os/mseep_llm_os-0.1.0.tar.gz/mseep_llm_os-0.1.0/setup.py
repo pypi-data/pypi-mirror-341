8ce7460c7c769a3cada8b1288460169579cb09d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:36 +0000

    Add package mseep-llm-os/0.1.0/mseep_llm_os-0.1.0.tar.gz

---
+++ packages/mseep-llm-os/mseep_llm_os-0.1.0.tar.gz/mseep_llm_os-0.1.0/setup.py
@@ -0,0 +1,9 @@
+# A minimal setup.py file for supporting editable installs
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-llm-os",
+    packages=find_packages(),
+    version="0.1.0",
+)
