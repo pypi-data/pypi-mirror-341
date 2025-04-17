7af02c7846ebdb3289b2a5a3e99ad76fb3fb79cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:14 +0000

    Add package ablt-python-api/0.0.7/ablt_python_api-0.0.7-py3-none-any.whl

---
+++ packages/ablt-python-api/ablt_python_api-0.0.7-py3-none-any.whl/ablt_python_api/utils/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Filename: __init__.py
+Author: Iliya Vereshchagin
+Copyright (c) 2023 aBLT.ai. All rights reserved.
+
+Created: 03.11.2023
+Last Modified: 03.11.2023
+
+Description:
+This file describes entry point for aBLT chat API.
+"""
+
+from .exceptions import DoneException
+from .logger_config import setup_logger
