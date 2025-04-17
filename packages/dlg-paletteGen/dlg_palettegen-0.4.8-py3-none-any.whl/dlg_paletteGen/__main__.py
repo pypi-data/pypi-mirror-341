51e52a150f086f9b2fbcf0b9d4c7be2e2219e37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:19:07 +0000

    Add package dlg-paletteGen/0.4.8/dlg_palettegen-0.4.8-py3-none-any.whl

---
+++ packages/dlg-paletteGen/dlg_palettegen-0.4.8-py3-none-any.whl/dlg_paletteGen/__main__.py
@@ -0,0 +1,7 @@
+"""Entry point for dlg_paletteGen."""
+
+from dlg_paletteGen.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    # palette = main()
+    main()
