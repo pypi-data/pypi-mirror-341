1efbcc64f724aa534025e408f28940360b07f696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 07:05:10 +0000

    Add package app-toolkit-package/1.0.1/app_toolkit_package-1.0.1-py3-none-any.whl

---
+++ packages/app-toolkit-package/app_toolkit_package-1.0.1-py3-none-any.whl/toolkit/api/fastapi/responses.py
@@ -0,0 +1,36 @@
+from typing import Any
+
+from fastapi import status
+from pydantic import BaseModel
+
+from ...types_app import ResponseType
+
+
+def _response(status_code: int, message: str, description: Any) -> ResponseType:
+    class Message(BaseModel):
+        detail: str = message
+
+    return {status_code: {"model": Message, "description": description}}
+
+
+def response_400(message: str, description: str | None = None) -> ResponseType:
+    return _response(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        message=message,
+        description=description or message,
+    )
+
+
+def response_400_already_exists(name: str) -> ResponseType:
+    return response_400(
+        message=f"{name} already exists",
+        description="The item already exists",
+    )
+
+
+def response_404(name: str) -> ResponseType:
+    return _response(
+        status_code=status.HTTP_404_NOT_FOUND,
+        message=f"{name} not found",
+        description="The item was not found",
+    )
---
+++ packages/app-toolkit-package/app_toolkit_package-1.0.1-py3-none-any.whl/toolkit/config/__init__.py
@@ -0,0 +1,4 @@
+"""
+Toolkit package provides convenient tools for `Application configuration` development.
+This includes base/ready-to-use configurations for application, redis and working/test databases.
+"""
