417d4a19f86a58f0b8cbabc1b67ddeffc07241f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 14:04:43 +0000

    Add package pvoctopusdemo/2.0.2/pvoctopusdemo-2.0.2.tar.gz

---
+++ packages/pvoctopusdemo/pvoctopusdemo-2.0.2.tar.gz/pvoctopusdemo-2.0.2/README.md
@@ -0,0 +1,43 @@
+# Octopus Speech-to-Index engine
+
+Made in Vancouver, Canada by [Picovoice](https://picovoice.ai)
+
+This package contains commandline program for indexing and searching inside audio files using Octopus Speech-to-Index engine.
+
+## Octopus
+
+Octopus is Picovoice's Speech-to-Index engine. It directly indexes speech without relying on a text representation.
+
+## Compatibility
+
+- Python 3.9+
+- Runs on Linux (x86_64), macOS (x86_64, arm64), Windows (x86_64).
+
+## AccessKey
+
+Octopus requires a valid Picovoice `AccessKey` at initialization. `AccessKey` acts as your credentials when using Octopus SDKs.
+You can get your `AccessKey` for free. Make sure to keep your `AccessKey` secret.
+Signup or Login to [Picovoice Console](https://console.picovoice.ai/) to get your `AccessKey`.
+
+## Installation
+
+```console
+sudo pip3 install pvoctopusdemo
+```
+
+## Usage
+
+This demo allows testing Octopus interactively through commandline. It accepts MP3, WAV, FLAC, and OPUS audio files with sample rates higher than 16kHz. The following command processes all files in the space-separated `{AUDIO_PATHS}` list:
+
+```console
+octopus_demo  --access_key {AccessKey} --audio_paths ${AUDIO_PATHS}
+```
+where `{AccessKey}` is an AccessKey which should be obtained from [Picovoice Console](https://console.picovoice.ai/).
+
+After processing audio files, you are asked for a search phrase:
+
+```console
+Enter search phrase (Ctrl+c to exit):
+```
+
+The search phrase can have several words separated by space, but each word should only consist of alphabetic characters. As shown in the prompt above, press `Ctrl` and `C` keys at the same time to exit the program.
---
+++ packages/pvoctopusdemo/pvoctopusdemo-2.0.2.tar.gz/pvoctopusdemo-2.0.2/setup.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+
+import setuptools
+
+os.system('git clean -dfx')
+
+package_folder = os.path.join(os.path.dirname(__file__), 'pvoctopusdemo')
+os.mkdir(package_folder)
+
+shutil.copy(os.path.join(os.path.dirname(__file__), '../../LICENSE'), package_folder)
+
+shutil.copy(
+    os.path.join(os.path.dirname(__file__), 'octopus_demo.py'),
+    os.path.join(package_folder, 'octopus_demo.py'))
+
+with open(os.path.join(os.path.dirname(__file__), 'MANIFEST.in'), 'w') as f:
+    f.write('include pvoctopusdemo/LICENSE\n')
+    f.write('include pvoctopusdemo/octopus_demo.py\n')
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    long_description = f.read()
+
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), "r") as f:
+    dependencies = f.read().strip().splitlines()
+
+setuptools.setup(
+    name="pvoctopusdemo",
+    version="2.0.2",
+    author="Picovoice",
+    author_email="hello@picovoice.ai",
+    description="⚠️ DEPRECATED: This package is no longer maintained.",
+    long_description="This package has been deprecated.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Picovoice/octopus",
+    packages=["pvoctopusdemo"],
+    install_requires=dependencies,
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Sound/Audio :: Speech"
+    ],
+    entry_points=dict(
+        console_scripts=[
+            'octopus_demo=pvoctopusdemo.octopus_demo:main',
+        ],
+    ),
+    python_requires='>=3.9',
+    keywords="Speech-to-Index, Voice Search, Keyword Spotting, Speech Recognition, Voice Recognition",
+)
