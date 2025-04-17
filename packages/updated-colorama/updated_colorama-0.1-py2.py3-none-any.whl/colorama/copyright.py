b38cedf9fe9608dee13ef0cfa2afab6bb05061e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 17:06:20 +0000

    Add package updated-colorama/0.1/updated_colorama-0.1-py2.py3-none-any.whl

---
+++ packages/updated-colorama/updated_colorama-0.1-py2.py3-none-any.whl/colorama/copyright.py
@@ -0,0 +1,6 @@
+# RU:
+# Этот файл никак не влияет на работу библиотеки.
+# Этот файл создан чтобы сказать, что права на библиотеку принадлежат Jonathan Hartley.
+# EN:
+# This file does not affect the library's functionality.
+# This file is created to acknowledge that the rights to the library belong to Jonathan Hartley.
