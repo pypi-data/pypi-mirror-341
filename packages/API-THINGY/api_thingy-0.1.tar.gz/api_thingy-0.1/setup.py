69eac356c0941aed45d4860963c960bc82782d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 08:06:25 +0000

    Add package API-THINGY/0.1/api_thingy-0.1.tar.gz

---
+++ packages/API-THINGY/api_thingy-0.1.tar.gz/api_thingy-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="API-THINGY",  # The name of your package
+    version="0.1",
+    packages=find_packages(),  # Automatically finds the 'api_thingy' directory
+    install_requires=[  # Add any external libraries here
+        # "requests",  # Example if you have any dependencies
+    ],
+    description="A short description of your package",
+    author="Your Name",
+    author_email="youremail@example.com",
+    url="https://github.com/Concon6321/API-THINGY",  # Your GitHub URL
+)
