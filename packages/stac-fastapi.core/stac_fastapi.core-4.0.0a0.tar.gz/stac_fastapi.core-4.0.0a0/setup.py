f7de3fff0cf8b5c2fbf90aa356d35b5d79ddc495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 15:05:47 +0000

    Add package stac-fastapi.core/4.0.0a0/stac_fastapi.core-4.0.0a0.tar.gz

---
+++ packages/stac-fastapi.core/stac_fastapi.core-4.0.0a0.tar.gz/stac_fastapi.core-4.0.0a0/setup.py
@@ -0,0 +1,47 @@
+"""stac_fastapi: core elasticsearch/ opensearch module."""
+
+from setuptools import find_namespace_packages, setup
+
+with open("README.md") as f:
+    desc = f.read()
+
+install_requires = [
+    "fastapi",
+    "attrs>=23.2.0",
+    "pydantic",
+    "stac_pydantic==3.1.*",
+    "stac-fastapi.api==5.1.1",
+    "stac-fastapi.extensions==5.1.1",
+    "stac-fastapi.types==5.1.1",
+    "orjson",
+    "overrides",
+    "geojson-pydantic",
+    "pygeofilter==0.3.1",
+    "jsonschema",
+    "slowapi==0.1.9",
+]
+
+setup(
+    name="stac_fastapi.core",
+    description="Core library for the Elasticsearch and Opensearch stac-fastapi backends.",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+    ],
+    url="https://github.com/stac-utils/stac-fastapi-elasticsearch-opensearch",
+    license="MIT",
+    packages=find_namespace_packages(),
+    zip_safe=False,
+    install_requires=install_requires,
+)
