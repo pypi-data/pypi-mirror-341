ec15ebb16c1a99162e6def8a3fe85b965560c353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:40 +0000

    Add package waves-workflows/0.13.1/waves_workflows-0.13.1-py3-none-any.whl

---
+++ packages/waves-workflows/waves_workflows-0.13.1-py3-none-any.whl/waves/conftest.py
@@ -0,0 +1,53 @@
+import pathlib
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--system-test-dir",
+        action="store",
+        type=pathlib.Path,
+        default=None,
+        help="system test build directory root",
+    )
+    parser.addoption(
+        "--unconditional-build",
+        action="store_true",
+        default=False,
+        help="pass unconditional build option to system tests",
+    )
+    parser.addoption(
+        "--abaqus-command",
+        action="store",
+        type=pathlib.Path,
+        default=None,
+        help="Abaqus command for system test CLI pass through",
+    )
+    parser.addoption(
+        "--cubit-command",
+        action="store",
+        type=pathlib.Path,
+        default=None,
+        help="Cubit command for system test CLI pass through",
+    )
+
+
+@pytest.fixture
+def system_test_directory(request):
+    return request.config.getoption("--system-test-dir")
+
+
+@pytest.fixture
+def unconditional_build(request):
+    return request.config.getoption("--unconditional-build")
+
+
+@pytest.fixture
+def abaqus_command(request):
+    return request.config.getoption("--abaqus-command")
+
+
+@pytest.fixture
+def cubit_command(request):
+    return request.config.getoption("--cubit-command")
---
+++ packages/waves-workflows/waves_workflows-0.13.1-py3-none-any.whl/waves/docs/_sources/tutorial_wip.rst
@@ -0,0 +1,15 @@
+########################
+Work-in-progress Lessons
+########################
+
+.. toctree::
+   :maxdepth: 1
+
+   tutorial_writing_builders
+   tutorial_remote_execution
+   tutorial_sbatch
+   tutorial_extend_study
+   tutorial_task_reuse
+   tutorial_setuptools_scm
+   tutorial_quinoa
+   tutorial_gmsh_calculix
