b7f72d4c7994055af8f7e2df30086da17876bd04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:12 +0000

    Add package ByUsi-LoadingBar/1.0.0/byusi_loadingbar-1.0.0.tar.gz

---
+++ packages/ByUsi-LoadingBar/byusi_loadingbar-1.0.0.tar.gz/byusi_loadingbar-1.0.0/byusi-lbar/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2025 ByUsi. All rights reserved.
+
+from .core import (
+    ProgressBar,
+    BarStyle,
+    Element,
+    PRESET_STYLES
+)
+
+__version__ = "1.0.0"
+__all__ = ['ProgressBar', 'BarStyle', 'Element', 'PRESET_STYLES']
---
+++ packages/ByUsi-LoadingBar/byusi_loadingbar-1.0.0.tar.gz/byusi_loadingbar-1.0.0/setup.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2025 ByUsi. All rights reserved.
+
+from setuptools import setup
+
+setup(
+    name="ByUsi-LoadingBar",
+    version="1.0.0",
+    packages=["byusi-lbar"],
+    install_requires=["colorama>=0.4.4"],
+    author="ByUsi Development Team",
+    description="Professional-grade progress visualization system",
+    license="Proprietary",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+)
