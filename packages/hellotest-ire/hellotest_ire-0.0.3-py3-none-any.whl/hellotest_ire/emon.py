292d8e127deb7184ede0399b8be534e6438302b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 08:06:14 +0000

    Add package hellotest-ire/0.0.3/hellotest_ire-0.0.3-py3-none-any.whl

---
+++ packages/hellotest-ire/hellotest_ire-0.0.3-py3-none-any.whl/hellotest_ire/emon.py
@@ -0,0 +1,19 @@
+def hello():
+    print("Hello I am ABS EMON. This is my first python package!")
+
+def name():
+    print("ABS EMON")
+
+def add(a, b):
+    print(a + b)
+
+def sub(a, b):
+    print(a-b)
+
+def pattern(n):
+    for i in range(n+1):
+        print(i*"*")
+
+def namata(x):
+    for i in range(1, 11):
+        print(f"{x} x {i} = {x * i}")
