ebc3c638decc157536c319010e461ca4fe0c2e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:58 +0000

    Add package autonomize-autorag/0.1.49/autonomize_autorag-0.1.49.tar.gz

---
+++ packages/autonomize-autorag/autonomize_autorag-0.1.49.tar.gz/autonomize_autorag-0.1.49/autorag/language_models/__init__.py
@@ -0,0 +1,45 @@
+# pylint: disable=missing-module-docstring, duplicate-code
+
+import importlib
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from autorag.language_models.anthropic import (  # pragma: no cover
+        AnthropicLanguageModel,
+    )
+    from autorag.language_models.base import LanguageModel  # pragma: no cover
+    from autorag.language_models.modelhub import (  # pragma: no cover
+        ModelhubLanguageModel,
+    )
+    from autorag.language_models.ollama import OllamaLanguageModel  # pragma: no cover
+    from autorag.language_models.openai import (  # pragma: no cover
+        AzureOpenAILanguageModel,
+        OpenAILanguageModel,
+    )
+
+__all__ = [
+    "ModelhubLanguageModel",
+    "LanguageModel",
+    "OllamaLanguageModel",
+    "OpenAILanguageModel",
+    "AzureOpenAILanguageModel",
+    "AnthropicLanguageModel",
+]
+
+_module_lookup = {
+    "ModelhubLanguageModel": "autorag.language_models.modelhub",
+    "LanguageModel": "autorag.language_models.base",
+    "OllamaLanguageModel": "autorag.language_models.ollama",
+    "OpenAILanguageModel": "autorag.language_models.openai",
+    "AzureOpenAILanguageModel": "autorag.language_models.openai",
+    "AnthropicLanguageModel": "autorag.language_models.anthropic",
+}
+
+
+def __getattr__(name: str) -> Any:
+    if name in _module_lookup:
+        module = importlib.import_module(_module_lookup[name])
+        return getattr(module, name)
+    raise AttributeError(
+        f"module {__name__} has no attribute {name}"
+    )  # pragma: no cover
