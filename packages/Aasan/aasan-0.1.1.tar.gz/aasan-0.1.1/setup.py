5a3de7e14b0bb3877baea47998642e53213ae5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:54 +0000

    Add package Aasan/0.1.1/aasan-0.1.1.tar.gz

---
+++ packages/Aasan/aasan-0.1.1.tar.gz/aasan-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Aasan",
+    version="0.1.1",
+    author="Aneesh Shukla",
+    #author_email="aneeshshukla.tech@gmail.com",
+    description="This python package helps make python more easier than it already is.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    #url="https://github.com/aneeshshukla/aasan",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
