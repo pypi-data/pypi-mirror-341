3e2faad2fa9a6d583c676427abea622a011b2f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 14:04:43 +0000

    Add package ob-metaflow-stubs/6.0.3.157/ob_metaflow_stubs-6.0.3.157-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.157-py2.py3-none-any.whl/metaflow-stubs/plugins/secrets/inline_secrets_provider.pyi
@@ -0,0 +1,25 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.7.2+obcheckpoint(0.2.1);ob(v1)                                                    #
+# Generated on 2025-04-17T01:41:06.483638                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import abc
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.plugins.secrets
+    import abc
+
+from . import SecretsProvider as SecretsProvider
+
+class InlineSecretsProvider(metaflow.plugins.secrets.SecretsProvider, metaclass=abc.ABCMeta):
+    def get_secret_as_dict(self, secret_id, options = {}, role = None):
+        """
+        Intended to be used for testing purposes only.
+        """
+        ...
+    ...
+
