c2480a58e7f94a9df14975fcb68f538845c3b96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:59 +0000

    Add package codegen-api-client/1.0.0/codegen_api_client-1.0.0-py3-none-any.whl

---
+++ packages/codegen-api-client/codegen_api_client-1.0.0-py3-none-any.whl/codegen_api_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from codegen_api_client.api.agents_api import AgentsApi
+from codegen_api_client.api.organizations_api import OrganizationsApi
+from codegen_api_client.api.users_api import UsersApi
+
