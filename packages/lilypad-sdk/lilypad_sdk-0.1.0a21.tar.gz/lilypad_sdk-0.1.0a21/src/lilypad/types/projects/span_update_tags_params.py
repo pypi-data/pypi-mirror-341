7e37a4bd10864b368b1af797421cd18bde78a486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 15:05:36 +0000

    Add package lilypad-sdk/0.1.0a21/lilypad_sdk-0.1.0a21.tar.gz

---
+++ packages/lilypad-sdk/lilypad_sdk-0.1.0a21.tar.gz/lilypad_sdk-0.1.0a21/src/lilypad/types/projects/span_update_tags_params.py
@@ -0,0 +1,27 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Union, Iterable, Optional
+from datetime import datetime
+from typing_extensions import Required, Annotated, TypedDict
+
+from ..._utils import PropertyInfo
+
+__all__ = ["SpanUpdateTagsParams", "Tag"]
+
+
+class SpanUpdateTagsParams(TypedDict, total=False):
+    tags: Optional[Iterable[Tag]]
+
+
+class Tag(TypedDict, total=False):
+    created_at: Required[Annotated[Union[str, datetime], PropertyInfo(format="iso8601")]]
+
+    name: Required[str]
+
+    organization_uuid: Required[str]
+
+    uuid: Required[str]
+
+    project_uuid: Optional[str]
