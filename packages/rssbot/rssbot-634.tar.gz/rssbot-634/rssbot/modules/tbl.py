721905b0b21efaaf1f748b6538711a252d9199a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:54 +0000

    Add package rssbot/634/rssbot-634.tar.gz

---
+++ packages/rssbot/rssbot-634.tar.gz/rssbot-634/rssbot/modules/tbl.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dpl": "rss",
+    "exp": "rss",
+    "imp": "rss",
+    "mre": "irc",
+    "nme": "rss",
+    "pwd": "irc",
+    "rem": "rss",
+    "res": "rss",
+    "rss": "rss",
+    "syn": "rss",
+    "ver": "ver"
+}
+
+
+MD5 = {
+    "irc": "bd26b077402c7791049ebf5d97a340ef",
+    "rss": "25aade0b6f4b56ef9aa0dbc7536bc790",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
+}
