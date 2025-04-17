c190911741f8cf615518f6b46b960f29955367ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:54 +0000

    Add package meseex/0.0.1/meseex-0.0.1-py3-none-any.whl

---
+++ packages/meseex/meseex-0.0.1-py3-none-any.whl/meseex/utils.py
@@ -0,0 +1,11 @@
+import inspect
+from typing import Callable
+from meseex.mr_meseex import MrMeseex
+
+
+def _expects_mr_meseex_param(method: Callable) -> bool:
+    """Checks if the method expects a MrMeseex parameter in its arguments."""
+    for param in inspect.signature(method).parameters.values():
+        if param.annotation == MrMeseex or param.name == "mr_meseex":
+            return True
+    return False
