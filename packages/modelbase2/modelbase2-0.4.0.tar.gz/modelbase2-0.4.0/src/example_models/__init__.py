960cf5ab0be50139d273288e7321819e23fe9fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 16:05:51 +0000

    Add package modelbase2/0.4.0/modelbase2-0.4.0.tar.gz

---
+++ packages/modelbase2/modelbase2-0.4.0.tar.gz/modelbase2-0.4.0/src/example_models/__init__.py
@@ -0,0 +1,28 @@
+"""Example models.
+
+This package provides functions for retrieving various predefined metabolic models.
+It includes models for example systems, linear chains, and specific metabolic pathways.
+
+"""
+
+from __future__ import annotations
+
+from .example1 import get_example1
+from .linear_chain import (
+    get_lin_chain_two_circles,
+    get_linear_chain_1v,
+    get_linear_chain_2v,
+)
+from .poolman2000 import get_model as get_poolman2000
+from .tpi import get_tpi_ald_model
+from .upper_glycolysis import get_model as get_upper_glycolysis
+
+__all__ = [
+    "get_example1",
+    "get_lin_chain_two_circles",
+    "get_linear_chain_1v",
+    "get_linear_chain_2v",
+    "get_poolman2000",
+    "get_tpi_ald_model",
+    "get_upper_glycolysis",
+]
