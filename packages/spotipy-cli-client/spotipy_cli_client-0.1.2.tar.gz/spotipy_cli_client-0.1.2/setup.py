66dcfd9517fe02ffec59d356396a519236f16b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:22 +0000

    Add package spotipy-cli-client/0.1.2/spotipy_cli_client-0.1.2.tar.gz

---
+++ packages/spotipy-cli-client/spotipy_cli_client-0.1.2.tar.gz/spotipy_cli_client-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="spotipy-cli-client",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=["click", "spotipy", "InquirerPy"],
+    entry_points={
+        "console_scripts": [
+            "spt = spotify_cli_client.cli:cli",
+        ],
+    },
+    author="Sudharshan V",
+    author_email="sudarshan61kv@gmail.com",
+    description="Simple Spotify Client",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
