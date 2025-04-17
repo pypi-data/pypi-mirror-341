6b46376b0d365d76e0233fc3d7a15c165dc97eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 17:05:29 +0000

    Add package mypackage-fayas/0.0.1/mypackage_fayas-0.0.1.tar.gz

---
+++ packages/mypackage-fayas/mypackage_fayas-0.0.1.tar.gz/mypackage_fayas-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+name="mypackage_fayas",
+version="0.0.1",
+author="Fayas",
+author_email="fayashajafakrudin@gmail.com",
+packages=find_packages(),
+)
