95ce538380f83bfaf6c838162fc0eb4cf9b2d3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 16:05:12 +0000

    Add package bodit-analysis-module/0.1.1/bodit_analysis_module-0.1.1.tar.gz

---
+++ packages/bodit-analysis-module/bodit_analysis_module-0.1.1.tar.gz/bodit_analysis_module-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bodit-analysis-module",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "boto3>=1.34.154",
+        "pandas>=2.2.3",
+        "numpy>=1.26.4",
+        "mysql-connector>=2.2.9",
+    ],
+    python_requires=">=3.8",
+    author="BODIT Inc.",
+    keywords="bodit, analysis, module",
+    description="BODIT Data Analysis Package",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+) 
