4844d4039aa583487d6f41b0251cfdb4126a5eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 15:05:36 +0000

    Add package remla/0.2.0/remla-0.2.0-py3-none-any.whl

---
+++ packages/remla/remla-0.2.0-py3-none-any.whl/remla/setup/mediaMTXGetFeed.js
@@ -0,0 +1,53 @@
+const video = document.getElementById('video');
+
+const message = document.getElementById('message');
+let defaultControls = false;
+
+const setMessage = (str) => {
+  if (str !== '') {
+    video.controls = false;
+  } else {
+    video.controls = defaultControls;
+  }
+  message.innerText = str;
+
+};
+
+const parseBoolString = (str, defaultVal) => {
+  str = (str || '');
+
+  if (['1', 'yes', 'true'].includes(str.toLowerCase())) {
+    return true;
+  }
+  if (['0', 'no', 'false'].includes(str.toLowerCase())) {
+    return false;
+  }
+  return defaultVal;
+};
+
+const loadAttributesFromQuery = () => {
+
+  const params = new URLSearchParams(window.location.search);
+  video.controls = parseBoolString(params.get('controls'), false);
+  video.muted = parseBoolString(params.get('muted'), true);
+  video.autoplay = parseBoolString(params.get('autoplay'), true);
+  video.playsInline = parseBoolString(params.get('playsinline'), true);
+
+  defaultControls = video.controls;
+};
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadAttributesFromQuery();
+
+  new MediaMTXWebRTCReader({
+    url: new URL('whep', window.location.href) + window.location.search,
+    onError: (err) => {
+      setMessage(err);
+    },
+    onTrack: (evt) => {
+      setMessage('');
+      video.srcObject = evt.streams[0];
+    },
+  });
+});
