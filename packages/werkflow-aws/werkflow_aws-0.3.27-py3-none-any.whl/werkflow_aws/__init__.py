0680c0d4ed55160471b4c1be28967c63b8985fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 14:05:27 +0000

    Add package werkflow-aws/0.3.27/werkflow_aws-0.3.27-py3-none-any.whl

---
+++ packages/werkflow-aws/werkflow_aws-0.3.27-py3-none-any.whl/werkflow_aws/__init__.py
@@ -0,0 +1,7 @@
+from .aws import AWS as AWS
+from .models import (
+    AWSCredentialsSet as AWSCredentialsSet,
+    AWSRegion as AWSRegion,
+    AWSRegionMap as AWSRegionMap,
+    RegionName as RegionName
+)
