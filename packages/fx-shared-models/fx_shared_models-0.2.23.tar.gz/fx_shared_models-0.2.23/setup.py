ee52fa827e929095dd628b285a503f0b3d25069b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 17:05:16 +0000

    Add package fx-shared-models/0.2.23/fx_shared_models-0.2.23.tar.gz

---
+++ packages/fx-shared-models/fx_shared_models-0.2.23.tar.gz/fx_shared_models-0.2.23/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='fx-shared-models',
+    version='0.2.23',
+    packages=find_namespace_packages(include=['shared_models*']),
+    install_requires=[
+        'Django>=3.0',
+        'django-environ>=0.10.0',
+    ],
+    python_requires='>=3.8',
+    author='FX Backend',
+    author_email='fxbackend@gmail.com',
+    description='Shared models for FX Backend',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/fxbackend/fx-shared-models',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    package_data={
+        'shared_models': [
+            'accounts/*'
+            'common/*',
+            'company_ba/*'
+            'customers/*',
+            'ib_commission/*',
+            'services/*',
+            'system_settings/*',
+            'transactions/*',
+            'referrals/*',
+            'ui_elements/*',
+        ],
+    },
+) 
