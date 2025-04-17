0e61bd5f56ebea1ef27bdd1927c088ebc6afd2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:18:46 +0000

    Add package django-telegram-framework/2.4.0/django_telegram_framework-2.4.0-py3-none-any.whl

---
+++ packages/django-telegram-framework/django_telegram_framework-2.4.0-py3-none-any.whl/telegram_framework/links.py
@@ -0,0 +1,50 @@
+from typing import Callable
+import importlib
+from telegram_framework import bots
+
+
+def on_command(handler: Callable, name: str):
+
+    def command_handler(bot):
+        return bots.register_command_handler(bot, handler, name)
+
+    return command_handler
+
+
+def on_message(handler: Callable):
+
+    def message_handler(bot):
+        return bots.register_message_handler(bot, handler)
+
+    return message_handler
+
+
+# def on_call(handler, call_data):
+#
+#     def call_handler(bot):
+#         return register_call_handler(bot, handler, call_data)
+#
+#     return call_handler
+
+
+def add_links(bot, links):
+    for link in links:
+        bot = link(bot)
+
+    return bot
+
+
+def get_root_links(module_name):
+    links_module = importlib.import_module(module_name)
+    return links_module.bot_links
+
+
+def include(links: list, module_name) -> list:
+    additional_links = get_root_links(module_name)
+    return links + additional_links
+
+
+def include_all(links: list, modules_list: list) -> list:
+    for module in modules_list:
+        links = include(links, module)
+    return links
---
+++ packages/django-telegram-framework/django_telegram_framework-2.4.0-py3-none-any.whl/telegram_framework/package.py
@@ -0,0 +1,6 @@
+"""
+Package info
+"""
+name = 'django-telegram-framework'
+version = '2.4.0'
+status = '3 - Alpha'
