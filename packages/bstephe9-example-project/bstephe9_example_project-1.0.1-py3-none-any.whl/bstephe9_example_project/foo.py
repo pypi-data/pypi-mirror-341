896c46fe6a25aa74e91ce36fde79836ba1318195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:23 +0000

    Add package bstephe9-example-project/1.0.1/bstephe9_example_project-1.0.1-py3-none-any.whl

---
+++ packages/bstephe9-example-project/bstephe9_example_project-1.0.1-py3-none-any.whl/bstephe9_example_project/foo.py
@@ -0,0 +1,26 @@
+def foo(bar: str) -> str:
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        bar: Description of input argument.
+
+    Returns:
+        Description of return value
+    """
+
+    return bar
+
+
+def a_function() -> int:
+    """Testing documentation.
+
+    Returns:
+        int: Returns 1
+    """
+    return 1
+
+
+if __name__ == "__main__":  # pragma: no cover
+    pass
