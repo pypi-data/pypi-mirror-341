198d39b76eaab90936ed2db9907bf79bb26e5359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:19:05 +0000

    Add package grooveshop-django-api/0.199.0/grooveshop_django_api-0.199.0.tar.gz

---
+++ packages/grooveshop-django-api/grooveshop_django_api-0.199.0.tar.gz/grooveshop_django_api-0.199.0/cart/services.py
@@ -0,0 +1,147 @@
+from __future__ import annotations
+
+from enum import Enum, unique
+from typing import TYPE_CHECKING
+
+from cart.models import Cart, CartItem
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest
+    from rest_framework.request import Request
+
+    from product.models.product import Product
+
+
+class CartServiceInitException(Exception):
+    pass
+
+
+class CartNotSetException(Exception):
+    pass
+
+
+class InvalidProcessCartOptionException(Exception):
+    pass
+
+
+@unique
+class ProcessCartOption(Enum):
+    MERGE = "merge"
+    CLEAN = "clean"
+    KEEP = "keep"
+
+
+class CartService:
+    def __init__(self, request: Request | HttpRequest):
+        if not request:
+            raise CartServiceInitException(
+                "Request must be provided to initialize CartService."
+            )
+
+        self.request = request
+        self.cart: Cart | None = None
+        self.cart_items: list[CartItem] = []
+        try:
+            self._initialize_cart()
+        except CartServiceInitException:
+            raise
+
+    def __str__(self):
+        return f"Cart {self.cart.user if self.cart and self.cart.user else 'Anonymous'}"
+
+    def __len__(self):
+        return self.cart.total_items if self.cart else 0
+
+    def __iter__(self):
+        if self.cart:
+            yield from self.cart_items
+
+    def _initialize_cart(self):
+        self.cart = self.get_or_create_cart()
+        self.cart_items = self.cart.get_items() if self.cart else []
+        if self.cart:
+            self.cart.refresh_last_activity()
+
+    def process_cart(self, option: ProcessCartOption):
+        pre_login_cart_id = self.request.session.get("pre_log_in_cart_id")
+        if isinstance(pre_login_cart_id, int | str):
+            pre_login_cart_id = int(pre_login_cart_id)
+
+        match option:
+            case ProcessCartOption.MERGE:
+                if pre_login_cart_id:
+                    pre_login_cart = self.get_cart_by_id(pre_login_cart_id)
+                    if pre_login_cart:
+                        self.merge_carts(pre_login_cart)
+            case ProcessCartOption.CLEAN:
+                self.clean_cart()
+            case ProcessCartOption.KEEP:
+                pass
+            case _:
+                raise InvalidProcessCartOptionException(
+                    f"Invalid option: {option}"
+                )
+
+    def get_or_create_cart(self):
+        user = self.request.user
+        if user.is_authenticated:
+            cart, created = Cart.objects.get_or_create(user=user)
+            self.request.session["cart_id"] = cart.id
+            return cart
+        else:
+            cart_id = self.request.session.get("cart_id")
+            cart = None
+            if cart_id:
+                cart = Cart.objects.filter(id=cart_id).first()
+            if not cart:
+                cart = Cart.objects.create()
+                self.request.session["cart_id"] = cart.id
+            return cart
+
+    def merge_carts(self, pre_login_cart: Cart):
+        for item in pre_login_cart.items.all():
+            if not CartItem.objects.filter(
+                cart=self.cart, product=item.product
+            ).exists():
+                item.cart = self.cart
+                item.save()
+        pre_login_cart.delete()
+        self.request.session["pre_log_in_cart_id"] = None
+
+    def clean_cart(self):
+        if self.cart:
+            self.cart.items.all().delete()
+            self.cart_items = []
+
+    @staticmethod
+    def get_cart_by_id(cart_id: int):
+        return Cart.objects.filter(id=cart_id).first()
+
+    def get_cart_item(self, product_id: int | None):
+        if self.cart:
+            return self.cart.items.filter(product_id=product_id).first()
+        return None
+
+    def create_cart_item(self, product: Product, quantity: int):
+        if not self.cart:
+            raise CartNotSetException("Cart is not set.")
+        cart_item = CartItem.objects.create(
+            cart=self.cart, product=product, quantity=quantity
+        )
+        self.cart_items = self.cart.get_items()
+        return cart_item
+
+    def update_cart_item(self, product_id: int, quantity: int):
+        if not self.cart:
+            raise CartNotSetException("Cart is not set.")
+        cart_item = self.cart.items.get(product_id=product_id)
+        cart_item.quantity = quantity
+        cart_item.save()
+        self.cart_items = self.cart.get_items()
+        return cart_item
+
+    def delete_cart_item(self, product_id: int):
+        if not self.cart:
+            raise CartNotSetException("Cart is not set.")
+        self.cart.items.filter(product_id=product_id).delete()
+        self.cart_items = self.cart.get_items()
---
+++ packages/grooveshop-django-api/grooveshop_django_api-0.199.0.tar.gz/grooveshop_django_api-0.199.0/core/management/commands/clear_logs.py
@@ -0,0 +1,35 @@
+from os import listdir, path, remove
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from core.logging import LogInfo
+
+
+class Command(BaseCommand):
+    help = "Delete all log files"
+
+    def handle(self, *args, **kwargs):
+        try:
+            logs_path = path.join(settings.BASE_DIR, "logs")
+            files = listdir(logs_path)
+
+            for file in files:
+                file_path = path.join(logs_path, file)
+                try:
+                    self.stdout.write(f"Deleting file: {file_path}")
+                    remove(file_path)
+                except Exception as e:
+                    warning_message = (
+                        f"Could not delete file {file_path}: {e!s}"
+                    )
+                    LogInfo.warning(warning_message)
+                    self.stdout.write(self.style.WARNING(warning_message))
+
+            message = "Attempted to remove all log files."
+            LogInfo.info(message)
+            self.stdout.write(self.style.SUCCESS(message))
+        except Exception as e:
+            error_message = f"Error while deleting log files: {e!s}"
+            LogInfo.error(error_message)
+            self.stdout.write(self.style.ERROR(error_message))
