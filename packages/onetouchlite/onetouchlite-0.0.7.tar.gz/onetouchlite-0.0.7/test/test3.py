cf276e5d828de168accabb23d081e9158aa9835b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:54 +0000

    Add package onetouchlite/0.0.7/onetouchlite-0.0.7.tar.gz

---
+++ packages/onetouchlite/onetouchlite-0.0.7.tar.gz/onetouchlite-0.0.7/test/test3.py
@@ -0,0 +1,26 @@
+import tqdm
+import onetouchlite as otl
+
+
+@otl.timer
+def circulate_tqdm():
+    for i in tqdm.tqdm(range(10_000_000)):
+        pass
+
+
+@otl.timer
+def circulate_otl():
+    for i in otl.pace(range(10_000_000)):
+        pass
+
+
+@otl.timer
+def circulate_decode():
+    for i in enumerate(otl.pace(range(10_000_000))):
+        pass
+
+
+circulate_tqdm()
+circulate_otl()
+circulate_decode()
+
