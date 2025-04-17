6491176445cc196d358522021d49f9e60c68dae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:54 +0000

    Add package literalai/0.1.300/literalai-0.1.300.tar.gz

---
+++ packages/literalai/literalai-0.1.300.tar.gz/literalai-0.1.300/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="literalai",
+    version="0.1.300",  # update version in literalai/version.py
+    description="An SDK for observability in Python applications",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Literal AI",
+    author_email="contact@literalai.com",
+    package_data={"literalai": ["py.typed"]},
+    packages=find_packages(),
+    license="Apache License 2.0",
+    install_requires=[
+        "packaging>=23.0",
+        "httpx>=0.23.0",
+        "pydantic>=1,<3",
+        "chevron>=0.14.0",
+        "traceloop-sdk>=0.33.12",
+    ],
+)
