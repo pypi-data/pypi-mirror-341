8146d2738142e64243e51616e1b310dde40bd96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:15 +0000

    Add package django-textbus/0.2.9/django_textbus-0.2.9-py3-none-any.whl

---
+++ packages/django-textbus/django_textbus-0.2.9-py3-none-any.whl/django_textbus/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoTextbusConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_textbus'
+    verbose_name = 'Django Textbus'
