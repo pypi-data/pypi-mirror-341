7138fcec888918ab3c22f8758136d6f52d8879e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:54 +0000

    Add package pyfunc2/0.1.39/pyfunc2-0.1.39-py3-none-any.whl

---
+++ packages/pyfunc2/pyfunc2-0.1.39-py3-none-any.whl/pyfunc2/config/__init__.py
@@ -0,0 +1,16 @@
+# Auto-generated __init__.py
+
+
+# Import necessary modules and functions here
+from .ftp_download import ftp_download
+from .get_config import get_config
+from .ftp_update import ftp_update
+from .get_email_path import get_email_path
+from .get_ftp_path import get_ftp_path
+
+# Public API of the package
+__all__ = [ftp_download, get_config, ftp_update, get_email_path, get_ftp_path]
+
+
+# Version of the pyfunc2 package
+from .._version import __version__
