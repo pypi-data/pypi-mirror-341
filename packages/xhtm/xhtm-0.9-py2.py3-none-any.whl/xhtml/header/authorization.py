e41e0f2e98d64c5a3fbff76035842727b9e4cfe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:06:03 +0000

    Add package xhtm/0.9/xhtm-0.9-py2.py3-none-any.whl

---
+++ packages/xhtm/xhtm-0.9-py2.py3-none-any.whl/xhtml/header/authorization.py
@@ -0,0 +1,61 @@
+# coding:utf-8
+
+class Authorization():
+    class Auth():
+        def __init__(self, type: str):
+            self.__type: str = type
+
+        @property
+        def type(self):
+            return self.__type
+
+    class Basic(Auth):
+        TYPE: str = "Basic"
+
+        def __init__(self, base64: str):
+            from base64 import b64decode
+
+            nameword: str = b64decode(base64).decode("utf-8")
+            username, password = nameword.split(":", maxsplit=1)
+            self.__username: str = username
+            self.__password: str = password
+            super().__init__(self.TYPE)
+
+        @property
+        def username(self):
+            return self.__username
+
+        @property
+        def password(self):
+            return self.__password
+
+    class Bearer(Auth):
+        TYPE: str = "Bearer"
+
+        def __init__(self, token: str):
+            super().__init__(self.TYPE)
+            self.__token = token
+
+        @property
+        def token(self):
+            return self.__token
+
+    class APIKey(Auth):
+        TYPE: str = "ApiKey"
+
+        def __init__(self, key: str):
+            super().__init__(self.TYPE)
+            self.__key = key
+
+        @property
+        def key(self):
+            return self.__key
+
+    @classmethod
+    def paser(cls, authorization: str) -> Auth:
+        k, v = authorization.split(" ", maxsplit=1)
+        return {
+            cls.Basic.TYPE: cls.Basic,
+            cls.Bearer.TYPE: cls.Bearer,
+            cls.APIKey.TYPE: cls.APIKey,
+        }[k](v)
