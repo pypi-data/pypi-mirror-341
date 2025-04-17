1cf5983ee7ed6c9b5af733186951cc0d10de2659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 17:05:36 +0000

    Add package sample-harini/0.0.1/sample_harini-0.0.1.tar.gz

---
+++ packages/sample-harini/sample_harini-0.0.1.tar.gz/sample_harini-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(
+name="sample_harini",
+version="0.0.1",
+author="Harini",
+author_email="harinikavi.25@gmail.com",
+packages=find_packages(),
+)
+
