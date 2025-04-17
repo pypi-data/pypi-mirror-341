d102b2ef2c593276df51f53f8b033875d204b0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 17:05:23 +0000

    Add package powerinfer-server/1.0.1/powerinfer_server-1.0.1.tar.gz

---
+++ packages/powerinfer-server/powerinfer_server-1.0.1.tar.gz/powerinfer_server-1.0.1/PIserver/commands/clone.py
@@ -0,0 +1,92 @@
+from PIserver.clients.FileDownloadClient import FileDownloadClient
+from PIserver.commands.command import Command
+from PIserver.utils.files import *
+from PIserver.constants import *
+from pathlib import Path
+from huggingface_hub import snapshot_download, HfApi
+from tqdm import tqdm
+
+class Clone_Model(Command):
+    def register_subcommand(self, subparser):
+        clone_parser = subparser.add_parser("clone", help="Download a model from https://powerinfer.com or update a local model.")
+        clone_parser.add_argument("model", help="The model name to clone.")
+        clone_parser.add_argument("local_dir", nargs='?', help="Clone the model into assigned local directory.")
+        clone_parser.add_argument("-hf", help="Clone a runnable gguf model from huggingface.")
+        
+    def execute(self, args):
+        mname = args.model
+        mname, tname = check_model_name_with_size(mname)
+        
+        # parse local path to store
+        local_dir_name = str(args.model).replace(":", "-").replace("/", "-")
+        local_path = Path(args.local_dir) if args.local_dir is not None else Path(DEFAULT_MODEL_PATH) / Path(local_dir_name)
+        if args.local_dir is None:
+            config = read_file(DEFAULT_CONFIG_FILE)
+            local_path = Path(config["model_path"]) / Path(local_dir_name)
+        local_path.mkdir(parents=True, exist_ok=True)
+            
+        if args.hf is not None:
+            print("Cloning from huggingface...")
+            try:
+                api = HfApi()
+                commits = api.list_repo_commits(args.hf)
+                model_version = commits[0].commit_id[:8] if commits else "unknown"
+                
+                need_download = self.check_model(args.model, model_version)
+                
+                snapshot_download(
+                    args.hf, 
+                    local_dir=local_path, 
+                    tqdm_class=tqdm
+                )
+                
+                total_size = get_folder_size(local_path)
+                add_row([mname, tname, total_size, model_version, str(local_path)])
+                print(f"Successfully cloned model {args.model} into {str(local_path)}.")
+            except KeyboardInterrupt:
+                print("Download stopped.")
+            return
+        
+        mname, uname = get_uname_from_model(mname)
+        print(f"Trying to clone model {mname} size {tname}...")
+        if tname == "":
+            return
+        # check remote access
+        getModelRequest = {
+            "mname": mname,
+            "tname": tname
+        }
+        if uname != "":
+            getModelRequest["uname"] = uname
+            
+        try:
+            client = FileDownloadClient()
+            info = client.getModelInfo(getModelRequest)
+            if info is None:
+                return
+            structure = info["dir_info"]
+            print("Model info: ", info)
+            print("structure: ", structure)
+            need_download = self.check_model(args.model, info["version"])
+            if not need_download:
+                return
+            # download & add to local model list    
+            if client.download(local_path, structure):
+                add_row([mname, tname, info["size"], info["version"], str(local_path)])
+        except KeyboardInterrupt:
+            print("Download stopped.")
+            return
+        
+    def check_model(self, mname, version) -> bool:
+        # check in the list about version and path
+        rows, rest = filter_rows(parse_condition(mname))
+        # check remote model version(metadata)
+        if len(rows) > 0:
+            if version == rows[0][3]:
+                print("The model is up to date. No need to download.")
+                return False
+            else: # delete old version
+                print("Removing the older version of model...")
+                remove_dir(rows[0][4])
+                write_rows(rest)
+        return True
---
+++ packages/powerinfer-server/powerinfer_server-1.0.1.tar.gz/powerinfer_server-1.0.1/setup.py
@@ -0,0 +1,80 @@
+from setuptools import setup
+from setuptools.command.install import install
+from PIserver.constants import *
+
+LOCAL_LIST_HEADER = ['MODEL_NAME', 'SIZE', 'BSIZE', 'VERSION', 'PATH']
+
+def generate_ssh_key():
+    try:
+        import paramiko
+        key = paramiko.RSAKey.generate(2048)
+        key.write_private_key_file(DEFAULT_SSH_PEM_KEY_PATH)
+        pub = key.get_base64()
+        with open(DEFAULT_SSH_PUB_KEY_PATH, 'w') as f:
+            f.write("ssh-rsa "+pub)
+    except ImportError:
+        print("paramiko is not installed. Skipping SSH key generation.")
+
+def generate_model_list_file():
+    import csv
+    DEFAULT_MODEL_LIST_FILE.touch(0o755, exist_ok=True)
+    with open(DEFAULT_MODEL_LIST_FILE, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(LOCAL_LIST_HEADER)
+        
+def generating_config_file():
+    import json
+    DEFAULT_CONFIG_FILE.touch(0o755, exist_ok=True)
+    with open(DEFAULT_CONFIG_FILE, 'w') as f:
+        json.dump(DEFAULT_CONFIG, f, indent=4)
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        
+        if not self.dry_run and not self.skip_build:
+            print(f"Starting to create default storing directory at {DEFAULT_STORAGE_PATH} ...")
+            
+            DEFAULT_STORAGE_PATH.mkdir(0o755, parents=True, exist_ok=True)
+            DEFAULT_MODEL_PATH.mkdir(0o755, parents=True, exist_ok=True)
+            generate_ssh_key() # FIXME: add ssh-key in production; in test don't change
+            generate_model_list_file()
+            
+            DEFAULT_INSTALL_PATH.mkdir(0o755, parents=True, exist_ok=True)
+            DEFAULT_ENGINE_LIST_FILE.touch(0o755, exist_ok=True)
+            
+            # compile and install the engine
+            # try:
+            #     engine_path = local_compile()
+            #     if engine_path is not None:
+            #         add_engine(DEFAULT_ENGINE_NAME, engine_path)
+            #         DEFAULT_CONFIG["engine"] = DEFAULT_ENGINE_NAME
+            # except Exception as e:
+            #     log_error(f"Failed to compile the engine: {e}")
+            #     print("Skipping engine installation. You can install it later using `powerinfer install` command.")
+            
+            
+            generating_config_file()
+
+setup(
+    name='powerinfer-server',
+    version='1.0.1',
+    author='ARL',
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+    install_requires=[
+        "paramiko>=2.11.0",
+        "fastapi>=0.68.0",
+        "uvicorn>=0.15.0",
+        "pathlib>=1.0.1",
+        "requests>=2.25.0",
+        "tqdm>=4.61.0",
+        "tabulate>=0.8.9",
+        "huggingface_hub>=0.10.1",
+        "mmap>=0.9.0",
+        "curses>=2.2.1",
+        "keyboard>=0.13.5",
+        "cryptography>=39.0.0",
+    ],
+)
