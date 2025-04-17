dc1075dfd69595989a8ec09d0e69af5c5d0e0024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 05:05:12 +0000

    Add package goodconf/6.1.1/goodconf-6.1.1-py3-none-any.whl

---
+++ packages/goodconf/goodconf-6.1.1-py3-none-any.whl/goodconf/contrib/django.py
@@ -0,0 +1,45 @@
+import argparse
+from contextlib import contextmanager
+from typing import List
+from collections.abc import Generator
+
+from .. import GoodConf
+from .argparse import argparser_add_argument
+
+
+@contextmanager
+def load_config_from_cli(
+    config: GoodConf, argv: list[str]
+) -> Generator[list[str], None, None]:
+    """Loads config, checking CLI arguments for a config file"""
+
+    # Monkey patch Django's command parser
+    from django.core.management.base import BaseCommand
+
+    original_parser = BaseCommand.create_parser
+
+    def patched_parser(self, prog_name, subcommand):
+        parser = original_parser(self, prog_name, subcommand)
+        argparser_add_argument(parser, config)
+        return parser
+
+    BaseCommand.create_parser = patched_parser
+
+    try:
+        parser = argparse.ArgumentParser(add_help=False)
+        argparser_add_argument(parser, config)
+
+        config_arg, default_args = parser.parse_known_args(argv)
+        config.load(config_arg.config)
+        yield default_args
+    finally:
+        # Put that create_parser back where it came from or so help me!
+        BaseCommand.create_parser = original_parser
+
+
+def execute_from_command_line_with_config(config: GoodConf, argv: list[str]):
+    """Load's config then runs Django's execute_from_command_line"""
+    with load_config_from_cli(config, argv) as args:
+        from django.core.management import execute_from_command_line
+
+        execute_from_command_line(args)
