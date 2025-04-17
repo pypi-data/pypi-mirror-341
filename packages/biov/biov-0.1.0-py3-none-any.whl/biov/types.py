f94ffbcffdc4f94b8181f7f2f4ef9a3a42f02b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 04:04:58 +0000

    Add package biov/0.1.0/biov-0.1.0-py3-none-any.whl

---
+++ packages/biov/biov-0.1.0-py3-none-any.whl/biov/types.py
@@ -0,0 +1,22 @@
+"""Types for BioV."""
+
+from typing import Any
+
+from Bio.Seq import Seq as _Seq
+from pydantic import (
+    GetCoreSchemaHandler,
+)
+from pydantic_core import CoreSchema, core_schema
+
+
+class Seq(_Seq):
+    """Extended from Bio.Seq.Seq for pydantic validation and serialization."""
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> CoreSchema:
+        return core_schema.no_info_plain_validator_function(
+            _Seq,
+            serialization=core_schema.to_string_ser_schema(),
+        )
