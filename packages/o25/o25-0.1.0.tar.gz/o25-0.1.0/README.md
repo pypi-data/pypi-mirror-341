fbbb89be596d6f62316beac3738994f4b19ade70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:18:51 +0000

    Add package o25/0.1.0/o25-0.1.0.tar.gz

---
+++ packages/o25/o25-0.1.0.tar.gz/o25-0.1.0/README.md
@@ -0,0 +1,8 @@
+# O25 - Python Port
+
+## Installation
+```bash
+conda create -n o25 python=3.9 numpy scipy matplotlib
+conda activate o25
+
+**Inquiries to**: jaime.pitarch@cnr.it.
---
+++ packages/o25/o25-0.1.0.tar.gz/o25-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='o25',
+    version='0.1.0',
+    author='Jaime Pitarch',
+    author_email='jaime.pitarch@cnr.it',
+    description='Bidirectional correction and IOP retrieval for aquatic optics',
+    long_description=open('../README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jaipipor/O25',
+    license='GPL-3.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
