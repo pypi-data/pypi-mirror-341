b5d2b8e51e3d4fca784d4be9ed026c9f28e1fdef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 16:05:42 +0000

    Add package WebsocketTest/1.0.2/WebsocketTest-1.0.2.tar.gz

---
+++ packages/WebsocketTest/WebsocketTest-1.0.2.tar.gz/WebsocketTest-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="WebsocketTest",
+    version="1.0.2",
+    author='chencheng',
+    python_requires=">=3.10",
+    packages=find_packages(exclude=["allure_report", "logs", "allure_results",  "config", "data"]),
+    description="websocket api autotest",
+    # long_description=open('README.md','r',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    install_requires = [
+        "allure_python_commons==2.13.5",
+        "numpy==2.2.4",
+        "pandas==2.2.3",
+        "pytest==8.2.2",
+        "PyYAML==6.0.2",
+        "websockets==12.0"
+    ],
+    entry_points={
+        'console_scripts': [
+            'wsrun=WebsocketTest.cli:main_run_alias'
+        ]
+    }
+)
