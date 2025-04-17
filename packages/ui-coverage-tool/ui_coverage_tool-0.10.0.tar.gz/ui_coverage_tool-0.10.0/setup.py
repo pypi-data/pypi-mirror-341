b7a12935a5b04139fdca23f36f35b9efc66c5910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 06:05:08 +0000

    Add package ui-coverage-tool/0.10.0/ui_coverage_tool-0.10.0.tar.gz

---
+++ packages/ui-coverage-tool/ui_coverage_tool-0.10.0.tar.gz/ui_coverage_tool-0.10.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ui-coverage-tool",
+    version="0.10.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "pyyaml>=6.0.2",
+        "pydantic>=2.0.0",
+        "pydantic-settings>=2.8.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ui-coverage-tool = ui_coverage_tool.cli.main:cli',
+        ],
+    },
+    author="Nikita Filonov",
+    author_email="filonov.nikitkaa@gmail.com",
+    description="TODO",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nikita-Filonov/ui-coverage-tool",
+    project_urls={
+        "Bug Tracker": "https://github.com/Nikita-Filonov/ui-coverage-tool/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
