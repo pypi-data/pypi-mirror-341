91b7128feda7aa980aa1ea8d54e64dbd4ea000ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 15:05:41 +0000

    Add package wedata-feature-engineering/0.1.3/wedata-feature-engineering-0.1.3.tar.gz

---
+++ packages/wedata-feature-engineering/wedata-feature-engineering-0.1.3.tar.gz/wedata-feature-engineering-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wedata-feature-engineering",
+    version="0.1.3",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        'pyspark>=3.0.0',
+        'delta-spark>=1.0.0',
+        'pandas>=1.0.0'
+    ],
+    python_requires='>=3.7',
+    author="meahqian",
+    author_email="",
+    description="Wedata Feature Engineering Library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="Apache 2.0",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
