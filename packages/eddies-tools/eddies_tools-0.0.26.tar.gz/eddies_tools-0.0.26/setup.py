5ca6adfe39c89e75f6154da827458f4766c4676d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 07:05:37 +0000

    Add package eddies-tools/0.0.26/eddies_tools-0.0.26.tar.gz

---
+++ packages/eddies-tools/eddies_tools-0.0.26.tar.gz/eddies_tools-0.0.26/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eddies_tools',
+    version="0.0.26",
+    packages=find_packages(),
+    include_package_data=True,  # This line is important
+)
