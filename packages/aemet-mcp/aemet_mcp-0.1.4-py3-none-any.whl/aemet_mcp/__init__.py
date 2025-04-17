3bc8506344df84919de9ebd3ff2983e6b291ddf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 07:05:37 +0000

    Add package aemet-mcp/0.1.4/aemet_mcp-0.1.4-py3-none-any.whl

---
+++ packages/aemet-mcp/aemet_mcp-0.1.4-py3-none-any.whl/aemet_mcp/__init__.py
@@ -0,0 +1,7 @@
+"""Integración con la API de AEMET integration mediante Model Context Protocol."""
+
+__version__ = "0.1.0"
+
+# Expose key classes and functions for easier imports
+from .aemet_mcp_es import *
+from .aemet_mcp_eng import *
