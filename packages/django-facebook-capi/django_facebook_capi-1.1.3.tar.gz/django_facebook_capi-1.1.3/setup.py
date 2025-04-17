d98eddf18428d9516812f50d00750d34ea6f2d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 03:18:56 +0000

    Add package django-facebook-capi/1.1.3/django_facebook_capi-1.1.3.tar.gz

---
+++ packages/django-facebook-capi/django_facebook_capi-1.1.3.tar.gz/django_facebook_capi-1.1.3/setup.py
@@ -0,0 +1,32 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='django_facebook_capi',
+    version='1.1.3',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Reusable Django app to track Meta/Facebook CAPI events server-side.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Weird-Labs-Pvt-Ltd/djang-facebook-capi',  # your repo
+    author='Preet Sonpal',
+    author_email='preet@weirdlabs.in',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+    ],
+    install_requires=[
+        'Django>=3.2',
+        'requests',
+    ],
+    python_requires='>=3.7',
+)
