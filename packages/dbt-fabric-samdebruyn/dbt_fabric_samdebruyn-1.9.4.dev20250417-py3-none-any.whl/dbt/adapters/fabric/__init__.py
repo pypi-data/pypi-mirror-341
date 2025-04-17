bfb86279d65200de5adc673f512dabe0e8c5c2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-17 14:05:13 +0000

    Add package dbt-fabric-samdebruyn/1.9.4.dev20250417/dbt_fabric_samdebruyn-1.9.4.dev20250417-py3-none-any.whl

---
+++ packages/dbt-fabric-samdebruyn/dbt_fabric_samdebruyn-1.9.4.dev20250417-py3-none-any.whl/dbt/adapters/fabric/__init__.py
@@ -0,0 +1,22 @@
+from dbt.adapters.base import AdapterPlugin
+from dbt.adapters.fabric.fabric_adapter import FabricAdapter
+from dbt.adapters.fabric.fabric_column import FabricColumn
+from dbt.adapters.fabric.fabric_configs import FabricConfigs
+from dbt.adapters.fabric.fabric_connection_manager import FabricConnectionManager
+from dbt.adapters.fabric.fabric_credentials import FabricCredentials
+from dbt.include import fabric
+
+Plugin = AdapterPlugin(
+    adapter=FabricAdapter,
+    credentials=FabricCredentials,
+    include_path=fabric.PACKAGE_PATH,
+)
+
+__all__ = [
+    "FabricAdapter",
+    "FabricColumn",
+    "FabricConfigs",
+    "FabricConnectionManager",
+    "FabricCredentials",
+    "Plugin",
+]
