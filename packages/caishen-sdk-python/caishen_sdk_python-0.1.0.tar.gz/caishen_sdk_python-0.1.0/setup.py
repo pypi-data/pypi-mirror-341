0a508f98ab36e0e12a3c407e4c711d81e387d54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 07:06:14 +0000

    Add package caishen-sdk-python/0.1.0/caishen_sdk_python-0.1.0.tar.gz

---
+++ packages/caishen-sdk-python/caishen_sdk_python-0.1.0.tar.gz/caishen_sdk_python-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="caishen_sdk_python",
+    version="0.1.0",
+    description="A simple Hello World Python SDK",
+    author="Your Name",
+    author_email="hello@caishen.tech",
+    url="https://github.com/CaishenTech/caishen_sdk_python/",  # Update with your URL or repository
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
