b2305d2310790109dd0d7cbd9e269864cd4f092e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 16:05:48 +0000

    Add package tokkit/0.0.2/tokkit-0.0.2-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/tokkit/tokkit-0.0.2-cp312-cp312-macosx_11_0_arm64.whl/tokkit/cpp/tokenizer.h
@@ -0,0 +1,20 @@
+#include <string>
+#include "dtypes.h"
+
+using namespace std;
+
+namespace bytepairtokenizer {
+    class BytePairTokenizer {
+        public:
+            BytePairTokenizer();
+            ~BytePairTokenizer();
+            DTYPE_BYTEPAIR_VOCAB vocab;
+            DTYPE_BYTEPAIR_REV_VOCAB revVocab;
+            int nextVocabIndex = 256;
+            int vocabSize = 256;
+            int size();
+            void fit(vector<int>& corpus, int maxVocabSize, int nIter);
+            vector<int> encode(string s);
+            string decode(vector<int> encoded);
+    };
+}
