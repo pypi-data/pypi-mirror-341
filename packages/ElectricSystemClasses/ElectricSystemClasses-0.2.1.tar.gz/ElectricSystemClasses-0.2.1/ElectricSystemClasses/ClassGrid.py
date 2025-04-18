2bcea0777f227d568a37185ee2db25f2c0c57a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 04:05:42 +0000

    Add package ElectricSystemClasses/0.2.1/ElectricSystemClasses-0.2.1.tar.gz

---
+++ packages/ElectricSystemClasses/ElectricSystemClasses-0.2.1.tar.gz/ElectricSystemClasses-0.2.1/ElectricSystemClasses/ClassGrid.py
@@ -0,0 +1,26 @@
+# Copyright 2025 ropimen
+#
+# This file is licensed under the Server Side Public License (SSPL), Version 1.0.
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+# https://www.mongodb.com/legal/licensing/server-side-public-license
+#
+# This file is part of ElectricSystemClasses.
+#
+# ElectricSystemClasses is a Python package providing a collection of classes for simulating electric systems.
+
+#class grid, it has no power limits
+class Grid:
+    def __init__(self, id):
+        self.id = id
+        self.power_history = [0]
+
+    def exchange(self, power_value):
+        self.power_history.append(power_value)
+
+    #class method to get update the power history if the grid is not exchanging in the simul frame
+    @classmethod
+    def updateGrid(cls, t):
+        if len(cls.power_history) < t:
+            cls.power_history.append(0)
