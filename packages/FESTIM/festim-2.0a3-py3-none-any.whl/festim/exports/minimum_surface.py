42b65520bb40c68b258bc47ab33d4d0fa0d24dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 04:06:05 +0000

    Add package FESTIM/2.0a3/festim-2.0a3-py3-none-any.whl

---
+++ packages/FESTIM/festim-2.0a3-py3-none-any.whl/festim/exports/minimum_surface.py
@@ -0,0 +1,37 @@
+from mpi4py import MPI
+
+import numpy as np
+
+from festim.exports.surface_quantity import SurfaceQuantity
+
+
+class MinimumSurface(SurfaceQuantity):
+    """Computes the minimum value of a field on a given surface
+
+    Args:
+        field (festim.Species): species for which the minimum surface is computed
+        surface (festim.SurfaceSubdomain): surface subdomain
+        filename (str, optional): name of the file to which the minimum surface is exported
+
+    Attributes:
+        see `festim.SurfaceQuantity`
+    """
+
+    @property
+    def title(self):
+        return f"Minimum {self.field.name} surface {self.surface.id}"
+
+    def compute(self):
+        """
+        Computes the minimum value of the field on the defined surface
+        subdomain, and appends it to the data list
+        """
+        solution = self.field.solution
+        indices = self.surface.locate_boundary_facet_indices(
+            solution.function_space.mesh
+        )
+
+        self.value = solution.function_space.mesh.comm.allreduce(
+            np.min(self.field.solution.x.array[indices]), op=MPI.MIN
+        )
+        self.data.append(self.value)
---
+++ packages/FESTIM/festim-2.0a3-py3-none-any.whl/festim/exports/minimum_volume.py
@@ -0,0 +1,35 @@
+from mpi4py import MPI
+
+import numpy as np
+
+from festim.exports.volume_quantity import VolumeQuantity
+
+
+class MinimumVolume(VolumeQuantity):
+    """Computes the minimum value of a field in a given volume
+
+    Args:
+        field (festim.Species): species for which the minimum volume is computed
+        volume (festim.VolumeSubdomain): volume subdomain
+        filename (str, optional): name of the file to which the minimum volume is exported
+
+    Attributes:
+        see `festim.VolumeQuantity`
+    """
+
+    @property
+    def title(self):
+        return f"Minimum {self.field.name} volume {self.volume.id}"
+
+    def compute(self):
+        """
+        Computes the minimum value of solution function within the defined volume
+        subdomain, and appends it to the data list
+        """
+        solution = self.field.solution
+        indices = self.volume.locate_subdomain_entities(solution.function_space.mesh)
+
+        self.value = solution.function_space.mesh.comm.allreduce(
+            np.min(self.field.solution.x.array[indices]), op=MPI.MIN
+        )
+        self.data.append(self.value)
