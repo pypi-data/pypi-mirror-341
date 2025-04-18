e822b7603579393a5bd94a55da9d51d6bfa5aa44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 15:06:51 +0000

    Add package rath/1.1.0/rath-1.1.0.tar.gz

---
+++ packages/rath/rath-1.1.0.tar.gz/rath-1.1.0/rath/scalars.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+from typing import Any, Callable, Generator, Type
+
+
+class ID(str):
+    """A custom scalar for IDs. If passed a pydantic model it an id property
+    it will automatically validate to that models id"""
+
+    def __init__(self, value: str) -> None:
+        """Initialize the ID"""
+        self.value = value
+
+    @classmethod
+    def __get_validators__(cls: Type["ID"]) -> Generator[Callable[..., Any], Any, Any]:
+        """Get validators"""
+        # one or more validators may be yielded which will be called in the
+        # order to validate the input, each validator will receive as an input
+        # the value returned from the previous validator
+        yield cls.validate
+
+    @classmethod
+    def validate(cls: Type["ID"], v: Any, *info) -> "ID":
+        """Validate the ID"""
+        if isinstance(v, BaseModel):
+            if hasattr(v, "id"):
+                return cls(v.id)  # type: ignore
+            else:
+                raise ValueError("It appears to be a pydantic model but has no id")
+
+        if isinstance(v, str):
+            if " " in v:
+                raise ValueError("IDs cannot contain spaces")
+            if len(v) == 0:
+                raise ValueError("IDs cannot be empty")
+            if not v.isascii():
+                raise ValueError("IDs cannot contain non-ascii characters")
+            if v.isidentifier():
+                raise ValueError("IDs cannot contain special characters")
+            
+            return cls(v)
+
+        if isinstance(v, int):
+            return cls(str(v))
+
+        raise TypeError(
+            f"Needs to be either a instance of BaseModel (with an id) or a string got {type(v)}"
+        )
