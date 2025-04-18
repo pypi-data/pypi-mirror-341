229d1d58b7d33e43b67288d9947a77e2f303588a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 07:06:12 +0000

    Add package InPostReturnLabel/0.0.1/inpostreturnlabel-0.0.1-py3-none-any.whl

---
+++ packages/InPostReturnLabel/inpostreturnlabel-0.0.1-py3-none-any.whl/InPostReturnLabel/Label.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2025-present Daniel Skowroński <daniel@skowron.ski>
+#
+# SPDX-License-Identifier: MIT
+import io
+import tempfile
+from PIL import Image, ImageDraw, ImageFont
+import segno
+
+def renderAndSave(code, font_path) -> str:
+  x=1000 # 100mm
+  y=1500 # 150mm
+
+  y_offset=int(y/10)
+  fs=x/9
+  th=x+y_offset+(y-x)/2+-fs/2
+  th=x+y_offset
+  scale=x/25
+  
+  qr_rendered = io.BytesIO()
+  qrcode = segno.make_qr(code)
+  qrcode.save(qr_rendered, scale=scale, kind='png', border=2)
+  qr = Image.open(qr_rendered)
+  code_text = f'{code:010}'
+  code_fmt=f" {code_text[0:3]} {code_text[3:6]} {code_text[6:9]} {code_text[9]} "
+  image = Image.new('RGB', (x,y), color=(255,255,255))
+  image.paste(qr,(0,y_offset))
+  image_draw=ImageDraw.Draw(image)
+  fnt = ImageFont.truetype(font_path, fs)
+  image_draw.text((0, th), code_fmt, font=fnt, fill=(0, 0, 0))
+  path=tempfile.mktemp()+".png"
+  image.save(path)
+  return path
---
+++ packages/InPostReturnLabel/inpostreturnlabel-0.0.1-py3-none-any.whl/InPostReturnLabel/cli/__init__.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2025-present Daniel Skowroński <daniel@skowron.ski>
+#
+# SPDX-License-Identifier: MIT
+import click
+import subprocess
+from InPostReturnLabel.__about__ import __version__
+from InPostReturnLabel.Label import renderAndSave
+from InPostReturnLabel.Code import isCodeValid
+
+@click.command()
+@click.version_option(version=__version__, prog_name="InPostReturnLabel")
+@click.argument('code', type=int)
+@click.option('-p', '--printer', type=str, 
+              help='Local CUPS printer name; if ommited, then file will be opened')
+@click.option('-f', '--font', type=str, 
+              help='Path to font file, default: /System/Library/Fonts/Monaco.ttf', 
+              default='/System/Library/Fonts/Monaco.ttf')
+def InPostReturnLabel(code, printer, font):
+  """InPostReturnLabel
+  
+  CODE is InPost return label code, 10 digits, e.g. 1234567890
+  """
+  if not isCodeValid(code):
+    click.echo(f"{code} is not a valid InPost return label code")
+    return
+  pathToLabel = renderAndSave(code, font)
+  click.echo(f"Label generated and stored at {pathToLabel}")
+  if printer:
+    click.echo(f"Sending label to {printer}")
+    subprocess.run(["lpr", "-P", printer, pathToLabel])
+  else:
+    click.echo(f"Opening label in default application")
+    subprocess.run(["open", pathToLabel])
+if __name__ == '__main__':
+    InPostReturnLabel()
