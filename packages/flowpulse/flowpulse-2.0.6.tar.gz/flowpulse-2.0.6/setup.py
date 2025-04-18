e0bf32d3b8984f43dd3911b340379ee5c3f5bbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 15:06:32 +0000

    Add package flowpulse/2.0.6/flowpulse-2.0.6.tar.gz

---
+++ packages/flowpulse/flowpulse-2.0.6.tar.gz/flowpulse-2.0.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flowpulse",
+    version="2.0.6",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "flowpulse": ["ExampleConfig.json", "logo.png"],
+        "flowpulse.services": ["logo.png"],
+    },
+    install_requires=["requests", "azure-devops", "pandas", "numpy", "matplotlib", "adjustText"],
+    entry_points={
+        "console_scripts": [
+            "flowpulse=flowpulse.main:main",
+        ],
+    },
+    author="Let People Work",
+    author_email="contact@letpeople.work",
+    description=(
+        "A package to generate flow metrics charts and run Monte Carlo Simulation "
+        "based Forecasts based on queries against Jira or Azure DevOps."
+    ),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://letpeople.work",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
