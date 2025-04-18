27bcfc808eb9527510a54a21c3fe621b9f9d6bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 16:05:31 +0000

    Add package drftokit/0.1.7/drftokit-0.1.7-py3-none-any.whl

---
+++ packages/drftokit/drftokit-0.1.7-py3-none-any.whl/drftokit/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.7"
+
+from .only.only import OrmOnlyServiceMixin, OrmOnlyMixin, OrmOnlyViewMixin
+
+__all__ = [
+    'OrmOnlyServiceMixin',
+    'OrmOnlyMixin',
+    'OrmOnlyViewMixin',
+]
