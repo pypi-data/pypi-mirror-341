de74e446fc04352c4d87c97263e2490259251b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 04:05:49 +0000

    Add package equiface/0.1.0/equiface-0.1.0-py3-none-any.whl

---
+++ packages/equiface/equiface-0.1.0-py3-none-any.whl/equiface/constants.py
@@ -0,0 +1,14 @@
+# File extensions allowed for image inputs
+SUPPORTED_EXTENSIONS = {'.jpg', '.jpeg', '.png'}
+
+# Default name for logging results
+LOG_FILE = "verification_results.yaml"
+
+# Default YOLO model path
+YOLO_MODEL_PATH = "yolo11n.pt"
+
+# Default preprocessing image shape
+IMAGE_SIZE = (160, 160)
+
+# Default threshold for cosine similarity
+DEFAULT_THRESHOLD = 0.74
