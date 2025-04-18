2c253eda304c706de1752cbea98ebe9ed444d63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:06:21 +0000

    Add package lightningpkg/0.0.1/lightningpkg-0.0.1.tar.gz

---
+++ packages/lightningpkg/lightningpkg-0.0.1.tar.gz/lightningpkg-0.0.1/lightningpkg/list.py
@@ -0,0 +1,14 @@
+list1 = ["Mike", "Jane", "Jack"]
+
+print(list1)
+
+
+print(list1[1])
+
+for n in list1:
+    print(n)
+
+
+del list1[2]
+
+print(list1)
---
+++ packages/lightningpkg/lightningpkg-0.0.1.tar.gz/lightningpkg-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup (
+    name='lightningpkg',
+    version='0.0.1',
+    description='It is a wine quality prediction package',
+    author='Juhe',
+    author_email='juhe.terence@gmail.com',
+    packages=find_packages(),
+    license='MIT'
+)
