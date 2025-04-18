9ef8c36b3d71f3b015cc157a30b578076cae539a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 07:06:17 +0000

    Add package smartreq/0.1.2/smartreq-0.1.2.tar.gz

---
+++ packages/smartreq/smartreq-0.1.2.tar.gz/smartreq-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smartreq",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "smartreq = smartreq.cli:main"
+        ]
+    },
+    author="Your Name",
+    description="CLI tool to auto-generate requirements.txt with exact versions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
