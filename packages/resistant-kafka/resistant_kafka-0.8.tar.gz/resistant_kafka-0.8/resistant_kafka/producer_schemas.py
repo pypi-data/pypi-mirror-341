d084a045f83d814c99651dc5fdb53658e750a0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 03:19:11 +0000

    Add package resistant-kafka/0.8/resistant_kafka-0.8.tar.gz

---
+++ packages/resistant-kafka/resistant_kafka-0.8.tar.gz/resistant_kafka-0.8/resistant_kafka/producer_schemas.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from typing import Any, Union
+
+from pydantic import BaseModel
+
+from resistant_kafka.common_schemas import KafkaSecurityConfig
+
+
+class ProducerConfig(BaseModel):
+    """
+        Configuration settings for a Kafka producer.
+
+        :param bootstrap_servers: str -- Kafka server address to connect the producer.
+        :param producer_name: str -- Name for identifying the producer (used mainly for logging/debugging).
+    """
+    bootstrap_servers: str
+    producer_name: str
+    security_config: Union[KafkaSecurityConfig, None] = None
+
+
+@dataclass
+class DataSend:
+    key: str
+    value: Any
+    headers: list[tuple] | None = None
