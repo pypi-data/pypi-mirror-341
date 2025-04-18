9c977b1e3c72416ca770a5aa75f697fbde3b28b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:06:27 +0000

    Add package c-two/0.1.16/c_two-0.1.16-py3-none-any.whl

---
+++ packages/c-two/c_two-0.1.16-py3-none-any.whl/c_two/__init__.py
@@ -0,0 +1,6 @@
+from . import crm
+from . import compo
+from . import message
+from .crm.meta import icrm, iicrm
+from .message.transferable import transfer, auto_transfer, Transferable, transferable
+
