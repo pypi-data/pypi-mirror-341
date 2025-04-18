44b3b91a5d9e0454e5bd17017a939d1f85ab4441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:06:19 +0000

    Add package jupiter-backtesting/0.1.2/jupiter_backtesting-0.1.2-py3-none-any.whl

---
+++ packages/jupiter-backtesting/jupiter_backtesting-0.1.2-py3-none-any.whl/jupiter_backtesting/__init__.py
@@ -0,0 +1,5 @@
+from jupiter_backtesting.strategy import Strategy
+from jupiter_backtesting.analyzer import Analyzer
+from jupiter_backtesting.optimizer import Optimizer
+from jupiter_backtesting.stats import SimpleStats
+from jupiter_backtesting.utils import Utils
