9be30ca4eaa391f9f44f1d95d13e22d0d0732723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 06:06:04 +0000

    Add package dibkb-scraper/0.3.1/dibkb_scraper-0.3.1.tar.gz

---
+++ packages/dibkb-scraper/dibkb_scraper-0.3.1.tar.gz/dibkb_scraper-0.3.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dibkb_scraper",
+    version="0.3.1",
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+        "beautifulsoup4",
+        "bs4",
+        "pydantic",
+        "fake-useragent"
+    ],
+    author="Dibas K Borborah",
+    author_email="dibas9110@gmail.com",
+    description="A scraper for Amazon product details and reviews using ASIN",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dibkb/dibkb_scraper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
