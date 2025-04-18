4f11b4d3094dc155fc97702813b847f7749f26f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:05:57 +0000

    Add package lb2ticket/0.1.0/lb2ticket-0.1.0-py3-none-any.whl

---
+++ packages/lb2ticket/lb2ticket-0.1.0-py3-none-any.whl/src/lb2ticket/workestre/model/comment.py
@@ -0,0 +1,19 @@
+
+from model.base_model import BaseModel
+from dataclasses import dataclass
+
+@dataclass
+class Comment(BaseModel):
+    id = None
+    comment = None
+
+    def __init__(self, comment=None, id=None):
+        self.comment = comment
+        self.id=id
+
+    # @property
+    def serialize(self):
+        return {
+           'id': self.id,
+           'comment': self.comment
+        }
