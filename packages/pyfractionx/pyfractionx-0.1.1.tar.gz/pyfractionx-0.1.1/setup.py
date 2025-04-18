6c879b22da7a2062e6f7c92b75d4da4e77373cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 16:05:44 +0000

    Add package pyfractionx/0.1.1/pyfractionx-0.1.1.tar.gz

---
+++ packages/pyfractionx/pyfractionx-0.1.1.tar.gz/pyfractionx-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyfractionx',
+    version='0.1.1',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A custom fraction math library for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/spremrahul007/pyfractionx',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # or your license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
