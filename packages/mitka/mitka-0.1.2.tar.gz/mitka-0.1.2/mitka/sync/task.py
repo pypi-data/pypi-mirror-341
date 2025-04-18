b4a5ddabe898109402bad6186ccfbf66134b5a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 15:06:59 +0000

    Add package mitka/0.1.2/mitka-0.1.2.tar.gz

---
+++ packages/mitka/mitka-0.1.2.tar.gz/mitka-0.1.2/mitka/sync/task.py
@@ -0,0 +1,26 @@
+import random
+import threading
+
+
+class KWTaskStopped(BaseException):
+    def __init__(self, reason: str | None):
+        super().__init__()
+        self.reason = reason
+
+
+class KWTaskToken:
+    def __init__(self):
+        self.ev = threading.Event()
+
+    def clear(self):
+        self.ev.clear()
+
+    def set(self):
+        self.ev.set()
+
+    def sleep(self, sec: float, reason: str | None = None):
+        if self.ev.wait(sec):
+            raise KWTaskStopped(reason)
+
+    def rsleep(self, sec: float, random_min: float = 0.5, random_max: float = 0.7):
+        self.sleep(sec + random.uniform(random_min, random_max))
