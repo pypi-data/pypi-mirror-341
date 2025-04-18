5f46c65927fac0eb815703ce3ae95c7994486b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 05:06:08 +0000

    Add package tempfilerepr/1.1.2/tempfilerepr-1.1.2.tar.gz

---
+++ packages/tempfilerepr/tempfilerepr-1.1.2.tar.gz/tempfilerepr-1.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tempfilerepr',
+    version='1.1.2',
+    author='Nasr',
+    author_email='nasr2python@gmail.com',
+    description='Encoder and decoder for the temp encoding.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://t.me/NasrPy',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
