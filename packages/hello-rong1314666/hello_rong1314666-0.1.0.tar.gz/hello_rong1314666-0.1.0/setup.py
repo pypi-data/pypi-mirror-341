fde3406ddfa3f6f198182231c1bc865313e0f22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 03:18:22 +0000

    Add package hello-rong1314666/0.1.0/hello_rong1314666-0.1.0.tar.gz

---
+++ packages/hello-rong1314666/hello_rong1314666-0.1.0.tar.gz/hello_rong1314666-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello_rong1314666",  # 包名，pip install 时用这个
+    version="0.1.0",
+    description="A simple hello tool",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="rong",
+    author_email="3505046257@qq.com",
+    url="https://github.com/你的用户名/mytool",  # 可选：放 GitHub 仓库地址
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
