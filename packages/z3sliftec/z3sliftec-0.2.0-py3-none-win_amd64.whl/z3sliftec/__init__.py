efdb9725fecd7387b08fc3bc4d791fa400f0ce9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 06:06:03 +0000

    Add package z3sliftec/0.2.0/z3sliftec-0.2.0-py3-none-win_amd64.whl

---
+++ packages/z3sliftec/z3sliftec-0.2.0-py3-none-win_amd64.whl/z3sliftec/__init__.py
@@ -0,0 +1,3 @@
+from .z3sliftec import *  # This imports all public functions and classes
+
+__all__ = ['get_device_info', 'get_input_data', 'init', 'set_brightness', 'set_external_led', 'set_gear', 'set_central_panel', 'load_rumble_motor', 'set_rpm_led', 'set_warning_led', 'show_me', 'start_input_listener', 'stop_input_listener', 'terminate']
