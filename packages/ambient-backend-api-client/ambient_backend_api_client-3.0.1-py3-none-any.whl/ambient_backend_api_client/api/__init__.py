117b641888bb6d07edfa2dee7d7b7e37ffd6f053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:06:15 +0000

    Add package ambient-backend-api-client/3.0.1/ambient_backend_api_client-3.0.1-py3-none-any.whl

---
+++ packages/ambient-backend-api-client/ambient_backend_api_client-3.0.1-py3-none-any.whl/ambient_backend_api_client/api/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+# import apis into api package
+from ambient_backend_api_client.api.clusters_api import ClustersApi
+from ambient_backend_api_client.api.commands_api import CommandsApi
+from ambient_backend_api_client.api.health_api import HealthApi
+from ambient_backend_api_client.api.nodes_api import NodesApi
+from ambient_backend_api_client.api.notifications_api import NotificationsApi
+from ambient_backend_api_client.api.oauth_api import OauthApi
+from ambient_backend_api_client.api.ping_api import PingApi
+from ambient_backend_api_client.api.registries_api import RegistriesApi
+from ambient_backend_api_client.api.requests_api import RequestsApi
+from ambient_backend_api_client.api.services_api import ServicesApi
+from ambient_backend_api_client.api.unimplemented_api import UnimplementedApi
+from ambient_backend_api_client.api.upgrade_software_api import UpgradeSoftwareApi
+from ambient_backend_api_client.api.users_api import UsersApi
+
