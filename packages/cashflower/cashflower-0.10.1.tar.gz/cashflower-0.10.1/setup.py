703c042e20a9046f5316d8ce3aeebf23fb468ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 03:18:06 +0000

    Add package cashflower/0.10.1/cashflower-0.10.1.tar.gz

---
+++ packages/cashflower/cashflower-0.10.1.tar.gz/cashflower-0.10.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+from pathlib import Path
+import numpy as np
+
+
+# Display README on PyPI page
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+extensions = [
+    Extension(name="cashflower.cython.discount",
+              sources=["cashflower/cython/discount.pyx"],
+              include_dirs=[np.get_include()]),
+]
+
+setup(
+    author="Zuzanna Chmielewska",
+    description="Framework for actuarial cash flow models",
+    ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'psutil',
+        'networkx',
+        'numpy'
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="cashflower",
+    package_data={
+            'cashflower': ["cython/discount.pyx"],
+    },
+    packages=find_packages(include=["cashflower", "cashflower.*"]),
+    project_urls={
+        'Source': 'https://github.com/acturtle/cashflower',
+        'Tracker': 'https://github.com/acturtle/cashflower/issues',
+        'Documentation': 'https://cashflower.acturtle.com',
+        'Cheat sheet': 'https://www.acturtle.com/static/pdf/cheat_sheet.pdf',
+    },
+    python_requires='>=3.11',
+    url="https://github.com/acturtle/cashflower",
+    version="0.10.1",
+)
