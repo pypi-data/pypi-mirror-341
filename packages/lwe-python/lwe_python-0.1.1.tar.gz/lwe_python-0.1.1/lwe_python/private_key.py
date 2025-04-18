0c74c3ad0465125ce65072e5b2ba6a927714e0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 07:06:02 +0000

    Add package lwe-python/0.1.1/lwe_python-0.1.1.tar.gz

---
+++ packages/lwe-python/lwe_python-0.1.1.tar.gz/lwe_python-0.1.1/lwe_python/private_key.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+class PrivateKey():
+  def __init__(self, secret, modulus=None):
+    self.secret = secret
+    self.modulus = None
+    if modulus is not None:
+      self.modulus = modulus
+
+
+  def save_to_keyfile(self, keyfile_path):
+    fp = open(os.path.join(keyfile_path, "sec.lwe.key"), "w")
+    data = json.dumps({
+      "sec": self.secret
+    })
+    fp.write(data)
+    fp.close()
