82c2de848459eea678f2f033a8fc701d39fc8d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 07:06:01 +0000

    Add package findingmodel/0.1.1/findingmodel-0.1.1.tar.gz

---
+++ packages/findingmodel/findingmodel-0.1.1.tar.gz/findingmodel-0.1.1/src/findingmodel/finding_info.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+
+
+class BaseFindingInfo(BaseModel):
+    """
+    Base class for finding information, with simple name and description ± synonyms
+    """
+
+    name: str = Field(..., title="Finding Name", description="The name of the finding")
+    synonyms: list[str] | None = Field(
+        default=None,
+        title="Synonyms",
+        description="Synonyms for the finding name, especially those used by radiologists, including acronyms",
+    )
+    description: str = Field(..., title="Description", description="The description of the finding")
+
+
+class DetailedFindingInfo(BaseFindingInfo):
+    """
+    Detailed class for finding information, with a detailed description and citations.
+    """
+
+    detail: str | None = Field(default=None, title="Detail", description="A detailed description of the finding")
+    citations: list[str] | None = Field(
+        default=None, title="Citations", description="Citations (ideally URLs) for the detailed description"
+    )
