e8b2ce42118077e0d08090f61f1d2802071b6c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 06:06:04 +0000

    Add package appium-python-client-shadowstep/0.8.3/appium_python_client_shadowstep-0.8.3-py3-none-any.whl

---
+++ packages/appium-python-client-shadowstep/appium_python_client_shadowstep-0.8.3-py3-none-any.whl/shadowstep/terminal/__init__.py
@@ -0,0 +1,13 @@
+from shadowstep.terminal.device_interface import DeviceInterface
+from shadowstep.terminal.device_factory import DeviceFactory
+from shadowstep.terminal.adb import Adb
+from shadowstep.terminal.terminal import Terminal
+from shadowstep.terminal.transport import Transport
+
+__all__ = [
+    'DeviceInterface',
+    'DeviceFactory',
+    'Adb',
+    'Terminal',
+    'Transport',
+]
