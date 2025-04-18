7f381ade10b52d871ece5b39904e0cbdf9cbeac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:05:53 +0000

    Add package qqtools/1.0.10/qqtools-1.0.10.tar.gz

---
+++ packages/qqtools/qqtools-1.0.10.tar.gz/qqtools-1.0.10/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="qqtools",
+    version="1.0.10",
+    author="qq",
+    author_email="qq@x1q.cc",
+    description="A small tool package for qq",
+    long_description="A small tool package for qq",
+    long_description_content_type="text/markdown",
+    url="https://github.com/kzhoa/qqtools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.10",
+    install_requires=[],
+)
