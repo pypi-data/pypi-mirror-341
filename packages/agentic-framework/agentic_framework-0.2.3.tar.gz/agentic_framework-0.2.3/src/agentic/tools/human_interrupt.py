228e14c372fbdc532ed7529cc2da1d99c54920f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 04:06:18 +0000

    Add package agentic-framework/0.2.3/agentic_framework-0.2.3.tar.gz

---
+++ packages/agentic-framework/agentic_framework-0.2.3.tar.gz/agentic_framework-0.2.3/src/agentic/tools/human_interrupt.py
@@ -0,0 +1,19 @@
+from typing import Callable
+
+from agentic.tools.base import BaseAgenticTool
+from agentic.events import PauseForInputResult
+from agentic.common import RunContext
+
+
+class HumanInterruptTool(BaseAgenticTool):
+    def __init__(self):
+        pass
+
+    def get_tools(self) -> list[Callable]:
+        return [self.stop_for_input]
+
+    def stop_for_input(self, request_message: str, run_context: RunContext):
+        """ Stop and ask the user for input """
+        if run_context.get("input"):
+            return run_context.get("input")
+        return PauseForInputResult({"input": request_message})
