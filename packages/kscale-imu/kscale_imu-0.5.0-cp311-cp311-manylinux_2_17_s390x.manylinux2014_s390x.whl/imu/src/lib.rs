8bb65b6694fd739f096a3f848e45c2f55b1a5d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:06:16 +0000

    Add package kscale-imu/0.5.0/kscale_imu-0.5.0-cp311-cp311-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/kscale-imu/kscale_imu-0.5.0-cp311-cp311-manylinux_2_17_s390x.manylinux2014_s390x.whl/imu/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod hexmove {
+    pub use ::hexmove::*;
+}
+
+pub mod hiwonder {
+    pub use ::hiwonder::*;
+}
+
+pub mod bno055 {
+    pub use ::linux_bno055::*;
+}
+
+pub mod bmi088 {
+    pub use ::linux_bmi088::*;
+}
