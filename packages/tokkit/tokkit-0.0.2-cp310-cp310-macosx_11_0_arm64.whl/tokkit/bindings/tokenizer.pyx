13fdd3af3da6254058711010e2e1ddacca1fe9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 16:05:48 +0000

    Add package tokkit/0.0.2/tokkit-0.0.2-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/tokkit/tokkit-0.0.2-cp310-cp310-macosx_11_0_arm64.whl/tokkit/bindings/tokenizer.pyx
@@ -0,0 +1,32 @@
+
+from libcpp.string cimport string
+from libcpp.vector cimport vector
+from tokkit.bindings.tokenizer cimport BytePairTokenizer
+from tokkit.bindings.tokenizer cimport dataLoader, dataSaver
+
+cdef class PyBytePairTokenizer:
+    cdef BytePairTokenizer c_bpt
+
+    def __init__(self):
+        self.c_bpt = BytePairTokenizer()
+
+    @property
+    def size(self):
+        return self.c_bpt.size()
+
+    def fit(self, vector[int] corpus, int  max_vocab_size, int n_iter):
+        self.c_bpt.fit(corpus, max_vocab_size, n_iter)
+    
+    def encode(self, string input_string):
+        return self.c_bpt.encode(input_string)
+    
+    def decode(self, vector[int] encoded):
+        return self.c_bpt.decode(encoded)
+    
+
+def data_loader(string filepath):
+    return dataLoader(filepath)
+
+
+def data_saver(vector[int] corpus, string filepath):
+    dataSaver(corpus, filepath)
