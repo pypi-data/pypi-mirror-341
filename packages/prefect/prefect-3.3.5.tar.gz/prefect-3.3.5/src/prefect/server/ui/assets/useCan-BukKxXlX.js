fad4c34d1c1a7be4cf352080fc759262096d3a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 07:06:24 +0000

    Add package prefect/3.3.5/prefect-3.3.5.tar.gz

---
+++ packages/prefect/prefect-3.3.5.tar.gz/prefect-3.3.5/src/prefect/server/ui/assets/useCan-BukKxXlX.js
@@ -0,0 +1,2 @@
+import{cC as n}from"./index-qcISwWN-.js";const c=Symbol("canInjectionKey");function a(){return n(c)}export{c,a as u};
+//# sourceMappingURL=useCan-BukKxXlX.js.map
---
+++ packages/prefect/prefect-3.3.5.tar.gz/prefect-3.3.5/src/prefect/server/ui/assets/usePrefectApi-Ba1gtIr7.js
@@ -0,0 +1,2 @@
+import{cC as r}from"./index-qcISwWN-.js";import{p as e}from"./api-CbQi_CNc.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-Ba1gtIr7.js.map
