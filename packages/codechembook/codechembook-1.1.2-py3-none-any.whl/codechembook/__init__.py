924666e5339be07d343cebe19d34142f6ce7f456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:06:03 +0000

    Add package codechembook/1.1.2/codechembook-1.1.2-py3-none-any.whl

---
+++ packages/codechembook/codechembook-1.1.2-py3-none-any.whl/codechembook/__init__.py
@@ -0,0 +1,11 @@
+# __init__.py
+
+__version__ = "1.1.2"
+
+from .quickPlots import *
+from .quickTools import *
+from .plotlyTemplates import *
+from .numericalTools import *
+from .symbols import *
+from .chemformula import *
+
