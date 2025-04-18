b3abc9fc17976e45f3687d5157bf9ad0608ef411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 06:06:07 +0000

    Add package docdata/0.0.5.dev0/docdata-0.0.5.dev0.tar.gz

---
+++ packages/docdata/docdata-0.0.5.dev0.tar.gz/docdata-0.0.5.dev0/tests/test_version.py
@@ -0,0 +1,17 @@
+"""Trivial version test."""
+
+import unittest
+
+from docdata.version import get_version
+
+
+class TestVersion(unittest.TestCase):
+    """Trivially test a version."""
+
+    def test_version_type(self) -> None:
+        """Test the version is a string.
+
+        This is only meant to be an example test.
+        """
+        version = get_version()
+        self.assertIsInstance(version, str)
