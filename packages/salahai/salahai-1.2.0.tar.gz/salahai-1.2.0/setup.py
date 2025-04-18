d79fd7dd9b2810030df48ec33eec4ef0d07d1673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 04:05:45 +0000

    Add package salahai/1.2.0/salahai-1.2.0.tar.gz

---
+++ packages/salahai/salahai-1.2.0.tar.gz/salahai-1.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='salahai',
+    version='1.2.0',
+    description='SalahAi - ذكاء اصطناعي ذكي',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Salah Hemdan',
+    author_email='salah1hemdaan@gmail.com',
+    url='https://facebook.com/cap.salah.hemdan',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
