750fb988fa86a23380d9a00c6044ad5a815398d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 06:05:41 +0000

    Add package innov8r/5.0.0/innov8r-5.0.0.tar.gz

---
+++ packages/innov8r/innov8r-5.0.0.tar.gz/innov8r-5.0.0/README.md
@@ -0,0 +1,12 @@
+# innov8r
+
+**innov8r** is a Python-based GUI application designed to streamline software development workflows with innovative tools and utilities. It provides developers with an intuitive and user-friendly interface for building efficient and scalable solutions.
+
+---
+
+## 📦 Installation
+
+Install the application using pip:
+
+```bash
+pip install innov8r
---
+++ packages/innov8r/innov8r-5.0.0.tar.gz/innov8r-5.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="innov8r",
+    version="5.0.0",
+    author="Shivkumar Chauhan",
+    author_email="sksinghat95@gmail.com",
+    description="Python-based GUI application designed to streamline software development workflows with innovative tools and utilities. It provides developers with an intuitive and user-friendly interface for building efficient and scalable solutions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
