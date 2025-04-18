68a5d710036ee4f52830b16dc845c73d98a4fb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 05:06:24 +0000

    Add package instantsplat/1.2.2/instantsplat-1.2.2-cp311-cp311-win_amd64.whl

---
+++ packages/instantsplat/instantsplat-1.2.2-cp311-cp311-win_amd64.whl/instantsplat/initializer/__init__.py
@@ -0,0 +1,6 @@
+from .abc import AbstractInitializer, InitializingCamera, InitializedPointCloud
+from .dataset import InitializedCameraDataset, TrainableCameraDataset, TrainableInitializedCameraDataset
+from .dust3r import Dust3rInitializer, Mast3rInitializer
+from .colmap import ColmapSparseInitializer, ColmapDenseInitializer
+from .align import AlignInitializer
+from .combinations import Dust3rAlign2ColmapDenseInitializer, Mast3rAlign2ColmapDenseInitializer
