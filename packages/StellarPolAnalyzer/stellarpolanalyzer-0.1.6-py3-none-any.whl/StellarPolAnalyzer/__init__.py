59425c9448b2f3a9bc733c5ab74d52ed319ba976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 07:05:55 +0000

    Add package StellarPolAnalyzer/0.1.6/stellarpolanalyzer-0.1.6-py3-none-any.whl

---
+++ packages/StellarPolAnalyzer/stellarpolanalyzer-0.1.6-py3-none-any.whl/StellarPolAnalyzer/__init__.py
@@ -0,0 +1,1 @@
+from .stellarpol_core import process_image, detect_stars, find_candidate_pairs, filter_pairs_by_mode, draw_pairs, align_images, save_fits_with_same_headers, process_four_polarimetric_images, compute_polarimetry_for_pairs, compute_full_polarimetry
