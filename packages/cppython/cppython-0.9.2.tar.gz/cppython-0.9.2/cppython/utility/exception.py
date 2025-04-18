00757ce05f0564c032c825e8ee8faa3c317afe7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 03:18:09 +0000

    Add package cppython/0.9.2/cppython-0.9.2.tar.gz

---
+++ packages/cppython/cppython-0.9.2.tar.gz/cppython-0.9.2/cppython/utility/exception.py
@@ -0,0 +1,47 @@
+"""Exception definitions"""
+
+
+class PluginError(Exception):
+    """Raised when there is a plugin error"""
+
+    def __init__(self, error: str) -> None:
+        """Initializes the error
+
+        Args:
+            error: The error message
+        """
+        self._error = error
+
+        super().__init__(error)
+
+    @property
+    def error(self) -> str:
+        """Returns the underlying error
+
+        Returns:
+            str -- The underlying error
+        """
+        return self._error
+
+
+class NotSupportedError(Exception):
+    """Raised when something is not supported"""
+
+    def __init__(self, error: str) -> None:
+        """Initializes the error
+
+        Args:
+            error: The error message
+        """
+        self._error = error
+
+        super().__init__(error)
+
+    @property
+    def error(self) -> str:
+        """Returns the underlying error
+
+        Returns:
+            str -- The underlying error
+        """
+        return self._error
