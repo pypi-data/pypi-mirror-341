a54a3bb0c84ebbe2f90cb54f0d5674b20f8f29d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 03:18:22 +0000

    Add package clacky-stats/0.1.4/clacky_stats-0.1.4.tar.gz

---
+++ packages/clacky-stats/clacky_stats-0.1.4.tar.gz/clacky_stats-0.1.4/bin/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.1.4"
+
+"""
+Clacky AI 代码贡献统计工具
+
+这个包提供了一组工具来分析和统计代码仓库中的贡献情况，
+特别关注 ClackyAI 与人类开发者之间的协作。
+
+主要功能：
+- 按时间段统计代码贡献
+- 按版本标签统计代码贡献
+- 支持周报和月报统计
+"""
+
+from bin.blame import BlameAnalyzer
+from bin.cli import main
+
+__all__ = ['BlameAnalyzer', 'main'] 
