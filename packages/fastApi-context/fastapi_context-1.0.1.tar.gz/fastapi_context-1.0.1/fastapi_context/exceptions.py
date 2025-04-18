edfbf81edafa5a2ff4a31adbbf4a6c15b7aad274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 16:05:24 +0000

    Add package fastApi-context/1.0.1/fastapi_context-1.0.1.tar.gz

---
+++ packages/fastApi-context/fastapi_context-1.0.1.tar.gz/fastapi_context-1.0.1/fastapi_context/exceptions.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+
+class ContextMiddlewareError(Exception):
+    """Base class for all exceptions raised by the ContextMiddleware."""
+
+    def __init__(
+        self,
+        status_code: int = 400,
+        error_code: int = 200,
+        data: Any = None,
+        message: str = "Bad Request",
+    ):
+        self.status_code = status_code
+        self.error_code = error_code
+        self.data = data
+        self.message = message
+
+
+class ContextMiddlewareConfigError(ContextMiddlewareError):
+    def __init__(self, message: str = "ContextMiddlewareConfig Error"):
+        super().__init__(message=message)
