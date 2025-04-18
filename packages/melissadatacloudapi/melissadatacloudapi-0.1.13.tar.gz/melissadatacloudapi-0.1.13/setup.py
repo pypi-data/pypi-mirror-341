a27f5414b98e7ed483abab608f6a54160e17be0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 05:05:55 +0000

    Add package melissadatacloudapi/0.1.13/melissadatacloudapi-0.1.13.tar.gz

---
+++ packages/melissadatacloudapi/melissadatacloudapi-0.1.13.tar.gz/melissadatacloudapi-0.1.13/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='melissadatacloudapi',
+    version='0.1.13',
+    packages=find_packages(),
+    install_requires=[],  # List any dependencies here
+)
