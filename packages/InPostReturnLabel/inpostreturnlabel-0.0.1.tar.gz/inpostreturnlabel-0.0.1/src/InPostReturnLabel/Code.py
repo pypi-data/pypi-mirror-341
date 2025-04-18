57bd7b537207069fbdf5bae25a9c666c4afdfbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 07:06:12 +0000

    Add package InPostReturnLabel/0.0.1/inpostreturnlabel-0.0.1.tar.gz

---
+++ packages/InPostReturnLabel/inpostreturnlabel-0.0.1.tar.gz/inpostreturnlabel-0.0.1/src/InPostReturnLabel/Code.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2025-present Daniel Skowroński <daniel@skowron.ski>
+#
+# SPDX-License-Identifier: MIT
+# TODO: this should be some kind of a class
+import numbers
+def isCodeValid(code):
+  if not isinstance(code, numbers.Number):
+    return False
+  if not code >= 0 and code <= 9999999999:
+    return False
+  return True
