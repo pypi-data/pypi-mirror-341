957f947b82c363b55124d419ad76c8b228c22c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:06:03 +0000

    Add package drftokit/0.1.5/drftokit-0.1.5.tar.gz

---
+++ packages/drftokit/drftokit-0.1.5.tar.gz/drftokit-0.1.5/drftokit/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.5"
+
+from .only.only import OrmOnlyServiceMixin, OrmOnlyMixin, OrmOnlyViewMixin
+
+__all__ = [
+    'OrmOnlyServiceMixin',
+    'OrmOnlyMixin',
+    'OrmOnlyViewMixin',
+]
