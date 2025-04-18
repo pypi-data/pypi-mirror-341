298e50b0b126e613c3ec438a9640842702ec58bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 16:05:43 +0000

    Add package octostar-streamlit/0.1.22/octostar_streamlit-0.1.22.tar.gz

---
+++ packages/octostar-streamlit/octostar_streamlit-0.1.22.tar.gz/octostar_streamlit-0.1.22/octostar_streamlit/frontend/src/components/GetSearchResults.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { Streamlit } from "streamlit-component-lib";
+import "./Button.less";
+import { forwardDesktopApiMethodCallToPlatform } from "../desktop";
+
+export const GetSearchResults = (props: any) => { // TODO: Better typing of props
+  console.log('VS101: GetSearchResults props', props);
+  useEffect(() => Streamlit.setFrameHeight(100), []);
+  const label = props?.params?.label || 'Fetch Search Results';
+
+  const getResults = React.useCallback(async () => {
+    const result = await forwardDesktopApiMethodCallToPlatform(props);
+    Streamlit.setComponentValue(result);
+  }, []);
+
+  return (
+    <button className="octostar-button" onClick={getResults}>{label}</button>
+  );
+};
+
+
