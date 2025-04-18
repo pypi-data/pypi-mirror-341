5c962018fb0c269934c2b6ce5341fd3d4a616438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-18 14:06:27 +0000

    Add package bus-mgmt-mcp-server/0.1.9/bus_mgmt_mcp_server-0.1.9.tar.gz

---
+++ packages/bus-mgmt-mcp-server/bus_mgmt_mcp_server-0.1.9.tar.gz/bus_mgmt_mcp_server-0.1.9/bus_mgmt_mcp_server/bus_mgmt_mcp_server.py
@@ -0,0 +1,22 @@
+from fastmcp import FastMCP, Client
+
+mcp = FastMCP("Bus Mgmt MCP Server")
+
+@mcp.tool()
+def greet(name: str) -> str:
+    return f"Hello, {name}!"
+
+# import asyncio
+# client = Client(mcp)
+#
+# async def call_tool(name: str):
+#     async with client:
+#         result = await client.call_tool("greet", {"name": name})
+#         print(result[0].text)
+#
+# asyncio.run(call_tool("Tom"))
+# asyncio.run(call_tool("Jill"))
+
+def main():
+    print("bus-mgmt-mcp-server is running")
+    mcp.run()
