e3f63283e5169e0673244247911f3dcc25f2cd61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:22 +0000

    Add package qrhost/1.0.1/qrhost-1.0.1.tar.gz

---
+++ packages/qrhost/qrhost-1.0.1.tar.gz/qrhost-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qrhost',
+    version='1.0.1',
+    description='A tool for hosting files and directories with QR codes',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Hamdaoui Ryan',
+    author_email='ryanhamdaoui45@gmail.com',
+    url='https://github.com/ryanhamdaoui/qrhost',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'qrcode',
+        'netifaces',
+    ],
+    license='MIT',  # Explicit license field
+    entry_points={
+        'console_scripts': [
+            'qrhost = qrhost:main',  # Points to the main function inside qrhost.py
+        ],
+    },
+    python_requires='>=3.6',
+)
