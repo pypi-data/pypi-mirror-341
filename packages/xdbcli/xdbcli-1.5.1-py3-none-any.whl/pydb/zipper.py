9a1636214e7f21dab4ef243fa1c5a69a9fbd80de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:48 +0000

    Add package xdbcli/1.5.1/xdbcli-1.5.1-py3-none-any.whl

---
+++ packages/xdbcli/xdbcli-1.5.1-py3-none-any.whl/pydb/zipper.py
@@ -0,0 +1,40 @@
+import os
+import gzip
+import shutil
+
+
+class Zip:
+    """
+    Class to compress and decompress files.
+    """
+    def __init__(self, filename):
+        self.filename = filename
+
+    def compress(self, output=None):
+        """
+        Return file with gz compress
+        :param output: filename to compress data.
+        """
+        if output is not None:
+            output_filename = output
+        else:
+            output_filename = f'{self.filename}.gz'
+
+        with open(self.filename, "rb") as input_file:
+            with gzip.open(output_filename, "wb") as output_file:
+                shutil.copyfileobj(input_file, output_file)
+        return self._get_stat(output_filename)
+
+    def decompress(self):
+        """
+        Return file with gz decompress
+        """
+        output_filename = self.filename[:-3]
+
+        with open(output_filename, "wb") as output_file:
+            with gzip.open(self.filename, "rb") as input_file:
+                shutil.copyfileobj(input_file, output_file)
+        return self._get_stat(output_filename)
+
+    def _get_stat(self, file: str):
+        return {self.filename: os.stat(self.filename).st_size, file: os.stat(file).st_size}
