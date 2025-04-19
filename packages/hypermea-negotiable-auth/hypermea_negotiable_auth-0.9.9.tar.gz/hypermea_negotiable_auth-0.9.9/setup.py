36ff4bbc2a62b0bd5f86f60740518cacfcb74dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 04:05:54 +0000

    Add package hypermea-negotiable-auth/0.9.9/hypermea_negotiable_auth-0.9.9.tar.gz

---
+++ packages/hypermea-negotiable-auth/hypermea_negotiable_auth-0.9.9.tar.gz/hypermea_negotiable_auth-0.9.9/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hypermea-negotiable-auth',
+    version='0.9.9',
+    description='Hypermea negotiable authentication',
+    long_description= open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    author='Michael Ottoson',
+    author_email='michael@pointw.com',
+    url='https://github.com/pointw-dev/hypermea-negotiable-auth',
+    keywords=['hypermea', 'eve', 'api', 'rest', 'auth', 'http'],
+    packages=find_packages(),
+    license='MIT',
+    install_requires=['eve>=0.8.0', 'authparser>=1.0'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: Security',
+    ]
+)
