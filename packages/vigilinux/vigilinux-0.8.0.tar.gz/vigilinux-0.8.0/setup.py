910d96a114ac88e44fd2fbb01f36665de1ace655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:42 +0000

    Add package vigilinux/0.8.0/vigilinux-0.8.0.tar.gz

---
+++ packages/vigilinux/vigilinux-0.8.0.tar.gz/vigilinux-0.8.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="vigilinux",  # Changed the name to vigilinux
+    version="0.8.0",
+    author="Subhan_Rauf",
+    author_email="raufsubhan45@gmail.com",
+    description="Vigi is an AI assistant for running commands in natural language.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/naumanAhmed3/VigiLinux-Shell-Interpreter",
+    packages=find_packages(),
+    package_data={"vigilinux": ["settings.json"]},  # Changed to vigilinux
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "google-generativeai",
+        "python-dotenv==0.19.2",
+        "setuptools",
+        "importlib_resources",
+    ],
+    entry_points={
+        "console_scripts": [
+            "vigi=vigilinux.main:main",  # Keeping the terminal command as "vigi"
+        ],
+    },
+)
