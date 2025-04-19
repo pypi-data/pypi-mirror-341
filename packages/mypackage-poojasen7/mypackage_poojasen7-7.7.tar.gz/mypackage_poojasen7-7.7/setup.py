5fd673b1854ce5e9b1fd01d6e4211c33016812ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 04:06:15 +0000

    Add package mypackage-poojasen7/7.7/mypackage_poojasen7-7.7.tar.gz

---
+++ packages/mypackage-poojasen7/mypackage_poojasen7-7.7.tar.gz/mypackage_poojasen7-7.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+name="mypackage_poojasen7",
+version="7.7",
+author="poojasenthilkumar",
+author_email="sdpooja06@gmail.com",
+package=find_packages(),
+)
