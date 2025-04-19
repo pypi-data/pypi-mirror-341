41f00bbd23a3fb41050fc702ae602d2d7cc03549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 04:05:59 +0000

    Add package pyfixest/0.29.0/pyfixest-0.29.0-py3-none-any.whl

---
+++ packages/pyfixest/pyfixest-0.29.0-py3-none-any.whl/pyfixest/estimation/__init__.py
@@ -0,0 +1,55 @@
+from pyfixest.estimation import literals
+from pyfixest.estimation.demean_ import (
+    demean,
+)
+from pyfixest.estimation.detect_singletons_ import (
+    detect_singletons,
+)
+from pyfixest.estimation.estimation import (
+    feglm,
+    feols,
+    fepois,
+)
+from pyfixest.estimation.fegaussian_ import Fegaussian
+from pyfixest.estimation.feiv_ import (
+    Feiv,
+)
+from pyfixest.estimation.felogit_ import Felogit
+from pyfixest.estimation.feols_ import (
+    Feols,
+)
+from pyfixest.estimation.fepois_ import (
+    Fepois,
+)
+from pyfixest.estimation.feprobit_ import Feprobit
+from pyfixest.estimation.FixestMulti_ import (
+    FixestMulti,
+)
+from pyfixest.estimation.model_matrix_fixest_ import (
+    model_matrix_fixest,
+)
+from pyfixest.estimation.multcomp import (
+    bonferroni,
+    rwolf,
+    wyoung,
+)
+
+__all__ = [
+    "Fegaussian",
+    "Feiv",
+    "Felogit",
+    "Feols",
+    "Fepois",
+    "Feprobit",
+    "FixestMulti",
+    "bonferroni",
+    "demean",
+    "detect_singletons",
+    "feglm",
+    "feols",
+    "fepois",
+    "literals",
+    "model_matrix_fixest",
+    "rwolf",
+    "wyoung",
+]
---
+++ packages/pyfixest/pyfixest-0.29.0-py3-none-any.whl/pyfixest/report/__init__.py
@@ -0,0 +1,19 @@
+from pyfixest.report.summarize import (
+    dtable,
+    etable,
+    make_table,
+    summary,
+)
+from pyfixest.report.visualize import (
+    coefplot,
+    iplot,
+)
+
+__all__ = [
+    "coefplot",
+    "dtable",
+    "etable",
+    "iplot",
+    "make_table",
+    "summary",
+]
