9b4c17cf094b97c5dd557e4c6568739dde28dea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:48 +0000

    Add package derive-client/0.2.16/derive_client-0.2.16.tar.gz

---
+++ packages/derive-client/derive_client-0.2.16.tar.gz/derive_client-0.2.16/derive_client/data_types/__init__.py
@@ -0,0 +1,51 @@
+"""Enums and Models used in the derive_client module"""
+
+from .enums import (
+    ActionType,
+    ChainID,
+    CollateralAsset,
+    Currency,
+    Environment,
+    InstrumentType,
+    OrderSide,
+    OrderStatus,
+    OrderType,
+    RfqStatus,
+    RPCEndPoints,
+    SubaccountType,
+    TimeInForce,
+    TxStatus,
+    UnderlyingCurrency,
+)
+from .models import (
+    Address,
+    CreateSubAccountData,
+    CreateSubAccountDetails,
+    DeriveAddresses,
+    MintableTokenData,
+    NonMintableTokenData,
+)
+
+__all__ = [
+    "TxStatus",
+    "ChainID",
+    "Currency",
+    "RPCEndPoints",
+    "InstrumentType",
+    "UnderlyingCurrency",
+    "OrderSide",
+    "OrderType",
+    "OrderStatus",
+    "TimeInForce",
+    "Environment",
+    "SubaccountType",
+    "CollateralAsset",
+    "ActionType",
+    "RfqStatus",
+    "Address",
+    "MintableTokenData",
+    "NonMintableTokenData",
+    "DeriveAddresses",
+    "CreateSubAccountDetails",
+    "CreateSubAccountData",
+]
---
+++ packages/derive-client/derive_client-0.2.16.tar.gz/derive_client-0.2.16/derive_client/data_types/models.py
@@ -0,0 +1,68 @@
+"""Models used in the bridge module."""
+
+
+from dataclasses import dataclass
+
+from derive_action_signing.module_data import ModuleData
+from derive_action_signing.utils import decimal_to_big_int
+from eth_abi.abi import encode
+from pydantic import BaseModel, ConfigDict
+from web3 import Web3
+
+from .enums import ChainID, Currency
+
+Address = str
+
+
+@dataclass
+class CreateSubAccountDetails:
+    amount: int
+    base_asset_address: str
+    sub_asset_address: str
+
+    def to_eth_tx_params(self):
+        return (
+            decimal_to_big_int(self.amount),
+            Web3.to_checksum_address(self.base_asset_address),
+            Web3.to_checksum_address(self.sub_asset_address),
+        )
+
+
+@dataclass
+class CreateSubAccountData(ModuleData):
+    amount: int
+    asset_name: str
+    margin_type: str
+    create_account_details: CreateSubAccountDetails
+
+    def to_abi_encoded(self):
+        return encode(
+            ['uint256', 'address', 'address'],
+            self.create_account_details.to_eth_tx_params(),
+        )
+
+    def to_json(self):
+        return {}
+
+
+class TokenData(BaseModel):
+    isAppChain: bool
+    connectors: dict[ChainID, dict[str, str]]
+    LyraTSAShareHandlerDepositHook: Address | None = None
+    LyraTSADepositHook: Address | None = None
+    isNewBridge: bool
+
+
+class MintableTokenData(TokenData):
+    Controller: Address
+    MintableToken: Address
+
+
+class NonMintableTokenData(TokenData):
+    Vault: Address
+    NonMintableToken: Address
+
+
+class DeriveAddresses(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    chains: dict[ChainID, dict[Currency, MintableTokenData | NonMintableTokenData]]
