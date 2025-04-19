08a8a930579dd9b2141207c1eb63c8a2ecf21acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:50 +0000

    Add package lightweight-charts-esistjosh/3.0.2/lightweight_charts_esistjosh-3.0.2.tar.gz

---
+++ packages/lightweight-charts-esistjosh/lightweight_charts_esistjosh-3.0.2.tar.gz/lightweight_charts_esistjosh-3.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='lightweight_charts_esistjosh',
+    version='3.0.2',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=[
+        'pandas',
+        'yfinance',
+        'pywebview>=5.0.5',
+    ],
+    package_data={
+        'lightweight_charts': ['js/*'],
+    },
+    author='EsIstJosh',
+    license='MIT/AGPL-3.0',
+    description="Python framework for TradingView's Lightweight Charts JavaScript library.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/EsIstJosh/lightweight-charts-python',
+)
