5add3c79e3073f2d382ab5d26097b078399b61b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:16:05 +0000

    Add package pingthis/1.0.2/pingthis-1.0.2.tar.gz

---
+++ packages/pingthis/pingthis-1.0.2.tar.gz/pingthis-1.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pingthis',
+    version='1.0.2',
+    packages=find_packages(),  # Automatically find packages
+    entry_points={
+        'console_scripts': [
+            'pingthis = pingthis.__main__:main'
+        ],
+    },
+    install_requires=[
+        'rich',
+    ],
+    author='eytin',
+    description='A simple internal network pinging tool.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/eytin/pingthis',  # Replace with your repo URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+)
