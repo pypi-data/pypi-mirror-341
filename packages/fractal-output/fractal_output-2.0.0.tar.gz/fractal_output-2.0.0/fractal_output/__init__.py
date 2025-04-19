178abc2749abcb07fa4e2c73682408e74407ace2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:33 +0000

    Add package fractal-output/2.0.0/fractal_output-2.0.0.tar.gz

---
+++ packages/fractal-output/fractal_output-2.0.0.tar.gz/fractal_output-2.0.0/fractal_output/__init__.py
@@ -0,0 +1,4 @@
+from .manager import Manager  # noqa
+from .resource import Item, Collection, Null, ResourceException  # noqa
+from .transformer import Transformer  # noqa
+from .version import __version__  # noqa
