8744d67f95c38ec256f6f38679a88c8d1ce22e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:03 +0000

    Add package BacenAPI/0.2.0/bacenapi-0.2.0.tar.gz

---
+++ packages/BacenAPI/bacenapi-0.2.0.tar.gz/bacenapi-0.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as arq:
+    readme = arq.read()
+
+setup(
+    name='BacenAPI',
+    version='0.2.0',
+    license='MIT',
+    author='Paulo Icaro, Lissandro Costa de Sousa, Francisco Gildemir Ferreira da Silva',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='lisandrosousa54@gmail.com',
+    keywords='time-serie, api_data, central_bank',
+    description='tools for manipulating time series data from the Central Bank of Brazil',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'requests'
+    ],
+    include_package_data=True,
+    package_data={
+        "BacenAPI": ["Date/*.txt"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',)
