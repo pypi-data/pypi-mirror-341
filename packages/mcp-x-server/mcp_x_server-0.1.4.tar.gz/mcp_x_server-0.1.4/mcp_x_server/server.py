624bcc534678bc00306c5748084860f81f1ed59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:24 +0000

    Add package mcp-x-server/0.1.4/mcp_x_server-0.1.4.tar.gz

---
+++ packages/mcp-x-server/mcp_x_server-0.1.4.tar.gz/mcp_x_server-0.1.4/mcp_x_server/server.py
@@ -0,0 +1,12 @@
+from fastmcp import FastMCP
+from mcp_x_server.agents.tweet_agent import agent
+
+mcp = FastMCP("X MCP 🚀", description="A simple MCP server")
+
+# Posting on X
+@mcp.tool()
+async def create_tweet(text: str) -> str:
+    """Create a tweet"""
+    # Ignore the return value
+    r = await agent.run(text) 
+    return r.output
