d1c1353fc23ed1404f161e92eebf6e58ab1c5a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:39 +0000

    Add package cookiecutter-python/2.5.3/cookiecutter_python-2.5.3-py2.py3-none-any.whl

---
+++ packages/cookiecutter-python/cookiecutter_python-2.5.3-py2.py3-none-any.whl/cookiecutter_python/backend/__init__.py
@@ -0,0 +1,12 @@
+from .gen_docs_common import get_docs_gen_internal_config
+from .main import generate
+from .post_main import CheckWebServerError
+from .sanitization import sanitize
+
+
+__all__ = [
+    'generate',
+    'CheckWebServerError',
+    'sanitize',
+    'get_docs_gen_internal_config',
+]
