38597af17e2ad6551a8a5a64bb55d7c966649403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:24 +0000

    Add package NetDecom/0.0.4.5/NetDecom-0.0.4.5.tar.gz

---
+++ packages/NetDecom/NetDecom-0.0.4.5.tar.gz/NetDecom-0.0.4.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NetDecom',  # Package name
+    version='0.0.4.5',  # Version number
+    description='Dimensionality Reduction and Decomposition of Undirected Graph Models and Bayesian Networks',  # Package description
+    author='Hugh',  # Author name
+    #author_email='',  # Author email
+    packages=find_packages(),  # Find all packages
+    long_description=open('README.md').read(),  # Read detailed description from README.md
+    long_description_content_type='text/markdown',  # Specify the format of the README file
+    classifiers=[  # Choose appropriate classifiers
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',  # Compatible with Python 3.9
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',  # Cross-platform
+    ],
+)
