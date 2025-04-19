e674df587c60da30f4f72172eeefda701ff9247d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:15:15 +0000

    Add package hologres-mcp-server/0.1.8/hologres_mcp_server-0.1.8.tar.gz

---
+++ packages/hologres-mcp-server/hologres_mcp_server-0.1.8.tar.gz/hologres_mcp_server-0.1.8/src/hologres_mcp_server/settings.py
@@ -0,0 +1,33 @@
+"""
+Settings module for Hologres MCP Server.
+"""
+
+import os
+
+
+SERVER_VERSION = "0.1.8"
+
+
+def get_db_config():
+    """Get database configuration from environment variables."""
+    user = os.getenv("HOLOGRES_USER")
+    password = os.getenv("HOLOGRES_PASSWORD")
+    options = None
+    if user is None or password is None:
+        user = os.getenv("ALIBABA_CLOUD_ACCESS_KEY_ID")
+        password = os.getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
+        options = f"sts_token={os.getenv("ALIBABA_CLOUD_SECURITY_TOKEN")}"
+
+    config = {
+        "host": os.getenv("HOLOGRES_HOST", "localhost"),
+        "port": os.getenv("HOLOGRES_PORT", "5432"),
+        "user": user,
+        "password": password,
+        "options": options,
+        "dbname": os.getenv("HOLOGRES_DATABASE"),
+        "application_name": f"hologres-mcp-server-{SERVER_VERSION}"
+    }
+    if not all([config["user"], config["password"], config["dbname"]]):
+        raise ValueError("Missing required database configuration.")
+    
+    return config
