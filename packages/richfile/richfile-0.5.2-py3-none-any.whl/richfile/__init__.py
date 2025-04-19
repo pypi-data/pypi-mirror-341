0b3e9c1fc5987d25a5670bd45098984c680eec06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 04:06:15 +0000

    Add package richfile/0.5.2/richfile-0.5.2-py3-none-any.whl

---
+++ packages/richfile/richfile-0.5.2-py3-none-any.whl/richfile/__init__.py
@@ -0,0 +1,21 @@
+__version__ = "0.5.2"
+
+VERSIONS_RICHFILE_SUPPORTED = [">=0.3.1", "<1.0.0"]
+PYTHON_VERSIONS_SUPPORTED = [">=3", "<4"]
+
+FILENAME_METADATA   = ".metadata.richfile"
+FILENAME_TYPELOOKUP = ".typelookup.richfile"
+
+JSON_INDENT = 4
+
+WINDOWS_RESERVED_NAMES = {
+    "CON", "PRN", "AUX", "NUL",
+    *{f"COM{i}" for i in range(1, 10)},
+    *{f"LPT{i}" for i in range(1, 10)},
+}
+
+
+## Import important stuff from util.py into top-level namespace
+from . import functions, util
+from .util import RichFile, load_folder, load_element, load_folder_metadata, save_object, invalid_chars_filename
+from . import demo
